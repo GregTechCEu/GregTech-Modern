34042fbdd1291a400f7d6f3e8acf85c0b0f5b242
KilaBash <yefancy@foxmail.com>
2023-03-19 21:54:50 +0800

    fix fabric custom ingredient

--- fabric/src/main/java/com/gregtechceu/gtceu/api/recipe/ingredient/fabric/SizedIngredientImpl.java
+++ fabric/src/main/java/com/gregtechceu/gtceu/api/recipe/ingredient/fabric/SizedIngredientImpl.java
@@ -4,12 +4,14 @@ import com.google.gson.JsonObject;
 import com.gregtechceu.gtceu.api.recipe.ingredient.SizedIngredient;
 import net.fabricmc.fabric.api.recipe.v1.ingredient.CustomIngredient;
 import net.fabricmc.fabric.api.recipe.v1.ingredient.CustomIngredientSerializer;
+import net.fabricmc.fabric.api.recipe.v1.ingredient.FabricIngredient;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,7 +21,7 @@ import java.util.List;
  * @date 2023/2/21
  * @implNote SizedIngredientImpl
  */
-public class SizedIngredientImpl extends SizedIngredient implements CustomIngredient {
+public class SizedIngredientImpl extends SizedIngredient implements FabricIngredient {
 
     protected SizedIngredientImpl(Ingredient inner, int amount) {
         super(inner, amount);
@@ -46,6 +48,11 @@ public class SizedIngredientImpl extends SizedIngredient implements CustomIngred
         return true;
     }
 
+    @Override
+    public boolean test(@Nullable ItemStack stack) {
+        return inner.test(stack);
+    }
+
     public static SizedIngredient create(Ingredient inner, int amount) {
         return new SizedIngredientImpl(inner, amount);
     }
@@ -59,17 +66,46 @@ public class SizedIngredientImpl extends SizedIngredient implements CustomIngred
     }
 
     @Override
-    public List<ItemStack> getMatchingStacks() {
-        return Arrays.stream(getItems()).toList();
+    public @Nullable CustomSizedIngredient getCustomIngredient() {
+        return new CustomSizedIngredient(this);
     }
 
-    @Override
-    public CustomIngredientSerializer<?> getSerializer() {
-        return Serializer.INSTANCE;
+    public static class CustomSizedIngredient implements CustomIngredient {
+
+        public final SizedIngredientImpl vanilla;
+
+        public CustomSizedIngredient(SizedIngredientImpl vanilla) {
+            this.vanilla = vanilla;
+        }
+
+        @Override
+        public boolean test(ItemStack stack) {
+            return vanilla.test(stack);
+        }
+
+        @Override
+        public List<ItemStack> getMatchingStacks() {
+            return Arrays.stream(vanilla.getItems()).toList();
+        }
+
+        @Override
+        public boolean requiresTesting() {
+            return true;
+        }
+
+        @Override
+        public CustomIngredientSerializer<?> getSerializer() {
+            return Serializer.INSTANCE;
+        }
+
+        @Override
+        public SizedIngredientImpl toVanilla() {
+            return vanilla;
+        }
     }
 
 
-    public static class Serializer implements CustomIngredientSerializer<SizedIngredientImpl> {
+    public static class Serializer implements CustomIngredientSerializer<CustomSizedIngredient> {
 
         public static Serializer INSTANCE = new Serializer();
 
@@ -79,45 +115,47 @@ public class SizedIngredientImpl extends SizedIngredient implements CustomIngred
         }
 
         @Override
-        public SizedIngredientImpl read(JsonObject json) {
+        public CustomSizedIngredient read(JsonObject json) {
             int amount = json.get("count").getAsInt();
             if (json.has("tag")) {
-                return new SizedIngredientImpl(json.get("tag").getAsString(), amount);
+                return new SizedIngredientImpl(json.get("tag").getAsString(), amount).getCustomIngredient();
             } else {
                 Ingredient inner = Ingredient.fromJson(json.get("ingredient"));
-                return new SizedIngredientImpl(inner, amount);
+                return new SizedIngredientImpl(inner, amount).getCustomIngredient();
             }
         }
 
         @Override
-        public void write(JsonObject json, SizedIngredientImpl ingredient) {
-            json.addProperty("count", ingredient.amount);
-            if (ingredient.tag != null) {
-                json.addProperty("tag", ingredient.tag);
+        public void write(JsonObject json, CustomSizedIngredient ingredient) {
+            var vanilla = ingredient.toVanilla();
+            json.addProperty("count", vanilla.amount);
+            if (vanilla.tag != null) {
+                json.addProperty("tag", vanilla.tag);
             } else {
-                json.add("ingredient", ingredient.inner.toJson());
+                json.add("ingredient", vanilla.inner.toJson());
             }
         }
 
         @Override
-        public SizedIngredientImpl read(FriendlyByteBuf buffer) {
+        public CustomSizedIngredient read(FriendlyByteBuf buffer) {
             int amount = buffer.readVarInt();
             if (buffer.readBoolean()) {
-                return new SizedIngredientImpl(buffer.readUtf(), amount);
+                return new SizedIngredientImpl(buffer.readUtf(), amount).getCustomIngredient();
             } else {
-                return new SizedIngredientImpl(Ingredient.fromNetwork(buffer), amount);
+                return new SizedIngredientImpl(Ingredient.fromNetwork(buffer), amount).getCustomIngredient();
             }
         }
 
         @Override
-        public void write(FriendlyByteBuf buffer, SizedIngredientImpl ingredient) {
-            buffer.writeVarInt(ingredient.getAmount());
-            if (ingredient.tag != null) {
+        public void write(FriendlyByteBuf buffer, CustomSizedIngredient ingredient) {
+            var vanilla = ingredient.toVanilla();
+            buffer.writeVarInt(vanilla.getAmount());
+            if (vanilla.tag != null) {
                 buffer.writeBoolean(true);
-                buffer.writeUtf(ingredient.tag);
+                buffer.writeUtf(vanilla.tag);
             } else {
                 buffer.writeBoolean(false);
-                ingredient.inner.toNetwork(buffer);
+                vanilla.inner.toNetwork(buffer);
             }
         }
     }
