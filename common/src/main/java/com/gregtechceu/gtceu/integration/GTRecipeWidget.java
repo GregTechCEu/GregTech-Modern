040023278f551dc9c81843050f628e8def720b89
DStrand1 <dane@strandboge.com>
2023-03-24 23:12:51 -0500

    fix empty slots missing in jei/rei recipes

--- common/src/main/java/com/gregtechceu/gtceu/integration/GTRecipeWidget.java
+++ common/src/main/java/com/gregtechceu/gtceu/integration/GTRecipeWidget.java
@@ -5,18 +5,23 @@ import com.gregtechceu.gtceu.api.capability.recipe.FluidRecipeCapability;
 import com.gregtechceu.gtceu.api.capability.recipe.ItemRecipeCapability;
 import com.gregtechceu.gtceu.api.recipe.GTRecipe;
 import com.gregtechceu.gtceu.api.recipe.RecipeHelper;
+import com.gregtechceu.gtceu.api.recipe.content.Content;
 import com.gregtechceu.gtceu.utils.GTUtil;
 import com.lowdragmc.lowdraglib.gui.widget.LabelWidget;
 import com.lowdragmc.lowdraglib.gui.widget.ProgressWidget;
 import com.lowdragmc.lowdraglib.gui.widget.WidgetGroup;
 import com.lowdragmc.lowdraglib.msic.FluidStorage;
+import com.lowdragmc.lowdraglib.side.fluid.IFluidStorage;
 import com.lowdragmc.lowdraglib.utils.CycleItemStackHandler;
 import com.lowdragmc.lowdraglib.utils.LocalizationUtils;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -28,33 +33,43 @@ public class GTRecipeWidget extends WidgetGroup {
     public GTRecipeWidget(GTRecipe recipe) {
         super(0, 0, recipe.recipeType.getJEISize().width, recipe.recipeType.getJEISize().height);
         setClientSideWidget();
+
+        List<List<ItemStack>> inputStacks = recipe.getInputContents(ItemRecipeCapability.CAP).stream()
+                .map(content -> content.content)
+                .map(ItemRecipeCapability.CAP::of)
+                .map(Ingredient::getItems)
+                .map(Arrays::stream)
+                .map(Stream::toList)
+                .collect(Collectors.toList());
+        while (inputStacks.size() < recipe.recipeType.getMaxInputs(ItemRecipeCapability.CAP)) inputStacks.add(null);
+
+        List<List<ItemStack>> outputStacks = recipe.getOutputContents(ItemRecipeCapability.CAP).stream()
+                .map(content -> content.content)
+                .map(ItemRecipeCapability.CAP::of)
+                .map(Ingredient::getItems)
+                .map(Arrays::stream)
+                .map(Stream::toList)
+                .collect(Collectors.toList());
+        while (outputStacks.size() < recipe.recipeType.getMaxOutputs(ItemRecipeCapability.CAP)) outputStacks.add(null);
+
+        IFluidStorage[] inputFluids = new IFluidStorage[recipe.recipeType.getMaxInputs(FluidRecipeCapability.CAP)];
+        List<Content> inputFluidContents = recipe.getInputContents(FluidRecipeCapability.CAP);
+        for (int i = 0; i < inputFluidContents.size(); i++) {
+            inputFluids[i] = new FluidStorage(FluidRecipeCapability.CAP.of(inputFluidContents.get(i).content));
+        }
+
+        IFluidStorage[] outputFluids = new IFluidStorage[recipe.recipeType.getMaxOutputs(FluidRecipeCapability.CAP)];
+        List<Content> outputFluidContents = recipe.getOutputContents(FluidRecipeCapability.CAP);
+        for (int i = 0; i < outputFluidContents.size(); i++) {
+            outputFluids[i] = new FluidStorage(FluidRecipeCapability.CAP.of(outputFluidContents.get(i).content));
+        }
+
         addWidget(recipe.recipeType.createUITemplate(ProgressWidget.JEIProgress,
-                new CycleItemStackHandler(
-                        recipe.getInputContents(ItemRecipeCapability.CAP).stream()
-                                .map(content -> content.content)
-                                .map(ItemRecipeCapability.CAP::of)
-                                .map(Ingredient::getItems)
-                                .map(Arrays::stream)
-                                .map(Stream::toList)
-                                .toList()),
-                new CycleItemStackHandler(
-                        recipe.getOutputContents(ItemRecipeCapability.CAP).stream()
-                                .map(content -> content.content)
-                                .map(ItemRecipeCapability.CAP::of)
-                                .map(Ingredient::getItems)
-                                .map(Arrays::stream)
-                                .map(Stream::toList)
-                                .toList()),
-                recipe.getInputContents(FluidRecipeCapability.CAP).stream()
-                        .map(content -> content.content)
-                        .map(FluidRecipeCapability.CAP::of)
-                        .map(FluidStorage::new)
-                        .toArray(FluidStorage[]::new),
-                recipe.getOutputContents(FluidRecipeCapability.CAP).stream()
-                        .map(content -> content.content)
-                        .map(FluidRecipeCapability.CAP::of)
-                        .map(FluidStorage::new)
-                        .toArray(FluidStorage[]::new)));
+                new CycleItemStackHandler(inputStacks),
+                new CycleItemStackHandler(outputStacks),
+                inputFluids,
+                outputFluids
+        ));
 
         int yOffset = 60;
         addWidget(new LabelWidget(3, yOffset,
