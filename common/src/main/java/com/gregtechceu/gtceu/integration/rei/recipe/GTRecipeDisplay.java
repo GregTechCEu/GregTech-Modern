272c3a0889fb554e542027ee4a34bd9b12958b5f
Max <maxh2709@gmail.com>
2023-03-29 15:23:27 +0200

    Cleanup REI display registry

--- common/src/main/java/com/gregtechceu/gtceu/integration/rei/recipe/GTRecipeDisplay.java
+++ common/src/main/java/com/gregtechceu/gtceu/integration/rei/recipe/GTRecipeDisplay.java
@@ -2,13 +2,12 @@ package com.gregtechceu.gtceu.integration.rei.recipe;
 
 import com.gregtechceu.gtceu.api.recipe.GTRecipe;
 import com.gregtechceu.gtceu.integration.GTRecipeWidget;
-import com.gregtechceu.gtceu.api.recipe.GTRecipeType;
 import com.lowdragmc.lowdraglib.gui.widget.WidgetGroup;
 import com.lowdragmc.lowdraglib.rei.ModularDisplay;
 
 public class GTRecipeDisplay extends ModularDisplay<WidgetGroup> {
 
-    public GTRecipeDisplay(GTRecipeType recipeMap, GTRecipe recipe) {
-        super(() -> new GTRecipeWidget(recipe), GTRecipeTypeDisplayCategory.CATEGORIES.apply(recipeMap));
+    public GTRecipeDisplay(GTRecipe recipe) {
+        super(() -> new GTRecipeWidget(recipe), GTRecipeTypeDisplayCategory.CATEGORIES.apply(recipe.recipeType));
     }
 }
