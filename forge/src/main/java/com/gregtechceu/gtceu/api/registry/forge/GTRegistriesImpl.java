4b3b8f9cca831a64d9f935e7a5084c348c883ff7
jorgsaa <jorgen.saanum@gmail.com>
2023-03-30 23:52:32 +0200

    Use registry keys instead of deprecated registries

--- forge/src/main/java/com/gregtechceu/gtceu/api/registry/forge/GTRegistriesImpl.java
+++ forge/src/main/java/com/gregtechceu/gtceu/api/registry/forge/GTRegistriesImpl.java
@@ -2,6 +2,7 @@ package com.gregtechceu.gtceu.api.registry.forge;
 
 import com.gregtechceu.gtceu.GTCEu;
 import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
@@ -12,6 +13,8 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import static net.minecraft.core.Registry.RECIPE_TYPE_REGISTRY;
+
 /**
  * @author KilaBash
  * @date 2023/2/20
@@ -21,19 +24,22 @@ public class GTRegistriesImpl {
     public static final DeferredRegister<TrunkPlacerType<?>> TRUNK_PLACER_TYPE = DeferredRegister.create(Registry.TRUNK_PLACER_TYPE_REGISTRY, GTCEu.MOD_ID);
 
     public static <V, T extends V> T register(Registry<V> registry, ResourceLocation name, T value) {
-        if (registry == Registry.RECIPE_TYPE) {
+        ResourceKey<?> registryKey = registry.key();
+
+        if (registryKey == RECIPE_TYPE_REGISTRY) {
             ForgeRegistries.RECIPE_TYPES.register(name, (RecipeType<?>) value);
-        } else if (registry == Registry.RECIPE_SERIALIZER) {
+        } else if (registryKey == Registry.RECIPE_SERIALIZER_REGISTRY) {
             ForgeRegistries.RECIPE_SERIALIZERS.register(name, (RecipeSerializer<?>) value);
-        } else if (registry == Registry.FEATURE) {
+        } else if (registryKey == Registry.FEATURE_REGISTRY) {
             ForgeRegistries.FEATURES.register(name, (Feature<?>) value);
-        } else if (registry == Registry.TRUNK_PLACER_TYPES) {
-            TRUNK_PLACER_TYPE.register(name.getPath(), () -> (TrunkPlacerType<?>)value);
-        } else if (registry == Registry.FOLIAGE_PLACER_TYPES) {
+        } else if (registryKey == Registry.FOLIAGE_PLACER_TYPE_REGISTRY) {
             ForgeRegistries.FOLIAGE_PLACER_TYPES.register(name, (FoliagePlacerType<?>)value);
+        } else if (registryKey == Registry.TRUNK_PLACER_TYPE_REGISTRY) {
+            TRUNK_PLACER_TYPE.register(name.getPath(), () -> (TrunkPlacerType<?>)value);
         } else {
             return Registry.register(registry, name, value);
         }
+
         return value;
     }
 
