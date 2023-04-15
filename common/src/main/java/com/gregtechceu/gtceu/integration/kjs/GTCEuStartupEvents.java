450302cc7b3f5a9877d114032e59fbaf2795e6d1
screret <joona.v.stenberg@gmail.com>
2023-04-15 15:57:56 +0300

    fixed registry validator overwriting vanilla

--- common/src/main/java/com/gregtechceu/gtceu/integration/kjs/GTCEuStartupEvents.java
+++ common/src/main/java/com/gregtechceu/gtceu/integration/kjs/GTCEuStartupEvents.java
@@ -2,18 +2,15 @@ package com.gregtechceu.gtceu.integration.kjs;
 
 import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.registry.GTRegistries;
-import com.gregtechceu.gtceu.api.registry.GTRegistry;
 import com.gregtechceu.gtceu.integration.kjs.events.GTRegistryEventJS;
-import dev.latvian.mods.kubejs.RegistryObjectBuilderTypes;
 import dev.latvian.mods.kubejs.event.EventGroup;
 import dev.latvian.mods.kubejs.event.EventHandler;
 import dev.latvian.mods.kubejs.event.Extra;
-import net.minecraft.resources.ResourceLocation;
 
 public interface GTCEuStartupEvents {
     EventGroup GROUP = EventGroup.of("GTCEuStartupEvents");
 
-    Extra REGISTRY_EXTRA = Extra.REQUIRES_ID.validator(GTCEuStartupEvents::validateRegistry);
+    Extra REGISTRY_EXTRA = Extra.REQUIRES_ID.copy().validator(GTCEuStartupEvents::validateRegistry);
     private static boolean validateRegistry(Object o) {
         try {
             return GTRegistries.REGISTRIES.containKey(GTCEu.appendId(o.toString()));
