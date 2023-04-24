2477e9c170e221fb3e7f71746b0cc3189229235a
screret <joona.v.stenberg@gmail.com>
2023-04-24 15:15:11 +0300

    MADE KUBEJS MATERIAL TAG STUFF WORK LESSGOOO also removed VERY many tags; filesize should go down quite a bit

--- common/src/main/java/com/gregtechceu/gtceu/core/mixins/TagLoaderMixin.java
+++ common/src/main/java/com/gregtechceu/gtceu/core/mixins/TagLoaderMixin.java
@@ -3,6 +3,7 @@ package com.gregtechceu.gtceu.core.mixins;
 import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.api.data.chemical.ChemicalHelper;
+import com.gregtechceu.gtceu.core.IGTTagLoader;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
@@ -10,7 +11,9 @@ import net.minecraft.tags.TagEntry;
 import net.minecraft.tags.TagKey;
 import net.minecraft.tags.TagLoader;
 import net.minecraft.world.item.Item;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -20,25 +23,41 @@ import java.util.List;
 import java.util.Map;
 
 @Mixin(TagLoader.class)
-public class TagLoaderMixin {
+public class TagLoaderMixin<T> implements IGTTagLoader<T> {
 
-    @Inject(method = "load", at = @At("RETURN"))
+    @Nullable
+    @Unique
+    private Registry<T> gtceu$storedRegistry;
+
+    @Inject(method = "load", at = @At(value = "RETURN"))
     public void gtceu$load(ResourceManager resourceManager, CallbackInfoReturnable<Map<ResourceLocation, List<TagLoader.EntryWithSource>>> cir) {
         var value = cir.getReturnValue();
+        if (gtceu$getRegistry() == null || gtceu$getRegistry() != Registry.ITEM) return;
         ChemicalHelper.UNIFICATION_ENTRY_ITEM.forEach((entry, itemLikes) -> {
             if (itemLikes.isEmpty()) return;
             var material = entry.material;
             if (material != null) {
                 var materialTags = entry.tagPrefix.getItemTags(material);
-                if (entry.tagPrefix.doGenerateItem(material)) {
-                    for (TagKey<Item> materialTag : materialTags) {
-                        List<TagLoader.EntryWithSource> tags = new ArrayList<>();
-                        itemLikes.forEach(item -> tags.add(new TagLoader.EntryWithSource(TagEntry.element(Registry.ITEM.getKey(item.asItem())), GTValues.CUSTOM_TAG_SOURCE)));
-                        GTCEu.LOGGER.info("added items " + itemLikes + " to tag " + materialTag);
-                        value.put(materialTag.location(), tags);
-                    }
+                for (TagKey<Item> materialTag : materialTags) {
+                    List<TagLoader.EntryWithSource> tags = new ArrayList<>();
+                    itemLikes.forEach(item -> tags.add(new TagLoader.EntryWithSource(TagEntry.element(Registry.ITEM.getKey(item.asItem())), GTValues.CUSTOM_TAG_SOURCE)));
+                    //GTCEu.LOGGER.info("added items " + itemLikes + " to tag " + materialTag);
+                    value.computeIfAbsent(materialTag.location(), path -> new ArrayList<>()).addAll(tags);
                 }
+
             }
         });
     }
+
+    @Override
+    @Nullable
+    public Registry<T> gtceu$getRegistry() {
+        return gtceu$storedRegistry;
+    }
+
+    @Override
+    public void gtceu$setRegistry(Registry<T> registry) {
+        this.gtceu$storedRegistry = registry;
+    }
+
 }
