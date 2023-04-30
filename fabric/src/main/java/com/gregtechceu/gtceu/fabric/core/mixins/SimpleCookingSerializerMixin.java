dda2679b9d0f062e4d0ae793ffc8ecd2feffc8b5
screret <joona.v.stenberg@gmail.com>
2023-04-30 20:04:03 +0300

    made SimpleCookingSerializerMixin be more compact

--- fabric/src/main/java/com/gregtechceu/gtceu/fabric/core/mixins/SimpleCookingSerializerMixin.java
+++ fabric/src/main/java/com/gregtechceu/gtceu/fabric/core/mixins/SimpleCookingSerializerMixin.java
@@ -2,18 +2,20 @@ package com.gregtechceu.gtceu.fabric.core.mixins;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.GsonHelper;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.Recipe;
-import net.minecraft.world.item.crafting.ShapedRecipe;
-import net.minecraft.world.item.crafting.SimpleCookingSerializer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.*;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 /**
  * @author KilaBash
@@ -21,23 +23,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  * @implNote SimpleCookingSerializer
  */
 @Mixin(SimpleCookingSerializer.class)
-public abstract class SimpleCookingSerializerMixin {
+public abstract class SimpleCookingSerializerMixin<T extends AbstractCookingRecipe> {
 
-    @Shadow @Final private SimpleCookingSerializer.CookieBaker factory;
+    @Shadow @Final private SimpleCookingSerializer.CookieBaker<T> factory;
 
     @Shadow @Final private int defaultCookingTime;
 
-    @Inject(method = "fromJson(Lnet/minecraft/resources/ResourceLocation;Lcom/google/gson/JsonObject;)Lnet/minecraft/world/item/crafting/AbstractCookingRecipe;",
-            at =@At(value = "HEAD"), cancellable = true)
-    public void getAppearance(ResourceLocation recipeId, JsonObject json, CallbackInfoReturnable<Recipe> cir) {
+    @Inject(method = "fromJson(Lnet/minecraft/resources/ResourceLocation;Lcom/google/gson/JsonObject;)Lnet/minecraft/world/item/crafting/AbstractCookingRecipe;", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/GsonHelper;getAsString(Lcom/google/gson/JsonObject;Ljava/lang/String;)Ljava/lang/String;", shift = At.Shift.BEFORE), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
+    public void gtceu$modifyItem(ResourceLocation recipeId, JsonObject json, CallbackInfoReturnable<T> cir,
+                                 String group, JsonElement ingredientJson, Ingredient ingredient) {
         if (json.has("result") && json.get("result").isJsonObject()) {
-            String string = GsonHelper.getAsString(json, "group", "");
-            JsonElement jsonElement = GsonHelper.isArrayNode(json, "ingredient") ? GsonHelper.getAsJsonArray(json, "ingredient") : GsonHelper.getAsJsonObject(json, "ingredient");
-            Ingredient ingredient = Ingredient.fromJson(jsonElement);
-            var itemStack = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(json, "result"));
-            float f = GsonHelper.getAsFloat(json, "experience", 0.0f);
-            int i = GsonHelper.getAsInt(json, "cookingtime", this.defaultCookingTime);
-            cir.setReturnValue(this.factory.create(recipeId, string, ingredient, itemStack, f, i));
+            ItemStack itemStack = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(json, "result"));
+            float exp = GsonHelper.getAsFloat(json, "experience", 0.0F);
+            int time = GsonHelper.getAsInt(json, "cookingtime", this.defaultCookingTime);
+            cir.setReturnValue(this.factory.create(recipeId, group, ingredient, itemStack, exp, time));
+            cir.cancel();
         }
     }
 
