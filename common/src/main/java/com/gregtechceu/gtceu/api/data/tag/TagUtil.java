e00a594ed0d152262313ebe992f2581a91b0d1a2
screret <joona.v.stenberg@gmail.com>
2023-05-14 13:26:06 +0300

    made custom (& dye, chest) tags be according to platform standard, removed CustomTagsImpl.java's in the process

--- common/src/main/java/com/gregtechceu/gtceu/api/data/tag/TagUtil.java
+++ common/src/main/java/com/gregtechceu/gtceu/api/data/tag/TagUtil.java
@@ -1,5 +1,6 @@
 package com.gregtechceu.gtceu.api.data.tag;
 
+import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtlib.Platform;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
@@ -24,12 +25,25 @@ public class TagUtil {
         return optionalTag(registry, Platform.isForge() ? new ResourceLocation("forge", path) : new ResourceLocation("c", path));
     }
 
+    public static <T> TagKey<T> createPlatformTag(Registry<T> registry, String forgePath, String fabricPath, boolean modTag) {
+        if (modTag) return optionalTag(registry, Platform.isForge() ? new ResourceLocation(GTCEu.MOD_ID, forgePath) : new ResourceLocation(GTCEu.MOD_ID, fabricPath));
+        return optionalTag(registry, Platform.isForge() ? new ResourceLocation("forge", forgePath) : new ResourceLocation("c", fabricPath));
+    }
+
+    public static <T> TagKey<T> createModTag(Registry<T> registry, String path) {
+        return optionalTag(registry, GTCEu.id(path));
+    }
+
     public static TagKey<Block> createBlockTag(String path) {
         return createTag(Registry.BLOCK, path, false);
     }
 
-    public static TagKey<Item> createBlockTag(String path, boolean vanilla) {
-        return createTag(Registry.ITEM, path, vanilla);
+    public static TagKey<Block> createBlockTag(String path, boolean vanilla) {
+        return createTag(Registry.BLOCK, path, vanilla);
+    }
+
+    public static TagKey<Block> createModBlockTag(String path) {
+        return createModTag(Registry.BLOCK, path);
     }
 
     public static TagKey<Item> createItemTag(String path) {
@@ -38,6 +52,19 @@ public class TagUtil {
 
     public static TagKey<Item> createItemTag(String path, boolean vanilla) {
         return createTag(Registry.ITEM, path, vanilla);
+    }
+
+    public static TagKey<Item> createPlatformItemTag(String forgePath, String fabricPath) {
+        return createPlatformItemTag(forgePath, fabricPath, false);
+    }
+
+
+    public static TagKey<Item> createPlatformItemTag(String forgePath, String fabricPath, boolean modTag) {
+        return createPlatformTag(Registry.ITEM, forgePath, fabricPath, modTag);
+    }
+
+    public static TagKey<Item> createModItemTag(String path) {
+        return createModTag(Registry.ITEM, path);
     }
 
     public static TagKey<Fluid> createFluidTag(String path) {
