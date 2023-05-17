9e7b79f41010495bdfd712189aff30311e8d72af
screret <joona.v.stenberg@gmail.com>
2023-05-17 16:26:40 +0300

    fixes all reported lang issues & should fix some not noticed before fixes #67, #75

--- common/src/main/java/com/gregtechceu/gtceu/api/data/chemical/fluid/FluidTypes.java
+++ common/src/main/java/com/gregtechceu/gtceu/api/data/chemical/fluid/FluidTypes.java
@@ -2,11 +2,11 @@ package com.gregtechceu.gtceu.api.data.chemical.fluid;
 
 public class FluidTypes {
 
-    public static final FluidType LIQUID = new FluidTypeLiquid("liquid", null, null, "gregtech.fluid.generic");
+    public static final FluidType LIQUID = new FluidTypeLiquid("liquid", null, null, "gtceu.fluid.generic");
 
-    public static final FluidType ACID = new FluidTypeAcid("acid", null, null, "gregtech.fluid.generic");
+    public static final FluidType ACID = new FluidTypeAcid("acid", null, null, "gtceu.fluid.generic");
 
-    public static final FluidType GAS = new FluidTypeGas("gas", null, null, "gregtech.fluid.generic");
+    public static final FluidType GAS = new FluidTypeGas("gas", null, null, "gtceu.fluid.generic");
 
-    public static final FluidType PLASMA = new FluidTypePlasma("plasma", "plasma", null, "gregtech.fluid.plasma");
+    public static final FluidType PLASMA = new FluidTypePlasma("plasma", "plasma", null, "gtceu.fluid.plasma");
 }
