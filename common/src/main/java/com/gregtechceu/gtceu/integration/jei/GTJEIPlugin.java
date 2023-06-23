df7c802980ad6acf76de339a23bffcb0a921fb0b
KilaBash <yefancy@foxmail.com>
2023-06-23 21:24:45 +0800

    bump up all recipe viewers

--- common/src/main/java/com/gregtechceu/gtceu/integration/jei/GTJEIPlugin.java
+++ common/src/main/java/com/gregtechceu/gtceu/integration/jei/GTJEIPlugin.java
@@ -2,23 +2,19 @@ package com.gregtechceu.gtceu.integration.jei;
 
 import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.machine.MachineDefinition;
-import com.gregtechceu.gtceu.api.machine.feature.IRecipeLogicMachine;
-import com.gregtechceu.gtceu.api.machine.feature.IUIMachine;
 import com.gregtechceu.gtceu.api.recipe.GTRecipeType;
-import com.gregtechceu.gtceu.api.registry.GTRegistries;
 import com.gregtechceu.gtceu.common.data.GTMachines;
 import com.gregtechceu.gtceu.integration.jei.multipage.MultiblockInfoCategory;
 import com.gregtechceu.gtceu.integration.jei.recipe.GTRecipeTypeCategory;
 import com.lowdragmc.lowdraglib.LDLib;
-import com.lowdragmc.lowdraglib.gui.modular.ModularUIGuiContainer;
-import com.lowdragmc.lowdraglib.utils.Position;
-import com.lowdragmc.lowdraglib.utils.Size;
 import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.RecipeTypes;
-import mezz.jei.api.gui.handlers.IGuiClickableArea;
-import mezz.jei.api.gui.handlers.IGuiContainerHandler;
 import mezz.jei.api.helpers.IJeiHelpers;
-import mezz.jei.api.registration.*;
+import mezz.jei.api.registration.IModIngredientRegistration;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
@@ -26,10 +22,6 @@ import net.minecraft.world.item.crafting.RecipeType;
 
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.regex.Pattern;
 
 /**
  * @author KilaBash
@@ -38,7 +30,8 @@ import java.util.regex.Pattern;
  */
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
-public class JEIPlugin implements IModPlugin {
+@JeiPlugin
+public class GTJEIPlugin implements IModPlugin {
     @Override
     public ResourceLocation getPluginUid() {
         return GTCEu.id("jei_plugin");
@@ -80,29 +73,5 @@ public class JEIPlugin implements IModPlugin {
         if (LDLib.isReiLoaded()) return;
         GTCEu.LOGGER.info("JEI register ingredients");
     }
-
-    @Override
-    public void registerGuiHandlers(IGuiHandlerRegistration registry) {
-        if (LDLib.isReiLoaded()) return;
-        GTCEu.LOGGER.info("JEI register click areas");
-        Pattern progressWidgetId = Pattern.compile("progress");
-        for (GTRecipeType type : GTRegistries.RECIPE_TYPES.values()) {
-            var group = type.createDefaultUITemplate(false);
-            var widget = group.getFirstWidgetById(progressWidgetId);
-            if (widget != null) {
-                Size widgetSize = widget.getSize();
-                Position widgetPos = widget.getPosition();
-                registry.addGuiContainerHandler(ModularUIGuiContainer.class, new IGuiContainerHandler<>() {
-                    @Override
-                    public Collection<IGuiClickableArea> getGuiClickableAreas(ModularUIGuiContainer containerScreen, double mouseX, double mouseY) {
-                        if (containerScreen.modularUI.holder instanceof IUIMachine machine && machine.self() instanceof IRecipeLogicMachine recipeLogicMachine && recipeLogicMachine.getRecipeType() == type) {
-                            IGuiClickableArea clickableArea = IGuiClickableArea.createBasic(widgetPos.x, widgetPos.y + widgetSize.height, widgetSize.width, widgetSize.height, GTRecipeTypeCategory.TYPES.apply(type));
-                            return List.of(clickableArea);
-                        }
-                        return Collections.emptyList();
-                    }
-                });
-            }
-        }
-    }
 }
+
