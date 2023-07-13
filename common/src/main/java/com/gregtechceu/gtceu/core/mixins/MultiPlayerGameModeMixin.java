9081823cb7b7b42c0beec1d6a226f139b53aaff8
Spencer <spenceralj@gmail.com>
2023-07-13 14:25:34 -0400

    Force server to break blocks instead of client

--- common/src/main/java/com/gregtechceu/gtceu/core/mixins/MultiPlayerGameModeMixin.java
+++ common/src/main/java/com/gregtechceu/gtceu/core/mixins/MultiPlayerGameModeMixin.java
@@ -1,14 +1,21 @@
 package com.gregtechceu.gtceu.core.mixins;
 
 import com.gregtechceu.gtceu.api.item.IItemUseFirst;
+import com.gregtechceu.gtceu.api.item.tool.GTToolType;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -20,6 +27,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  */
 @Mixin(MultiPlayerGameMode.class)
 public class MultiPlayerGameModeMixin {
+    @Shadow @Final private Minecraft minecraft;
+
     @Inject(
             method = {"performUseItemOn"},
             at = {@At(
@@ -37,5 +46,22 @@ public class MultiPlayerGameModeMixin {
                 cir.setReturnValue(result);
             }
         }
+    }
+
+    @Inject(
+            method = {"destroyBlock"},
+            at = {@At("HEAD")},
+            cancellable = true
+    )
+    private void destroyBlock(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        if (minecraft.player == null || !GTToolType.MINING_HAMMER.is(minecraft.player.getMainHandItem()) || minecraft.player.isCrouching()) return;
+
+        cir.cancel();
+        Level level = minecraft.level;
+
+        if (level == null) return;
+        BlockState state = level.getBlockState(pos);
+
+        state.getBlock().destroy(level, pos, state);
     }
 }
