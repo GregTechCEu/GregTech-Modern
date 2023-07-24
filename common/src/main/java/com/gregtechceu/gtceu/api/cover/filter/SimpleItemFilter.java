6f3a673bc0f05be122d06f58a3999b2b11971d47
KilaBash <yefancy@foxmail.com>
2023-07-24 13:39:19 +0800

    clean up

--- common/src/main/java/com/gregtechceu/gtceu/api/cover/filter/SimpleItemFilter.java
+++ common/src/main/java/com/gregtechceu/gtceu/api/cover/filter/SimpleItemFilter.java
@@ -37,7 +37,6 @@ public class SimpleItemFilter implements ItemFilter {
     @Getter
     protected int maxStackSize;
 
-    protected PhantomSlotWidget[] filterSlots = new PhantomSlotWidget[9];
 
     protected SimpleItemFilter() {
         Arrays.fill(matches, ItemStack.EMPTY);
@@ -75,17 +74,12 @@ public class SimpleItemFilter implements ItemFilter {
 
     public void setBlackList(boolean blackList) {
         isBlackList = blackList;
-        onUpdated.accept(this);
-
-        for (PhantomSlotWidget filterSlot : filterSlots) {
-            filterSlot.setMaxStackSize(isBlackList ? 1 : Integer.MAX_VALUE);
-        }
-
         if (blackList) {
-            for (ItemStack match : matches) {
-                match.setCount(1);
-            }
+            setMaxStackSize(1);
+        } else {
+            setMaxStackSize(Integer.MAX_VALUE);
         }
+        onUpdated.accept(this);
     }
 
     @Override
@@ -100,23 +94,35 @@ public class SimpleItemFilter implements ItemFilter {
 
     public WidgetGroup openConfigurator(int x, int y) {
         WidgetGroup group = new WidgetGroup(x, y, 18 * 3 + 25, 18 * 3); // 80 55
+        var filterSlots = new PhantomSlotWidget[9];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 final int index = i * 3 + j;
                 var handler = new ItemStackTransfer(matches[index]);
-                var slot = new PhantomSlotWidget(handler, 0, i * 18, j * 18);
-                slot.setMaxStackSize(isBlackList ? 1 : Integer.MAX_VALUE);
+                filterSlots[i] = new PhantomSlotWidget(handler, 0, i * 18, j * 18) {
+                    @Override
+                    public void updateScreen() {
+                        super.updateScreen();
+                        setMaxStackSize(maxStackSize);
+                    }
+
+                    @Override
+                    public void detectAndSendChanges() {
+                        super.detectAndSendChanges();
+                        setMaxStackSize(maxStackSize);
+                    }
+                };
+                var slot = filterSlots[i];
                 slot.setChangeListener(() -> {
                     matches[index] = handler.getStackInSlot(0);
                     onUpdated.accept(this);
                 }).setBackground(GuiTextures.SLOT);
-                this.filterSlots[index] = slot; // Used to update max stack size on change
                 group.addWidget(slot);
             }
         }
-        group.addWidget(new ToggleButtonWidget(18 * 3 + 2, (18 * 0) + 9, 18, 18,
+        group.addWidget(new ToggleButtonWidget(18 * 3 + 2, 9, 18, 18,
                 GuiTextures.BUTTON_BLACKLIST, this::isBlackList, this::setBlackList));
-        group.addWidget(new ToggleButtonWidget(18 * 3 + 2, (18 * 1) + 9, 18, 18,
+        group.addWidget(new ToggleButtonWidget(18 * 3 + 2, (18) + 9, 18, 18,
                 GuiTextures.BUTTON_FILTER_NBT, this::isIgnoreNbt, this::setIgnoreNbt));
         return group;
     }
@@ -153,13 +159,6 @@ public class SimpleItemFilter implements ItemFilter {
 
     public void setMaxStackSize(int maxStackSize) {
         this.maxStackSize = maxStackSize;
-
-        for (PhantomSlotWidget filterSlot : filterSlots) {
-            if (null == filterSlot)
-                continue;
-
-            filterSlot.setMaxStackSize(maxStackSize);
-        }
 
         for (ItemStack match : matches) {
             match.setCount(Math.min(match.getCount(), maxStackSize));
