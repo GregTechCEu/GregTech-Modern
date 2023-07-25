d37dca00f6ea2dfdba7e4c23f5b638b3edc99363
mikerooni <139889766+mikerooni@users.noreply.github.com>
2023-07-25 22:07:20 +0200

    style: Reformat cover & cover item definitions

--- common/src/main/java/com/gregtechceu/gtceu/common/data/GTCovers.java
+++ common/src/main/java/com/gregtechceu/gtceu/common/data/GTCovers.java
@@ -19,13 +19,45 @@ import java.util.Arrays;
  */
 public class GTCovers {
 
-    public final static CoverDefinition FACADE = register("facade", FacadeCover::new, FacadeCoverRenderer.INSTANCE);
-    public final static CoverDefinition ITEM_FILTER = register("item_filter", ItemFilterCover::new, new SimpleCoverRenderer(GTCEu.id("block/cover/overlay_item_filter")));
-    public final static CoverDefinition FLUID_FILTER = register("fluid_filter", FluidFilterCover::new, new SimpleCoverRenderer(GTCEu.id("block/cover/overlay_fluid_filter")));
-    public final static CoverDefinition INFINITE_WATER = register("infinite_water", InfiniteWaterCover::new, new SimpleCoverRenderer(GTCEu.id("block/cover/overlay_infinite_water")));
-    public final static CoverDefinition[] CONVEYORS = registerTiered("conveyor", ConveyorCover::new, tier -> ConveyorCoverRenderer.INSTANCE, GTValues.LV, GTValues.MV, GTValues.HV, GTValues.EV, GTValues.IV, GTValues.LuV);
-    public final static CoverDefinition[] ROBOT_ARMS = registerTiered("robot_arm", RobotArmCover::new, tier -> RobotArmCoverRenderer.INSTANCE, GTValues.LV, GTValues.MV, GTValues.HV, GTValues.EV, GTValues.IV, GTValues.LuV);
-    public final static CoverDefinition[] PUMPS = registerTiered("pump", PumpCover::new, tier -> PumpCoverRenderer.INSTANCE, GTValues.LV, GTValues.MV, GTValues.HV, GTValues.EV, GTValues.IV, GTValues.LuV);
+    public final static CoverDefinition FACADE = register(
+            "facade", FacadeCover::new,
+            FacadeCoverRenderer.INSTANCE
+    );
+
+    public final static CoverDefinition ITEM_FILTER = register(
+            "item_filter", ItemFilterCover::new,
+            new SimpleCoverRenderer(GTCEu.id("block/cover/overlay_item_filter"))
+    );
+
+    public final static CoverDefinition FLUID_FILTER = register(
+            "fluid_filter", FluidFilterCover::new,
+            new SimpleCoverRenderer(GTCEu.id("block/cover/overlay_fluid_filter"))
+    );
+
+    public final static CoverDefinition INFINITE_WATER = register(
+            "infinite_water", InfiniteWaterCover::new,
+            new SimpleCoverRenderer(GTCEu.id("block/cover/overlay_infinite_water"))
+    );
+
+    public final static CoverDefinition[] CONVEYORS = registerTiered(
+            "conveyor", ConveyorCover::new,
+            tier -> ConveyorCoverRenderer.INSTANCE, GTValues.LV, GTValues.MV, GTValues.HV, GTValues.EV, GTValues.IV, GTValues.LuV
+    );
+
+    public final static CoverDefinition[] ROBOT_ARMS = registerTiered(
+            "robot_arm", RobotArmCover::new,
+            tier -> RobotArmCoverRenderer.INSTANCE, GTValues.LV, GTValues.MV, GTValues.HV, GTValues.EV, GTValues.IV, GTValues.LuV
+    );
+
+    public final static CoverDefinition[] PUMPS = registerTiered(
+            "pump", PumpCover::new,
+            tier -> PumpCoverRenderer.INSTANCE, GTValues.LV, GTValues.MV, GTValues.HV, GTValues.EV, GTValues.IV, GTValues.LuV
+    );
+
+
+    ///////////////////////////////////////////////
+    //***********     UTIL METHODS    ***********//
+    ///////////////////////////////////////////////
 
     public static CoverDefinition register(String id, CoverDefinition.CoverBehaviourProvider behaviorCreator) {
         return register(id, behaviorCreator, new SimpleCoverRenderer(GTCEu.id("block/cover/" + id)));
