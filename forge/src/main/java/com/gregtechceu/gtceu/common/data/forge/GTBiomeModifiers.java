600f7e24c96d681409836223feb4e91ea6328d4f
screret <68943070+screret@users.noreply.github.com>
2023-07-25 11:40:47 +0300

    "fix" oregen on forge (the BiomeModifier didn't want to work so i made it manually (#204)
    Co-authored-by: Screret <joona.v.steberg@gmail.com>

--- forge/src/main/java/com/gregtechceu/gtceu/common/data/forge/GTBiomeModifiers.java
+++ forge/src/main/java/com/gregtechceu/gtceu/common/data/forge/GTBiomeModifiers.java
@@ -16,10 +16,8 @@ import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.common.world.ForgeBiomeModifiers;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.holdersets.AnyHolderSet;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 public class GTBiomeModifiers {
     public static final ResourceKey<BiomeModifier> ORE = ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, GTCEu.id("ore"));
@@ -29,21 +27,16 @@ public class GTBiomeModifiers {
     public static void bootstrap(BootstapContext<BiomeModifier> ctx, CompletableFuture<HolderLookup.Provider> provider) {
         HolderGetter<Biome> biomeLookup = ctx.lookup(Registries.BIOME);
         HolderGetter<PlacedFeature> placedFeatureRegistry = ctx.lookup(Registries.PLACED_FEATURE);
-        HolderSet<Biome> biomes; // ctx.registryLookup(Registries.BIOME).get()
-        try {
-            biomes = new AnyHolderSet<>(provider.get().lookupOrThrow(Registries.BIOME));
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
+        /*HolderSet<Biome> biomes = new AnyHolderSet<>(ctx.registryLookup(Registries.BIOME).orElseThrow());
         Holder<PlacedFeature> featureHolder = placedFeatureRegistry.getOrThrow(GTPlacements.ORE);
         ctx.register(ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
                 biomes,
                 HolderSet.direct(featureHolder),
                 GenerationStep.Decoration.UNDERGROUND_ORES
-        ));
+        ));*/
 
-        biomes = biomeLookup.getOrThrow(CustomTags.HAS_RUBBER_TREE);
-        featureHolder = placedFeatureRegistry.getOrThrow(GTPlacements.RUBBER_CHECKED);
+        HolderSet<Biome> biomes = biomeLookup.getOrThrow(CustomTags.HAS_RUBBER_TREE);
+        Holder<PlacedFeature> featureHolder = placedFeatureRegistry.getOrThrow(GTPlacements.RUBBER_CHECKED);
         ctx.register(RUBBER, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
                 biomes,
                 HolderSet.direct(featureHolder),
