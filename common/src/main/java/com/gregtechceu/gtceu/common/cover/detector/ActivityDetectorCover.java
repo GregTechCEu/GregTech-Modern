c1ca1d23711b70de52c8cef0e0d41fb0557a027a
mikerooni <139889766+mikerooni@users.noreply.github.com>
2023-07-28 10:33:34 +0200

    refactor: remove managed field holder from ActivityDetectorCover

--- common/src/main/java/com/gregtechceu/gtceu/common/cover/detector/ActivityDetectorCover.java
+++ common/src/main/java/com/gregtechceu/gtceu/common/cover/detector/ActivityDetectorCover.java
@@ -3,8 +3,6 @@ package com.gregtechceu.gtceu.common.cover.detector;
 import com.gregtechceu.gtceu.api.capability.GTCapabilityHelper;
 import com.gregtechceu.gtceu.api.capability.ICoverable;
 import com.gregtechceu.gtceu.api.cover.CoverDefinition;
-import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder;
-import lombok.Getter;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.Direction;
 
@@ -14,9 +12,6 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class ActivityDetectorCover extends DetectorCover {
-    @Getter
-    public static final ManagedFieldHolder MANAGED_FIELD_HOLDER = new ManagedFieldHolder(ActivityDetectorCover.class, DetectorCover.MANAGED_FIELD_HOLDER);
-
     public ActivityDetectorCover(CoverDefinition definition, ICoverable coverHolder, Direction attachedSide) {
         super(definition, coverHolder, attachedSide);
     }
