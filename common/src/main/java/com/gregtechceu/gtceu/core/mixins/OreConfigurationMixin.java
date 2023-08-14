b530cd863454941bbe1132bdcb485e3386495e65
Mikerooni <139889766+mikerooni@users.noreply.github.com>
2023-08-15 00:57:43 +0200

    feat: remove vanilla ore generation (#296)
    * feat: remove vanilla ore generation
    
    * feat: make ore generation removal configurable
    
    * feat: remove vanilla's large ore veins
    
    * chore: run datagen

---
+++ common/src/main/java/com/gregtechceu/gtceu/core/mixins/OreConfigurationMixin.java
@@ -0,0 +1,39 @@
+package com.gregtechceu.gtceu.core.mixins;
+
+import com.gregtechceu.gtceu.config.ConfigHolder;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration.TargetBlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.List;
+
+@Mixin(OreConfiguration.class)
+public class OreConfigurationMixin {
+    @Unique
+    private static final List<Block> EXCLUDED_BLOCKS = List.of(
+            Blocks.COAL_ORE, Blocks.DEEPSLATE_COAL_ORE,
+            Blocks.COPPER_ORE, Blocks.DEEPSLATE_COPPER_ORE,
+            Blocks.IRON_ORE, Blocks.DEEPSLATE_IRON_ORE,
+            Blocks.GOLD_ORE, Blocks.DEEPSLATE_GOLD_ORE, Blocks.NETHER_GOLD_ORE,
+            Blocks.DIAMOND_ORE, Blocks.DEEPSLATE_DIAMOND_ORE,
+            Blocks.EMERALD_ORE, Blocks.DEEPSLATE_EMERALD_ORE,
+            Blocks.LAPIS_ORE, Blocks.DEEPSLATE_LAPIS_ORE,
+            Blocks.REDSTONE_ORE, Blocks.DEEPSLATE_REDSTONE_ORE,
+            Blocks.NETHER_QUARTZ_ORE
+    );
+
+    @ModifyVariable(method = "<init>(Ljava/util/List;IF)V", at = @At("HEAD"), index = 1, argsOnly = true)
+    private static List<TargetBlockState> gtceu$init(List<TargetBlockState> targetStates) {
+        if (ConfigHolder.INSTANCE == null || !ConfigHolder.INSTANCE.worldgen.removeVanillaOreGen)
+            return targetStates;
+
+        return targetStates.stream()
+                .filter(targetState -> !EXCLUDED_BLOCKS.contains(targetState.state.getBlock()))
+                .toList();
+    }
+}
