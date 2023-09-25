f6350bee857c92cc84fce3c384a5a9b38d32b608
mikerooni <139889766+mikerooni@users.noreply.github.com>
2023-09-25 17:23:56 +0200

    feat: add fluid pipe tooltips

--- common/src/main/java/com/gregtechceu/gtceu/common/block/FluidPipeBlock.java
+++ common/src/main/java/com/gregtechceu/gtceu/common/block/FluidPipeBlock.java
@@ -2,21 +2,19 @@ package com.gregtechceu.gtceu.common.block;
 
 import com.gregtechceu.gtceu.api.block.MaterialPipeBlock;
 import com.gregtechceu.gtceu.api.blockentity.PipeBlockEntity;
-import com.gregtechceu.gtceu.api.capability.ICoverable;
 import com.gregtechceu.gtceu.api.data.chemical.material.Material;
+import com.gregtechceu.gtceu.api.data.chemical.material.properties.FluidPipeProperties;
 import com.gregtechceu.gtceu.api.data.chemical.material.properties.PropertyKey;
-import com.gregtechceu.gtceu.api.item.tool.GTToolType;
 import com.gregtechceu.gtceu.client.model.PipeModel;
-import com.gregtechceu.gtceu.common.item.CoverPlaceBehavior;
 import com.gregtechceu.gtceu.common.data.GTBlockEntities;
 import com.gregtechceu.gtceu.common.pipelike.fluidpipe.FluidPipeData;
 import com.gregtechceu.gtceu.common.pipelike.fluidpipe.FluidPipeType;
 import com.gregtechceu.gtceu.common.pipelike.fluidpipe.LevelFluidPipeNet;
+import com.gregtechceu.gtceu.utils.GTUtil;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockAndTintGetter;
@@ -24,8 +22,6 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.EntityCollisionContext;
-import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
@@ -73,6 +69,28 @@ public class FluidPipeBlock extends MaterialPipeBlock<FluidPipeType, FluidPipeDa
     @Override
     public void appendHoverText(ItemStack stack, @Nullable BlockGetter level, List<Component> tooltip, TooltipFlag flag) {
         super.appendHoverText(stack, level, tooltip, flag);
+        FluidPipeProperties properties = pipeType.modifyProperties(createRawData(defaultBlockState(), stack)).properties();
+
+        tooltip.add(Component.translatable("gtceu.universal.tooltip.fluid_transfer_rate", properties.getThroughput()));
+        tooltip.add(Component.translatable("gtceu.fluid_pipe.max_temperature", properties.getMaxFluidTemperature()));
+
+        if (properties.getChannels() > 1) {
+            tooltip.add(Component.translatable("gtceu.fluid_pipe.channels", properties.getChannels()));
+        }
+
+        if (!GTUtil.isShiftDown()) {
+            tooltip.add(Component.translatable("gtceu.tooltip.fluid_pipe_hold_shift"));
+            return;
+        }
+
+        if (properties.isGasProof())
+            tooltip.add(Component.translatable("gtceu.fluid_pipe.gas_proof"));
+        else
+            tooltip.add(Component.translatable("gtceu.fluid_pipe.not_gas_proof"));
+
+        if (properties.isAcidProof()) tooltip.add(Component.translatable("gtceu.fluid_pipe.acid_proof"));
+        if (properties.isCryoProof()) tooltip.add(Component.translatable("gtceu.fluid_pipe.cryo_proof"));
+        if (properties.isPlasmaProof()) tooltip.add(Component.translatable("gtceu.fluid_pipe.plasma_proof"));
     }
 
     @Override
