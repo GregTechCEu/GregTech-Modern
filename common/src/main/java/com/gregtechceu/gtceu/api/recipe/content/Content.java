843d3b001cb34605ca80f64009dfb535898cab47
screret <68943070+screret@users.noreply.github.com>
2023-10-23 14:54:12 +0300

    fix #488

--- common/src/main/java/com/gregtechceu/gtceu/api/recipe/content/Content.java
+++ common/src/main/java/com/gregtechceu/gtceu/api/recipe/content/Content.java
@@ -35,7 +35,7 @@ public class Content {
     }
 
     public Content copy(RecipeCapability<?> capability, @Nullable ContentModifier modifier) {
-        if (modifier == null) {
+        if (modifier == null || this.chance == 0.0f) {
             return new Content(capability.copyContent(content), chance, tierChanceBoost, slotName, uiName);
         } else {
             return new Content(capability.copyContent(content, modifier), chance, tierChanceBoost, slotName, uiName);
