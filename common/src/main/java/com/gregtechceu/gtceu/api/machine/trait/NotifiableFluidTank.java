9b83c721cada795dbbed419cecbda219f4e7a816
KilaBash <yefancy@foxmail.com>
2023-10-28 02:38:43 +0900

    fix fluid slot overflow

--- common/src/main/java/com/gregtechceu/gtceu/api/machine/trait/NotifiableFluidTank.java
+++ common/src/main/java/com/gregtechceu/gtceu/api/machine/trait/NotifiableFluidTank.java
@@ -66,6 +66,9 @@ public class NotifiableFluidTank extends NotifiableRecipeHandlerTrait<FluidIngre
         for (FluidStorage storage : this.storages) {
             storage.setOnContentsChanged(this::onContentsChanged);
         }
+        if (io == IO.IN) {
+            this.allowSameFluids = true;
+        }
     }
 
     public NotifiableFluidTank(MetaMachine machine, int slots, long capacity, IO io) {
@@ -219,9 +222,25 @@ public class NotifiableFluidTank extends NotifiableRecipeHandlerTrait<FluidIngre
     public long fill(FluidStack resource, boolean simulate, boolean notifyChanges) {
         if (resource.isEmpty()) return 0;
         long filled = 0;
-        for (int i = 0; i < getTanks(); i++) {
-            filled += fill(i, resource.copy(resource.getAmount() - filled), simulate, notifyChanges);
-            if (filled == resource.getAmount()) break;
+        FluidStorage existingStorage = null;
+        if (!allowSameFluids) {
+            for (var storage : storages) {
+                if (!storage.getFluid().isEmpty() && storage.getFluid().isFluidEqual(resource)) {
+                    existingStorage = storage;
+                    break;
+                }
+            }
+        }
+        if (existingStorage == null) {
+            for (int i = 0; i < getTanks(); i++) {
+                if (filled > 0 && !allowSameFluids) {
+                    break;
+                }
+                filled += fill(i, resource.copy(resource.getAmount() - filled), simulate, notifyChanges);
+                if (filled == resource.getAmount()) break;
+            }
+        } else {
+            filled += existingStorage.fill(resource.copy(resource.getAmount() - filled), simulate, notifyChanges);
         }
         if (notifyChanges && filled > 0 && !simulate) {
             onContentsChanged();
