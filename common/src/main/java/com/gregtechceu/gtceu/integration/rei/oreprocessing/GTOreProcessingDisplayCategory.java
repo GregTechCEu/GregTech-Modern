2fb59a698bfb6e2a819e941650d26bf33c807084
Arbor <44860537+Arborsm@users.noreply.github.com>
2023-11-20 15:07:00 +0800

    Update Ore Processing Diagram (#558)
    Signed-off-by: Arborsm <BlueGrayx@qq.com>
    Co-authored-by: Arborsm <BlueGrayx@qq.com>

--- common/src/main/java/com/gregtechceu/gtceu/integration/rei/oreprocessing/GTOreProcessingDisplayCategory.java
+++ common/src/main/java/com/gregtechceu/gtceu/integration/rei/oreprocessing/GTOreProcessingDisplayCategory.java
@@ -1,13 +1,9 @@
 package com.gregtechceu.gtceu.integration.rei.oreprocessing;
 
 import com.gregtechceu.gtceu.GTCEu;
-import com.gregtechceu.gtceu.api.data.chemical.ChemicalHelper;
+import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.api.data.chemical.material.Material;
-import com.gregtechceu.gtceu.api.machine.MachineDefinition;
-import com.gregtechceu.gtceu.api.recipe.GTRecipeType;
 import com.gregtechceu.gtceu.api.registry.GTRegistries;
-import com.gregtechceu.gtceu.common.data.GTMaterials;
-import com.gregtechceu.gtceu.common.data.GTRecipeTypes;
 import com.lowdragmc.lowdraglib.gui.texture.ItemStackTexture;
 import com.lowdragmc.lowdraglib.rei.IGui2Renderer;
 import com.lowdragmc.lowdraglib.rei.ModularUIDisplayCategory;
@@ -19,17 +15,12 @@ import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
 
 import javax.annotation.Nonnull;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static com.gregtechceu.gtceu.api.data.chemical.material.properties.PropertyKey.ORE;
-import static com.gregtechceu.gtceu.api.data.tag.TagPrefix.ingot;
-import static com.gregtechceu.gtceu.common.data.GTRecipeTypes.*;
+import static com.gregtechceu.gtceu.common.data.GTMachines.*;
 
 public class GTOreProcessingDisplayCategory extends ModularUIDisplayCategory<GTOreProcessingDisplay> {
     public static final CategoryIdentifier<GTOreProcessingDisplay> CATEGORY = CategoryIdentifier.of(GTCEu.id("ore_processing_diagram"));
@@ -74,22 +65,12 @@ public class GTOreProcessingDisplayCategory extends ModularUIDisplayCategory<GTO
     }
 
     public static void registerWorkstations(CategoryRegistry registry) {
-        List<MachineDefinition> registeredMachines = new ArrayList<>();
-        GTRecipeType[] validTypes = new GTRecipeType[] {
-                MACERATOR_RECIPES,ORE_WASHER_RECIPES,THERMAL_CENTRIFUGE_RECIPES,CENTRIFUGE_RECIPES,CHEMICAL_BATH_RECIPES,ELECTROMAGNETIC_SEPARATOR_RECIPES,SIFTER_RECIPES
-        };
-        for (MachineDefinition machine : GTRegistries.MACHINES) {
-            if (machine.getRecipeTypes() != null) {
-                for (GTRecipeType type : machine.getRecipeTypes()){
-                    for (GTRecipeType validType : validTypes){
-                        if (type == validType && !registeredMachines.contains(machine)) {
-                            registry.addWorkstations(GTOreProcessingDisplayCategory.CATEGORY, EntryStacks.of(machine.asStack()));
-                            registeredMachines.add(machine);
-                        }
-                    }
-                }
-            }
-        }
+        registry.addWorkstations(GTOreProcessingDisplayCategory.CATEGORY, EntryStacks.of(MACERATOR[GTValues.LV].asStack()));
+        registry.addWorkstations(GTOreProcessingDisplayCategory.CATEGORY, EntryStacks.of(ORE_WASHER[GTValues.LV].asStack()));
+        registry.addWorkstations(GTOreProcessingDisplayCategory.CATEGORY, EntryStacks.of(THERMAL_CENTRIFUGE[GTValues.LV].asStack()));
+        registry.addWorkstations(GTOreProcessingDisplayCategory.CATEGORY, EntryStacks.of(CENTRIFUGE[GTValues.LV].asStack()));
+        registry.addWorkstations(GTOreProcessingDisplayCategory.CATEGORY, EntryStacks.of(CHEMICAL_BATH[GTValues.LV].asStack()));
+        registry.addWorkstations(GTOreProcessingDisplayCategory.CATEGORY, EntryStacks.of(ELECTROMAGNETIC_SEPARATOR[GTValues.LV].asStack()));
+        registry.addWorkstations(GTOreProcessingDisplayCategory.CATEGORY, EntryStacks.of(SIFTER[GTValues.LV].asStack()));
     }
-
 }
