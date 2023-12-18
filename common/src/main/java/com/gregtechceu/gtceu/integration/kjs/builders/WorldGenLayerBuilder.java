5a5c6b1747a0e2ea562a7db882fb4502e06ff721
mikerooni <139889766+mikerooni@users.noreply.github.com>
2023-12-18 20:02:42 +0100

    refactor(kjs): improve worldgen layer builder

--- common/src/main/java/com/gregtechceu/gtceu/integration/kjs/builders/WorldGenLayerBuilder.java
+++ common/src/main/java/com/gregtechceu/gtceu/integration/kjs/builders/WorldGenLayerBuilder.java
@@ -1,24 +1,20 @@
 package com.gregtechceu.gtceu.integration.kjs.builders;
 
-import com.gregtechceu.gtceu.api.data.worldgen.IWorldGenLayer;
 import com.gregtechceu.gtceu.api.data.worldgen.SimpleWorldGenLayer;
 import com.gregtechceu.gtceu.api.registry.registrate.BuilderBase;
+import dev.latvian.mods.kubejs.level.gen.ruletest.AnyMatchRuleTest;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import lombok.Setter;
 import lombok.experimental.Accessors;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Supplier;
 
 @Accessors(fluent = true, chain = true)
 public class WorldGenLayerBuilder extends BuilderBase<SimpleWorldGenLayer> {
-    @Setter
-    public transient IWorldGenLayer.RuleTestSupplier target;
-
-    @Setter
+    public transient List<RuleTest> targets = new ObjectArrayList<>();
     public transient List<ResourceLocation> dimensions = new ObjectArrayList<>();
 
     public WorldGenLayerBuilder(ResourceLocation id, Object... args) {
@@ -27,12 +23,17 @@ public class WorldGenLayerBuilder extends BuilderBase<SimpleWorldGenLayer> {
 
     @Override
     public SimpleWorldGenLayer register() {
-        this.value = new SimpleWorldGenLayer(this.id.getPath(), target, Set.copyOf(dimensions));
+        this.value = new SimpleWorldGenLayer(this.id.getPath(), () -> new AnyMatchRuleTest(targets), Set.copyOf(dimensions));
         return value;
     }
 
-    public WorldGenLayerBuilder addDimension(ResourceLocation dimension) {
-        this.dimensions.add(dimension);
+    public WorldGenLayerBuilder targets(RuleTest... tags) {
+        this.targets.addAll(Arrays.asList(tags));
+        return this;
+    }
+
+    public WorldGenLayerBuilder dimensions(ResourceLocation... dimension) {
+        this.dimensions.addAll(Arrays.asList(dimension));
         return this;
     }
 }
