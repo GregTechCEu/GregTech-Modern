82c4dd2d3ee2933ec96d7bd4056ebf4c5c1b378b
Spencer <75862693+TacoMonkey11@users.noreply.github.com>
2023-12-24 10:26:58 -0500

    Multiply mining hammer durability by 3

--- common/src/main/java/com/gregtechceu/gtceu/common/data/GTItems.java
+++ common/src/main/java/com/gregtechceu/gtceu/common/data/GTItems.java
@@ -159,7 +159,13 @@ public class GTItems {
                 for (GTToolType toolType : GTToolType.values()) {
                     if (property.hasType(toolType)) {
                         TOOL_ITEMS.put(tier, toolType, REGISTRATE.item("%s_%s".formatted(tier.material.getName().toLowerCase(Locale.ROOT), toolType.name), p -> GTToolItem.create(toolType, tier, p))
-                                .properties(p -> p.craftRemainder(Items.AIR))
+                                .properties(p -> {
+                                    if (toolType.equals(GTToolType.MINING_HAMMER)) {
+                                        return p.craftRemainder(Items.AIR).durability(tier.getUses() * 3);
+                                    } else {
+                                        return p.craftRemainder(Items.AIR);
+                                    }
+                                })
                                 .setData(ProviderType.LANG, NonNullBiConsumer.noop())
                                 .model(NonNullBiConsumer.noop())
                                 .color(() -> GTToolItem::tintColor)
