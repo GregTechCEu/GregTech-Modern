9ba281ef43e43efb2718590bf4cb0521a5e03582
mikerooni <139889766+mikerooni@users.noreply.github.com>
2024-01-09 23:51:36 +0100

    chore(drop-fabric): remove fabric subproject

--- fabric/src/main/java/com/gregtechceu/gtceu/common/item/tool/behavior/fabric/GrassPathBehaviorImpl.java
+++
@@ -1,9 +0,0 @@
-package com.gregtechceu.gtceu.common.item.tool.behavior.fabric;
-
-import com.gregtechceu.gtceu.common.item.tool.behavior.GrassPathBehavior;
-
-public class GrassPathBehaviorImpl extends GrassPathBehavior {
-    public static GrassPathBehavior create() {
-        return new GrassPathBehaviorImpl();
-    }
-}
--- fabric/src/main/java/com/gregtechceu/gtceu/common/item/tool/behavior/fabric/ScrapeBehaviorImpl.java
+++
@@ -1,9 +0,0 @@
-package com.gregtechceu.gtceu.common.item.tool.behavior.fabric;
-
-import com.gregtechceu.gtceu.common.item.tool.behavior.ScrapeBehavior;
-
-public class ScrapeBehaviorImpl extends ScrapeBehavior {
-    public static ScrapeBehavior create() {
-        return new ScrapeBehaviorImpl();
-    }
-}
