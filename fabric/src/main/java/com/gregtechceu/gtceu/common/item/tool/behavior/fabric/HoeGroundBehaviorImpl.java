b3bebaec8f2cc5b59e8b1c7dc458812ac662043f
mikerooni <139889766+mikerooni@users.noreply.github.com>
2024-01-10 22:45:02 +0100

    chore(drop-fabric): remove fabric subproject

--- fabric/src/main/java/com/gregtechceu/gtceu/common/item/tool/behavior/fabric/HoeGroundBehaviorImpl.java
+++
@@ -1,9 +0,0 @@
-package com.gregtechceu.gtceu.common.item.tool.behavior.fabric;
-
-import com.gregtechceu.gtceu.common.item.tool.behavior.HoeGroundBehavior;
-
-public class HoeGroundBehaviorImpl extends HoeGroundBehavior {
-    public static HoeGroundBehavior create() {
-        return new HoeGroundBehaviorImpl();
-    }
-}
--- fabric/src/main/java/com/gregtechceu/gtceu/common/item/tool/behavior/fabric/LogStripBehaviorImpl.java
+++
@@ -1,9 +0,0 @@
-package com.gregtechceu.gtceu.common.item.tool.behavior.fabric;
-
-import com.gregtechceu.gtceu.common.item.tool.behavior.LogStripBehavior;
-
-public class LogStripBehaviorImpl extends LogStripBehavior {
-    public static LogStripBehavior create() {
-        return new LogStripBehaviorImpl();
-    }
-}
