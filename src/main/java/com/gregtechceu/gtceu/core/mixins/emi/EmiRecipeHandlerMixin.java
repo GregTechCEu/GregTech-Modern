2d954e41f439593469095c2b512ebef9ee5dbb21
Arbor <44860537+Arborsm@users.noreply.github.com>
2024-01-11 19:47:16 +0800

    fix EMI crashes with GT crates (#739)

--- src/main/java/com/gregtechceu/gtceu/core/mixins/emi/EmiRecipeHandlerMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/emi/EmiRecipeHandlerMixin.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class EmiRecipeHandlerMixin {
     @Inject(method = "getAllHandlers", at = @At("HEAD"), cancellable = true, remap = false)
     private static <T extends AbstractContainerMenu> void AddGTEmiRecipeHandler(AbstractContainerScreen<T> screen, CallbackInfoReturnable<List<EmiRecipeHandler<T>>> cir){
-        if (screen != null) {
+        if (screen != null && !(screen.getMenu() instanceof InventoryMenu)) {
             if (screen.getMenu() instanceof ModularUIContainer) {
                 cir.setReturnValue((List<EmiRecipeHandler<T>>) (List<?>) List.of(new GTEmiRecipeHandler()));
             }
