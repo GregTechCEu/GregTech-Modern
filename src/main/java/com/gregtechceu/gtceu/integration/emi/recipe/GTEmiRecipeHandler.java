a75df85cfd9baab547dcf13e4738cd2edb79ce2e
mikerooni <139889766+mikerooni@users.noreply.github.com>
2024-01-17 19:55:48 +0100

    fix: EMI crash when viewing recipes from crafting tree

--- src/main/java/com/gregtechceu/gtceu/integration/emi/recipe/GTEmiRecipeHandler.java
+++ src/main/java/com/gregtechceu/gtceu/integration/emi/recipe/GTEmiRecipeHandler.java
@@ -5,22 +5,29 @@ import com.lowdragmc.lowdraglib.gui.widget.SlotWidget;
 import com.lowdragmc.lowdraglib.jei.IngredientIO;
 import dev.emi.emi.api.recipe.EmiRecipe;
 import dev.emi.emi.api.recipe.handler.StandardRecipeHandler;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.Slot;
 
 import java.util.List;
 
-public class GTEmiRecipeHandler implements StandardRecipeHandler<ModularUIContainer> {
+public class GTEmiRecipeHandler implements StandardRecipeHandler<AbstractContainerMenu> {
     @Override
-    public List<Slot> getInputSources(ModularUIContainer handler) {
-        return handler.getModularUI().getSlotMap().values().stream()
+    public List<Slot> getInputSources(AbstractContainerMenu handler) {
+        if (!(handler instanceof ModularUIContainer))
+            return List.of();
+
+        return ((ModularUIContainer) handler).getModularUI().getSlotMap().values().stream()
                 .filter(e -> e.getIngredientIO() == IngredientIO.INPUT || e.isPlayerContainer || e.isPlayerHotBar)
                 .map(SlotWidget::getHandle)
                 .toList();
     }
 
     @Override
-    public List<Slot> getCraftingSlots(ModularUIContainer handler) {
-        return handler.getModularUI().getSlotMap().values().stream()
+    public List<Slot> getCraftingSlots(AbstractContainerMenu handler) {
+        if (!(handler instanceof ModularUIContainer))
+            return List.of();
+
+        return ((ModularUIContainer) handler).getModularUI().getSlotMap().values().stream()
                 .filter(e -> e.getIngredientIO() == IngredientIO.INPUT)
                 .map(SlotWidget::getHandle)
                 .toList();
