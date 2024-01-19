9448c02d00f72e82a8734771831e39090bd15c35
screret <68943070+screret@users.noreply.github.com>
2024-01-19 17:22:04 +0200

    fix: IGTool AOE size UI wasn't openable

--- src/main/java/com/gregtechceu/gtceu/api/item/IGTTool.java
+++ src/main/java/com/gregtechceu/gtceu/api/item/IGTTool.java
@@ -17,7 +17,6 @@ import com.gregtechceu.gtceu.api.item.capability.ElectricItem;
 import com.gregtechceu.gtceu.api.item.component.ElectricStats;
 import com.gregtechceu.gtceu.api.item.component.IItemUIFactory;
 import com.gregtechceu.gtceu.api.item.component.forge.IComponentCapability;
-import com.gregtechceu.gtceu.api.item.gui.PlayerInventoryHolder;
 import com.gregtechceu.gtceu.api.item.tool.GTToolType;
 import com.gregtechceu.gtceu.api.item.tool.IGTToolDefinition;
 import com.gregtechceu.gtceu.api.item.tool.ToolHelper;
@@ -87,7 +86,7 @@ import static com.gregtechceu.gtceu.api.item.tool.ToolHelper.*;
 import static net.minecraft.world.item.Item.BASE_ATTACK_DAMAGE_UUID;
 import static net.minecraft.world.item.Item.BASE_ATTACK_SPEED_UUID;
 
-public interface IGTTool extends IItemUIFactory, ItemLike {
+public interface IGTTool extends HeldItemUIFactory.IHeldItemUIHolder, ItemLike {
 
     Material getMaterial();
     
@@ -361,7 +360,7 @@ public interface IGTTool extends IItemUIFactory, ItemLike {
         if (player.level().isClientSide) return false;
         getToolStats().getBehaviors().forEach(behavior -> behavior.onBlockStartBreak(stack, pos, player));
 
-        if (!player.isCrouching()) {
+        if (!player.isShiftKeyDown()) {
             ServerPlayer playerMP = (ServerPlayer) player;
             int result = -1;
             if (isTool(stack, GTToolType.SHEARS)) {
@@ -409,7 +408,7 @@ public interface IGTTool extends IItemUIFactory, ItemLike {
             if (entityLiving instanceof Player && playSoundOnBlockDestroy()) {
                 // sneaking disables AOE, which means it is okay to play the sound
                 // not checking this means the sound will play for every AOE broken block, which is very loud
-                if (entityLiving.isCrouching()) {
+                if (entityLiving.isShiftKeyDown()) {
                     playSound((Player) entityLiving);
                 }
             }
@@ -574,13 +573,15 @@ public interface IGTTool extends IItemUIFactory, ItemLike {
 
     default InteractionResultHolder<ItemStack> definition$use(Level world, Player player, InteractionHand hand) {
         ItemStack stack = player.getItemInHand(hand);
-        if (!world.isClientSide) {
-            // TODO: relocate to keybind action when keybind PR happens
-            if (player.isCrouching() && getMaxAoEDefinition(stack) != AoESymmetrical.none()) {
-                PlayerInventoryHolder.openHandItemUI(player, hand);
-                return InteractionResultHolder.success(stack);
+        // TODO: relocate to keybind action when keybind PR happens
+        if (player.isShiftKeyDown() && getMaxAoEDefinition(stack) != AoESymmetrical.none()) {
+            ItemStack heldItem = player.getItemInHand(hand);
+            if (player instanceof ServerPlayer serverPlayer) {
+                HeldItemUIFactory.INSTANCE.openUI(serverPlayer, hand);
             }
+            return InteractionResultHolder.success(heldItem);
         }
+
 
         for (IToolBehavior behavior : getToolStats().getBehaviors()) {
             if (behavior.onItemRightClick(world, player, hand).getResult() == InteractionResult.SUCCESS) {
@@ -795,7 +796,8 @@ public interface IGTTool extends IItemUIFactory, ItemLike {
         }
     }
 
-    default ModularUI createUI(HeldItemUIFactory.HeldItemHolder holder, Player entityPlayer) {
+    @Override
+    default ModularUI createUI(Player entityPlayer, HeldItemUIFactory.HeldItemHolder holder) {
         CompoundTag tag = getBehaviorsTag(holder.getHeld());
         AoESymmetrical defaultDefinition = getMaxAoEDefinition(holder.getHeld());
         return new ModularUI(120, 80, holder, entityPlayer).background(GuiTextures.BACKGROUND)
