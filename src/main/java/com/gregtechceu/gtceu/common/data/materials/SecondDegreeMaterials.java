7b8d6f695cb56732d0cd8c69c2042422604b7b50
Irgendwer01 <67506320+Irgendwer01@users.noreply.github.com>
2024-01-21 19:40:04 +0100

    Port Flint Tools

--- src/main/java/com/gregtechceu/gtceu/common/data/materials/SecondDegreeMaterials.java
+++ src/main/java/com/gregtechceu/gtceu/common/data/materials/SecondDegreeMaterials.java
@@ -371,8 +371,8 @@ public class SecondDegreeMaterials {
                 .color(0xc7c7c7).secondaryColor(0x212121).iconSet(FLINT)
                 .flags(NO_SMASHING, MORTAR_GRINDABLE, DECOMPOSITION_BY_CENTRIFUGING)
                 .components(SiliconDioxide, 1)
-                .toolStats(ToolProperty.Builder.of(0.0F, 1.0F, 64, 1)
-                        .types(GTToolType.MORTAR, GTToolType.KNIFE)
+                .toolStats(ToolProperty.Builder.of(1.5F, 1.0F, 64, 2)
+                        .types(GTToolType.MORTAR, GTToolType.KNIFE, GTToolType.AXE, GTToolType.PICKAXE, GTToolType.HOE, GTToolType.SWORD, GTToolType.SHOVEL)
                         .enchantability(5).ignoreCraftingTools()
                         .enchantment(Enchantments.FIRE_ASPECT, 2).build())
                 .buildAndRegister();
