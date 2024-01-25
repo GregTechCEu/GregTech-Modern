52c37788688ee6d73a264b068d149766d42b0c3b
screret <68943070+screret@users.noreply.github.com>
2024-01-25 18:49:48 +0200

    clean up mixin

--- src/main/java/com/gregtechceu/gtceu/core/mixins/GuiGraphicsMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/GuiGraphicsMixin.java
@@ -17,21 +17,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GuiGraphics.class)
 public class GuiGraphicsMixin {
 
-    @Inject(method = "renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isBarVisible()Z"))
-    private void gtceu$renderCustomDurabilityBars(Font font, ItemStack stack, int x, int y, String text, CallbackInfo ci, @Share("didRender") LocalBooleanRef didRender) {
-        if (stack.isBarVisible()) {
-            if (stack.getItem() instanceof IGTTool toolItem) {
-                ToolChargeBarRenderer.renderBarsTool((GuiGraphics) (Object) this, toolItem, stack, x, y);
-                didRender.set(true);
-            } else if (stack.getItem() instanceof ComponentItem componentItem) {
-                ToolChargeBarRenderer.renderBarsItem((GuiGraphics) (Object) this, componentItem, stack, x, y);
-                didRender.set(true);
-            }
+    @Inject(
+        method = "renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V",
+        at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/client/gui/GuiGraphics;minecraft:Lnet/minecraft/client/Minecraft;",
+            shift = At.Shift.BEFORE,
+            ordinal = 0))
+    private void gtceu$renderCustomDurabilityBars(Font font, ItemStack stack, int x, int y, String text, CallbackInfo ci) {
+        if (stack.getItem() instanceof IGTTool toolItem) {
+            ToolChargeBarRenderer.renderBarsTool((GuiGraphics) (Object) this, toolItem, stack, x, y);
+        } else if (stack.getItem() instanceof ComponentItem componentItem) {
+            ToolChargeBarRenderer.renderBarsItem((GuiGraphics) (Object) this, componentItem, stack, x, y);
         }
-    }
-
-    @ModifyExpressionValue(method = "renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isBarVisible()Z"))
-    private boolean gtceu$wrapRenderOriginalBar(boolean original, @Share("didRender") LocalBooleanRef didRender) {
-        return original && !didRender.get();
     }
 }
