2b5b83439bdfe083f834768f34bd6f478119efc4
screret <68943070+screret@users.noreply.github.com>
2024-01-30 18:38:22 +0200

    god fucking damnit what

--- src/main/java/com/gregtechceu/gtceu/data/recipe/configurable/RecipeRemoval.java
+++ src/main/java/com/gregtechceu/gtceu/data/recipe/configurable/RecipeRemoval.java
@@ -210,8 +210,6 @@ public class RecipeRemoval {
         registry.accept(new ResourceLocation("minecraft:crimson_button"));
         registry.accept(new ResourceLocation("minecraft:warped_button"));
         registry.accept(new ResourceLocation("minecraft:mangrove_button"));
-        registry.accept(new ResourceLocation("minecraft:cherry_button"));
-        registry.accept(new ResourceLocation("minecraft:bamboo_button"));
     }
 
     private static void hardToolArmorRecipes(Consumer<ResourceLocation> registry) {
