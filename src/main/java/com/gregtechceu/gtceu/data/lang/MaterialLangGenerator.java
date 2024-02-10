ba4f4965379e2c0f8bbcf6c8b1e11ed18afa34f3
rmi <remi2.vlieghe@icloud.com>
2024-02-10 15:20:29 +0100

    Moved RTM Alloy localization replacement

--- src/main/java/com/gregtechceu/gtceu/data/lang/MaterialLangGenerator.java
+++ src/main/java/com/gregtechceu/gtceu/data/lang/MaterialLangGenerator.java
@@ -2,19 +2,14 @@ package com.gregtechceu.gtceu.data.lang;
 
 import com.gregtechceu.gtceu.api.data.chemical.material.Material;
 import com.gregtechceu.gtceu.api.data.chemical.material.registry.MaterialRegistry;
-import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.tterrag.registrate.providers.RegistrateLangProvider;
 
 import static com.gregtechceu.gtceu.utils.FormattingUtil.toEnglishName;
-import static com.gregtechceu.gtceu.data.lang.LangHandler.replace;
 
 public class MaterialLangGenerator {
 
     public static void generate(RegistrateLangProvider provider, MaterialRegistry registry) {
-        for (Material material : registry.getAllMaterials()) {
+        for (Material material : registry.getAllMaterials())
             provider.add(material.getUnlocalizedName(), toEnglishName(material.getName()));
-        }
-
-        replace(provider, GTMaterials.RTMAlloy.getUnlocalizedName(),"RTM Alloy");
     }
 }
