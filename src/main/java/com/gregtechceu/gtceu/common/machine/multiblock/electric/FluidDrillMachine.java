3aa61b2b687a5d0d392f68b1ae0c516798a7310e
screret <68943070+screret@users.noreply.github.com>
2024-03-22 13:39:17 +0200

    fix some issues with fluid drilling rigs (#970) + add extra tooltips (#982)

--- src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/electric/FluidDrillMachine.java
+++ src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/electric/FluidDrillMachine.java
@@ -2,19 +2,30 @@ package com.gregtechceu.gtceu.common.machine.multiblock.electric;
 
 import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.GTValues;
+import com.gregtechceu.gtceu.api.capability.IEnergyContainer;
+import com.gregtechceu.gtceu.api.capability.recipe.EURecipeCapability;
+import com.gregtechceu.gtceu.api.capability.recipe.IO;
 import com.gregtechceu.gtceu.api.data.tag.TagPrefix;
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 import com.gregtechceu.gtceu.api.machine.feature.ITieredMachine;
 import com.gregtechceu.gtceu.api.machine.multiblock.WorkableElectricMultiblockMachine;
 import com.gregtechceu.gtceu.api.machine.trait.RecipeLogic;
+import com.gregtechceu.gtceu.api.misc.EnergyContainerList;
 import com.gregtechceu.gtceu.common.data.GTBlocks;
 import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.gregtechceu.gtceu.common.machine.trait.FluidDrillLogic;
+import com.gregtechceu.gtceu.utils.FormattingUtil;
+import com.gregtechceu.gtceu.utils.GTUtil;
 import lombok.Getter;
+import net.minecraft.ChatFormatting;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
+import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.material.Fluid;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
@@ -41,18 +52,48 @@ public class FluidDrillMachine extends WorkableElectricMultiblockMachine impleme
         return new FluidDrillLogic(this);
     }
 
+    @NotNull
+    @Override
+    public FluidDrillLogic getRecipeLogic() {
+        return (FluidDrillLogic) super.getRecipeLogic();
+    }
+
     public int getEnergyTier() {
-        return Math.min(this.tier + 1 , Math.max(this.tier, getOverclockTier()));
+        var energyContainer = this.getCapabilitiesProxy().get(IO.IN, EURecipeCapability.CAP);
+        if (energyContainer == null) return this.tier;
+        var energyCont = new EnergyContainerList(energyContainer.stream().filter(IEnergyContainer.class::isInstance).map(IEnergyContainer.class::cast).toList());
+
+        return Math.min(this.tier + 1, Math.max(this.tier, GTUtil.getFloorTierByVoltage(energyCont.getInputVoltage())));
     }
 
     @Override
     public void addDisplayText(List<Component> textList) {
-        super.addDisplayText(textList);
         if (isFormed()) {
             int energyContainer = getEnergyTier();
             long maxVoltage = GTValues.V[energyContainer];
             String voltageName = GTValues.VNF[energyContainer];
             textList.add(Component.translatable("gtceu.multiblock.max_energy_per_tick", maxVoltage, voltageName));
+
+            if (getRecipeLogic().getVeinFluid() != null) {
+                // Fluid name
+                Fluid drilledFluid = getRecipeLogic().getVeinFluid();
+                Component fluidInfo = drilledFluid.getFluidType().getDescription().copy().withStyle(ChatFormatting.GREEN);
+                textList.add(Component.translatable("gtceu.multiblock.fluid_rig.drilled_fluid", fluidInfo).withStyle(ChatFormatting.GRAY));
+
+                // Fluid amount
+                Component amountInfo = Component.literal(FormattingUtil.formatNumbers(
+                    getRecipeLogic().getFluidToProduce() * 20L / FluidDrillLogic.MAX_PROGRESS) +
+                    " mB/s").withStyle(ChatFormatting.BLUE);
+                textList.add(Component.translatable("gtceu.multiblock.fluid_rig.fluid_amount", amountInfo).withStyle(ChatFormatting.GRAY));
+            } else {
+                Component noFluid = Component.translatable("gtceu.multiblock.fluid_rig.no_fluid_in_area").withStyle(ChatFormatting.RED);
+                textList.add(Component.translatable("gtceu.multiblock.fluid_rig.drilled_fluid", noFluid).withStyle(ChatFormatting.GRAY));
+            }
+        } else {
+            Component tooltip = Component.translatable("gtceu.multiblock.invalid_structure.tooltip").withStyle(ChatFormatting.GRAY);
+            textList.add(Component.translatable("gtceu.multiblock.invalid_structure")
+                .withStyle(Style.EMPTY.withColor(ChatFormatting.RED)
+                    .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, tooltip))));
         }
     }
 
@@ -86,6 +127,7 @@ public class FluidDrillMachine extends WorkableElectricMultiblockMachine impleme
         return GTBlocks.CASING_STEEL_SOLID.get();
     }
 
+    @SuppressWarnings("DataFlowIssue")
     public static Block getFrameState(int tier) {
         if (tier == GTValues.MV)
             return GTBlocks.MATERIAL_BLOCKS.get(TagPrefix.frameGt, GTMaterials.Steel).get();
