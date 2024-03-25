8fadcb6c0ee4da6a74d6b8dac0ab1ce35cd14ea2
screret <68943070+screret@users.noreply.github.com>
2024-03-25 11:41:15 +0200

    port 1.12#2139, 1.12#1741

--- src/main/java/com/gregtechceu/gtceu/api/misc/EnergyContainerList.java
+++ src/main/java/com/gregtechceu/gtceu/api/misc/EnergyContainerList.java
@@ -1,16 +1,131 @@
 package com.gregtechceu.gtceu.api.misc;
 
 import com.gregtechceu.gtceu.api.capability.IEnergyContainer;
+import lombok.Getter;
 import net.minecraft.core.Direction;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 public class EnergyContainerList implements IEnergyContainer {
 
     private final List<? extends IEnergyContainer> energyContainerList;
+    @Getter
+    private final long inputVoltage;
+    @Getter
+    private final long outputVoltage;
+    /**
+     * Always < 4. A list with amps > 4 will always be compacted into more voltage at fewer amps.
+     */
+    @Getter
+    private final long inputAmperage;
+    /**
+     * Always < 4. A list with amps > 4 will always be compacted into more voltage at fewer amps.
+     */
+    @Getter
+    private final long outputAmperage;
+
+    /** The highest single energy container's input voltage in the list. */
+    @Getter
+    private final long highestInputVoltage;
+    /** The number of energy containers at the highest input voltage in the list. */
+    @Getter
+    private final int numHighestInputContainers;
 
     public EnergyContainerList(List<? extends IEnergyContainer> energyContainerList) {
         this.energyContainerList = energyContainerList;
+        long totalInputVoltage = 0;
+        long totalOutputVoltage = 0;
+        long inputAmperage = 0;
+        long outputAmperage = 0;
+        long highestInputVoltage = 0;
+        int numHighestInputContainers = 0;
+        for (IEnergyContainer container : energyContainerList) {
+            totalInputVoltage += container.getInputVoltage() * container.getInputAmperage();
+            totalOutputVoltage += container.getOutputVoltage() * container.getOutputAmperage();
+            inputAmperage += container.getInputAmperage();
+            outputAmperage += container.getOutputAmperage();
+            if (container.getInputVoltage() > highestInputVoltage) {
+                highestInputVoltage = container.getInputVoltage();
+            }
+        }
+        for (IEnergyContainer container : energyContainerList) {
+            if (container.getInputVoltage() == highestInputVoltage) {
+                numHighestInputContainers++;
+            }
+        }
+
+        long[] voltageAmperage = calculateVoltageAmperage(totalInputVoltage, inputAmperage);
+        this.inputVoltage = voltageAmperage[0];
+        this.inputAmperage = voltageAmperage[1];
+        voltageAmperage = calculateVoltageAmperage(totalOutputVoltage, outputAmperage);
+        this.outputVoltage = voltageAmperage[0];
+        this.outputAmperage = voltageAmperage[1];
+        this.highestInputVoltage = highestInputVoltage;
+        this.numHighestInputContainers = numHighestInputContainers;
+    }
+
+    /**
+     * Computes the correct max voltage and amperage values
+     *
+     * @param voltage  the sum of voltage * amperage for each hatch
+     * @param amperage the total amperage of all hatches
+     *
+     * @return [newVoltage, newAmperage]
+     */
+    @NotNull
+    private static long[] calculateVoltageAmperage(long voltage, long amperage) {
+        if (voltage > 1 && amperage > 1) {
+            // don't operate if there is no voltage or no amperage
+            if (hasPrimeFactorGreaterThanTwo(amperage)) {
+                // scenarios like 3A, 5A, 6A, etc.
+                // treated as 1A of the sum of voltage * amperage for each hatch
+                amperage = 1;
+            } else if (isPowerOfFour(amperage)) {
+                // scenarios like 4A, 16A, etc.
+                // treated as 1A of the sum of voltage * amperage for each hatch
+                amperage = 1;
+            } else if (amperage % 4 == 0) {
+                // scenarios like 8A, 32A, etc.
+                // reduced to an amperage < 4 and equivalent voltage for the new amperage
+                while (amperage > 4) {
+                    amperage /= 4;
+                }
+                voltage /= amperage;
+            } else if (amperage == 2) {
+                // exactly 2A, all other cases covered by earlier checks
+                // reduced to the voltage per amp
+                voltage /= amperage;
+            } else {
+                // fallback case, that should never be hit
+                // forced to 1A to prevent excess power draw/output if something falls through
+                amperage = 1;
+            }
+        }
+        return new long[] { voltage, amperage };
+    }
+
+    private static boolean hasPrimeFactorGreaterThanTwo(long l) {
+        int i = 2;
+        final long max = l / 2;
+        while (i <= max) {
+            if (l % i == 0) {
+                if (i > 2) return true;
+                l /= i;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if a number is a power of 4. Does not include 1, despite it being 4**0.
+     */
+    private static boolean isPowerOfFour(long l) {
+        if (l == 0) return false;
+        if ((l & (l - 1)) != 0) return false;
+        return (l & 0x55555555) != 0;
     }
 
     @Override
@@ -58,34 +173,6 @@ public class EnergyContainerList implements IEnergyContainer {
     }
 
     @Override
-    public long getInputAmperage() {
-        return 1L;
-    }
-
-    @Override
-    public long getOutputAmperage() {
-        return 1L;
-    }
-
-    @Override
-    public long getInputVoltage() {
-        long inputVoltage = 0L;
-        for (IEnergyContainer container : energyContainerList) {
-            inputVoltage += container.getInputVoltage() * container.getInputAmperage();
-        }
-        return inputVoltage;
-    }
-
-    @Override
-    public long getOutputVoltage() {
-        long outputVoltage = 0L;
-        for (IEnergyContainer container : energyContainerList) {
-            outputVoltage += container.getOutputVoltage() * container.getOutputAmperage();
-        }
-        return outputVoltage;
-    }
-
-    @Override
     public boolean inputsEnergy(Direction side) {
         return true;
     }
@@ -93,5 +180,18 @@ public class EnergyContainerList implements IEnergyContainer {
     @Override
     public boolean outputsEnergy(Direction side) {
         return true;
+    }
+
+    @Override
+    public String toString() {
+        return "EnergyContainerList{" +
+            "energyContainerList=" + energyContainerList +
+            ", energyStored=" + getEnergyStored() +
+            ", energyCapacity=" + getEnergyCapacity() +
+            ", inputVoltage=" + inputVoltage +
+            ", inputAmperage=" + inputAmperage +
+            ", outputVoltage=" + outputVoltage +
+            ", outputAmperage=" + outputAmperage +
+            '}';
     }
 }
