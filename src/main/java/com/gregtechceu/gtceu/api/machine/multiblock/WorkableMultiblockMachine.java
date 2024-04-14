1f86a5ceac5baed769b76f359dfb10f47a9952c3
screret <68943070+screret@users.noreply.github.com>
2024-04-14 10:20:09 +0300

    add recipe to beforeWorking callback

--- src/main/java/com/gregtechceu/gtceu/api/machine/multiblock/WorkableMultiblockMachine.java
+++ src/main/java/com/gregtechceu/gtceu/api/machine/multiblock/WorkableMultiblockMachine.java
@@ -228,13 +228,13 @@ public abstract class WorkableMultiblockMachine extends MultiblockControllerMach
     }
 
     @Override
-    public boolean beforeWorking() {
+    public boolean beforeWorking(@Nullable GTRecipe recipe) {
         for (IMultiPart part : getParts()) {
             if (!part.beforeWorking(this)) {
                 return false;
             }
         }
-        return IWorkableMultiController.super.beforeWorking();
+        return IWorkableMultiController.super.beforeWorking(recipe);
     }
 
     @Override
