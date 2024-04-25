0ffa1f6d63dcf700b227415da1b3793d464437e5
ctrlaltmilk <iris@ctrlaltmilk.net>
2024-04-24 20:24:59 -0700

    Add special lang-generation case for "Iron III Chloride" (#1059)

--- src/main/java/com/gregtechceu/gtceu/data/lang/MaterialLangGenerator.java
+++ src/main/java/com/gregtechceu/gtceu/data/lang/MaterialLangGenerator.java
@@ -2,6 +2,7 @@ package com.gregtechceu.gtceu.data.lang;
 
 import com.gregtechceu.gtceu.api.data.chemical.material.Material;
 import com.gregtechceu.gtceu.api.data.chemical.material.registry.MaterialRegistry;
+import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.tterrag.registrate.providers.RegistrateLangProvider;
 
 import static com.gregtechceu.gtceu.utils.FormattingUtil.toEnglishName;
@@ -9,7 +10,12 @@ import static com.gregtechceu.gtceu.utils.FormattingUtil.toEnglishName;
 public class MaterialLangGenerator {
 
     public static void generate(RegistrateLangProvider provider, MaterialRegistry registry) {
-        for (Material material : registry.getAllMaterials())
-            provider.add(material.getUnlocalizedName(), toEnglishName(material.getName()));
+        for (Material material : registry.getAllMaterials()) {
+            if (material == GTMaterials.Iron3Chloride) {
+                provider.add(material.getUnlocalizedName(), "Iron III Chloride");
+            } else {
+                provider.add(material.getUnlocalizedName(), toEnglishName(material.getName()));
+            }
+        }
     }
 }
