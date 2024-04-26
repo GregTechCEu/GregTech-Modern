f7aa48848c33b6e07b72412b9fd7450b14f1bc3d
ctrlaltmilk <iris@ctrlaltmilk.net>
2024-04-26 16:34:17 -0700

    Relocate fix to its proper place

--- src/main/java/com/gregtechceu/gtceu/data/lang/MaterialLangGenerator.java
+++ src/main/java/com/gregtechceu/gtceu/data/lang/MaterialLangGenerator.java
@@ -2,7 +2,6 @@ package com.gregtechceu.gtceu.data.lang;
 
 import com.gregtechceu.gtceu.api.data.chemical.material.Material;
 import com.gregtechceu.gtceu.api.data.chemical.material.registry.MaterialRegistry;
-import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.tterrag.registrate.providers.RegistrateLangProvider;
 
 import static com.gregtechceu.gtceu.utils.FormattingUtil.toEnglishName;
@@ -10,12 +9,7 @@ import static com.gregtechceu.gtceu.utils.FormattingUtil.toEnglishName;
 public class MaterialLangGenerator {
 
     public static void generate(RegistrateLangProvider provider, MaterialRegistry registry) {
-        for (Material material : registry.getAllMaterials()) {
-            if (material == GTMaterials.Iron3Chloride) {
-                provider.add(material.getUnlocalizedName(), "Iron III Chloride");
-            } else {
-                provider.add(material.getUnlocalizedName(), toEnglishName(material.getName()));
-            }
-        }
+        for (Material material : registry.getAllMaterials())
+            provider.add(material.getUnlocalizedName(), toEnglishName(material.getName()));
     }
 }
