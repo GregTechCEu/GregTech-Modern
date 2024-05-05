67349f10b97f58ce906e0d83af263f4b28f8c938
screret <68943070+screret@users.noreply.github.com>
2024-05-05 19:30:29 +0300

    fix laser & optical pipes not dropping anything (#1197)

--- src/main/java/com/gregtechceu/gtceu/data/block/GTBlocks.java
+++ src/main/java/com/gregtechceu/gtceu/data/block/GTBlocks.java
@@ -397,7 +397,7 @@ public class GTBlocks {
         var type = LaserPipeType.values()[slot];
         var entry = REGISTRATE.block("%s_laser_pipe".formatted(type.getSerializedName()), (p) -> new LaserPipeBlock(p, type))
             .initialProperties(() -> Blocks.IRON_BLOCK)
-            .properties(p -> p.dynamicShape().noOcclusion().noLootTable())
+            .properties(p -> p.dynamicShape().noOcclusion())
             .blockstate(NonNullBiConsumer.noop())
             .defaultLoot()
             .tag(CustomTags.MINEABLE_WITH_WIRE_CUTTER)
@@ -424,7 +424,7 @@ public class GTBlocks {
         var type = OpticalPipeType.values()[index];
         var entry = REGISTRATE.block("%s_optical_pipe".formatted(type.getSerializedName()), (p) -> new OpticalPipeBlock(p, type))
             .initialProperties(() -> Blocks.IRON_BLOCK)
-            .properties(p -> p.dynamicShape().noOcclusion().noLootTable())
+            .properties(p -> p.dynamicShape().noOcclusion())
             .blockstate(NonNullBiConsumer.noop())
             .defaultLoot()
             .tag(CustomTags.MINEABLE_WITH_WIRE_CUTTER)
