428401348f9be7b4691732fba02f5c328da28451
screret <68943070+screret@users.noreply.github.com>
2024-05-09 20:18:24 +0300

    fix sign rendering (#1217)

---
+++ src/main/java/com/gregtechceu/gtceu/common/block/GTCeilingHangingSignBlock.java
@@ -0,0 +1,32 @@
+package com.gregtechceu.gtceu.common.block;
+
+import com.gregtechceu.gtceu.common.blockentity.GTHangingSignBlockEntity;
+import com.gregtechceu.gtceu.common.data.GTBlockEntities;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.CeilingHangingSignBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.SignBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.WoodType;
+
+import javax.annotation.Nullable;
+
+public class GTCeilingHangingSignBlock extends CeilingHangingSignBlock {
+    public GTCeilingHangingSignBlock(Properties properties, WoodType type) {
+        super(properties, type);
+    }
+
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new GTHangingSignBlockEntity(GTBlockEntities.GT_HANGING_SIGN.get(), pos, state);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> blockEntityType) {
+        return createTickerHelper(blockEntityType, GTBlockEntities.GT_HANGING_SIGN.get(), SignBlockEntity::tick);
+    }
+}
