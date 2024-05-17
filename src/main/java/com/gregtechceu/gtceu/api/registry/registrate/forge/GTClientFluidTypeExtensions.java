7bbeb54712753b01cfd792877dd7ced6614686c2
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2024-05-17 05:13:45 -0600

    cleanup the kube recipe schema (#1268)
    * cleanup the kube recipe schema
    
    * spotles

--- src/main/java/com/gregtechceu/gtceu/api/registry/registrate/forge/GTClientFluidTypeExtensions.java
+++ src/main/java/com/gregtechceu/gtceu/api/registry/registrate/forge/GTClientFluidTypeExtensions.java
@@ -4,10 +4,10 @@ import com.gregtechceu.gtceu.GTCEu;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.client.extensions.common.IClientFluidTypeExtensions;
 
 import lombok.Getter;
 import lombok.Setter;
-import net.neoforged.neoforge.client.extensions.common.IClientFluidTypeExtensions;
 
 public class GTClientFluidTypeExtensions implements IClientFluidTypeExtensions {
 
