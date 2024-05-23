3d500a9242fb14823c335bb96722759465d4c72d
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2024-05-22 23:21:32 -0600

    Fluid Pipe Jade compat (#1288)
    * Fluid Pipe Jade compat
    
    * fix the fluid id

--- src/main/java/com/gregtechceu/gtceu/integration/jade/GTJadePlugin.java
+++ src/main/java/com/gregtechceu/gtceu/integration/jade/GTJadePlugin.java
@@ -2,11 +2,10 @@ package com.gregtechceu.gtceu.integration.jade;
 
 import com.gregtechceu.gtceu.api.item.IGTTool;
 import com.gregtechceu.gtceu.api.item.tool.GTToolItem;
+import com.gregtechceu.gtceu.common.block.FluidPipeBlock;
+import com.gregtechceu.gtceu.common.blockentity.FluidPipeBlockEntity;
 import com.gregtechceu.gtceu.common.data.GTItems;
-import com.gregtechceu.gtceu.integration.jade.provider.ControllableBlockProvider;
-import com.gregtechceu.gtceu.integration.jade.provider.ElectricContainerBlockProvider;
-import com.gregtechceu.gtceu.integration.jade.provider.RecipeLogicProvider;
-import com.gregtechceu.gtceu.integration.jade.provider.WorkableBlockProvider;
+import com.gregtechceu.gtceu.integration.jade.provider.*;
 import com.tterrag.registrate.util.entry.ItemEntry;
 import com.tterrag.registrate.util.entry.ItemProviderEntry;
 import net.minecraft.world.item.Item;
@@ -15,6 +14,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import snownee.jade.addon.harvest.HarvestToolProvider;
 import snownee.jade.addon.harvest.SimpleToolHandler;
+import snownee.jade.addon.universal.FluidStorageProvider;
 import snownee.jade.api.IWailaClientRegistration;
 import snownee.jade.api.IWailaCommonRegistration;
 import snownee.jade.api.IWailaPlugin;
@@ -30,6 +30,8 @@ public class GTJadePlugin implements IWailaPlugin {
         registration.registerBlockDataProvider(new WorkableBlockProvider(), BlockEntity.class);
         registration.registerBlockDataProvider(new ControllableBlockProvider(), BlockEntity.class);
         registration.registerBlockDataProvider(new RecipeLogicProvider(), BlockEntity.class);
+
+        registration.registerFluidStorage(FluidPipeStorageProvider.INSTANCE, FluidPipeBlockEntity.class);
     }
 
     @Override
@@ -38,6 +40,8 @@ public class GTJadePlugin implements IWailaPlugin {
         registration.registerBlockComponent(new WorkableBlockProvider(), Block.class);
         registration.registerBlockComponent(new ControllableBlockProvider(), Block.class);
         registration.registerBlockComponent(new RecipeLogicProvider(), Block.class);
+
+        registration.registerFluidStorageClient(FluidPipeStorageProvider.INSTANCE);
     }
 
     static {
