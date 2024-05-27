62f7efc4e9c79bd8f19e58285a4069c1511ebed9
screret <68943070+screret@users.noreply.github.com>
2024-05-27 19:08:19 +0300

    add spotless formatting to 1.20.1 (#1334)
    * add spotless to 1.20.1
    
    * clean up the GH actions
    
    * run spotless 🥰
    
    * remove some unused classes
    
    * add spotless check action
    
    * reviews

--- src/main/java/com/gregtechceu/gtceu/api/advancement/IAdvancementTrigger.java
+++
@@ -1,9 +0,0 @@
-package com.gregtechceu.gtceu.api.advancement;
-
-import net.minecraft.advancements.CriterionTrigger;
-import net.minecraft.server.level.ServerPlayer;
-
-public interface IAdvancementTrigger<T extends IAdvancementCriterion> extends CriterionTrigger<T> {
-
-    void trigger(ServerPlayer player);
-}
