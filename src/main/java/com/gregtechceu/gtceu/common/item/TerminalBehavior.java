ccd71ef5ffce89a58f9f1b737b7276f71e0c0fa6
screret <68943070+screret@users.noreply.github.com>
2024-06-03 13:31:53 +0300

    add config toggle for compass (#1362)
    * add config toggle for compass
    
    * filter to only GT pages

--- src/main/java/com/gregtechceu/gtceu/common/item/TerminalBehavior.java
+++ src/main/java/com/gregtechceu/gtceu/common/item/TerminalBehavior.java
@@ -4,14 +4,19 @@ import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.item.component.IItemUIFactory;
 import com.gregtechceu.gtceu.api.machine.MetaMachine;
 import com.gregtechceu.gtceu.api.machine.feature.multiblock.IMultiController;
+import com.gregtechceu.gtceu.config.ConfigHolder;
 
 import com.lowdragmc.lowdraglib.gui.compass.CompassView;
 import com.lowdragmc.lowdraglib.gui.factory.HeldItemUIFactory;
 import com.lowdragmc.lowdraglib.gui.modular.ModularUI;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 
@@ -38,5 +43,14 @@ public class TerminalBehavior implements IItemUIFactory {
     @Override
     public ModularUI createUI(HeldItemUIFactory.HeldItemHolder holder, Player entityPlayer) {
         return new ModularUI(holder, entityPlayer).widget(new CompassView(GTCEu.MOD_ID));
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Item item, Level level, Player player, InteractionHand usedHand) {
+        if (!ConfigHolder.INSTANCE.gameplay.enableCompass) {
+            ItemStack heldItem = player.getItemInHand(usedHand);
+            return InteractionResultHolder.pass(heldItem);
+        }
+        return IItemUIFactory.super.use(item, level, player, usedHand);
     }
 }
