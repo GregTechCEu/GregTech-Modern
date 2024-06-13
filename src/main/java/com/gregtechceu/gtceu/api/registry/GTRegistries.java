3123be312668e24ffc9b233627b00b40efbf8201
screret <68943070+screret@users.noreply.github.com>
2024-06-13 15:05:16 +0300

    remove wrong nullability annotation

--- src/main/java/com/gregtechceu/gtceu/api/registry/GTRegistries.java
+++ src/main/java/com/gregtechceu/gtceu/api/registry/GTRegistries.java
@@ -113,7 +113,6 @@ public final class GTRegistries {
         GLOBAL_LOOT_MODIFIES.register(eventBus);
     }
 
-    @Nullable
     public static RegistryAccess builtinRegistry() {
         return Platform.getFrozenRegistry();
     }
