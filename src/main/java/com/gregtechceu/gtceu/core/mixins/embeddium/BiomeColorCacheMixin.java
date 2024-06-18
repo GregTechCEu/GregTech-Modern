de528a572be85426855200ac4839c897908fb101
screret <68943070+screret@users.noreply.github.com>
2024-06-18 21:30:06 +0300

    fix embeddium crash

--- src/main/java/com/gregtechceu/gtceu/core/mixins/embeddium/BiomeColorCacheMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/embeddium/BiomeColorCacheMixin.java
@@ -3,23 +3,25 @@ package com.gregtechceu.gtceu.core.mixins.embeddium;
 import com.gregtechceu.gtceu.client.EnvironmentalHazardClientHandler;
 import com.gregtechceu.gtceu.config.ConfigHolder;
 
+import net.minecraft.client.renderer.BiomeColors;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.ChunkPos;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import me.jellysquid.mods.sodium.client.world.biome.BiomeColorCache;
 import me.jellysquid.mods.sodium.client.world.biome.BiomeColorSource;
+import net.minecraft.world.level.ColorResolver;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(value = BiomeColorCache.class, remap = false)
 public class BiomeColorCacheMixin {
 
-    @ModifyExpressionValue(method = "getColor",
+    @ModifyExpressionValue(method = "getColor(Lnet/minecraft/world/level/ColorResolver;III)I",
                            at = @At(value = "INVOKE",
                                     target = "Lme/jellysquid/mods/sodium/client/util/color/BoxBlur$ColorBuffer;get(II)I"))
     private int gtceu$modifyBiomeAverageColorByHazard(int color,
-                                                      BiomeColorSource source, int blockX, int blockY, int blockZ) {
+                                                      ColorResolver source, int blockX, int blockY, int blockZ) {
         if (!ConfigHolder.INSTANCE.gameplay.environmentalHazards) {
             return color;
         }
@@ -29,11 +31,11 @@ public class BiomeColorCacheMixin {
         ChunkPos chunkPos = new ChunkPos(pos);
 
         // because spotless decided to be dumb about if statement indentation
-        if (source == BiomeColorSource.GRASS) {
+        if (source == BiomeColors.GRASS_COLOR_RESOLVER) {
             return clientHandler.colorZone(color, chunkPos);
-        } else if (source == BiomeColorSource.FOLIAGE) {
+        } else if (source == BiomeColors.FOLIAGE_COLOR_RESOLVER) {
             return clientHandler.colorZone(color, chunkPos);
-        } else if (source == BiomeColorSource.WATER) {
+        } else if (source == BiomeColors.WATER_COLOR_RESOLVER) {
             return clientHandler.colorZone(color, chunkPos);
         }
 
