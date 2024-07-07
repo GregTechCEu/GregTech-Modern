c438c9d09916fcb91e61d48102a24b82c3e16425
Gate Guardian <xtzcy76040108@163.com>
2024-07-07 15:44:55 +0800

    change mixin location

--- src/main/java/com/gregtechceu/gtceu/core/mixins/BlockMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/BlockMixin.java
@@ -9,17 +9,17 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import org.jetbrains.annotations.Nullable;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.List;
+
+import javax.annotation.Nullable;
 
 @Mixin(Block.class)
 public abstract class BlockMixin {
@@ -28,16 +28,15 @@ public abstract class BlockMixin {
      * Because most mods override Block#getDrops instead of using LootItemFunction to save custom data,
      * we use Mixin instead of GlobalLootModifier for compatibility.
      */
-    @ModifyExpressionValue(method = "dropResources(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;Z)V",
-                           at = @At(value = "INVOKE",
-                                    target = "Lnet/minecraft/world/level/block/Block;getDrops(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)Ljava/util/List;"))
-    private static List<ItemStack> gtceu$onDropResources(List<ItemStack> original, BlockState state, Level level,
-                                                         BlockPos pos, @Nullable BlockEntity blockEntity,
-                                                         @Nullable Entity entity, ItemStack tool) {
+    @ModifyReturnValue(method = "getDrops(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)Ljava/util/List;",
+                       at = @At(value = "RETURN"))
+    private static List<ItemStack> gtceu$modifyDrops(List<ItemStack> original, BlockState state, ServerLevel level,
+                                                     BlockPos pos, @Nullable BlockEntity blockEntity,
+                                                     @Nullable Entity entity, ItemStack tool) {
         if (!tool.isEmpty() && entity instanceof Player player) {
             boolean isSilktouch = EnchantmentHelper.hasSilkTouch(tool);
             int fortuneLevel = tool.getEnchantmentLevel(Enchantments.BLOCK_FORTUNE);
-            return ToolEventHandlers.onHarvestDrops(player, tool, (ServerLevel) level, pos, state, isSilktouch,
+            return ToolEventHandlers.onHarvestDrops(player, tool, level, pos, state, isSilktouch,
                     fortuneLevel,
                     original, 1);
         }
