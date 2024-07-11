f1932f38a75b88335cc8d3a219a0fb1eb517a88c
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2024-07-10 22:03:09 -0600

    more fixes

--- src/main/java/com/gregtechceu/gtceu/common/item/tool/behavior/LighterBehavior.java
+++ src/main/java/com/gregtechceu/gtceu/common/item/tool/behavior/LighterBehavior.java
@@ -18,8 +18,6 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
