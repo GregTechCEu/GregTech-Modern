e39480bcbe3c2fcca1915c4e97d33183f1d8b6e0
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2024-07-28 17:47:10 -0600

    screret's changes

--- src/main/java/com/gregtechceu/gtceu/api/fluids/store/FluidStorageKeys.java
+++ src/main/java/com/gregtechceu/gtceu/api/fluids/store/FluidStorageKeys.java
@@ -13,13 +13,13 @@ public final class FluidStorageKeys {
 
     public static final FluidStorageKey LIQUID = new FluidStorageKey(GTCEu.id("liquid"),
             MaterialIconType.liquid,
-            m -> prefixedRegistredName("liquid.", FluidStorageKeys.LIQUID, m),
+            m -> prefixedRegistredName("liquid_", FluidStorageKeys.LIQUID, m),
             m -> m.hasProperty(PropertyKey.DUST) ? "gtceu.fluid.liquid_generic" : "gtceu.fluid.generic",
             FluidState.LIQUID, 0);
 
     public static final FluidStorageKey GAS = new FluidStorageKey(GTCEu.id("gas"),
             MaterialIconType.gas,
-            m -> prefixedRegistredName("gas.", FluidStorageKeys.GAS, m),
+            m -> prefixedRegistredName("gaseous_", FluidStorageKeys.GAS, m),
             m -> {
                 if (m.hasProperty(PropertyKey.DUST)) {
                     return "gtceu.fluid.gas_vapor";
@@ -36,13 +36,13 @@ public final class FluidStorageKeys {
 
     public static final FluidStorageKey PLASMA = new FluidStorageKey(GTCEu.id("plasma"),
             MaterialIconType.plasma,
-            m -> "plasma." + m.getName(),
+            m -> m.getName() + "_plasma" ,
             m -> "gtceu.fluid.plasma",
             FluidState.PLASMA, -1);
 
     public static final FluidStorageKey MOLTEN = new FluidStorageKey(GTCEu.id("molten"),
             MaterialIconType.molten,
-            m -> "molten." + m.getName(),
+            m -> "molten_" + m.getName(),
             m -> "gtceu.fluid.molten",
             FluidState.LIQUID, -1);
 
