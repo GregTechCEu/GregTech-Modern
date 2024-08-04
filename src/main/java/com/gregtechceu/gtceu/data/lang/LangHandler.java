1a59c44b291b16d9887a6be124958f8ff6b0077f
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2024-08-04 04:03:07 -0600

    use the right methods this time

--- src/main/java/com/gregtechceu/gtceu/data/lang/LangHandler.java
+++ src/main/java/com/gregtechceu/gtceu/data/lang/LangHandler.java
@@ -1206,7 +1206,7 @@ public class LangHandler {
         provider.add("config.jade.plugin_gtceu.maintenance_info", "[GTCEu] Maintenance Info");
         provider.add("config.jade.plugin_gtceu.multiblock_structure", "[GTCEu] MultiBlock Structure");
         provider.add("config.jade.plugin_gtceu.parallel_info", "[GTCEu] Parallel Info");
-        provider.add("config.jade.plugin_gtceu.transformer_provider", "[GTCEu] Transformer Info");
+        provider.add("config.jade.plugin_gtceu.transformer", "[GTCEu] Transformer Info");
         provider.add("config.jade.plugin_gtceu.stained_color", "[GTCEu] Stained Block Info");
         // gui
         provider.add("gtceu.recipe_type.show_recipes", "Show Recipes");
