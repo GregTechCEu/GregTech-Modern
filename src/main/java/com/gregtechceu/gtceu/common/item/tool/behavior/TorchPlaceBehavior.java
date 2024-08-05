dae80587edc16ad147d3ff3fec2e74a1f363eab0
Rundas <38040866+Rundas01@users.noreply.github.com>
2024-08-05 22:32:43 +0200

    Fixed Torch Placement Behavior
    Beware of cursed code, uncursing ideas are welcome xD

--- src/main/java/com/gregtechceu/gtceu/common/item/tool/behavior/TorchPlaceBehavior.java
+++ src/main/java/com/gregtechceu/gtceu/common/item/tool/behavior/TorchPlaceBehavior.java
@@ -30,7 +30,6 @@ import java.util.List;
 
 import static com.gregtechceu.gtceu.api.item.tool.ToolHelper.TORCH_PLACING_KEY;
 
-// TODO this currently voids the used tool as well as a torch. how fix?
 public class TorchPlaceBehavior implements IToolBehavior {
 
     public static final TorchPlaceBehavior INSTANCE = new TorchPlaceBehavior();
@@ -42,64 +41,70 @@ public class TorchPlaceBehavior implements IToolBehavior {
         Player player = context.getPlayer();
         InteractionHand hand = context.getHand();
 
-        ItemStack stack = player.getItemInHand(hand);
-        CompoundTag behaviourTag = ToolHelper.getBehaviorsTag(stack);
-        if (behaviourTag.getBoolean(TORCH_PLACING_KEY)) {
-            int cachedTorchSlot;
-            ItemStack slotStack;
-            if (behaviourTag.getBoolean(ToolHelper.TORCH_PLACING_CACHE_SLOT_KEY)) {
-                cachedTorchSlot = behaviourTag.getInt(ToolHelper.TORCH_PLACING_CACHE_SLOT_KEY);
-                if (cachedTorchSlot < 0) {
-                    slotStack = player.getInventory().offhand.get(0);
-                } else {
-                    slotStack = player.getInventory().items.get(cachedTorchSlot);
-                }
-                if (checkAndPlaceTorch(context, slotStack)) {
-                    return InteractionResult.SUCCESS;
-                }
+        ItemStack pickaxeStack = player.getItemInHand(hand);  // Preserve the pickaxe stack
+        CompoundTag behaviourTag = ToolHelper.getBehaviorsTag(pickaxeStack);
+        if (!behaviourTag.getBoolean(TORCH_PLACING_KEY)) {
+            return InteractionResult.PASS;
+        }
+        int cachedTorchSlot;
+        ItemStack slotStack;
+        if (behaviourTag.getBoolean(ToolHelper.TORCH_PLACING_CACHE_SLOT_KEY)) {
+            cachedTorchSlot = behaviourTag.getInt(ToolHelper.TORCH_PLACING_CACHE_SLOT_KEY);
+            if (cachedTorchSlot < 0) {
+                slotStack = player.getInventory().offhand.get(0);
+            } else {
+                slotStack = player.getInventory().items.get(cachedTorchSlot);
             }
-            for (int i = 0; i < player.getInventory().offhand.size(); i++) {
-                slotStack = player.getInventory().offhand.get(i);
-                if (checkAndPlaceTorch(context, slotStack)) {
-                    behaviourTag.putInt(ToolHelper.TORCH_PLACING_CACHE_SLOT_KEY, -(i + 1));
-                    return InteractionResult.SUCCESS;
-                }
+            if (checkAndPlaceTorch(context, slotStack)) {
+                return InteractionResult.SUCCESS;
             }
-            for (int i = 0; i < player.getInventory().items.size(); i++) {
-                slotStack = player.getInventory().items.get(i);
-                if (checkAndPlaceTorch(context, slotStack)) {
-                    behaviourTag.putInt(ToolHelper.TORCH_PLACING_CACHE_SLOT_KEY, i);
-                    return InteractionResult.SUCCESS;
-                }
+        }
+        for (int i = 0; i < player.getInventory().offhand.size(); i++) {
+            slotStack = player.getInventory().offhand.get(i);
+            if (checkAndPlaceTorch(context, slotStack)) {
+                behaviourTag.putInt(ToolHelper.TORCH_PLACING_CACHE_SLOT_KEY, -(i + 1));
+                return InteractionResult.SUCCESS;
+            }
+        }
+        for (int i = 0; i < player.getInventory().items.size(); i++) {
+            slotStack = player.getInventory().items.get(i);
+            if (checkAndPlaceTorch(context, slotStack)) {
+                behaviourTag.putInt(ToolHelper.TORCH_PLACING_CACHE_SLOT_KEY, i);
+                return InteractionResult.SUCCESS;
             }
         }
         return InteractionResult.PASS;
     }
 
     private static boolean checkAndPlaceTorch(UseOnContext context, ItemStack slotStack) {
-        if (!slotStack.isEmpty()) {
-            Item slotItem = slotStack.getItem();
-            if (slotItem instanceof BlockItem slotItemBlock) {
-                Block slotBlock = slotItemBlock.getBlock();
-                if (slotBlock == Blocks.TORCH ||
-                        slotStack.is(TagUtil.createItemTag("torches"))) {
-                    BlockPos pos = context.getClickedPos();
-                    BlockState state = context.getLevel().getBlockState(pos);
-                    if (!state.canBeReplaced()) {
-                        pos = pos.relative(context.getClickedFace());
-                    }
-                    if (context.getPlayer().mayUseItemAt(pos, context.getClickedFace(), slotStack)) {
-                        var blockPlaceContext = new BlockPlaceContext(context);
-                        BlockState slotState = slotBlock.getStateForPlacement(blockPlaceContext);
-                        if (slotItemBlock.place(blockPlaceContext).consumesAction()) {
-                            slotState = context.getLevel().getBlockState(pos);
-                            SoundType soundtype = slotState.getBlock().getSoundType(slotState);
-                            context.getLevel().playSound(context.getPlayer(), pos, soundtype.getPlaceSound(),
-                                    SoundSource.BLOCKS,
-                                    (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
-                            if (!context.getPlayer().isCreative()) slotStack.shrink(1);
-                            return true;
+        if (slotStack.isEmpty()) {
+            return false;
+        }
+        Item slotItem = slotStack.getItem();
+        if (slotItem instanceof BlockItem slotItemBlock) {
+            Block slotBlock = slotItemBlock.getBlock();
+            if (slotBlock == Blocks.TORCH || slotStack.is(TagUtil.createItemTag("torches"))) {
+                BlockPos pos = context.getClickedPos();
+                BlockState state = context.getLevel().getBlockState(pos);
+                if (!state.canBeReplaced()) {
+                    pos = pos.relative(context.getClickedFace());
+                }
+                if (context.getPlayer().mayUseItemAt(pos, context.getClickedFace(), slotStack)) {
+                    var blockPlaceContext = new BlockPlaceContext(context) {
+                        @Override
+                        public ItemStack getItemInHand() {
+                            return slotStack;
                         }
+                    };
+                    BlockState slotState;
+                    if (slotItemBlock.place(blockPlaceContext).consumesAction()) {
+                        slotState = context.getLevel().getBlockState(pos);
+                        SoundType soundtype = slotState.getBlock().getSoundType(slotState);
+                        context.getLevel().playSound(context.getPlayer(), pos, soundtype.getPlaceSound(),
+                                SoundSource.BLOCKS,
+                                (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
+                        //Shrinking the stack resulted in 2 torches being used. Getting rid of the original line magically fixes it, in both creative and survival. Don't ask why, I don't have a single clue.
+                        return true;
                     }
                 }
             }
