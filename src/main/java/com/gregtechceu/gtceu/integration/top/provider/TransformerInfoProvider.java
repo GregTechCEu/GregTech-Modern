6365d0682d5a351e2fb019dfcb27769d3ae1c714
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2024-08-04 18:56:04 -0600

    use the right classes this time for transformer information (#1641)

--- src/main/java/com/gregtechceu/gtceu/integration/top/provider/TransformerInfoProvider.java
+++ src/main/java/com/gregtechceu/gtceu/integration/top/provider/TransformerInfoProvider.java
@@ -5,55 +5,54 @@ import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.api.machine.MetaMachine;
 import com.gregtechceu.gtceu.common.machine.electric.TransformerMachine;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
 
 import mcjty.theoneprobe.api.IProbeHitData;
 import mcjty.theoneprobe.api.IProbeInfo;
-import org.jetbrains.annotations.Nullable;
+import mcjty.theoneprobe.api.IProbeInfoProvider;
+import mcjty.theoneprobe.api.ProbeMode;
 
-public class TransformerInfoProvider extends CapabilityInfoProvider<TransformerMachine> {
-
-    @Override
-    protected @Nullable TransformerMachine getCapability(Level level, BlockPos pos, @Nullable Direction side) {
-        if (MetaMachine.getMachine(level, pos) instanceof TransformerMachine transformer) {
-            return transformer;
-        }
-        return null;
-    }
+public class TransformerInfoProvider implements IProbeInfoProvider {
 
     @Override
-    protected void addProbeInfo(TransformerMachine capability, IProbeInfo probeInfo, Player player,
-                                BlockEntity blockEntity, IProbeHitData data) {
-        boolean transformUp = capability.isTransformUp();
-        int voltage = capability.getTier();
-        int amp = capability.getBaseAmp();
-        int side = capability.getFrontFacing().get3DDataValue();
+    public void addProbeInfo(ProbeMode probeMode, IProbeInfo iProbeInfo, Player player, Level level,
+                             BlockState blockState, IProbeHitData iProbeHitData) {
+        if (MetaMachine.getMachine(level, iProbeHitData.getPos()) instanceof TransformerMachine transformer) {
 
-        IProbeInfo verticalPane = probeInfo.vertical(probeInfo.defaultLayoutStyle().spacing(0));
-        if (transformUp) {
-            verticalPane.text(Component.translatable("gtceu.top.transform_up",
-                    (GTValues.VNF[voltage] + " §r(" + amp * 4 + "A) -> " + GTValues.VNF[voltage + 1] + " §r(" + amp +
-                            "A)")));
-        } else {
-            verticalPane.text(Component.translatable("gtceu.top.transform_down",
-                    (GTValues.VNF[voltage + 1] + " §r(" + amp + "A) -> " + GTValues.VNF[voltage] + " §r(" + amp * 4 +
-                            "A)")));
-        }
+            boolean transformUp = transformer.isTransformUp();
+            int voltage = transformer.getTier();
+            int amp = transformer.getBaseAmp();
+            int side = transformer.getFrontFacing().get3DDataValue();
 
-        if (data.getSideHit() == Direction.from3DDataValue(side)) {
-            verticalPane.text(
-                    Component.translatable((transformUp ? "gtceu.top.transform_output" : "gtceu.top.transform_input"),
-                            (GTValues.VNF[voltage + 1] + " §r(" + amp + "A)")));
-        } else {
-            verticalPane.text(
-                    Component.translatable((transformUp ? "gtceu.top.transform_input" : "gtceu.top.transform_output"),
-                            (GTValues.VNF[voltage] + " §r(" + amp * 4 + "A)")));
+            IProbeInfo verticalPane = iProbeInfo.vertical(iProbeInfo.defaultLayoutStyle().spacing(0));
+            if (transformUp) {
+                verticalPane.text(Component.translatable("gtceu.top.transform_up",
+                        (GTValues.VNF[voltage] + " §r(" + amp * 4 + "A) -> " + GTValues.VNF[voltage + 1] + " §r(" +
+                                amp +
+                                "A)")));
+            } else {
+                verticalPane.text(Component.translatable("gtceu.top.transform_down",
+                        (GTValues.VNF[voltage + 1] + " §r(" + amp + "A) -> " + GTValues.VNF[voltage] + " §r(" +
+                                amp * 4 +
+                                "A)")));
+            }
+
+            if (iProbeHitData.getSideHit() == Direction.from3DDataValue(side)) {
+                verticalPane.text(
+                        Component.translatable(
+                                (transformUp ? "gtceu.top.transform_output" : "gtceu.top.transform_input"),
+                                (GTValues.VNF[voltage + 1] + " §r(" + amp + "A)")));
+            } else {
+                verticalPane.text(
+                        Component.translatable(
+                                (transformUp ? "gtceu.top.transform_input" : "gtceu.top.transform_output"),
+                                (GTValues.VNF[voltage] + " §r(" + amp * 4 + "A)")));
+            }
         }
     }
 
