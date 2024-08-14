8b8f5728cf0e9136daf61443911bd33a72da873f
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2024-08-14 15:10:26 +0400

    Add axe mineable tags to rubber wood blocks (#1716)

--- src/main/java/com/gregtechceu/gtceu/common/data/GTBlocks.java
+++ src/main/java/com/gregtechceu/gtceu/common/data/GTBlocks.java
@@ -1163,6 +1163,7 @@ public class GTBlocks {
             .initialProperties(() -> Blocks.STRIPPED_SPRUCE_LOG)
             .lang("Stripped Rubber Log")
             .blockstate((ctx, provider) -> provider.logBlock(ctx.get()))
+            .tag(BlockTags.MINEABLE_WITH_AXE)
             .simpleItem()
             .register();
     public static final BlockEntry<RotatedPillarBlock> RUBBER_WOOD = REGISTRATE
@@ -1171,6 +1172,7 @@ public class GTBlocks {
             .lang("Rubber Wood")
             .blockstate((ctx, provider) -> provider.axisBlock(ctx.get(),
                     provider.blockTexture(GTBlocks.RUBBER_LOG.get()), provider.blockTexture(GTBlocks.RUBBER_LOG.get())))
+            .tag(BlockTags.MINEABLE_WITH_AXE)
             .simpleItem()
             .register();
     public static final BlockEntry<RotatedPillarBlock> STRIPPED_RUBBER_WOOD = REGISTRATE
@@ -1179,6 +1181,7 @@ public class GTBlocks {
             .lang("Stripped Rubber Wood")
             .blockstate((ctx, provider) -> provider.axisBlock(ctx.get(), provider.blockTexture(ctx.get()),
                     provider.blockTexture(ctx.get())))
+            .tag(BlockTags.MINEABLE_WITH_AXE)
             .simpleItem()
             .register();
 
@@ -1298,7 +1301,7 @@ public class GTBlocks {
             .block("rubber_stairs", (p) -> new StairBlock(RUBBER_PLANK::getDefaultState, p))
             .initialProperties(() -> Blocks.SPRUCE_STAIRS)
             .lang("Rubber Stairs")
-            .tag(BlockTags.STAIRS)
+            .tag(BlockTags.STAIRS, BlockTags.MINEABLE_WITH_AXE)
             .blockstate((ctx, prov) -> prov.stairsBlock(ctx.getEntry(), prov.blockTexture(GTBlocks.RUBBER_PLANK.get())))
             .item()
             .tag(ItemTags.STAIRS)
@@ -1462,7 +1465,7 @@ public class GTBlocks {
             .block("treated_wood_stairs", (p) -> new StairBlock(TREATED_WOOD_PLANK::getDefaultState, p))
             .initialProperties(() -> Blocks.SPRUCE_STAIRS)
             .lang("Treated Wood Stairs")
-            .tag(BlockTags.STAIRS)
+            .tag(BlockTags.STAIRS, BlockTags.MINEABLE_WITH_AXE)
             .blockstate((ctx, prov) -> prov.stairsBlock(ctx.getEntry(),
                     prov.blockTexture(GTBlocks.TREATED_WOOD_PLANK.get())))
             .item()
