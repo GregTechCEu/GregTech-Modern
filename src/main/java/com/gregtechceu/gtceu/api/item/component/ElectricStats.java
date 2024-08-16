dc8ff01091d91067f3ce1bebdedc7c591a180176
RainPumpkin <rainpumpkin7266@gmail.com>
2024-08-16 22:37:41 +0100

    spotless

--- src/main/java/com/gregtechceu/gtceu/api/item/component/ElectricStats.java
+++ src/main/java/com/gregtechceu/gtceu/api/item/component/ElectricStats.java
@@ -1,10 +1,10 @@
 package com.gregtechceu.gtceu.api.item.component;
 
 import com.gregtechceu.gtceu.api.GTValues;
-import com.gregtechceu.gtceu.api.capability.forge.GTCapability;
 import com.gregtechceu.gtceu.api.capability.GTCapabilityHelper;
 import com.gregtechceu.gtceu.api.capability.IElectricItem;
 import com.gregtechceu.gtceu.api.capability.compat.FeCompat;
+import com.gregtechceu.gtceu.api.capability.forge.GTCapability;
 import com.gregtechceu.gtceu.api.item.capability.ElectricItem;
 import com.gregtechceu.gtceu.api.item.component.forge.IComponentCapability;
 import com.gregtechceu.gtceu.config.ConfigHolder;
