f542c62c18b1bfd53262b0855dfc8dbba5a1484d
screret <68943070+screret@users.noreply.github.com>
2024-08-18 07:30:16 +0300

    fix crates voiding contents on world load (#1753)
    Co-authored-by: Ghostipedia <Ghostipedia@gmail.com>
    Co-authored-by: YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>

--- src/main/java/com/gregtechceu/gtceu/common/machine/storage/CrateMachine.java
+++ src/main/java/com/gregtechceu/gtceu/common/machine/storage/CrateMachine.java
@@ -32,7 +32,6 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 
 import lombok.Getter;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -63,6 +62,7 @@ public class CrateMachine extends MetaMachine implements IUIMachine, IMachineMod
     private final int inventorySize;
     @Getter
     @RequireRerender
+    @Persisted
     @DescSynced
     private boolean isTaped;
 
@@ -115,22 +115,6 @@ public class CrateMachine extends MetaMachine implements IUIMachine, IMachineMod
             }
         }
         return IInteractedMachine.super.onUse(state, world, pos, player, hand, hit);
-    }
-
-    @Override
-    public void loadCustomPersistedData(@NotNull CompoundTag tag) {
-        super.loadCustomPersistedData(tag);
-        inventory.loadCustomPersistedData(tag.getCompound("inventory"));
-        if (tag.contains("taped")) {
-            this.isTaped = tag.getBoolean("taped");
-        }
-    }
-
-    @Override
-    public void saveCustomPersistedData(@NotNull CompoundTag tag, boolean forDrop) {
-        super.saveCustomPersistedData(tag, forDrop);
-        tag.put("inventory", inventory.storage.serializeNBT());
-        tag.putBoolean("taped", isTaped);
     }
 
     @Override
