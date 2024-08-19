884ed612f90edc28d4c1c1d2ee04c6ba9db39d61
liansishen <liansishen1992@gmail.com>
2024-08-19 15:01:56 +0800

    fix when remove me pattern buffer, items in Shared Item Inventory not… (#1778)

--- src/main/java/com/gregtechceu/gtceu/integration/ae2/machine/MEPatternBufferPartMachine.java
+++ src/main/java/com/gregtechceu/gtceu/integration/ae2/machine/MEPatternBufferPartMachine.java
@@ -407,6 +407,7 @@ public class MEPatternBufferPartMachine extends MEBusPartMachine
     public void onDrops(List<ItemStack> drops, Player entity) {
         super.onDrops(drops, entity);
         clearInventory(drops, patternInventory);
+        clearInventory(drops, shareInventory);
     }
 
     public class InternalSlot implements ITagSerializable<CompoundTag>, IContentChangeAware {
