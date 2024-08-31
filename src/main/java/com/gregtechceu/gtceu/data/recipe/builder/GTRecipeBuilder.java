05700919108d7669199d117b81666c8dcb5903b8
screret <68943070+screret@users.noreply.github.com>
2024-08-31 19:03:00 +0300

    add generic recipe builder methods for usage with CraftingComponent

--- src/main/java/com/gregtechceu/gtceu/data/recipe/builder/GTRecipeBuilder.java
+++ src/main/java/com/gregtechceu/gtceu/data/recipe/builder/GTRecipeBuilder.java
@@ -166,10 +166,22 @@ public class GTRecipeBuilder {
         return builder.copy(builder.id).onSave(null).recipeType(recipeType);
     }
 
+    public <T> GTRecipeBuilder input(RecipeCapability<T> capability, T obj) {
+        (perTick ? tickInput : input).computeIfAbsent(capability, c -> new ArrayList<>())
+                .add(new Content(capability.of(obj), chance, maxChance, tierChanceBoost, slotName, uiName));
+        return this;
+    }
+
     public <T> GTRecipeBuilder input(RecipeCapability<T> capability, T... obj) {
         (perTick ? tickInput : input).computeIfAbsent(capability, c -> new ArrayList<>()).addAll(Arrays.stream(obj)
                 .map(capability::of)
                 .map(o -> new Content(o, chance, maxChance, tierChanceBoost, slotName, uiName)).toList());
+        return this;
+    }
+
+    public <T> GTRecipeBuilder output(RecipeCapability<T> capability, T obj) {
+        (perTick ? tickOutput : output).computeIfAbsent(capability, c -> new ArrayList<>())
+                .add(new Content(capability.of(obj), chance, maxChance, tierChanceBoost, slotName, uiName));
         return this;
     }
 
@@ -180,10 +192,22 @@ public class GTRecipeBuilder {
         return this;
     }
 
+    public <T> GTRecipeBuilder inputs(RecipeCapability<T> capability, Object obj) {
+        (perTick ? tickInput : input).computeIfAbsent(capability, c -> new ArrayList<>())
+                .add(new Content(capability.of(obj), chance, maxChance, tierChanceBoost, slotName, uiName));
+        return this;
+    }
+
     public <T> GTRecipeBuilder inputs(RecipeCapability<T> capability, Object... obj) {
         (perTick ? tickInput : input).computeIfAbsent(capability, c -> new ArrayList<>()).addAll(Arrays.stream(obj)
                 .map(capability::of)
                 .map(o -> new Content(o, chance, maxChance, tierChanceBoost, slotName, uiName)).toList());
+        return this;
+    }
+
+    public <T> GTRecipeBuilder outputs(RecipeCapability<T> capability, Object obj) {
+        (perTick ? tickOutput : output).computeIfAbsent(capability, c -> new ArrayList<>())
+                .add(new Content(capability.of(obj), chance, maxChance, tierChanceBoost, slotName, uiName));
         return this;
     }
 
@@ -248,6 +272,56 @@ public class GTRecipeBuilder {
 
     public GTRecipeBuilder outputCWU(int cwu) {
         return output(CWURecipeCapability.CAP, cwu);
+    }
+
+    public GTRecipeBuilder inputItems(Object input) {
+        if (input instanceof Item item) {
+            return inputItems(item);
+        } else if (input instanceof Supplier<?> supplier && supplier.get() instanceof ItemLike item) {
+            return inputItems(item.asItem());
+        } else if (input instanceof ItemStack stack) {
+            return inputItems(stack);
+        } else if (input instanceof Ingredient ingredient) {
+            return inputItems(ingredient);
+        } else if (input instanceof UnificationEntry entry) {
+            return inputItems(entry);
+        } else if (input instanceof TagKey<?> tag) {
+            return inputItems((TagKey<Item>) tag);
+        } else if (input instanceof MachineDefinition machine) {
+            return inputItems(machine);
+        } else {
+            GTCEu.LOGGER.error(
+                    "gt recipe {} input item is not one of: Item, Supplier<Item>, ItemStack, Ingredient, UnificationEntry, TagKey<Item>, MachineDefinition",
+                    id);
+            return this;
+        }
+    }
+
+    public GTRecipeBuilder inputItems(Object input, int count) {
+        if (input instanceof Item item) {
+            return inputItems(item, count);
+        } else if (input instanceof Supplier<?> supplier && supplier.get() instanceof ItemLike item) {
+            return inputItems(item.asItem(), count);
+        } else if (input instanceof ItemStack stack) {
+            return inputItems(stack.copyWithCount(count));
+        } else if (input instanceof Ingredient ingredient) {
+            return inputItems(ingredient, count);
+        } else if (input instanceof UnificationEntry entry) {
+            return inputItems(entry, count);
+        } else if (input instanceof TagKey<?> tag) {
+            return inputItems((TagKey<Item>) tag, count);
+        } else if (input instanceof MachineDefinition machine) {
+            return inputItems(machine, count);
+        } else {
+            GTCEu.LOGGER.error(
+                    "gt recipe {} input item is not one of: Item, Supplier<Item>, ItemStack, Ingredient, UnificationEntry, TagKey<Item>, MachineDefinition",
+                    id);
+            return this;
+        }
+    }
+
+    public GTRecipeBuilder inputItems(Ingredient inputs) {
+        return input(ItemRecipeCapability.CAP, inputs);
     }
 
     public GTRecipeBuilder inputItems(Ingredient... inputs) {
@@ -323,6 +397,44 @@ public class GTRecipeBuilder {
         return inputItems(machine.asStack(count));
     }
 
+    public GTRecipeBuilder outputItems(Object input) {
+        if (input instanceof Item item) {
+            return outputItems(item);
+        } else if (input instanceof Supplier<?> supplier && supplier.get() instanceof ItemLike item) {
+            return outputItems(item.asItem());
+        } else if (input instanceof ItemStack stack) {
+            return outputItems(stack);
+        } else if (input instanceof UnificationEntry entry) {
+            return outputItems(entry);
+        } else if (input instanceof MachineDefinition machine) {
+            return outputItems(machine);
+        } else {
+            GTCEu.LOGGER.error(
+                    "gt recipe {} output item is not one of: Item, Supplier<Item>, ItemStack, Ingredient, UnificationEntry, TagKey<Item>, MachineDefinition",
+                    id);
+            return this;
+        }
+    }
+
+    public GTRecipeBuilder outputItems(Object input, int count) {
+        if (input instanceof Item item) {
+            return outputItems(item, count);
+        } else if (input instanceof Supplier<?> supplier && supplier.get() instanceof ItemLike item) {
+            return outputItems(item.asItem(), count);
+        } else if (input instanceof ItemStack stack) {
+            return outputItems(stack.copyWithCount(count));
+        } else if (input instanceof UnificationEntry entry) {
+            return outputItems(entry, count);
+        } else if (input instanceof MachineDefinition machine) {
+            return outputItems(machine, count);
+        } else {
+            GTCEu.LOGGER.error(
+                    "gt recipe {} output item is not one of: Item, Supplier<Item>, ItemStack, Ingredient, UnificationEntry, TagKey<Item>, MachineDefinition",
+                    id);
+            return this;
+        }
+    }
+
     public GTRecipeBuilder outputItems(Ingredient... inputs) {
         return output(ItemRecipeCapability.CAP, inputs);
     }
@@ -344,12 +456,12 @@ public class GTRecipeBuilder {
                 Arrays.stream(outputs).map(SizedIngredient::create).toArray(Ingredient[]::new));
     }
 
-    public GTRecipeBuilder outputItems(Item input, int amount) {
-        return outputItems(new ItemStack(input, amount));
+    public GTRecipeBuilder outputItems(Item output, int amount) {
+        return outputItems(new ItemStack(output, amount));
     }
 
-    public GTRecipeBuilder outputItems(Item input) {
-        return outputItems(new ItemStack(input));
+    public GTRecipeBuilder outputItems(Item output) {
+        return outputItems(new ItemStack(output));
     }
 
     public GTRecipeBuilder outputItems(Supplier<? extends ItemLike> input) {
@@ -366,6 +478,14 @@ public class GTRecipeBuilder {
 
     public GTRecipeBuilder outputItems(TagPrefix orePrefix, Material material, int count) {
         return outputItems(ChemicalHelper.get(orePrefix, material, count));
+    }
+
+    public GTRecipeBuilder outputItems(UnificationEntry entry) {
+        return outputItems(entry.tagPrefix, entry.material);
+    }
+
+    public GTRecipeBuilder outputItems(UnificationEntry entry, int count) {
+        return outputItems(entry.tagPrefix, entry.material, count);
     }
 
     public GTRecipeBuilder outputItems(MachineDefinition machine) {
@@ -657,7 +777,7 @@ public class GTRecipeBuilder {
     /**
      * Set a chanced output logic for a specific capability.
      * all capabilities default to OR logic if not set.
-     * 
+     *
      * @param cap   the {@link RecipeCapability} to set the logic for
      * @param logic the {@link ChanceLogic} to use
      * @return this builder
@@ -706,8 +826,8 @@ public class GTRecipeBuilder {
     public GTRecipeBuilder inputFluids(FluidStack... inputs) {
         return input(FluidRecipeCapability.CAP, Arrays.stream(inputs).map(fluid -> {
             if (!Platform.isForge() && fluid.getFluid() == Fluids.WATER) { // Special case for fabric, because there all
-                                                                           // fluids have to be tagged as water to
-                                                                           // function as water when placed.
+                // fluids have to be tagged as water to
+                // function as water when placed.
                 return FluidIngredient.of(fluid);
             } else {
                 return FluidIngredient.of(
@@ -1079,7 +1199,7 @@ public class GTRecipeBuilder {
 
     /**
      * An entry for an autogenerated research recipe for producing a data item containing research data.
-     * 
+     *
      * @param researchId    the id of the research to store
      * @param researchStack the stack to scan for research
      * @param dataStack     the stack to contain the data
@@ -1093,5 +1213,7 @@ public class GTRecipeBuilder {
                                       @NotNull ItemStack dataStack,
                                       int duration,
                                       int EUt,
-                                      int CWUt) {}
+                                      int CWUt) {
+
+    }
 }
