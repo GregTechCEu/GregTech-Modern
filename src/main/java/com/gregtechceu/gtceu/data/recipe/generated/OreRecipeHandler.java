650a351a1c732eeb616166e7c78465055030acb0
Rundas <38040866+rundas01@users.noreply.github.com>
2024-09-01 11:26:49 +0300

    Fix reslocs for several autogenerated recipes (#1862)

--- src/main/java/com/gregtechceu/gtceu/data/recipe/generated/OreRecipeHandler.java
+++ src/main/java/com/gregtechceu/gtceu/data/recipe/generated/OreRecipeHandler.java
@@ -233,12 +233,12 @@ public class OreRecipeHandler {
                     ChemicalHelper.get(rawOre, material, 9),
                     ChemicalHelper.getTag(rawOreBlock, material));
         }
-        COMPRESSOR_RECIPES.recipeBuilder("compress_" + material.getName() + "to_ore_block")
+        COMPRESSOR_RECIPES.recipeBuilder("compress_" + material.getName() + "_to_ore_block")
                 .inputItems(rawOre, material, 9)
                 .outputItems(rawOreBlock, material)
                 .duration(300).EUt(2).save(provider);
 
-        FORGE_HAMMER_RECIPES.recipeBuilder("decompress_" + material.getName() + "to_raw_ore")
+        FORGE_HAMMER_RECIPES.recipeBuilder("decompress_" + material.getName() + "_to_raw_ore")
                 .inputItems(rawOreBlock, material)
                 .outputItems(rawOre, material, 9)
                 .circuitMeta(2)
