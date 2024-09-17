855e6fdaec3978ca0e8c9fa3273b3d904c41045d
EasterFG <easterfg@gmail.com>
2024-09-17 19:32:08 +0800

    remove unused import

--- src/main/java/com/gregtechceu/gtceu/api/gui/misc/IGhostFluidTarget.java
+++ src/main/java/com/gregtechceu/gtceu/api/gui/misc/IGhostFluidTarget.java
@@ -35,7 +35,7 @@ public interface IGhostFluidTarget extends IGhostIngredientTarget {
             return Collections.emptyList();
         } else {
             final Rect2i rectangle = getRectangleBox();
-            return Lists.newArrayList(new Target[] { new Target() {
+            return Lists.newArrayList(new Target[]{new Target() {
 
                 @NotNull
                 public Rect2i getArea() {
@@ -56,7 +56,7 @@ public interface IGhostFluidTarget extends IGhostIngredientTarget {
                         acceptFluid(ingredientStack);
                     }
                 }
-            } });
+            }});
         }
     }
 
@@ -72,7 +72,7 @@ public interface IGhostFluidTarget extends IGhostIngredientTarget {
         }
 
         if (LDLib.isJeiLoaded() && ingredient instanceof net.minecraftforge.fluids.FluidStack fluidStack) {
-            ingredient = com.lowdragmc.lowdraglib.side.fluid.FluidStack.create(fluidStack.getFluid(), fluidStack.getAmount(), fluidStack.getTag());
+            ingredient = FluidStack.create(fluidStack.getFluid(), fluidStack.getAmount(), fluidStack.getTag());
         }
         return ingredient;
     }
