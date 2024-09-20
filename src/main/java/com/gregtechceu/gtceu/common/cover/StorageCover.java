71746a46ac21e1d3ea5e1e0b151f12fa10158358
Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>
2024-09-20 15:21:46 -0600

    final commit

--- src/main/java/com/gregtechceu/gtceu/common/cover/StorageCover.java
+++ src/main/java/com/gregtechceu/gtceu/common/cover/StorageCover.java
@@ -5,8 +5,12 @@ import com.gregtechceu.gtceu.api.cover.CoverBehavior;
 import com.gregtechceu.gtceu.api.cover.CoverDefinition;
 import com.gregtechceu.gtceu.api.cover.IUICover;
 
+import com.lowdragmc.lowdraglib.gui.widget.SlotWidget;
 import com.lowdragmc.lowdraglib.gui.widget.Widget;
 import com.lowdragmc.lowdraglib.gui.widget.WidgetGroup;
+import com.lowdragmc.lowdraglib.misc.ItemStackTransfer;
+import com.lowdragmc.lowdraglib.syncdata.annotation.DescSynced;
+import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
 
 import net.minecraft.core.Direction;
 
@@ -14,17 +18,34 @@ import org.jetbrains.annotations.NotNull;
 
 public class StorageCover extends CoverBehavior implements IUICover {
 
+    @Persisted
+    @DescSynced
+    public final ItemStackTransfer inventory;
+    private int inventorysize = 18;
+    private int xoffset = 7;
+    private int yoffset = 7;
+
     public StorageCover(@NotNull CoverDefinition definition, @NotNull ICoverable coverableView,
                         @NotNull Direction attachedSide) {
         super(definition, coverableView, attachedSide);
+        inventory = new ItemStackTransfer(inventorysize) {
+
+            @Override
+            public int getSlotLimit(int slot) {
+                return 1;
+            }
+        };
     }
-    //////////////////////////////////////
-    // *********** GUI ***********//
-    //////////////////////////////////////
 
     @Override
     public Widget createUIWidget() {
         final var group = new WidgetGroup(0, 0, 176, 85);
+        for (int slot = 0; slot < 18; slot++) {
+
+            group.addWidget(new SlotWidget(inventory, slot, xoffset + (slot % 9) * 18, yoffset + (slot / 9) * 18));
+
+        }
+
         return group;
     }
 }
