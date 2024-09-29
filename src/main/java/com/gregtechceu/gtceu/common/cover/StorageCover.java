f4f7542e83c253f4dd8986a0b3adbf780e65567d
JuiceyBeans <75553966+JuiceyBeans@users.noreply.github.com>
2024-09-29 03:26:46 +0100

    Drop held items when broken

--- src/main/java/com/gregtechceu/gtceu/common/cover/StorageCover.java
+++ src/main/java/com/gregtechceu/gtceu/common/cover/StorageCover.java
@@ -11,10 +11,14 @@ import com.lowdragmc.lowdraglib.gui.widget.WidgetGroup;
 import com.lowdragmc.lowdraglib.misc.ItemStackTransfer;
 import com.lowdragmc.lowdraglib.syncdata.annotation.DescSynced;
 import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
+import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder;
 
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.ItemStack;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class StorageCover extends CoverBehavior implements IUICover {
 
@@ -24,6 +28,8 @@ public class StorageCover extends CoverBehavior implements IUICover {
     private int inventorysize = 18;
     private int xoffset = 7;
     private int yoffset = 7;
+    public static final ManagedFieldHolder MANAGED_FIELD_HOLDER = new ManagedFieldHolder(StorageCover.class,
+            CoverBehavior.MANAGED_FIELD_HOLDER);
 
     public StorageCover(@NotNull CoverDefinition definition, @NotNull ICoverable coverableView,
                         @NotNull Direction attachedSide) {
@@ -35,6 +41,20 @@ public class StorageCover extends CoverBehavior implements IUICover {
                 return 1;
             }
         };
+    }
+
+    @Override
+    public ManagedFieldHolder getFieldHolder() {
+        return MANAGED_FIELD_HOLDER;
+    }
+
+    @Override
+    public List<ItemStack> getAdditionalDrops() {
+        var list = super.getAdditionalDrops();
+        for (int slot = 0; slot < inventorysize; slot++) {
+            list.add(inventory.getStackInSlot(slot));
+        }
+        return list;
     }
 
     @Override
