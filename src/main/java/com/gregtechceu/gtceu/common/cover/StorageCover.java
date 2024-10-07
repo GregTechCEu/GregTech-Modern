ce49fa234e55b463cf573f4873e02ad1b6b5cc83
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2024-10-07 14:03:51 -0600

    finish up ui for storage covers, refactor it and removed erroneous tag to gt frames

--- src/main/java/com/gregtechceu/gtceu/common/cover/StorageCover.java
+++ src/main/java/com/gregtechceu/gtceu/common/cover/StorageCover.java
@@ -5,6 +5,7 @@ import com.gregtechceu.gtceu.api.cover.CoverBehavior;
 import com.gregtechceu.gtceu.api.cover.CoverDefinition;
 import com.gregtechceu.gtceu.api.cover.IUICover;
 
+import com.lowdragmc.lowdraglib.gui.widget.LabelWidget;
 import com.lowdragmc.lowdraglib.gui.widget.SlotWidget;
 import com.lowdragmc.lowdraglib.gui.widget.Widget;
 import com.lowdragmc.lowdraglib.gui.widget.WidgetGroup;
@@ -12,6 +13,7 @@ import com.lowdragmc.lowdraglib.misc.ItemStackTransfer;
 import com.lowdragmc.lowdraglib.syncdata.annotation.DescSynced;
 import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
 import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder;
+import com.lowdragmc.lowdraglib.utils.LocalizationUtils;
 
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.ItemStack;
@@ -25,16 +27,15 @@ public class StorageCover extends CoverBehavior implements IUICover {
     @Persisted
     @DescSynced
     public final ItemStackTransfer inventory;
-    private int inventorysize = 18;
-    private int xoffset = 7;
-    private int yoffset = 7;
+    private final int SIZE = 18;
+
     public static final ManagedFieldHolder MANAGED_FIELD_HOLDER = new ManagedFieldHolder(StorageCover.class,
             CoverBehavior.MANAGED_FIELD_HOLDER);
 
     public StorageCover(@NotNull CoverDefinition definition, @NotNull ICoverable coverableView,
                         @NotNull Direction attachedSide) {
         super(definition, coverableView, attachedSide);
-        inventory = new ItemStackTransfer(inventorysize) {
+        inventory = new ItemStackTransfer(SIZE) {
 
             @Override
             public int getSlotLimit(int slot) {
@@ -51,7 +52,7 @@ public class StorageCover extends CoverBehavior implements IUICover {
     @Override
     public List<ItemStack> getAdditionalDrops() {
         var list = super.getAdditionalDrops();
-        for (int slot = 0; slot < inventorysize; slot++) {
+        for (int slot = 0; slot < SIZE; slot++) {
             list.add(inventory.getStackInSlot(slot));
         }
         return list;
@@ -59,11 +60,18 @@ public class StorageCover extends CoverBehavior implements IUICover {
 
     @Override
     public Widget createUIWidget() {
-        final var group = new WidgetGroup(0, 0, 126, 65);
-        for (int slot = 0; slot < inventorysize; slot++) {
-            group.addWidget(new SlotWidget(inventory, slot, xoffset + (slot % 6) * 18, yoffset + (slot / 6) * 18));
+        final var group = new WidgetGroup(0, 0, 126, 87);
+
+        group.addWidget(new LabelWidget(10, 5, LocalizationUtils.format(getUITitle())));
+
+        for (int slot = 0; slot < SIZE; slot++) {
+            group.addWidget(new SlotWidget(inventory, slot, 7 + (slot % 6) * 18, 21 + (slot / 6) * 18));
         }
 
         return group;
+    }
+
+    private String getUITitle() {
+        return "cover.storage.title";
     }
 }
