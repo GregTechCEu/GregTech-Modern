e9f6d16d19b6411a72a47a12c24d2f161cff9e30
screret <68943070+screret@users.noreply.github.com>
2024-10-11 21:24:30 +0300

    fix model (needs refirement)

--- src/main/java/com/gregtechceu/gtceu/client/renderer/block/SurfaceRockRenderer.java
+++ src/main/java/com/gregtechceu/gtceu/client/renderer/block/SurfaceRockRenderer.java
@@ -4,14 +4,17 @@ import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.data.pack.GTDynamicResourcePack;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.core.Direction;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.models.BlockModelGenerators;
 import net.minecraft.data.models.blockstates.MultiVariantGenerator;
+import net.minecraft.data.models.blockstates.PropertyDispatch;
 import net.minecraft.data.models.blockstates.Variant;
 import net.minecraft.data.models.blockstates.VariantProperties;
 import net.minecraft.data.models.model.DelegatedModel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -32,13 +35,18 @@ public class SurfaceRockRenderer {
         for (SurfaceRockRenderer model : MODELS) {
             ResourceLocation blockId = BuiltInRegistries.BLOCK.getKey(model.block);
             ResourceLocation modelId = blockId.withPrefix("block/");
-            ResourceLocation itemId = blockId.withPrefix("");
 
             GTDynamicResourcePack.addBlockModel(modelId, new DelegatedModel(GTCEu.id("block/surface_rock")));
             GTDynamicResourcePack.addBlockState(blockId, MultiVariantGenerator
                     .multiVariant(model.block, Variant.variant().with(VariantProperties.MODEL, modelId))
-                    .with(BlockModelGenerators.createFacingDispatch()));
-            GTDynamicResourcePack.addItemModel(itemId, new DelegatedModel(modelId));
+                    .with(PropertyDispatch.property(BlockStateProperties.FACING)
+                            .select(Direction.DOWN, Variant.variant())
+                            .select(Direction.UP, Variant.variant().with(VariantProperties.X_ROT, VariantProperties.Rotation.R180))
+                            .select(Direction.NORTH, Variant.variant().with(VariantProperties.Y_ROT, VariantProperties.Rotation.R90))
+                            .select(Direction.SOUTH, Variant.variant().with(VariantProperties.Y_ROT, VariantProperties.Rotation.R270))
+                            .select(Direction.WEST, Variant.variant().with(VariantProperties.X_ROT, VariantProperties.Rotation.R270))
+                            .select(Direction.EAST, Variant.variant().with(VariantProperties.X_ROT, VariantProperties.Rotation.R90))));
+            GTDynamicResourcePack.addItemModel(blockId, new DelegatedModel(modelId));
         }
     }
 
