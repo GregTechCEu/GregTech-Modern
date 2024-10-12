41ad6439bc376fa45003d9c8b6cc2c571474e04c
screret <68943070+screret@users.noreply.github.com>
2024-10-12 17:04:25 +0300

    update KJS to latest dev build deprecates `injectRuntimeRecipes`, inject those ourselves again

--- src/main/java/com/gregtechceu/gtceu/core/mixins/RecipeManagerLateMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/RecipeManagerLateMixin.java
@@ -2,46 +2,76 @@ package com.gregtechceu.gtceu.core.mixins;
 
 import com.gregtechceu.gtceu.api.recipe.GTRecipe;
 import com.gregtechceu.gtceu.api.recipe.GTRecipeType;
-import com.gregtechceu.gtceu.data.recipe.GTRecipes;
+import com.gregtechceu.gtceu.data.recipe.builder.GTRecipeBuilder;
 
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementHolder;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.data.recipes.RecipeBuilder;
+import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.neoforged.neoforge.common.conditions.ICondition;
 
+import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
 import com.google.gson.JsonElement;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
-@Mixin(value = RecipeManager.class, priority = 500)
-public abstract class RecipeManagerMixin {
+@Mixin(value = RecipeManager.class, priority = 1500)
+public abstract class RecipeManagerLateMixin {
 
     @Shadow
     private Multimap<RecipeType<?>, RecipeHolder<?>> byType;
 
-    @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V",
-            at = @At("HEAD"))
-    private void gtceu$removeRecipes(Map<ResourceLocation, JsonElement> map, ResourceManager pResourceManager,
-                                     ProfilerFiller pProfiler, CallbackInfo ci) {
-        GTRecipes.recipeRemoval(map::remove);
-    }
+    @Shadow
+    private Map<ResourceLocation, RecipeHolder<?>> byName;
 
-    // only fires if KJS is NOT loaded.
     @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V",
             at = @At(value = "TAIL"))
     private void gtceu$cloneVanillaRecipes(Map<ResourceLocation, JsonElement> map, ResourceManager resourceManager,
                                            ProfilerFiller profiler, CallbackInfo ci) {
+        var recipesByName = new HashMap<>(byName);
+        byName.values().forEach(holder -> {
+            if (holder.value() instanceof GTRecipe gtRecipe) {
+                new GTRecipeBuilder(gtRecipe, gtRecipe.recipeType)
+                        .id(holder.id().withPath(path -> path.substring(path.indexOf('/') + 1)))
+                        .onSave(gtRecipe.recipeType.getRecipeBuilder().onSave)
+                        .save(new RecipeOutput() {
+
+                            @Override
+                            public Advancement.Builder advancement() {
+                                return Advancement.Builder.recipeAdvancement()
+                                        .parent(RecipeBuilder.ROOT_RECIPE_ADVANCEMENT);
+                            }
+
+                            @Override
+                            public void accept(@NotNull ResourceLocation id, @NotNull Recipe<?> recipe,
+                                               @Nullable AdvancementHolder advancement, ICondition... conditions) {
+                                recipesByName.put(id, new RecipeHolder<>(id, recipe));
+                            }
+                        });
+            }
+        });
+        gtceu$replaceRecipes(recipesByName);
+
         for (RecipeType<?> recipeType : BuiltInRegistries.RECIPE_TYPE) {
             if (recipeType instanceof GTRecipeType gtRecipeType) {
                 gtRecipeType.getLookup().removeAllRecipes();
@@ -75,5 +105,18 @@ public abstract class RecipeManagerMixin {
                 }
             }
         }
+    }
+
+    @Unique
+    public void gtceu$replaceRecipes(Map<ResourceLocation, RecipeHolder<?>> map) {
+        byName = map;
+
+        var recipesByType = ImmutableMultimap.<RecipeType<?>, RecipeHolder<?>>builder();
+
+        for (var entry : map.entrySet()) {
+            recipesByType.put(entry.getValue().value().getType(), entry.getValue());
+        }
+
+        byType = recipesByType.build();
     }
 }
