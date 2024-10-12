7ddb30310205908dd645192d6177ec6d034450e4
kross <135918757+krossgg@users.noreply.github.com>
2024-10-12 19:25:20 -0400

    Requested changes

--- src/main/java/com/gregtechceu/gtceu/core/mixins/ForgeHooksMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/ForgeHooksMixin.java
@@ -12,18 +12,17 @@ import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.util.BlockSnapshot;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ForgeHooks.class)
+@Mixin(value = ForgeHooks.class, remap = false)
 public class ForgeHooksMixin {
 
     @Inject(method = "onPlaceItemIntoWorld",
             at = @At(value = "INVOKE",
-                     opcode = Opcodes.GETFIELD,
+                     remap = true,
                      target = "Lnet/minecraft/world/level/block/state/BlockState;onPlace(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Z)V"))
     private static void gtceu$onPlaceBlock(UseOnContext context, CallbackInfoReturnable<InteractionResult> cir,
                                            @Local Level level, @Local BlockSnapshot snap) {
