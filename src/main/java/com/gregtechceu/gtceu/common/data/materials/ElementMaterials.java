ff6742741e6ad9966749dae8f519e867278b459b
JuiceyBeans <75553966+JuiceyBeans@users.noreply.github.com>
2024-10-18 06:00:18 +0100

    Add Rotor flag to copper

--- src/main/java/com/gregtechceu/gtceu/common/data/materials/ElementMaterials.java
+++ src/main/java/com/gregtechceu/gtceu/common/data/materials/ElementMaterials.java
@@ -206,7 +206,7 @@ public class ElementMaterials {
                 .ore()
                 .color(0xe77c56).secondaryColor(0xe4673e).iconSet(BRIGHT)
                 .appendFlags(EXT_METAL, MORTAR_GRINDABLE, GENERATE_SPRING, GENERATE_SPRING_SMALL, GENERATE_RING,
-                        GENERATE_FINE_WIRE)
+                        GENERATE_FINE_WIRE, GENERATE_ROTOR)
                 .element(GTElements.Cu)
                 .cableProperties(GTValues.V[2], 1, 2)
                 .fluidPipeProperties(1696, 6, true)
