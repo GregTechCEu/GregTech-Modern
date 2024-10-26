817aa5f44a0d8768f0b6bec1215a458ca9a38ec8
mikerooni <139889766+mikerooni@users.noreply.github.com>
2024-10-26 21:28:29 +0200

    chore: add gradle subprojects

---
+++ API/src/main/java/com/gregtechceu/gtceu/api/GTCEuAPIMod.java
@@ -0,0 +1,8 @@
+package com.gregtechceu.gtceu.api;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(GTCEuAPIMod.MOD_ID)
+public class GTCEuAPIMod {
+    public static final String MOD_ID = "gtceu_api";
+}
---
+++ GTLib/src/main/java/com/gregtechceu/gtceu/lib/GTCEuLibMod.java
@@ -0,0 +1,8 @@
+package com.gregtechceu.gtceu.lib;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(GTCEuLibMod.MOD_ID)
+public class GTCEuLibMod {
+    public static final String MOD_ID = "gtceu_lib";
+}
