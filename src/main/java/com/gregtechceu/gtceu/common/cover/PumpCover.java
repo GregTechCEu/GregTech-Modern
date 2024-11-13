774c345e27935e53b6728880031b9306e2bc8115
Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>
2024-11-13 13:25:06 -0700

    spotless Mfer did you run it

--- src/main/java/com/gregtechceu/gtceu/common/cover/PumpCover.java
+++ src/main/java/com/gregtechceu/gtceu/common/cover/PumpCover.java
@@ -29,7 +29,6 @@ import com.lowdragmc.lowdraglib.syncdata.annotation.DescSynced;
 import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
 import com.lowdragmc.lowdraglib.syncdata.annotation.RequireRerender;
 import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder;
-import com.lowdragmc.lowdraglib.utils.LocalizationUtils;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
