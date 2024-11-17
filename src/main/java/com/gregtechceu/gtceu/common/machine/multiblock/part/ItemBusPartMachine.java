6143873a74510c109100a72b37a0c9b52f7fc909
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2024-11-17 01:38:41 -0700

    Update ItemBusPartMachine.java

--- src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/part/ItemBusPartMachine.java
+++ src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/part/ItemBusPartMachine.java
@@ -195,8 +195,7 @@ public class ItemBusPartMachine extends TieredIOPartMachine implements IDistinct
     public void attachConfigurators(ConfiguratorPanel configuratorPanel) {
         if (this.io == IO.OUT) {
             IDistinctPart.super.superAttachConfigurators(configuratorPanel);
-        }
-        else if (this.io == IO.IN) {
+        } else if (this.io == IO.IN) {
             IDistinctPart.super.attachConfigurators(configuratorPanel);
             configuratorPanel.attachConfigurators(new CircuitFancyConfigurator(circuitInventory.storage));
         }
