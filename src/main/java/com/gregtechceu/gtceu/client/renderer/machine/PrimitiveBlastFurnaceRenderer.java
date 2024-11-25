e3dd6ca04ce22315170d054d313f1f00b3593a7c
omergunr100 <omer.er10@gmail.com>
2024-11-25 09:00:32 +0200

    render lava block inside of active pbf

--- src/main/java/com/gregtechceu/gtceu/client/renderer/machine/PrimitiveBlastFurnaceRenderer.java
+++ src/main/java/com/gregtechceu/gtceu/client/renderer/machine/PrimitiveBlastFurnaceRenderer.java
@@ -1,32 +1,27 @@
 package com.gregtechceu.gtceu.client.renderer.machine;
 
 import com.gregtechceu.gtceu.api.blockentity.MetaMachineBlockEntity;
-import com.gregtechceu.gtceu.api.machine.MachineDefinition;
-import com.gregtechceu.gtceu.api.machine.MetaMachine;
+import com.gregtechceu.gtceu.client.util.RenderUtil;
 import com.gregtechceu.gtceu.common.machine.multiblock.primitive.PrimitiveBlastFurnaceMachine;
-import com.lowdragmc.lowdraglib.client.scene.WorldSceneRenderer;
-import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.Minecraft;
+
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.block.model.BakedQuad;
-import net.minecraft.client.resources.model.ModelState;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.model.data.ModelData;
-import net.minecraftforge.client.model.pipeline.VertexConsumerWrapper;
-import org.jetbrains.annotations.Nullable;
+import net.minecraftforge.client.RenderTypeHelper;
 
-import java.util.List;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import org.joml.Matrix4f;
 
 public class PrimitiveBlastFurnaceRenderer extends WorkableCasingMachineRenderer {
+
     public PrimitiveBlastFurnaceRenderer(ResourceLocation base, ResourceLocation overlay) {
         super(base, overlay);
     }
@@ -38,20 +33,29 @@ public class PrimitiveBlastFurnaceRenderer extends WorkableCasingMachineRenderer
 
     @Override
     @OnlyIn(Dist.CLIENT)
-    public void render(BlockEntity blockEntity, float partialTicks, PoseStack stack, MultiBufferSource buffer, int combinedLight, int combinedOverlay) {
+    public void render(BlockEntity blockEntity, float partialTicks, PoseStack stack, MultiBufferSource buffer,
+                       int combinedLight, int combinedOverlay) {
         super.render(blockEntity, partialTicks, stack, buffer, combinedLight, combinedOverlay);
 
-        if(blockEntity instanceof MetaMachineBlockEntity mm) {
-            if(mm.metaMachine instanceof PrimitiveBlastFurnaceMachine pbf) {
+        if (blockEntity instanceof MetaMachineBlockEntity mm) {
+            if (mm.metaMachine instanceof PrimitiveBlastFurnaceMachine pbf) {
                 if (pbf.isActive()) {
-                    BlockPos pos = pbf.getPos().offset(pbf.getFrontFacing().getOpposite().getNormal());
-                    WorldSceneRenderer.VertexConsumerWrapper vertexConsumer = new WorldSceneRenderer.VertexConsumerWrapper(buffer.getBuffer(RenderType.solid()));
-                    vertexConsumer.addOffset((pos.getX() - (pos.getX() & 15)), (pos.getY() - (pos.getY() & 15)),
-                            (pos.getZ() - (pos.getZ() & 15)));
-                    Minecraft.getInstance().getBlockRenderer().renderLiquid(
-                            pos,
-                            pbf.getLevel(), vertexConsumer, Blocks.LAVA.defaultBlockState(),
-                            Fluids.LAVA.defaultFluidState());
+                    Direction opposite = pbf.getFrontFacing().getOpposite();
+                    RenderType lavaRenderType = ItemBlockRenderTypes.getRenderLayer(Fluids.LAVA.defaultFluidState());
+                    combinedLight = LightTexture.FULL_BRIGHT;
+
+                    stack.pushPose();
+                    Matrix4f pose = stack.last().pose();
+
+                    pose.translate(opposite.getStepX(), opposite.getStepY(), opposite.getStepZ());
+
+                    VertexConsumer vertexConsumer = buffer
+                            .getBuffer(RenderTypeHelper.getEntityRenderType(lavaRenderType, true));
+
+                    RenderUtil.renderFluidBlockFace(pose, vertexConsumer, Fluids.LAVA,
+                            RenderUtil.FluidTextureType.STILL, Direction.UP, combinedOverlay, combinedLight);
+
+                    stack.popPose();
                 }
             }
         }
