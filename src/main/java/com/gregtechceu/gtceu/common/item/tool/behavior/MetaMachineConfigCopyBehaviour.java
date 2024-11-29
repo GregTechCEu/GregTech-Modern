4994fe703841446b76042d3d6427bc7e29519c1d
omergunr100 <omer.er10@gmail.com>
2024-11-29 16:21:19 +0200

    handling edge cases

--- src/main/java/com/gregtechceu/gtceu/common/item/tool/behavior/MetaMachineConfigCopyBehaviour.java
+++ src/main/java/com/gregtechceu/gtceu/common/item/tool/behavior/MetaMachineConfigCopyBehaviour.java
@@ -7,12 +7,16 @@ import com.gregtechceu.gtceu.api.machine.MetaMachine;
 import com.gregtechceu.gtceu.api.machine.feature.IAutoOutputFluid;
 import com.gregtechceu.gtceu.api.machine.feature.IAutoOutputItem;
 import com.gregtechceu.gtceu.api.machine.feature.IMufflableMachine;
-import com.gregtechceu.gtceu.api.machine.multiblock.MultiblockControllerMachine;
 
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
@@ -70,30 +74,29 @@ public class MetaMachineConfigCopyBehaviour implements IInteractionItem, IAddInf
             if (blockEntity.getOwner() != null && !blockEntity.getOwner().isPlayerInTeam(context.getPlayer()))
                 return InteractionResult.FAIL;
             MetaMachine machine = blockEntity.getMetaMachine();
-            if (machine instanceof MultiblockControllerMachine) return InteractionResult.PASS;
             if (context.isSecondaryUseActive())
                 return handleCopy(stack, machine);
-            else if (stack.getOrCreateTag().contains(CONFIG_DATA))
+            else if (stack.hasTag() && stack.getTag().contains(CONFIG_DATA))
                 return handlePaste(stack, machine);
-        }
-        if (context.isSecondaryUseActive() && context.getLevel().getBlockState(context.getClickedPos()).isAir()) {
-            stack.getOrCreateTag().remove(CONFIG_DATA);
-            return InteractionResult.SUCCESS;
-        }
-        return InteractionResult.PASS;
+        } else
+            if (context.isSecondaryUseActive() && context.getLevel().getBlockState(context.getClickedPos()).isAir()) {
+                stack.setTag(null);
+                return InteractionResult.SUCCESS;
+            }
+        return InteractionResult.SUCCESS;
     }
 
     public static InteractionResult handleCopy(ItemStack stack, MetaMachine machine) {
         CompoundTag configData = new CompoundTag();
         configData.putInt(ORIGINAL_FRONT, directionToInt(machine.getFrontFacing()));
-        if (machine instanceof IAutoOutputItem autoOutputItem) {
+        if (machine instanceof IAutoOutputItem autoOutputItem && autoOutputItem.getOutputFacingItems() != null) {
             CompoundTag itemTag = new CompoundTag();
             itemTag.putInt(DIRECTION, directionToInt(autoOutputItem.getOutputFacingItems()));
             itemTag.putBoolean(AUTO, autoOutputItem.isAutoOutputItems());
             itemTag.putBoolean(INPUT_FROM_OUTPUT_SIDE, autoOutputItem.isAllowInputFromOutputSideItems());
             configData.put(ITEM_CONFIG, itemTag);
         }
-        if (machine instanceof IAutoOutputFluid autoOutputFluid) {
+        if (machine instanceof IAutoOutputFluid autoOutputFluid && autoOutputFluid.getOutputFacingFluids() != null) {
             CompoundTag fluidTag = new CompoundTag();
             fluidTag.putInt(DIRECTION, directionToInt(autoOutputFluid.getOutputFacingFluids()));
             fluidTag.putBoolean(AUTO, autoOutputFluid.isAutoOutputFluids());
@@ -103,29 +106,32 @@ public class MetaMachineConfigCopyBehaviour implements IInteractionItem, IAddInf
         if (machine instanceof IMufflableMachine mufflableMachine) {
             configData.putBoolean(MUFFLED, mufflableMachine.isMuffled());
         }
-        stack.getOrCreateTag().put(CONFIG_DATA, configData);
+        if (!configData.isEmpty()) {
+            stack.getOrCreateTag().put(CONFIG_DATA, configData);
+        }
         return InteractionResult.SUCCESS;
     }
 
     public static InteractionResult handlePaste(ItemStack stack, MetaMachine machine) {
+        if (!stack.hasTag() || !stack.getTag().contains(CONFIG_DATA)) return InteractionResult.PASS;
         CompoundTag root = stack.getOrCreateTag();
         CompoundTag configData = root.getCompound(CONFIG_DATA);
         Direction originalFront = intToDirection(configData.getInt(ORIGINAL_FRONT));
-        if (machine instanceof IAutoOutputItem autoOutputItem) {
+        if (configData.contains(ITEM_CONFIG) && machine instanceof IAutoOutputItem autoOutputItem) {
             CompoundTag itemData = configData.getCompound(ITEM_CONFIG);
             autoOutputItem.setOutputFacingItems(getRelativeDirection(originalFront, machine.getFrontFacing(),
                     intToDirection(itemData.getInt(DIRECTION))));
             autoOutputItem.setAutoOutputItems(itemData.getBoolean(AUTO));
             autoOutputItem.setAllowInputFromOutputSideItems(itemData.getBoolean(INPUT_FROM_OUTPUT_SIDE));
         }
-        if (machine instanceof IAutoOutputFluid autoOutputFluid) {
+        if (configData.contains(FLUID_CONFIG) && machine instanceof IAutoOutputFluid autoOutputFluid) {
             CompoundTag fluidData = configData.getCompound(FLUID_CONFIG);
             autoOutputFluid.setOutputFacingFluids(getRelativeDirection(originalFront, machine.getFrontFacing(),
                     intToDirection(fluidData.getInt(DIRECTION))));
             autoOutputFluid.setAutoOutputFluids(fluidData.getBoolean(AUTO));
             autoOutputFluid.setAllowInputFromOutputSideFluids(fluidData.getBoolean(INPUT_FROM_OUTPUT_SIDE));
         }
-        if (machine instanceof IMufflableMachine mufflableMachine) {
+        if (configData.contains(MUFFLED) && machine instanceof IMufflableMachine mufflableMachine) {
             mufflableMachine.setMuffled(configData.getBoolean(MUFFLED));
         }
         return InteractionResult.SUCCESS;
