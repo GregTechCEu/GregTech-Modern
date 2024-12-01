86b6bb4211361dab9f457de6b00068e0f97e061b
Rain <kitgxrl@gmail.com>
2024-11-30 19:51:50 -0500

    Use || instead of |

--- src/main/java/com/gregtechceu/gtceu/integration/emi/recipe/Ae2PatternTerminalHandler.java
+++ src/main/java/com/gregtechceu/gtceu/integration/emi/recipe/Ae2PatternTerminalHandler.java
@@ -37,7 +37,7 @@ public class Ae2PatternTerminalHandler<T extends PatternEncodingTermMenu> implem
 
     @Override
     public boolean supportsRecipe(EmiRecipe recipe) {
-        return recipe instanceof GTEmiRecipe | recipe instanceof MultiblockInfoEmiRecipe;
+        return recipe instanceof GTEmiRecipe || recipe instanceof MultiblockInfoEmiRecipe;
     }
 
     @Override
