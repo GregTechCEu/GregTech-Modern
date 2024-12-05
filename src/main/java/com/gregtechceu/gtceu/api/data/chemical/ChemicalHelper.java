a7d02e90b08063404096f798048ee618d4f55621
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2024-12-04 22:02:02 -0700

    start of stone block recipes

--- src/main/java/com/gregtechceu/gtceu/api/data/chemical/ChemicalHelper.java
+++ src/main/java/com/gregtechceu/gtceu/api/data/chemical/ChemicalHelper.java
@@ -11,6 +11,7 @@ import com.gregtechceu.gtceu.api.data.tag.TagPrefix;
 import com.gregtechceu.gtceu.api.data.tag.TagUtil;
 import com.gregtechceu.gtceu.api.fluids.store.FluidStorageKey;
 import com.gregtechceu.gtceu.common.data.GTItems;
+import com.gregtechceu.gtceu.data.recipe.misc.StoneMachineRecipes;
 import com.gregtechceu.gtceu.data.recipe.misc.WoodMachineRecipes;
 import com.gregtechceu.gtceu.data.tags.TagsHandler;
 import com.gregtechceu.gtceu.utils.SupplierMemoizer;
@@ -398,5 +399,6 @@ public class ChemicalHelper {
         }
         GTItems.toUnify.forEach(ChemicalHelper::registerUnificationItems);
         WoodMachineRecipes.registerUnificationInfo();
+        StoneMachineRecipes.registerUnificationInfo();
     }
 }
--- src/main/java/com/gregtechceu/gtceu/common/data/GTRecipes.java
+++ src/main/java/com/gregtechceu/gtceu/common/data/GTRecipes.java
@@ -61,6 +61,7 @@ public class GTRecipes {
         MiscRecipeLoader.init(consumer);
         VanillaStandardRecipes.init(consumer);
         WoodMachineRecipes.init(consumer);
+        StoneMachineRecipes.init(consumer);
         CraftingRecipeLoader.init(consumer);
         FuelRecipes.init(consumer);
         FusionLoader.init(consumer);
