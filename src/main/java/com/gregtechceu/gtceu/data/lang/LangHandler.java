96c064effd7d9539708abcebcc06eed3dda38337
RainPumpkin <rainpumpkin7266@gmail.com>
2024-12-17 17:34:08 +0000

    time fix ft. lang changes and datagen

--- src/main/java/com/gregtechceu/gtceu/data/lang/LangHandler.java
+++ src/main/java/com/gregtechceu/gtceu/data/lang/LangHandler.java
@@ -121,8 +121,9 @@ public class LangHandler {
 
         provider.add("command.gtceu.medical_condition.get", "Player %s has these medical conditions:");
         provider.add("command.gtceu.medical_condition.get.empty", "Player %s has no medical conditions.");
-        provider.add("command.gtceu.medical_condition.get.element", "Condition %s§r: %s seconds");
-        provider.add("command.gtceu.medical_condition.get.element.permanent", "Condition %s§r: %s seconds (permanent)");
+        provider.add("command.gtceu.medical_condition.get.element", "Condition %s§r: %s minutes %s seconds");
+        provider.add("command.gtceu.medical_condition.get.element.permanent",
+                "Condition %s§r: %s minutes %s seconds (permanent)");
         provider.add("command.gtceu.dump_data.success", "Dumped %s resources from registry %s to %s");
         provider.add("command.gtceu.place_vein.failure", "Failed to place vein %s at position %s");
         provider.add("command.gtceu.place_vein.success", "Placed vein %s at position %s");
