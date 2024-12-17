1237b481de796a41f26b328ee5f42adb63f75b8d
screret <68943070+screret@users.noreply.github.com>
2024-12-17 13:43:20 +0200

    entirely remake how we do KJS machine builders to be cleaner and more aligned to how we do it in java

---
+++ src/main/java/com/gregtechceu/gtceu/integration/kjs/builders/machine/KJSSteamMachineBuilder.java
@@ -0,0 +1,70 @@
+package com.gregtechceu.gtceu.integration.kjs.builders.machine;
+
+import com.gregtechceu.gtceu.GTCEu;
+import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.gregtechceu.gtceu.api.machine.MachineDefinition;
+import com.gregtechceu.gtceu.api.machine.MetaMachine;
+import com.gregtechceu.gtceu.api.machine.steam.SimpleSteamMachine;
+import com.gregtechceu.gtceu.api.registry.registrate.BuilderBase;
+import com.gregtechceu.gtceu.api.registry.registrate.MachineBuilder;
+import com.gregtechceu.gtceu.client.renderer.machine.WorkableSteamMachineRenderer;
+import com.gregtechceu.gtceu.common.registry.GTRegistration;
+import com.gregtechceu.gtceu.utils.FormattingUtil;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import net.minecraft.resources.ResourceLocation;
+
+@Accessors(fluent = true, chain = true)
+public class KJSSteamMachineBuilder extends BuilderBase<MachineDefinition> {
+
+    @Setter
+    public volatile boolean hasHighPressure = true;
+    @Setter
+    public volatile SteamCreationFunction machine;
+    @Setter
+    public volatile SteamDefinitionFunction definition = (isHP, def) -> def.tier(isHP ? 1 : 0);
+
+
+    public KJSSteamMachineBuilder(ResourceLocation id) {
+        super(id);
+    }
+
+    @Override
+    public MachineDefinition register() {
+        MachineBuilder<?> lowPressureBuilder = GTRegistration.REGISTRATE.machine(
+                String.format("lp_%s", this.id.getPath()),
+                holder -> machine.create(holder, false));
+        lowPressureBuilder.langValue("Low Pressure " + FormattingUtil.toEnglishName(this.id.getPath()))
+                .tier(0)
+                .recipeModifier(SimpleSteamMachine::recipeModifier)
+                .renderer(() -> new WorkableSteamMachineRenderer(false, id.withPrefix("block/machines/")));
+        var lowPressure = definition.create(false, lowPressureBuilder)
+                .register();
+
+        if (hasHighPressure) {
+            MachineBuilder<?> highPressureBuilder = GTRegistration.REGISTRATE.machine(
+                    String.format("lp_%s", this.id.getPath()),
+                    holder -> machine.create(holder, true));
+            highPressureBuilder.langValue("High Pressure " + FormattingUtil.toEnglishName(this.id.getPath()))
+                    .tier(1)
+                    .recipeModifier(SimpleSteamMachine::recipeModifier)
+                    .renderer(() -> new WorkableSteamMachineRenderer(true, id.withPrefix("block/machines/")));
+            var highPressure = definition.create(true, highPressureBuilder)
+                    .register();
+        }
+
+        return lowPressure;
+    }
+
+    @FunctionalInterface
+    public interface SteamCreationFunction {
+
+        MetaMachine create(IMachineBlockEntity holder, boolean isHighPressure);
+    }
+
+    @FunctionalInterface
+    public interface SteamDefinitionFunction {
+
+        MachineBuilder<?> create(boolean isHighPressure, MachineBuilder<?> builder);
+    }
+}
