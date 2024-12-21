0365471c5f53524dfd5d1a942484aba79572e940
kross <135918757+krossgg@users.noreply.github.com>
2024-12-21 17:48:39 -0500

    Fix various issues with 1.6.0 (#2611)

--- src/main/java/com/gregtechceu/gtceu/integration/emi/circuit/GTProgrammedCircuitCategory.java
+++ src/main/java/com/gregtechceu/gtceu/integration/emi/circuit/GTProgrammedCircuitCategory.java
@@ -32,7 +32,7 @@ public class GTProgrammedCircuitCategory extends EmiRecipeCategory {
 
     @Override
     public Component getName() {
-        return Component.translatable("gtceu.jei.programmed_circuit_page");
+        return Component.translatable("gtceu.jei.programmed_circuit");
     }
 
     public static class GTProgrammedCircuitWrapper extends ModularEmiRecipe<GTProgrammedCircuitWidget> {
