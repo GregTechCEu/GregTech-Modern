b98902aa8709d8bb67c46590c647d93bb798c460
omergunr100 <omer.er10@gmail.com>
2024-12-27 12:00:14 +0200

    settings are now saved properly
    apparently mixin into the static block of an interface is a big no no

--- src/main/java/com/gregtechceu/gtceu/core/mixins/ftbchunks/FTBChunksClientConfigMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/ftbchunks/FTBChunksClientConfigMixin.java
@@ -2,20 +2,18 @@ package com.gregtechceu.gtceu.core.mixins.ftbchunks;
 
 import com.gregtechceu.gtceu.integration.map.ftbchunks.FTBChunksOptions;
 
-import dev.ftb.mods.ftbchunks.client.FTBChunksClientConfig;
+import dev.ftb.mods.ftbchunks.client.FTBChunksClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = FTBChunksClientConfig.class, remap = false)
-public interface FTBChunksClientConfigMixin {
+@Mixin(value = FTBChunksClient.class, remap = false)
+public abstract class FTBChunksClientConfigMixin {
 
-    @Inject(method = "<clinit>",
-            at = @At(value = "INVOKE",
-                     shift = At.Shift.AFTER,
-                     target = "Ldev/ftb/mods/ftblibrary/snbt/config/SNBTConfig;create(Ljava/lang/String;)Ldev/ftb/mods/ftblibrary/snbt/config/SNBTConfig;"))
-    private static void gtceu$injectSaveConfig(CallbackInfo ci) {
+    @Inject(method = "init",
+            at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbchunks/client/FTBChunksClientConfig;init()V"))
+    public void gtceu$injectSaveConfig(CallbackInfo ci) {
         FTBChunksOptions.initialize();
     }
 }
