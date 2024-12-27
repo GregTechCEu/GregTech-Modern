c50d18b1b9dbdd1344ce628488e2d47dfb7891df
omergunr100 <omer.er10@gmail.com>
2024-12-27 09:21:20 +0200

    replaced current player dimension with event dimension

--- src/main/java/com/gregtechceu/gtceu/integration/map/ftbchunks/FTBChunksPlugin.java
+++ src/main/java/com/gregtechceu/gtceu/integration/map/ftbchunks/FTBChunksPlugin.java
@@ -3,23 +3,24 @@ package com.gregtechceu.gtceu.integration.map.ftbchunks;
 import com.gregtechceu.gtceu.integration.map.GroupingMapRenderer;
 
 import dev.ftb.mods.ftbchunks.api.client.event.MapIconEvent;
-import dev.ftb.mods.ftbchunks.client.map.MapDimension;
+import dev.ftb.mods.ftbchunks.client.map.MapManager;
 
 public class FTBChunksPlugin {
 
     private FTBChunksPlugin() {}
 
     public static void addEventListeners() {
+        FTBChunksOptions.initialize();
         MapIconEvent.MINIMAP.register(FTBChunksPlugin::mapIconEventHandler);
         MapIconEvent.LARGE_MAP.register(FTBChunksPlugin::mapIconEventHandler);
     }
 
     private static void mapIconEventHandler(MapIconEvent event) {
         if (GroupingMapRenderer.getInstance().doShowLayer("ore_veins")) {
-            MapDimension.getCurrent()
-                    .ifPresent(mapDimension -> FTBChunksRenderer.oreElements.row(mapDimension.dimension)
-                            .values()
-                            .forEach(event::add));
+            MapManager.getInstance().ifPresent(manager -> FTBChunksRenderer.oreElements
+                    .row(event.getDimension())
+                    .values()
+                    .forEach(event::add));
         }
     }
 }
