92f8aa49368ed9aa14493cbb7c0271b25eb65631
omergunr100 <omer.er10@gmail.com>
2024-12-28 10:53:14 +0200

    button for hiding/showing depleted veins works again

--- src/main/java/com/gregtechceu/gtceu/core/mixins/ftbchunks/LargeMapScreenMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/ftbchunks/LargeMapScreenMixin.java
@@ -1,6 +1,7 @@
 package com.gregtechceu.gtceu.core.mixins.ftbchunks;
 
 import com.gregtechceu.gtceu.integration.map.ButtonState;
+import com.gregtechceu.gtceu.integration.map.ftbchunks.FTBChunksOptions;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Items;
@@ -11,6 +12,7 @@ import dev.ftb.mods.ftblibrary.icon.ItemIcon;
 import dev.ftb.mods.ftblibrary.ui.BaseScreen;
 import dev.ftb.mods.ftblibrary.ui.Button;
 import dev.ftb.mods.ftblibrary.ui.SimpleButton;
+import dev.ftb.mods.ftblibrary.util.TooltipList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -29,13 +31,14 @@ public abstract class LargeMapScreenMixin extends BaseScreen {
     @Inject(method = "addWidgets", at = @At(value = "TAIL"))
     private void gtceu$injectAddWidgets(CallbackInfo ci) {
         gtceu$injectedWidgets.clear();
+        var prefix = "gtceu.button.";
         for (var button : ButtonState.getAllButtons()) {
             var icon = switch (button.name) {
                 case "ore_veins" -> ItemIcon.getItemIcon(Items.RAW_IRON);
                 case "bedrock_fluids" -> ItemIcon.getItemIcon(Items.BUCKET);
                 default -> Icons.INFO;
             };
-            var buttonWidget = new SimpleButton(this, Component.translatable("gtceu.button." + button.name),
+            var buttonWidget = new SimpleButton(this, Component.translatable(prefix + button.name),
                     icon, (b, m) -> {
                         ButtonState.toggleButton(button);
                         refreshWidgets();
@@ -43,6 +46,19 @@ public abstract class LargeMapScreenMixin extends BaseScreen {
             add(buttonWidget);
             gtceu$injectedWidgets.add(buttonWidget);
         }
+        var hideDepletedButton = new SimpleButton(this, Component.translatable("gtceu.button.hide_depleted"),
+                ItemIcon.getItemIcon(Items.SPYGLASS), (b, m) -> {
+                    FTBChunksOptions.toggleLayer("hide_depleted", !FTBChunksOptions.showLayer("hide_depleted"));
+                }) {
+
+            @Override
+            public void addMouseOverText(TooltipList list) {
+                var lang = prefix + (FTBChunksOptions.hideDepleted() ? "show_depleted" : "hide_depleted");
+                list.add(Component.translatable(lang));
+            }
+        };
+        add(hideDepletedButton);
+        gtceu$injectedWidgets.add(hideDepletedButton);
     }
 
     @Inject(method = "alignWidgets", at = @At(value = "TAIL"))
