e707f51f080714417ae6f617af9598056bc289b6
omergunr100 <omer.er10@gmail.com>
2024-12-31 02:42:41 +0200

    replaced the set with a fastutils array list

--- src/main/java/com/gregtechceu/gtceu/api/item/tool/TreeFellingHelper.java
+++ src/main/java/com/gregtechceu/gtceu/api/item/tool/TreeFellingHelper.java
@@ -14,6 +14,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 
 import java.util.*;
@@ -29,7 +30,7 @@ public class TreeFellingHelper {
     private final Deque<BlockPos> orderedBlocks;
     private int tick;
 
-    public static final Set<TreeFellingHelper> helpers = new ObjectOpenHashSet<>();
+    public static final List<TreeFellingHelper> helpers = ObjectArrayList.of();
 
     private TreeFellingHelper(ServerPlayer player, ItemStack tool, Deque<BlockPos> orderedBlocks) {
         this.player = player;
