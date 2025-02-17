0e0a7f19ff9882610f3d7633248af847293485f1
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2025-02-17 20:55:15 +0000

    Requested changes

--- src/main/java/com/gregtechceu/gtceu/common/recipe/condition/DaytimeCondition.java
+++ src/main/java/com/gregtechceu/gtceu/common/recipe/condition/DaytimeCondition.java
@@ -6,12 +6,9 @@ import com.gregtechceu.gtceu.api.recipe.RecipeCondition;
 import com.gregtechceu.gtceu.api.recipe.condition.RecipeConditionType;
 import com.gregtechceu.gtceu.common.data.GTRecipeConditions;
 
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
-import net.minecraft.util.GsonHelper;
 import net.minecraft.world.level.Level;
 
-import com.google.gson.JsonObject;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import lombok.Getter;
@@ -25,9 +22,6 @@ public class DaytimeCondition extends RecipeCondition {
     public static final Codec<DaytimeCondition> CODEC = RecordCodecBuilder
             .create(instance -> RecipeCondition.isReverse(instance)
                     .apply(instance, DaytimeCondition::new));
-
-    public final static DaytimeCondition INSTANCE = new DaytimeCondition();
-    private float level;
 
     public DaytimeCondition(boolean isReverse) {
         super(isReverse);
@@ -56,33 +50,5 @@ public class DaytimeCondition extends RecipeCondition {
     @Override
     public RecipeCondition createTemplate() {
         return new DaytimeCondition();
-    }
-
-    @NotNull
-    @Override
-    public JsonObject serialize() {
-        JsonObject config = super.serialize();
-        config.addProperty("level", level);
-        return config;
-    }
-
-    @Override
-    public RecipeCondition deserialize(@NotNull JsonObject config) {
-        super.deserialize(config);
-        level = GsonHelper.getAsFloat(config, "level", 0);
-        return this;
-    }
-
-    @Override
-    public RecipeCondition fromNetwork(FriendlyByteBuf buf) {
-        super.fromNetwork(buf);
-        level = buf.readFloat();
-        return this;
-    }
-
-    @Override
-    public void toNetwork(FriendlyByteBuf buf) {
-        super.toNetwork(buf);
-        buf.writeFloat(level);
     }
 }
