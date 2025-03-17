a5b2ed6d42ed067a308a4f1aef0cdefb6e2c33f0
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2025-03-17 17:49:12 +0000

    Where were you when the last remnant of Create compat was kil

--- src/main/java/com/gregtechceu/gtceu/data/recipe/CustomTags.java
+++ src/main/java/com/gregtechceu/gtceu/data/recipe/CustomTags.java
@@ -155,8 +155,6 @@ public class CustomTags {
     public static final TagKey<Block> ENDSTONE_ORE_REPLACEABLES = TagUtil.createBlockTag("end_stone_ore_replaceables");
     public static final TagKey<Block> CONCRETE_BLOCK = TagUtil.createBlockTag("concretes");
     public static final TagKey<Block> CONCRETE_POWDER_BLOCK = TagUtil.createBlockTag("concrete_powders");
-    public static final TagKey<Block> CREATE_SEATS = TagUtil.optionalTag(Registries.BLOCK,
-            new ResourceLocation(GTValues.MODID_CREATE, "seats"));
     public static final TagKey<Block> CLEANROOM_FLOORS = TagUtil.createModBlockTag("cleanroom_floors");
 
     public static final TagKey<Biome> IS_SWAMP = TagUtil.createTag(Registries.BIOME, "is_swamp", false);
