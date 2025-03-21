db77cb88acdd8f5f7d803febce990c5c49230a9f
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2025-03-21 15:04:43 -0400

    add centrifuge recipes for energium and lapotron

--- src/main/java/com/gregtechceu/gtceu/data/recipe/misc/BatteryRecipes.java
+++ src/main/java/com/gregtechceu/gtceu/data/recipe/misc/BatteryRecipes.java
@@ -260,6 +260,12 @@ public class BatteryRecipes {
                 .circuitMeta(1)
                 .outputItems(ENERGIUM_DUST, 9)
                 .duration(600).EUt(VA[MV]).save(provider);
+        
+        CENTRIFUGE_RECIPES.recipeBuilder("centrifuge_energium_dust")
+            .inputItems(ENERGIUM_DUST, 9)
+            .outputItems(dust, Redstone, 5)
+            .outputItems(dust, Ruby, 4)
+            .duration(1200).EUt(VA[ULV]).save(provider);
 
         AUTOCLAVE_RECIPES.recipeBuilder("energy_crystal_water")
                 .inputItems(ENERGIUM_DUST, 9)
@@ -297,6 +303,12 @@ public class BatteryRecipes {
                 .circuitMeta(2)
                 .outputItems(dust, Lapotron, 5)
                 .duration(200).EUt(VA[HV]).save(provider);
+        
+        CENTRIFUGE_RECIPES.recipeBuilder("centrifuge_lapotron_dust")
+            .inputItems(dust, Lapotron, 5)
+            .outputItems(ENERGIUM_DUST, 3)
+            .outputItems(dust, Lapis, 2)
+            .duration(800).EUt(VA[ULV]).save(provider);
 
         AUTOCLAVE_RECIPES.recipeBuilder("lapotron_gem_water")
                 .inputItems(dust, Lapotron, 15)
