a87815c42cd9725ac8dca6b71969a7eeca09d3ce
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2025-04-17 20:52:37 -0600

    refactor a bit

--- src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/electric/gcym/LargeMacerationTowerMachine.java
+++ src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/electric/gcym/LargeMacerationTowerMachine.java
@@ -6,16 +6,14 @@ import com.gregtechceu.gtceu.api.capability.recipe.ItemRecipeCapability;
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 import com.gregtechceu.gtceu.api.machine.multiblock.WorkableElectricMultiblockMachine;
 import com.gregtechceu.gtceu.api.machine.trait.NotifiableItemStackHandler;
+import com.gregtechceu.gtceu.api.machine.trait.RecipeLogic;
 import com.gregtechceu.gtceu.api.pattern.util.RelativeDirection;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.phys.AABB;
 import net.minecraftforge.items.ItemHandlerHelper;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Collections;
 import java.util.List;
@@ -24,8 +22,8 @@ import java.util.Objects;
 public class LargeMacerationTowerMachine extends WorkableElectricMultiblockMachine {
 
     private AABB grindBound;
-    @Nullable
-    private List<NotifiableItemStackHandler> holders = null;
+    @NotNull
+    private List<NotifiableItemStackHandler> holders = List.of();
 
     public LargeMacerationTowerMachine(IMachineBlockEntity holder) {
         super(holder);
@@ -38,52 +36,43 @@ public class LargeMacerationTowerMachine extends WorkableElectricMultiblockMachi
         super.onStructureFormed();
         updateBounds();
         holders = Objects
-                .requireNonNullElseGet(getCapabilitiesProxy().get(IO.IN, ItemRecipeCapability.CAP),
+                .requireNonNullElseGet(getCapabilitiesFlat(IO.IN, ItemRecipeCapability.CAP),
                         Collections::<IRecipeHandler<?>>emptyList)
                 .stream()
-                .filter(handler -> !handler.isProxy())
                 .filter(NotifiableItemStackHandler.class::isInstance)
                 .map(NotifiableItemStackHandler.class::cast)
                 .toList();
-    }
 
-    @Override
-    public boolean onWorking() {
-        hurtEntities();
-        return super.onWorking();
+        subscribeServerTick(this::hurtEntities);
     }
 
     @Override
     public void onLoad() {
         super.onLoad();
         updateBounds();
+
+        subscribeServerTick(this::hurtEntities);
     }
 
     private void updateBounds() {
-        Direction up = RelativeDirection.UP.getRelativeFacing(getFrontFacing(), getUpwardsFacing(), isFlipped());
-        Direction back = getFrontFacing().getOpposite();
-        Direction clockWise = getFrontFacing().getClockWise(up.getAxis());
-        Direction counterClockWise = getFrontFacing().getCounterClockWise(up.getAxis());
-
-        BlockPos middlePos = self().getPos()
-                .offset(back.getNormal().multiply(2))
-                .offset(up.getNormal().multiply(2));
-        var fl = middlePos.offset(getFrontFacing().getNormal()).offset(clockWise.getNormal().multiply(2))
-                .offset(up.getOpposite().getNormal());
-        var br = middlePos.offset(back.getNormal().multiply(2)).offset(counterClockWise.getNormal());
+        var fl = RelativeDirection.offsetPos(getPos(), getFrontFacing(), getUpwardsFacing(), isFlipped(), 1, 1, -1);
+        var br = RelativeDirection.offsetPos(getPos(), getFrontFacing(), getUpwardsFacing(), isFlipped(), 2, -2, -4);
         grindBound = new AABB(fl, br);
     }
 
     private void hurtEntities() {
         if (isRemote() || getLevel() == null) return;
-        getLevel().getEntities(null, grindBound).forEach(
-                e -> e.hurt(e.damageSources().cramming(), 2.0f));
 
+        if (recipeLogic.getStatus() == RecipeLogic.Status.WORKING) {
+            getLevel().getEntities(null, grindBound).forEach(
+                    e -> e.hurt(e.damageSources().cramming(), 2.0f));
+        }
+
+        if (holders.isEmpty()) return;
 
         List<ItemEntity> items = getLevel().getEntitiesOfClass(ItemEntity.class, grindBound);
         for (ItemEntity item : items) {
-            if (item.isRemoved() || holder == null) continue;
-            assert holders != null;
+            if (item.isRemoved()) continue;
             for (var holder : holders) {
                 item.setItem(ItemHandlerHelper.insertItem(holder, item.getItem(), false));
                 if (item.getItem().isEmpty()) {
