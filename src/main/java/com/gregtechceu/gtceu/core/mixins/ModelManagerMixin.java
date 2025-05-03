565301d0072152038b23eafa2c792f0b32fe4d5f
screret <68943070+screret@users.noreply.github.com>
2025-05-03 14:19:50 +0300

    fix GT trying to load models after game loading error crashing the game

--- src/main/java/com/gregtechceu/gtceu/core/mixins/ModelManagerMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/ModelManagerMixin.java
@@ -13,6 +13,7 @@ import net.minecraft.server.packs.resources.PreparableReloadListener;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.profiling.ProfilerFiller;
 
+import net.neoforged.fml.ModLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -29,6 +30,11 @@ public abstract class ModelManagerMixin {
                                          ResourceManager resourceManager, ProfilerFiller preparationsProfiler,
                                          ProfilerFiller reloadProfiler, Executor backgroundExecutor,
                                          Executor gameExecutor, CallbackInfoReturnable<CompletableFuture<Void>> cir) {
+        if (ModLoader.hasErrors()) {
+            GTCEu.LOGGER.warn("GregTech Model loading CANCELLED because loading errors have been encountered");
+            return;
+        }
+
         long startTime = System.currentTimeMillis();
         // turns out these do have to be init in here after all, as they check for asset existence. whoops.
         MaterialBlockRenderer.reinitModels();
