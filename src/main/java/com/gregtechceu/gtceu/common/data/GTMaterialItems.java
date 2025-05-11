3062af6f5393a20b7d9c2188b49749f4741dcdce
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2025-05-10 18:40:11 -0600

    oops

--- src/main/java/com/gregtechceu/gtceu/common/data/GTMaterialItems.java
+++ src/main/java/com/gregtechceu/gtceu/common/data/GTMaterialItems.java
@@ -1,7 +1,6 @@
 package com.gregtechceu.gtceu.common.data;
 
 import com.gregtechceu.gtceu.api.GTCEuAPI;
-import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.api.data.chemical.material.Material;
 import com.gregtechceu.gtceu.api.data.chemical.material.properties.PropertyKey;
 import com.gregtechceu.gtceu.api.data.chemical.material.registry.MaterialRegistry;
@@ -22,11 +21,7 @@ import com.tterrag.registrate.providers.ProviderType;
 import com.tterrag.registrate.util.entry.ItemEntry;
 import com.tterrag.registrate.util.entry.ItemProviderEntry;
 import com.tterrag.registrate.util.nullness.NonNullBiConsumer;
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
@@ -58,66 +53,8 @@ public class GTMaterialItems {
                     .toList(),
             GTToolType.getTypes().values().stream().toList());
 
-    static FileWriter MaterialCSV;
-    static FileWriter TagPrefixCSV;
-    static FileWriter MTPCSV;
-
     // Material Items
     public static void generateMaterialItems() {
-        try {
-            MaterialCSV = new FileWriter("material.csv");
-            TagPrefixCSV = new FileWriter("tagprefix.csv");
-            MTPCSV = new FileWriter("mtp.csv");
-            String matColumns = "id,material_name,molar_mass,price_per_pound\n";
-            String tagPrefixColumns = "id,tag_prefix,material_amount\n";
-            MaterialCSV.write(matColumns);
-            TagPrefixCSV.write(tagPrefixColumns);
-        } catch (IOException ignored) {
-
-        }
-
-        int id = 1;
-        Object2IntMap<Material> materialIDs = new Object2IntOpenHashMap<>();
-        for (var registry : GTCEuAPI.materialManager.getRegistries()) {
-            for (var mat : registry.getAllMaterials()) {
-                float price = GTValues.RNG.nextFloat() * 160.0f;
-                try {
-                    StringBuffer s = new StringBuffer();
-                    String[] strings = mat.getResourceLocation().getPath().split("_");
-                    String name = "";
-                    for (var st : strings) {
-                        name += st.substring(0, 1).toUpperCase() + st.substring(1);
-                    }
-                    materialIDs.put(mat, id);
-                    s.append(id).append(",").append(name).append(",").append(mat.getMass()).append(",")
-                            .append(String.format("%.2f", price)).append('\n');
-                    MaterialCSV.write(s.toString());
-                } catch (IOException ignored) {}
-                id++;
-            }
-        }
-
-        id = 1;
-        Object2IntMap<TagPrefix> tagIDs = new Object2IntOpenHashMap<>();
-        for (var tagPrefix : TagPrefix.values()) {
-            if (tagPrefix.materialAmount() == -1) continue;
-            try {
-                StringBuffer s = new StringBuffer();
-                tagIDs.put(tagPrefix, id);
-                s.append(id).append(",").append(tagPrefix.name).append(',')
-                        .append(String.format("%.4f", tagPrefix.materialAmount() / (float) GTValues.M)).append('\n');
-                TagPrefixCSV.write(s.toString());
-            } catch (IOException ignored) {}
-            id++;
-        }
-        var fluidTag = new TagPrefix("fluid");
-        tagIDs.put(fluidTag, tagIDs.size());
-
-        try {
-            MaterialCSV.close();
-            TagPrefixCSV.close();
-        } catch (IOException ignored) {}
-
         REGISTRATE.creativeModeTab(() -> MATERIAL_ITEM);
         for (var tagPrefix : TagPrefix.values()) {
             if (tagPrefix.doGenerateItem()) {
@@ -125,37 +62,13 @@ public class GTMaterialItems {
                     GTRegistrate registrate = registry.getRegistrate();
                     for (Material material : registry.getAllMaterials()) {
                         if (tagPrefix.doGenerateItem(material)) {
-                            if (tagPrefix.materialAmount() != -1) {
-                                StringBuilder s = new StringBuilder();
-                                s.append(materialIDs.getInt(material)).append(",").append(tagIDs.getInt(tagPrefix))
-                                        .append("\n");
-                                try {
-                                    MTPCSV.write(s.toString());
-                                } catch (IOException ignored) {}
-                            }
+
                             generateMaterialItem(tagPrefix, material, registrate);
                         }
                     }
                 }
             }
         }
-
-        for (MaterialRegistry registry : GTCEuAPI.materialManager.getRegistries()) {
-            for (Material material : registry.getAllMaterials()) {
-                if (material.hasProperty(PropertyKey.FLUID)) {
-                    StringBuilder s = new StringBuilder();
-                    s.append(materialIDs.getInt(material)).append(",").append(tagIDs.getInt(fluidTag)).append("\n");
-                    try {
-                        MTPCSV.write(s.toString());
-                    } catch (IOException ignored) {}
-                }
-            }
-        }
-
-        try {
-            MTPCSV.close();
-        } catch (IOException ignored) {}
-
         MATERIAL_ITEMS = MATERIAL_ITEMS_BUILDER.build();
     }
 
