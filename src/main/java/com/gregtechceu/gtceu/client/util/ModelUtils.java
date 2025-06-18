a721b6644f3d28bfa39961aefe4ac26afce8782f
screret <68943070+screret@users.noreply.github.com>
2025-06-18 20:00:34 +0300

    remove CTM dependency

--- src/main/java/com/gregtechceu/gtceu/client/util/ModelUtils.java
+++ src/main/java/com/gregtechceu/gtceu/client/util/ModelUtils.java
@@ -1,5 +1,8 @@
 package com.gregtechceu.gtceu.client.util;
 
+import com.gregtechceu.gtceu.GTCEu;
+import it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet;
+import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.BlockModel;
@@ -11,34 +14,59 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ModelEvent;
+import net.minecraftforge.client.event.ModelEvent.ModifyBakingResult;
+import net.minecraftforge.client.model.IQuadTransformer;
+import net.minecraftforge.client.model.QuadTransformers;
 import net.minecraftforge.client.model.data.ModelData;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import team.chisel.ctm.client.util.Quad;
 
 import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
+@Mod.EventBusSubscriber(modid = GTCEu.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ModelUtils {
 
     private ModelUtils() {}
+
+    private static final Set<Consumer<ModifyBakingResult>> BAKE_EVENT_LISTENERS = new ReferenceOpenHashSet<>();
+
+    private static final Function<Float, IQuadTransformer> OFFSET_BY = Util.memoize(by -> {
+        if (by == 0.0f) return QuadTransformers.empty();
+
+        return quad -> {
+            var vertices = quad.getVertices();
+            Direction direction = quad.getDirection();
+
+            for (int i = 0; i < 4; i++) {
+                int offset = i * IQuadTransformer.STRIDE + IQuadTransformer.POSITION;
+                float x = Float.intBitsToFloat(vertices[offset]);
+                float y = Float.intBitsToFloat(vertices[offset + 1]);
+                float z = Float.intBitsToFloat(vertices[offset + 2]);
+                x += by * direction.getStepX();
+                y += by * direction.getStepY();
+                z += by * direction.getStepZ();
+
+                vertices[offset] = Float.floatToRawIntBits(x);
+                vertices[offset + 1] = Float.floatToRawIntBits(y);
+                vertices[offset + 2] = Float.floatToRawIntBits(z);
+            }
+        };
+    });
 
     public static List<BakedQuad> getBakedModelQuads(BakedModel model, BlockAndTintGetter level, BlockPos pos,
                                                      BlockState state, Direction side, RandomSource rand) {
         return model.getQuads(state, side, rand, model.getModelData(level, pos, state, ModelData.EMPTY), null);
     }
 
-    public static BakedQuad offsetQuad(BakedQuad baked, float by) {
-        Quad quad = Quad.from(baked);
-        Direction quadOrientation = baked.getDirection();
-
-        float xOffset = by * quadOrientation.getStepX();
-        float yOffset = by * quadOrientation.getStepY();
-        float zOffset = by * quadOrientation.getStepZ();
-        for (int v = 0; v < 4; v++) {
-            quad = quad.withVert(v, quad.getVert(v).add(xOffset, yOffset, zOffset));
-        }
-        return quad.rebake();
+    public static BakedQuad offsetQuad(BakedQuad quad, float by) {
+        return OFFSET_BY.apply(by).process(quad);
     }
 
     public static ModelManager getModelManager() {
@@ -95,6 +123,17 @@ public class ModelUtils {
             case WEST -> BlockModelRotation.X0_Y270;
             case EAST -> BlockModelRotation.X0_Y90;
         };
+    }
+
+    public static void registerBakeEventListener(Consumer<ModifyBakingResult> consumer) {
+        BAKE_EVENT_LISTENERS.add(consumer);
+    }
+
+    @SubscribeEvent
+    public static void onModifyBakingResult(ModelEvent.ModifyBakingResult event) {
+        for (var consumer : BAKE_EVENT_LISTENERS) {
+            consumer.accept(event);
+        }
     }
 
 }
