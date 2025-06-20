9b9384d8d8848fa0af52ea329e1d7c5f9ce965de
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2025-06-20 10:33:38 +0100

    Misc integration lang stuff

--- src/main/java/com/gregtechceu/gtceu/data/lang/IntegrationLang.java
+++ src/main/java/com/gregtechceu/gtceu/data/lang/IntegrationLang.java
@@ -114,6 +114,7 @@ public class IntegrationLang {
         provider.add("gtceu.top.progress_tick", " / %s t");
     }
 
+    /** Xaero, JourneyMap*/
     private static void initMinimapLang(RegistrateLangProvider provider) {
         provider.add("gtceu.minimap.ore_vein.depleted", "Depleted");
 
@@ -122,12 +123,13 @@ public class IntegrationLang {
         provider.add("button.gtceu.mark_as_depleted.name", "Mark as Depleted");
         provider.add("button.gtceu.toggle_waypoint.name", "Toggle Waypoint");
 
-        provider.add("gtceu.journeymap.options.layers", "Prospection layers");
+        provider.add("gtceu.journeymap.options.layers", "Prospection Layers");
         provider.add("gtceu.journeymap.options.layers.ore_veins", "Show Ore Veins");
         provider.add("gtceu.journeymap.options.layers.bedrock_fluids", "Show Bedrock Fluid Veins");
         provider.add("gtceu.journeymap.options.layers.hide_depleted", "Hide Depleted Veins");
     }
 
+    /** FTB Teams, Argonauts*/
     private static void initOwnershipLang(RegistrateLangProvider provider) {
         provider.add("gtceu.ownership.name.player", "Player");
         provider.add("gtceu.ownership.name.ftb", "FTB Teams");
