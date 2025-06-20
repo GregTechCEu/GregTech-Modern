5153b6e3ae24cb76888894a26a60277285befe92
screret <68943070+screret@users.noreply.github.com>
2025-06-20 18:23:02 +0300

    LD endpoints don't need to be tiered hull models

--- src/main/java/com/gregtechceu/gtceu/common/data/GTMachines.java
+++ src/main/java/com/gregtechceu/gtceu/common/data/GTMachines.java
@@ -304,7 +304,7 @@ public class GTMachines {
             .langValue("Long Distance Item Pipeline Endpoint")
             .rotationState(RotationState.ALL)
             .tier(LV)
-            .tieredHullModel(GTCEu.id("block/machine/long_distance_item_pipeline_endpoint"))
+            .simpleModel(GTCEu.id("block/machine/long_distance_item_pipeline_endpoint"))
             .tooltips(LangHandler.getMultiLang("gtceu.machine.endpoint.tooltip"))
             .tooltipBuilder((stack, tooltip) -> {
                 if (ConfigHolder.INSTANCE.machines.ldItemPipeMinDistance > 0) {
@@ -319,7 +319,7 @@ public class GTMachines {
             .langValue("Long Distance Fluid Pipeline Endpoint")
             .rotationState(RotationState.ALL)
             .tier(LV)
-            .tieredHullModel(GTCEu.id("block/machine/long_distance_fluid_pipeline_endpoint"))
+            .simpleModel(GTCEu.id("block/machine/long_distance_fluid_pipeline_endpoint"))
             .tooltips(Component.translatable("gtceu.machine.endpoint.tooltip.0"),
                     Component.translatable("gtceu.machine.endpoint.tooltip.1"),
                     Component.translatable("gtceu.machine.endpoint.tooltip.2"))
