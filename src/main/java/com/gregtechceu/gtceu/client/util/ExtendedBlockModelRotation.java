973fedf5315e5ac4446ca99152f73e9cfc45267b
screret <68943070+screret@users.noreply.github.com>
2025-06-23 22:22:25 +0300

    fix extended rotations having south's and west's Z rotation flipped

--- src/main/java/com/gregtechceu/gtceu/client/util/ExtendedBlockModelRotation.java
+++ src/main/java/com/gregtechceu/gtceu/client/util/ExtendedBlockModelRotation.java
@@ -16,33 +16,33 @@ import lombok.Getter;
 public enum ExtendedBlockModelRotation {
 
     // DUNSWE -> SWNE
-    DOWN_SOUTH(90, 0, 270),
-    DOWN_WEST(90, 0, 180),
+    DOWN_SOUTH(90, 0, 180),
+    DOWN_WEST(90, 0, 270),
     DOWN_NORTH(90, 0, 0),
     DOWN_EAST(90, 0, 90),
 
-    UP_SOUTH(270, 0, 90),
-    UP_WEST(270, 0, 0),
+    UP_SOUTH(270, 0, 0),
+    UP_WEST(270, 0, 90),
     UP_NORTH(270, 0, 180),
     UP_EAST(270, 0, 270),
 
-    NORTH_SOUTH(0, 0, 270),
-    NORTH_WEST(0, 0, 180),
+    NORTH_SOUTH(0, 0, 180),
+    NORTH_WEST(0, 0, 270),
     NORTH_NORTH(0, 0, 0), // Default,
     NORTH_EAST(0, 0, 90),
 
-    SOUTH_SOUTH(0, 180, 90),
-    SOUTH_WEST(0, 180, 180),
+    SOUTH_SOUTH(0, 180, 180),
+    SOUTH_WEST(0, 180, 270),
     SOUTH_NORTH(0, 180, 0),
-    SOUTH_EAST(0, 180, 270),
+    SOUTH_EAST(0, 180, 90),
 
-    WEST_SOUTH(0, 270, 270),
-    WEST_WEST(0, 270, 180),
+    WEST_SOUTH(0, 270, 180),
+    WEST_WEST(0, 270, 270),
     WEST_NORTH(0, 270, 0),
     WEST_EAST(0, 270, 90),
 
-    EAST_SOUTH(0, 90, 270),
-    EAST_WEST(0, 90, 180),
+    EAST_SOUTH(0, 90, 180),
+    EAST_WEST(0, 90, 270),
     EAST_NORTH(0, 90, 0),
     EAST_EAST(0, 90, 90);
 
