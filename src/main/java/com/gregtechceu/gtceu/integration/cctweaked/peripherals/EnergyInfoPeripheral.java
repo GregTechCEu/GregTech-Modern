898348be2c4683e6dcce5bd6bbc0bb90777579ee
Matthias Mann <MatthiasMann@gmx.de>
2025-07-03 09:33:41 +0200

    adjust style as requested by review

--- src/main/java/com/gregtechceu/gtceu/integration/cctweaked/peripherals/EnergyInfoPeripheral.java
+++ src/main/java/com/gregtechceu/gtceu/integration/cctweaked/peripherals/EnergyInfoPeripheral.java
@@ -6,8 +6,6 @@ import dan200.computercraft.api.lua.LuaFunction;
 import dan200.computercraft.api.lua.MethodResult;
 import dan200.computercraft.api.peripheral.GenericPeripheral;
 
-import java.math.BigInteger;
-
 public class EnergyInfoPeripheral implements GenericPeripheral {
 
     public String id() {
@@ -15,18 +13,12 @@ public class EnergyInfoPeripheral implements GenericPeripheral {
     }
 
     @LuaFunction
-    static public MethodResult getEnergyStored(IEnergyInfoProvider infoProvider) {
-        return toResult(infoProvider.getEnergyInfo().stored());
+    public static MethodResult getEnergyStored(IEnergyInfoProvider infoProvider) {
+        return MethodResult.of(infoProvider.getEnergyInfo().stored());
     }
 
     @LuaFunction
-    static public MethodResult getEnergyCapacity(IEnergyInfoProvider infoProvider) {
-        return toResult(infoProvider.getEnergyInfo().capacity());
-    }
-
-    private static BigInteger MAX_LONG = BigInteger.valueOf(Long.MAX_VALUE);
-
-    private static MethodResult toResult(BigInteger val) {
-        return MethodResult.of(val);
+    public static MethodResult getEnergyCapacity(IEnergyInfoProvider infoProvider) {
+        return MethodResult.of(infoProvider.getEnergyInfo().capacity());
     }
 }
