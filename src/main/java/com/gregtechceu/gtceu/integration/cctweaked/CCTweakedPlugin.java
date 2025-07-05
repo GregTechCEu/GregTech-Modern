780c86d3842803599f8445c4eb8c7d7a038db828
Matthias Mann <MatthiasMann@gmx.de>
2025-07-05 16:53:35 +0200

    add CC support for IControllable and IWorkable

--- src/main/java/com/gregtechceu/gtceu/integration/cctweaked/CCTweakedPlugin.java
+++ src/main/java/com/gregtechceu/gtceu/integration/cctweaked/CCTweakedPlugin.java
@@ -9,7 +9,11 @@ import dan200.computercraft.api.ForgeComputerCraftAPI;
 public class CCTweakedPlugin {
 
     public static void init() {
+        ComputerCraftAPI.registerGenericSource(new ControllablePeripheral());
         ComputerCraftAPI.registerGenericSource(new EnergyInfoPeripheral());
+        ComputerCraftAPI.registerGenericSource(new WorkablePeripheral());
+        ForgeComputerCraftAPI.registerGenericCapability(GTCapability.CAPABILITY_CONTROLLABLE);
         ForgeComputerCraftAPI.registerGenericCapability(GTCapability.CAPABILITY_ENERGY_INFO_PROVIDER);
+        ForgeComputerCraftAPI.registerGenericCapability(GTCapability.CAPABILITY_WORKABLE);
     }
 }
---
+++ src/main/java/com/gregtechceu/gtceu/integration/cctweaked/peripherals/ControllablePeripheral.java
@@ -0,0 +1,31 @@
+package com.gregtechceu.gtceu.integration.cctweaked.peripherals;
+
+import com.gregtechceu.gtceu.api.capability.IControllable;
+
+import dan200.computercraft.api.lua.LuaFunction;
+import dan200.computercraft.api.lua.MethodResult;
+import dan200.computercraft.api.peripheral.GenericPeripheral;
+
+public class ControllablePeripheral implements GenericPeripheral {
+
+    public String id() {
+        return "gtceu:workable";
+    }
+
+    @LuaFunction
+    public static MethodResult isWorkingEnabled(IControllable controllable) {
+        return MethodResult.of(controllable.isWorkingEnabled());
+    }
+
+    @LuaFunction
+    public static MethodResult setWorkingEnabled(IControllable controllable, boolean enabled) {
+        boolean wasEnabled = controllable.isWorkingEnabled();
+        controllable.setWorkingEnabled(enabled);
+        return MethodResult.of(wasEnabled);
+    }
+
+    @LuaFunction
+    public static void setSuspendAfterFinish(IControllable controllable, boolean suspendAfterFinish) {
+        controllable.setSuspendAfterFinish(suspendAfterFinish);
+    }
+}
