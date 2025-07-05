505c57b4fe9b2d402e52a8c770b19fc11ec2864d
screret <68943070+screret@users.noreply.github.com>
2025-07-05 22:01:28 +0300

    make active block updates not run neighbor updates

--- src/main/java/com/gregtechceu/gtceu/api/block/ActiveBlock.java
+++ src/main/java/com/gregtechceu/gtceu/api/block/ActiveBlock.java
@@ -8,6 +8,9 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.Property;
+
+import org.jetbrains.annotations.ApiStatus;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
@@ -28,6 +31,10 @@ public class ActiveBlock extends AppearanceBlock {
         builder.add(ACTIVE);
     }
 
+    /**
+     * @deprecated Use {@link BlockState#setValue(Property, Comparable)}
+     */
+    @ApiStatus.Obsolete(since = "7.0.0")
     public BlockState changeActive(BlockState state, boolean active) {
         if (state.is(this)) {
             return state.setValue(ACTIVE, active);
@@ -35,6 +42,10 @@ public class ActiveBlock extends AppearanceBlock {
         return state;
     }
 
+    /**
+     * @deprecated Use {@link BlockState#getValue(Property)}
+     */
+    @ApiStatus.Obsolete(since = "7.0.0")
     public boolean isActive(BlockState state) {
         return state.getValue(ACTIVE);
     }
