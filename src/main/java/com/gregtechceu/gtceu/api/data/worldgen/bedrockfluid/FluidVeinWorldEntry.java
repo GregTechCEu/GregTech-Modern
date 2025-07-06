edbff6c2c9f35f96f98628f8fabd3334018952b2
Matthias Mann <MatthiasMann@gmx.de>
2025-07-06 16:49:22 +0200

    use MemoizedSupplier for BedrockFluidDefinition

--- src/main/java/com/gregtechceu/gtceu/api/data/worldgen/bedrockfluid/FluidVeinWorldEntry.java
+++ src/main/java/com/gregtechceu/gtceu/api/data/worldgen/bedrockfluid/FluidVeinWorldEntry.java
@@ -2,6 +2,8 @@ package com.gregtechceu.gtceu.api.data.worldgen.bedrockfluid;
 
 import com.gregtechceu.gtceu.api.registry.GTRegistries;
 import com.gregtechceu.gtceu.config.ConfigHolder;
+import com.gregtechceu.gtceu.utils.memoization.GTMemoizer;
+import com.gregtechceu.gtceu.utils.memoization.MemoizedSupplier;
 
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
@@ -12,24 +14,38 @@ import org.jetbrains.annotations.Nullable;
 
 public class FluidVeinWorldEntry {
 
+    private final MemoizedSupplier<BedrockFluidDefinition> vein;
     @Nullable
     @Getter
-    private BedrockFluidDefinition vein;
+    private String vein_id;
     @Getter
     private int fluidYield;
     @Getter
     private int operationsRemaining;
 
     public FluidVeinWorldEntry(@Nullable BedrockFluidDefinition vein, int fluidYield, int operationsRemaining) {
-        this.vein = vein;
+        this(GTMemoizer.memoize(() -> vein));
+        if (vein != null) {
+            ResourceLocation key = GTRegistries.BEDROCK_FLUID_DEFINITIONS.getKey(vein);
+            if (key != null)
+                this.vein_id = key.toString();
+        }
         this.fluidYield = fluidYield;
         this.operationsRemaining = operationsRemaining;
     }
 
-    private FluidVeinWorldEntry() {}
+    private FluidVeinWorldEntry(MemoizedSupplier<BedrockFluidDefinition> vein) {
+        this.vein = vein;
+    }
 
+    @Nullable
+    public BedrockFluidDefinition getVein() {
+        return this.vein.get();
+    }
+
+    @Nullable
     public BedrockFluidDefinition getDefinition() {
-        return this.vein;
+        return this.vein.get();
     }
 
     @SuppressWarnings("unused")
@@ -46,24 +62,32 @@ public class FluidVeinWorldEntry {
         var tag = new CompoundTag();
         tag.putInt("fluidYield", fluidYield);
         tag.putInt("operationsRemaining", operationsRemaining);
-        if (vein != null && GTRegistries.BEDROCK_FLUID_DEFINITIONS.getKey(vein) != null) {
-            tag.putString("vein", GTRegistries.BEDROCK_FLUID_DEFINITIONS.getKey(vein).toString());
+        if (vein_id != null) {
+            tag.putString("vein", vein_id);
         }
         return tag;
     }
 
     @NotNull
     public static FluidVeinWorldEntry readFromNBT(@NotNull CompoundTag tag) {
-        FluidVeinWorldEntry info = new FluidVeinWorldEntry();
-        info.fluidYield = tag.getInt("fluidYield");
-        info.operationsRemaining = tag.getInt("operationsRemaining");
+        String vein_id;
+        MemoizedSupplier<BedrockFluidDefinition> vein;
 
         if (tag.contains("vein")) {
-            ResourceLocation id = new ResourceLocation(tag.getString("vein"));
-            if (GTRegistries.BEDROCK_FLUID_DEFINITIONS.containKey(id)) {
-                info.vein = GTRegistries.BEDROCK_FLUID_DEFINITIONS.get(id);
-            }
+            vein_id = tag.getString("vein");
+            vein = GTMemoizer.memoize(() -> {
+                ResourceLocation key = new ResourceLocation(vein_id);
+                return GTRegistries.BEDROCK_FLUID_DEFINITIONS.get(key);
+            });
+        } else {
+            vein_id = null;
+            vein = GTMemoizer.memoize(() -> null);
         }
+
+        FluidVeinWorldEntry info = new FluidVeinWorldEntry(vein);
+        info.vein_id = vein_id;
+        info.fluidYield = tag.getInt("fluidYield");
+        info.operationsRemaining = tag.getInt("operationsRemaining");
         return info;
     }
 }
