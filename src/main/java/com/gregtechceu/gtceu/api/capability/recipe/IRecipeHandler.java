e41e623965b311dc751b8dfb7cc1409135e95811
jurrejelle <jurre@jilles.com>
2025-07-06 15:21:03 +0200

    depricate

--- src/main/java/com/gregtechceu/gtceu/api/capability/recipe/IRecipeHandler.java
+++ src/main/java/com/gregtechceu/gtceu/api/capability/recipe/IRecipeHandler.java
@@ -56,13 +56,13 @@ public interface IRecipeHandler<K> extends IFilteredHandler<K> {
     }
 
     /**
-     * Deprecated since all contents should be searched.
      * Returns {@code true} if this {@code IRecipeHandler} has content to be searched.
      * The main use of this is differentiating circuit inventories from item inventories
      * 
      * @return {@code true} if this {@code IRecipeHandler} has content to be searched
+     * @deprecated since all contents should be searched.
      */
-    @Deprecated(since = "7.0.0")
+    @Deprecated(since = "7.0.0", forRemoval = true)
     default boolean shouldSearchContent() {
         return true;
     }
