072c06534587b559941bca09dffea5863c6db576
Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>
2025-07-20 21:47:32 -0600

    add the controller as a recipe output for the multiblock preview to view on lcick

--- src/main/java/com/gregtechceu/gtceu/integration/emi/multipage/MultiblockInfoEmiRecipe.java
+++ src/main/java/com/gregtechceu/gtceu/integration/emi/multipage/MultiblockInfoEmiRecipe.java
@@ -7,9 +7,13 @@ import com.lowdragmc.lowdraglib.emi.ModularEmiRecipe;
 import com.lowdragmc.lowdraglib.gui.widget.WidgetGroup;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.stack.EmiStack;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MultiblockInfoEmiRecipe extends ModularEmiRecipe<WidgetGroup> {
 
@@ -28,5 +32,10 @@ public class MultiblockInfoEmiRecipe extends ModularEmiRecipe<WidgetGroup> {
     @Override
     public @Nullable ResourceLocation getId() {
         return definition.getId();
+    }
+
+    @Override
+    public List<EmiStack> getOutputs() {
+        return List.of(EmiStack.of(new ItemStack(definition.getItem())));
     }
 }
