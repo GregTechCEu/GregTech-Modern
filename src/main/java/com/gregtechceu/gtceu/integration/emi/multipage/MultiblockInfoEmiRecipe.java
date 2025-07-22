8b5e9fae47c4f5766756effff16b7f696fc940d2
Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>
2025-07-21 18:14:25 -0600

    oops added recipe tree support

--- src/main/java/com/gregtechceu/gtceu/integration/emi/multipage/MultiblockInfoEmiRecipe.java
+++ src/main/java/com/gregtechceu/gtceu/integration/emi/multipage/MultiblockInfoEmiRecipe.java
@@ -6,14 +6,12 @@ import com.gregtechceu.gtceu.api.machine.MultiblockMachineDefinition;
 import com.lowdragmc.lowdraglib.emi.ModularEmiRecipe;
 import com.lowdragmc.lowdraglib.gui.widget.WidgetGroup;
 
-import dev.emi.emi.api.widget.SlotWidget;
-import dev.emi.emi.api.widget.Widget;
-import dev.emi.emi.api.widget.WidgetHolder;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemStack;
 
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
 import dev.emi.emi.api.stack.EmiStack;
+import dev.emi.emi.api.widget.SlotWidget;
+import dev.emi.emi.api.widget.WidgetHolder;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -26,20 +24,20 @@ public class MultiblockInfoEmiRecipe extends ModularEmiRecipe<WidgetGroup> {
     public MultiblockInfoEmiRecipe(MultiblockMachineDefinition definition) {
         super(() -> PatternPreviewWidget.getPatternWidget(definition));
         this.definition = definition;
-
     }
-
 
     @Override
     public void addWidgets(WidgetHolder widgets) {
         super.addWidgets(widgets);
-        slotWidget = new SlotWidget(EmiStack.of(definition.getItem().asItem()), 0, 0)
+        // numbers gotten from the size of the uh
+        slotWidget = new SlotWidget(EmiStack.of(definition.getItem().asItem()), 138, 12)
                 .recipeContext(this)
                 .drawBack(false);
 
         widgets.add(slotWidget);
     }
-        @Override
+
+    @Override
     public EmiRecipeCategory getCategory() {
         return MultiblockInfoEmiCategory.CATEGORY;
     }
@@ -53,6 +51,4 @@ public class MultiblockInfoEmiRecipe extends ModularEmiRecipe<WidgetGroup> {
     public List<EmiStack> getOutputs() {
         return List.of(EmiStack.of(definition.getItem()));
     }
-
 }
-
