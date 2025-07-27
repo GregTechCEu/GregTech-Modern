abddc5df763a1f59431aaf0ca0c4024f96123072
Caedis <Caedis@users.noreply.github.com>
2025-07-27 14:39:07 -0500

    Remove test keybind

--- src/main/java/com/gregtechceu/gtceu/utils/input/SyncedKeyMappings.java
+++ src/main/java/com/gregtechceu/gtceu/utils/input/SyncedKeyMappings.java
@@ -9,11 +9,6 @@ import net.minecraftforge.fml.ModLoader;
 import com.mojang.blaze3d.platform.InputConstants;
 
 public class SyncedKeyMappings {
-
-    public static final SyncedKeyMapping ARMOR_MODE_SWITCH = SyncedKeyMapping
-            .createConfigurable("gtceu.key.armor_mode_switch_synctest", KeyConflictContext.IN_GAME,
-                    InputConstants.KEY_M);
-
     public static void init() {
         if (GTCEu.isClientSide()) {
             MinecraftForge.EVENT_BUS.register(SyncedKeyMapping.class);
