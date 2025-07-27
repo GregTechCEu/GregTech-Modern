bcb7af891a8e4b033dd43f9e60b0f27f59962c7c
Caedis <Caedis@users.noreply.github.com>
2025-07-27 14:41:30 -0500

    spotless

--- src/main/java/com/gregtechceu/gtceu/utils/input/SyncedKeyMappings.java
+++ src/main/java/com/gregtechceu/gtceu/utils/input/SyncedKeyMappings.java
@@ -2,13 +2,11 @@ package com.gregtechceu.gtceu.utils.input;
 
 import com.gregtechceu.gtceu.GTCEu;
 
-import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoader;
 
-import com.mojang.blaze3d.platform.InputConstants;
-
 public class SyncedKeyMappings {
+
     public static void init() {
         if (GTCEu.isClientSide()) {
             MinecraftForge.EVENT_BUS.register(SyncedKeyMapping.class);
