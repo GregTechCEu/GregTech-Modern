cb0b86f101ef3273864e590a72d8cf07cd49028f
screret <68943070+screret@users.noreply.github.com>
2025-07-29 13:33:57 +0300

    use custom render type for monitor images for cleaner code

--- src/main/java/com/gregtechceu/gtceu/client/renderer/GTRenderTypes.java
+++ src/main/java/com/gregtechceu/gtceu/client/renderer/GTRenderTypes.java
@@ -1,12 +1,16 @@
 package com.gregtechceu.gtceu.client.renderer;
 
+import net.minecraft.Util;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
+
+import java.util.function.Function;
 
 @OnlyIn(Dist.CLIENT)
 public class GTRenderTypes extends RenderType {
@@ -18,6 +22,16 @@ public class GTRenderTypes extends RenderType {
                     .setShaderState(RenderStateShard.POSITION_COLOR_SHADER)
                     .setTransparencyState(RenderStateShard.TRANSLUCENT_TRANSPARENCY)
                     .createCompositeState(false));
+    private static final Function<ResourceLocation, RenderType> GUI_TEXTURE = Util.memoize((texture) -> {
+        return create("gui_texture", DefaultVertexFormat.POSITION_COLOR_TEX_LIGHTMAP, VertexFormat.Mode.QUADS,
+                RenderType.TRANSIENT_BUFFER_SIZE, false, true,
+                RenderType.CompositeState.builder()
+                        .setShaderState(RENDERTYPE_TEXT_SHADER)
+                        .setTextureState(new RenderStateShard.TextureStateShard(texture, false, false))
+                        .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
+                        .setLightmapState(LIGHTMAP)
+                        .createCompositeState(false));
+    });
 
     private GTRenderTypes(String name, VertexFormat format, VertexFormat.Mode mode, int bufferSize,
                           boolean affectsCrumbling, boolean sortOnUpload, Runnable setupState, Runnable clearState) {
@@ -26,5 +40,9 @@ public class GTRenderTypes extends RenderType {
 
     public static RenderType getLightRing() {
         return LIGHT_RING;
+    }
+
+    public static RenderType guiTexture(ResourceLocation texture) {
+        return GUI_TEXTURE.apply(texture);
     }
 }
