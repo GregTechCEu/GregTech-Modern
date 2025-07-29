422d845665e69c1d09ea6e5fc72942c28ba616e9
screret <68943070+screret@users.noreply.github.com>
2025-07-29 13:34:25 +0300

    remove the dynamic background

--- src/main/java/com/gregtechceu/gtceu/client/renderer/machine/impl/CentralMonitorRender.java
+++ src/main/java/com/gregtechceu/gtceu/client/renderer/machine/impl/CentralMonitorRender.java
@@ -11,7 +11,6 @@ import com.gregtechceu.gtceu.common.machine.multiblock.electric.CentralMonitorMa
 import com.gregtechceu.gtceu.common.machine.multiblock.electric.monitor.MonitorGroup;
 
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
@@ -19,10 +18,7 @@ import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.mojang.math.Axis;
 import com.mojang.serialization.Codec;
-import org.joml.Matrix4f;
 
 public class CentralMonitorRender extends DynamicRender<CentralMonitorMachine, CentralMonitorRender> {
 
@@ -30,9 +26,7 @@ public class CentralMonitorRender extends DynamicRender<CentralMonitorMachine, C
     public static final Codec<CentralMonitorRender> CODEC = Codec.unit(CentralMonitorRender::new);
     public static final DynamicRenderType<CentralMonitorMachine, CentralMonitorRender> TYPE = new DynamicRenderType<>(CODEC);
     // spotless:on
-    private static final float BACKGROUND_OFFSET_XY = 1 / 16f;
-    private static final float BACKGROUND_OFFSET_Z = -0.01f;
-    private static final float SCREEN_OFFSET_Z = 0.03f;
+    private static final float SCREEN_OFFSET_Z = 0.01f;
 
     public CentralMonitorRender() {}
 
@@ -66,34 +60,7 @@ public class CentralMonitorRender extends DynamicRender<CentralMonitorMachine, C
                 }
             }
         }
-
-        PoseStack.Pose pose = poseStack.last();
-        for (int row = 0; row <= machine.getUpDist() + machine.getDownDist(); row++) {
-            for (int col = 0; col <= machine.getLeftDist() + machine.getRightDist(); col++) {
-                IMonitorComponent component = machine.getComponent(row, col);
-                if (component == null || !component.isMonitor()) continue;
-
-                float minX = col, minY = row, maxX = minX + 1, maxY = minY + 1;
-                if (machine.isMonitor(row, col - 1)) minX += BACKGROUND_OFFSET_XY;
-                if (machine.isMonitor(row - 1, col)) minY += BACKGROUND_OFFSET_XY;
-                if (machine.isMonitor(row, col + 1)) maxX -= BACKGROUND_OFFSET_XY;
-                if (machine.isMonitor(row + 1, col)) maxY -= BACKGROUND_OFFSET_XY;
-
-                renderRect(pose, buffer, 0, minX, minY, maxX, maxY, BACKGROUND_OFFSET_Z);
-            }
-        }
         poseStack.popPose();
-    }
-
-    public void renderRect(PoseStack.Pose last, MultiBufferSource bufferSource, int color,
-                           float minX, float minY, float maxX, float maxY, float z) {
-        VertexConsumer vertexConsumer = bufferSource.getBuffer(RenderType.gui());
-        Matrix4f pose = last.pose();
-
-        vertexConsumer.vertex(pose, minX, maxY, z).color(color).endVertex();
-        vertexConsumer.vertex(pose, maxX, maxY, z).color(color).endVertex();
-        vertexConsumer.vertex(pose, maxX, minY, z).color(color).endVertex();
-        vertexConsumer.vertex(pose, minX, minY, z).color(color).endVertex();
     }
 
     @Override
