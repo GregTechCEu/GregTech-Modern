41ff26262b340287de881e2929c4f6fa0bb9f6cf
TarLaboratories <levtaranov3@gmail.com>
2025-08-04 18:18:19 +0300

    multiblock copying

--- src/main/java/com/gregtechceu/gtceu/common/item/tool/behavior/MetaMachineConfigCopyBehaviour.java
+++ src/main/java/com/gregtechceu/gtceu/common/item/tool/behavior/MetaMachineConfigCopyBehaviour.java
@@ -12,6 +12,7 @@ import com.gregtechceu.gtceu.api.machine.MetaMachine;
 import com.gregtechceu.gtceu.api.machine.feature.IAutoOutputFluid;
 import com.gregtechceu.gtceu.api.machine.feature.IAutoOutputItem;
 import com.gregtechceu.gtceu.api.machine.feature.IMufflableMachine;
+import com.gregtechceu.gtceu.api.machine.multiblock.MultiblockControllerMachine;
 import com.gregtechceu.gtceu.api.pattern.util.RelativeDirection;
 import com.gregtechceu.gtceu.api.registry.GTRegistries;
 import com.gregtechceu.gtceu.api.transfer.item.CustomItemStackHandler;
@@ -21,6 +22,7 @@ import com.gregtechceu.gtceu.common.machine.owner.MachineOwner;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.*;
 import net.minecraft.network.chat.CommonComponents;
@@ -29,6 +31,7 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -63,6 +66,10 @@ public class MetaMachineConfigCopyBehaviour implements IInteractionItem, IAddInf
     public static final String COVER_ITEM = "cover_item";
     public static final String COVER_CONFIG = "cover_config";
     public static final String ORIGINAL_COVER_SIDE = "cover_side";
+    public static final String POSITION = "position";
+    public static final String BLOCK_ITEM = "place_item";
+    public static final String PARTS = "parts";
+    public static final String MULTIBLOCK = "multiblock";
 
     public static final Component ENABLED = Component.translatable("cover.voiding.label.enabled")
             .withStyle(ChatFormatting.GREEN);
@@ -120,9 +127,9 @@ public class MetaMachineConfigCopyBehaviour implements IInteractionItem, IAddInf
                 return InteractionResult.FAIL;
             }
             if (context.isSecondaryUseActive()) {
-                return handleCopy(stack, machine);
+                return handleCopy(stack.getOrCreateTag(), machine);
             } else if (stack.getTagElement(CONFIG_DATA) != null) {
-                return handlePaste(stack, machine, context.getPlayer() == null ? null : new CustomItemStackHandler(context.getPlayer().getInventory().items));
+                return handlePaste(stack.getOrCreateTag(), machine, context.getPlayer() == null ? null : new CustomItemStackHandler(context.getPlayer().getInventory().items));
             }
         } else if (context.isSecondaryUseActive() && context.getLevel().getBlockState(context.getClickedPos()).isAir()) {
             stack.removeTagKey(CONFIG_DATA);
@@ -132,7 +139,7 @@ public class MetaMachineConfigCopyBehaviour implements IInteractionItem, IAddInf
         return InteractionResult.SUCCESS;
     }
 
-    public static InteractionResult handleCopy(ItemStack stack, MetaMachine machine) {
+    public static InteractionResult handleCopy(CompoundTag tag, MetaMachine machine) {
         CompoundTag configData = new CompoundTag();
         configData.putString(ORIGINAL_FRONT, directionToString(machine.getFrontFacing()));
         if (machine instanceof IAutoOutputItem autoOutputItem && autoOutputItem.getOutputFacingItems() != null) {
@@ -163,16 +170,34 @@ public class MetaMachineConfigCopyBehaviour implements IInteractionItem, IAddInf
             }
         }
         if (!covers.isEmpty()) configData.put(COVERS, covers);
+        if (machine.getLevel() != null && machine instanceof MultiblockControllerMachine multiblock) {
+            CompoundTag multiblockTag = new CompoundTag();
+            ListTag parts = new ListTag();
+            for (BlockPos partPos : multiblock.getPartPositions()) {
+                CompoundTag partTag = new CompoundTag();
+                partTag.put(POSITION, posToTag(partPos.subtract(machine.getPos())));
+                partTag.put(BLOCK_ITEM,
+                        new ItemStack(machine.getLevel().getBlockState(partPos).getBlock().asItem()).serializeNBT());
+                if (machine.getLevel().getBlockEntity(partPos) instanceof IMachineBlockEntity machineBE) {
+                    CompoundTag partConfig = new CompoundTag();
+                    handleCopy(partConfig, machineBE.getMetaMachine());
+                    partTag.put(CONFIG_DATA, partConfig);
+                }
+                parts.add(partTag);
+            }
+            multiblockTag.put(PARTS, parts);
+            configData.put(MULTIBLOCK, multiblockTag);
+        }
         if (!configData.isEmpty()) {
-            stack.getOrCreateTag().put(CONFIG_DATA, configData);
+            tag.put(CONFIG_DATA, configData);
         }
         return InteractionResult.SUCCESS;
     }
 
-    public static InteractionResult handlePaste(ItemStack stack, MetaMachine machine,
+    public static InteractionResult handlePaste(CompoundTag tag, MetaMachine machine,
                                                 @Nullable IItemHandler itemHandler) {
-        CompoundTag configData = stack.getTagElement(CONFIG_DATA);
-        if (configData == null) return InteractionResult.PASS;
+        if (!tag.contains(CONFIG_DATA)) return InteractionResult.PASS;
+        CompoundTag configData = tag.getCompound(CONFIG_DATA);
         Direction originalFront = tagToDirection(configData.get(ORIGINAL_FRONT));
         if (configData.contains(ITEM_CONFIG) && machine instanceof IAutoOutputItem autoOutputItem) {
             pasteOutputConfig(originalFront, machine.getFrontFacing(), configData.getCompound(ITEM_CONFIG),
@@ -214,6 +239,41 @@ public class MetaMachineConfigCopyBehaviour implements IInteractionItem, IAddInf
                 CompoundTag coverConfig = compoundTag.getCompound(COVER_CONFIG);
                 if (consumeItems(itemHandler, cover.getItemsRequiredForConfigPaste(coverConfig))) {
                     cover.pasteConfig(coverConfig);
+                }
+            }
+        }
+        if (configData.contains(MULTIBLOCK) && machine instanceof MultiblockControllerMachine multiblock &&
+                machine.getLevel() != null) {
+            Direction front = multiblock.getFrontFacing();
+            Direction spin = multiblock.getUpwardsFacing();
+            boolean flipped = multiblock.isFlipped();
+            Direction right = RelativeDirection.RIGHT.getRelative(front, spin, flipped);
+            Direction up = RelativeDirection.UP.getRelative(front, spin, flipped);
+            CompoundTag multiblockTag = configData.getCompound(MULTIBLOCK);
+            ListTag parts = multiblockTag.getList(PARTS, Tag.TAG_COMPOUND);
+            Map<Predicate<ItemStack>, Integer> requiredItems = new HashMap<>();
+            for (Tag part : parts) if (part instanceof CompoundTag partTag) {
+                requiredItems.put(stack -> stack.is(ItemStack.of(partTag.getCompound(BLOCK_ITEM)).getItem()), 1);
+            }
+            if (consumeItems(itemHandler, requiredItems)) {
+                for (Tag part : parts) {
+                    if (!(part instanceof CompoundTag partTag)) continue;
+                    BlockPos relPos = posFromTag(partTag.get(POSITION));
+                    BlockPos.MutableBlockPos pos = new BlockPos(relPos.get(right.getAxis()), relPos.get(up.getAxis()),
+                            relPos.get(front.getAxis())).mutable();
+                    pos.move(machine.getPos());
+                    ItemStack stack = ItemStack.of(partTag.getCompound(BLOCK_ITEM));
+                    if (stack.getItem() instanceof BlockItem blockItem) {
+                        machine.getLevel().setBlockAndUpdate(pos, blockItem.getBlock().defaultBlockState());
+                        if (partTag.contains(CONFIG_DATA) &&
+                                machine.getLevel().getBlockEntity(pos) instanceof IMachineBlockEntity machineBE) {
+                            machineBE.getMetaMachine().setFrontFacing(
+                                    RelativeDirection.getActualDirection(
+                                            originalFront, multiblock.getFrontFacing(),
+                                            tagToDirection(partTag.getCompound(CONFIG_DATA).get(ORIGINAL_FRONT))));
+                            handlePaste(partTag.getCompound(CONFIG_DATA), machineBE.getMetaMachine(), itemHandler);
+                        }
+                    }
                 }
             }
         }
@@ -293,5 +353,16 @@ public class MetaMachineConfigCopyBehaviour implements IInteractionItem, IAddInf
         }
         for (Integer slot : itemCountBySlot.keySet()) itemHandler.extractItem(slot, itemCountBySlot.get(slot), false);
         return true;
+    }
+
+    private static Tag posToTag(BlockPos pos) {
+        return new IntArrayTag(List.of(pos.getX(), pos.getY(), pos.getZ()));
+    }
+
+    private static BlockPos posFromTag(Tag tag) {
+        if (tag instanceof IntArrayTag intArrayTag)
+            return new BlockPos(intArrayTag.get(0).getAsInt(), intArrayTag.get(1).getAsInt(),
+                    intArrayTag.get(2).getAsInt());
+        return new BlockPos(0, 0, 0);
     }
 }
