e995898a89ced9369246d7857a4b520c0d7c982e
楓 <r0yalist@outlook.com>
2025-08-12 17:38:38 +0800

    Use `isEmpty()`
    Co-authored-by: screret <68943070+screret@users.noreply.github.com>

--- src/main/java/com/gregtechceu/gtceu/api/misc/forge/QuantumFluidHandlerItemStack.java
+++ src/main/java/com/gregtechceu/gtceu/api/misc/forge/QuantumFluidHandlerItemStack.java
@@ -42,7 +42,7 @@ public class QuantumFluidHandlerItemStack implements IFluidHandlerItem, ICapabil
             return FluidStack.EMPTY;
         }
         FluidStack stack = FluidStack.loadFluidStackFromNBT(tagCompound.getCompound("stored"));
-        if (stack != FluidStack.EMPTY) {
+        if (!stack.isEmpty()) {
             stack.setAmount(GTMath.saturatedCast(tagCompound.getLong("storedAmount")));
         }
         return stack;
