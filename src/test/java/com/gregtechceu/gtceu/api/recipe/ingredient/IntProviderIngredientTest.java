278d1a3bb8fc581773383ad4b5c542529f2539eb
Dilithium Thoride <lilithiumathena@gmail.com>
2025-08-20 00:29:13 -0500

    failure?

--- src/test/java/com/gregtechceu/gtceu/api/recipe/ingredient/IntProviderIngredientTest.java
+++ src/test/java/com/gregtechceu/gtceu/api/recipe/ingredient/IntProviderIngredientTest.java
@@ -11,7 +11,6 @@ import com.gregtechceu.gtceu.api.machine.SimpleTieredMachine;
 import com.gregtechceu.gtceu.api.machine.multiblock.WorkableMultiblockMachine;
 import com.gregtechceu.gtceu.api.machine.trait.NotifiableEnergyContainer;
 import com.gregtechceu.gtceu.api.machine.trait.NotifiableItemStackHandler;
-import com.gregtechceu.gtceu.api.recipe.GTRecipe;
 import com.gregtechceu.gtceu.api.recipe.GTRecipeType;
 import com.gregtechceu.gtceu.common.machine.multiblock.part.FluidHatchPartMachine;
 import com.gregtechceu.gtceu.common.machine.multiblock.part.ItemBusPartMachine;
@@ -22,6 +21,7 @@ import net.minecraft.gametest.framework.BeforeBatch;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
@@ -30,178 +30,209 @@ import net.minecraftforge.gametest.GameTestHolder;
 import net.minecraftforge.gametest.PrefixGameTestTemplate;
 
 import static com.gregtechceu.gtceu.api.recipe.OverclockingLogic.*;
-import static com.gregtechceu.gtceu.common.data.GTRecipeModifiers.*;
-import static com.gregtechceu.gtceu.common.data.GTRecipeTypes.LARGE_CHEMICAL_RECIPES;
 
+/*
+ * TODO:
+ * do 3 passes of most tests as a safeguard against bad rolls
+ * Same output more than once
+ * Out of bounds
+ * Output a multiple of batchparallels
+ * 
+ */
 @PrefixGameTestTemplate(false)
 @GameTestHolder(GTCEu.MOD_ID)
 public class IntProviderIngredientTest {
 
-    private static GTRecipeType LCR_RECIPE_TYPE;
     private static GTRecipeType CR_RECIPE_TYPE;
-    private static GTRecipeType ASSEMBLER_RECIPE_TYPE;
+    private static GTRecipeType LCR_RECIPE_TYPE;
+    private static GTRecipeType CENTRIFUGE_RECIPE_TYPE;
 
-    @BeforeBatch(batch = "OverclockLogic")
+    @BeforeBatch(batch = "RangedIngredients")
     public static void prepare(ServerLevel level) {
-        LCR_RECIPE_TYPE = TestUtils.createRecipeType("ranged_ingredient_lcr_tests");
         CR_RECIPE_TYPE = TestUtils.createRecipeType("ranged_ingredient_cr_tests");
-        ASSEMBLER_RECIPE_TYPE = TestUtils.createRecipeType("ranged_inputs_assembler_tests");
+        LCR_RECIPE_TYPE = TestUtils.createRecipeType("ranged_ingredient_lcr_tests");
+        CENTRIFUGE_RECIPE_TYPE = TestUtils.createRecipeType("ranged_inputs_centrifuge_tests");
 
-        LCR_RECIPE_TYPE.getLookup().addRecipe(LCR_RECIPE_TYPE
-                .recipeBuilder(GTCEu.id("test_overclock_logic"))
-                .inputItems(new ItemStack(Items.RED_BED))
+        CR_RECIPE_TYPE.getLookup().addRecipe(CR_RECIPE_TYPE
+                .recipeBuilder(GTCEu.id("test_ranged_input_item_cr"))
+                .inputItemsRanged(new ItemStack(Items.BLUE_BED), UniformInt.of(1, 9))
+                .inputItems(new ItemStack(Blocks.COBBLESTONE))
                 .outputItems(new ItemStack(Blocks.STONE))
                 .EUt(GTValues.V[GTValues.HV])
-                .duration(20)
-                // NBT has a schematic in it with an HV energy input hatch
+                .duration(2)
                 .buildRawRecipe());
 
         CR_RECIPE_TYPE.getLookup().addRecipe(CR_RECIPE_TYPE
-                .recipeBuilder(GTCEu.id("test_overclock_logic_5"))
-                .inputItems(new ItemStack(Items.BROWN_BED))
-                .outputItems(new ItemStack(Blocks.STONE))
-                .EUt(GTValues.V[GTValues.MV])
-                .duration(16)
-                // NBT has a schematic in it with an HV charged singleblock CR in it
+                .recipeBuilder(GTCEu.id("test_ranged_output_item_cr"))
+                .inputItems(new ItemStack(Blocks.BRICK_SLAB))
+                .outputItemsRanged(new ItemStack(Blocks.STONE), UniformInt.of(1, 9))
+                .EUt(GTValues.V[GTValues.HV])
+                .duration(2)
                 .buildRawRecipe());
+
+//        LCR_RECIPE_TYPE.getLookup().addRecipe(LCR_RECIPE_TYPE
+//                .recipeBuilder(GTCEu.id("test_ranged_input_item_lcr"))
+//                .inputItemsRanged(new ItemStack(Items.GREEN_BED), UniformInt.of(1, 8))
+//                .inputItems(new ItemStack(Blocks.COBBLESTONE))
+//                .outputItems(new ItemStack(Blocks.STONE))
+//                .EUt(GTValues.V[GTValues.HV])
+//                .duration(4)
+//                .buildRawRecipe());
+//
+//        CENTRIFUGE_RECIPE_TYPE.getLookup().addRecipe(CENTRIFUGE_RECIPE_TYPE
+//                .recipeBuilder(GTCEu.id("test_ranged_input_item_cent"))
+//                .inputItemsRanged(new ItemStack(Items.RED_BED), UniformInt.of(1, 8))
+//                .inputItems(new ItemStack(Blocks.COBBLESTONE))
+//                .outputItems(new ItemStack(Blocks.STONE))
+//                .EUt(GTValues.V[GTValues.HV])
+//                .duration(16)
+//                .buildRawRecipe());
     }
 
     private static MetaMachine getMetaMachine(BlockEntity entity) {
         return ((MetaMachineBlockEntity) entity).getMetaMachine();
     }
 
-    private record BusHolder(ItemBusPartMachine inputBus1, ItemBusPartMachine inputBus2, ItemBusPartMachine outputBus1,
-                             FluidHatchPartMachine outputHatch1, WorkableMultiblockMachine controller) {}
+//    private record BusHolder(ItemBusPartMachine inputBus1, FluidHatchPartMachine inputHatch1,
+//                             ItemBusPartMachine outputBus1,
+//                             FluidHatchPartMachine outputHatch1, WorkableMultiblockMachine controller) {}
 
     /**
-     * Retrieves the busses for this specific template and force a multiblock structure check
+     * Retrieves the busses for this LCR template and force a multiblock structure check
      *
      * @param helper the GameTestHelper
      * @return the busses, in the BusHolder record.
      */
-    private static BusHolder getBussesAndForm(GameTestHelper helper) {
+/*    private static BusHolder getBussesAndFormLCR(GameTestHelper helper) {
         WorkableMultiblockMachine controller = (WorkableMultiblockMachine) getMetaMachine(
-                helper.getBlockEntity(new BlockPos(1, 2, 0)));
+                helper.getBlockEntity(new BlockPos(2, 1, 0)));
         TestUtils.formMultiblock(controller);
         controller.setRecipeType(LCR_RECIPE_TYPE);
         ItemBusPartMachine inputBus1 = (ItemBusPartMachine) getMetaMachine(
                 helper.getBlockEntity(new BlockPos(2, 1, 0)));
-        ItemBusPartMachine inputBus2 = (ItemBusPartMachine) getMetaMachine(
+        FluidHatchPartMachine inputHatch1 = (FluidHatchPartMachine) getMetaMachine(
                 helper.getBlockEntity(new BlockPos(2, 2, 0)));
         ItemBusPartMachine outputBus1 = (ItemBusPartMachine) getMetaMachine(
                 helper.getBlockEntity(new BlockPos(0, 1, 0)));
         FluidHatchPartMachine outputHatch1 = (FluidHatchPartMachine) getMetaMachine(
                 helper.getBlockEntity(new BlockPos(0, 2, 0)));
-        return new BusHolder(inputBus1, inputBus2, outputBus1, outputHatch1, controller);
+        return new BusHolder(inputBus1, inputHatch1, outputBus1, outputHatch1, controller);
     }
 
-    // Test for running HV recipe at HV
-    @GameTest(template = "lcr_input_separation", batch = "OverclockLogic", setupTicks = 40, timeoutTicks = 200)
-    public static void overclockLogicOnTierNothingChanges(GameTestHelper helper) {
-        BusHolder busHolder = getBussesAndForm(helper);
-        busHolder.inputBus1.getInventory().setStackInSlot(0, new ItemStack(Items.RED_BED));
-        // One tick to start, 20 for the recipe to run
-        helper.succeedOnTickWhen(21, () -> {
-            helper.assertTrue(
-                    TestUtils.isItemStackEqual(busHolder.outputBus1.getInventory().getStackInSlot(0),
-                            new ItemStack(Blocks.STONE)),
-                    "Item didn't craft at the right tick with an on-tier recipe" +
-                            busHolder.outputBus1.getInventory().getStackInSlot(0).getDisplayName());
-        });
+    /**
+     * Retrieves the busses for this Large Centrifuge template and force a multiblock structure check
+     *
+     * @param helper the GameTestHelper
+     * @return the busses, in the BusHolder record.
+     */
+/*    private static BusHolder getBussesAndFormLCENT(GameTestHelper helper) {
+        WorkableMultiblockMachine controller = (WorkableMultiblockMachine) getMetaMachine(
+                helper.getBlockEntity(new BlockPos(1, 1, 0)));
+        TestUtils.formMultiblock(controller);
+        controller.setRecipeType(CENTRIFUGE_RECIPE_TYPE);
+        ItemBusPartMachine inputBus1 = (ItemBusPartMachine) getMetaMachine(
+                helper.getBlockEntity(new BlockPos(2, 0, 0)));
+        FluidHatchPartMachine inputHatch1 = (FluidHatchPartMachine) getMetaMachine(
+                helper.getBlockEntity(new BlockPos(2, 1, 0)));
+        ItemBusPartMachine outputBus1 = (ItemBusPartMachine) getMetaMachine(
+                helper.getBlockEntity(new BlockPos(0, 0, 0)));
+        FluidHatchPartMachine outputHatch1 = (FluidHatchPartMachine) getMetaMachine(
+                helper.getBlockEntity(new BlockPos(0, 1, 0)));
+        return new BusHolder(inputBus1, inputHatch1, outputBus1, outputHatch1, controller);
     }
 
-    // Test for running LV 1t recipe at HV
-    @GameTest(template = "lcr_input_separation", batch = "OverclockLogic", setupTicks = 40, timeoutTicks = 200)
-    public static void overclockLogicTwoTiersAbove16Parallels(GameTestHelper helper) {
-        BusHolder busHolder = getBussesAndForm(helper);
-        busHolder.inputBus1.getInventory().setStackInSlot(0, new ItemStack(Items.STICK, 64));
-        // One tick to start, 4 for the recipe to run (16/t from ULV recipe to HV)
-        helper.succeedOnTickWhen(5, () -> {
-            helper.assertTrue(
-                    TestUtils.isItemStackEqual(busHolder.outputBus1.getInventory().getStackInSlot(0),
-                            new ItemStack(Blocks.STONE, 64)),
-                    "Item didn't craft at the right tick with an on-tier recipe" +
-                            busHolder.outputBus1.getInventory().getStackInSlot(0).getDisplayName());
-        });
-    }
-
-    // Test for running EV recipe at HV
-    @GameTest(template = "lcr_input_separation", batch = "OverclockLogic", setupTicks = 40, timeoutTicks = 200)
-    public static void overclockLogicOverTierNothingHappens(GameTestHelper helper) {
-        BusHolder busHolder = getBussesAndForm(helper);
-        busHolder.inputBus1.getInventory().setStackInSlot(0, new ItemStack(Items.BROWN_BED));
-        helper.failIfEver(() -> {
-            helper.assertFalse(
-                    busHolder.outputBus1.getInventory().getStackInSlot(0).getItem().equals(Blocks.STONE.asItem()),
-                    "Item crafted at one tier over when it shouldn't have");
-        });
-        helper.succeed();
-    }
-
-
-    // Test for charge usage of a singleblock HV chemical reactor running an HV recipe
-    @GameTest(template = "singleblock_charged_cr", batch = "OverclockLogic")
-    public static void overclockLogicHVPowerTest(GameTestHelper helper) {
+    /*
+     * // Test for running HV recipe at HV
+     * 
+     * @GameTest(template = "lcr_input_separation", batch = "RangedIngredients", setupTicks = 40, timeoutTicks = 200)
+     * public static void overclockLogicOnTierNothingChanges(GameTestHelper helper) {
+     * BusHolder busHolder = getBussesAndFormLCR(helper);
+     * busHolder.inputBus1.getInventory().setStackInSlot(0, new ItemStack(Items.RED_BED));
+     * // One tick to start, 20 for the recipe to run
+     * helper.succeedOnTickWhen(21, () -> {
+     * helper.assertTrue(
+     * TestUtils.isItemStackEqual(busHolder.outputBus1.getInventory().getStackInSlot(0),
+     * new ItemStack(Blocks.STONE)),
+     * "Item didn't craft at the right tick with an on-tier recipe" +
+     * busHolder.outputBus1.getInventory().getStackInSlot(0).getDisplayName());
+     * });
+     * }
+     * 
+     * // Test for running LV 1t recipe at HV
+     * 
+     * @GameTest(template = "lcr_input_separation", batch = "RangedIngredients", setupTicks = 40, timeoutTicks = 200)
+     * public static void overclockLogicTwoTiersAbove16Parallels(GameTestHelper helper) {
+     * BusHolder busHolder = getBussesAndFormLCR(helper);
+     * busHolder.inputBus1.getInventory().setStackInSlot(0, new ItemStack(Items.STICK, 64));
+     * // One tick to start, 4 for the recipe to run (16/t from ULV recipe to HV)
+     * helper.succeedOnTickWhen(5, () -> {
+     * helper.assertTrue(
+     * TestUtils.isItemStackEqual(busHolder.outputBus1.getInventory().getStackInSlot(0),
+     * new ItemStack(Blocks.STONE, 64)),
+     * "Item didn't craft at the right tick with an on-tier recipe" +
+     * busHolder.outputBus1.getInventory().getStackInSlot(0).getDisplayName());
+     * });
+     * }
+     */
+    // Test for singleblock machine with ranged item input
+    @GameTest(template = "singleblock_charged_cr", batch = "RangedIngredients")
+    public static void singleblockRangedItemInput(GameTestHelper helper) {
         SimpleTieredMachine machine = (SimpleTieredMachine) getMetaMachine(
                 helper.getBlockEntity(new BlockPos(0, 1, 0)));
 
         machine.setRecipeType(CR_RECIPE_TYPE);
-        NotifiableEnergyContainer energyContainer = (NotifiableEnergyContainer) machine
-                .getCapabilitiesFlat(IO.IN, EURecipeCapability.CAP).get(0);
         NotifiableItemStackHandler itemIn = (NotifiableItemStackHandler) machine
                 .getCapabilitiesFlat(IO.IN, ItemRecipeCapability.CAP).get(0);
         NotifiableItemStackHandler itemOut = (NotifiableItemStackHandler) machine
                 .getCapabilitiesFlat(IO.OUT, ItemRecipeCapability.CAP).get(0);
 
-        long originalCharge = GTValues.V[GTValues.HV] * 64L;
-        helper.assertTrue(energyContainer.getEnergyStored() == originalCharge,
-                "Singleblock charged CR NBT changed, machine not fully charged anymore");
 
-        itemIn.setStackInSlot(0, new ItemStack(Items.RED_BED));
-        // 1t to turn on, 16t to run the recipe
-        helper.succeedOnTickWhen(17, () -> {
-            helper.assertTrue(TestUtils.isItemStackEqual(
-                            itemOut.getStackInSlot(0),
-                            new ItemStack(Blocks.STONE, 1)),
-                    "Singleblock CR didn't run recipe in correct time");
-            long chargeUsed = originalCharge - energyContainer.getEnergyStored();
-            long chargeNeeded = GTValues.V[GTValues.HV] * 16L;
-            helper.assertTrue(chargeUsed == chargeNeeded,
-                    "Recipe didn't consume right amount, instead of " + chargeNeeded + " it used " + chargeUsed);
+        int runs = 7;
+        itemIn.setStackInSlot(0, new ItemStack(Items.BLUE_BED, 64));
+        itemIn.setStackInSlot(1, new ItemStack(Items.COBBLESTONE, runs));
+        // 1t to turn on, 12t to run the recipe 4 times
+        helper.succeedOnTickWhen(runs * 2 + 1, () -> {
+            ItemStack results = itemIn.getStackInSlot(0);
+            helper.assertTrue(TestUtils.isItemStackEqual(itemOut.getStackInSlot(0), new ItemStack(Blocks.STONE, runs)),
+                    "Singleblock CR didn't complete correct number of recipes, completed " +
+                            itemOut.getStackInSlot(0).getCount());
+            helper.assertTrue(TestUtils.isWithinRange(results, 1, 57),
+                    "Singleblock CR didn't consume correct number of items, consumed " +
+                            (64 - results.getCount()) + "items");
+            helper.assertFalse((results.getCount() == 1),
+                    "Singleblock CR rolled max value on every roll");
+            helper.assertFalse((results.getCount() == 57),
+                    "Singleblock CR rolled min value on every roll");
+
         });
     }
 
-    // Test for charge usage of a singleblock HV chemical reactor running an MV recipe
-    @GameTest(template = "singleblock_charged_cr", batch = "OverclockLogic")
-    public static void overclockLogicMVPowerTest(GameTestHelper helper) {
+    // Test for singleblock machine with ranged item input
+    @GameTest(template = "singleblock_charged_cr", batch = "RangedIngredients")
+    public static void singleblockRangedItemOutput(GameTestHelper helper) {
         SimpleTieredMachine machine = (SimpleTieredMachine) getMetaMachine(
                 helper.getBlockEntity(new BlockPos(0, 1, 0)));
 
         machine.setRecipeType(CR_RECIPE_TYPE);
-        NotifiableEnergyContainer energyContainer = (NotifiableEnergyContainer) machine
-                .getCapabilitiesFlat(IO.IN, EURecipeCapability.CAP).get(0);
         NotifiableItemStackHandler itemIn = (NotifiableItemStackHandler) machine
                 .getCapabilitiesFlat(IO.IN, ItemRecipeCapability.CAP).get(0);
         NotifiableItemStackHandler itemOut = (NotifiableItemStackHandler) machine
                 .getCapabilitiesFlat(IO.OUT, ItemRecipeCapability.CAP).get(0);
 
-        long originalCharge = GTValues.V[GTValues.HV] * 64L;
-        helper.assertTrue(energyContainer.getEnergyStored() == originalCharge,
-                "Singleblock charged CR NBT changed, machine not fully charged anymore");
 
-        itemIn.setStackInSlot(0, new ItemStack(Items.BROWN_BED));
-        // 1t to turn on, 8t to run the recipe (single overclock)
-        helper.succeedOnTickWhen(9, () -> {
-            helper.assertTrue(TestUtils.isItemStackEqual(
-                            itemOut.getStackInSlot(0),
-                            new ItemStack(Blocks.STONE, 1)),
-                    "Singleblock CR didn't run recipe in correct time");
-            long chargeUsed = originalCharge - energyContainer.getEnergyStored();
-            // One overclock ups EU/t by STD_VOLTAGE_FACTOR, decreases time by STD_DURATION_FACTOR_INV
-            long chargeNeeded = (long) ((GTValues.V[GTValues.MV] * STD_VOLTAGE_FACTOR) *
-                    (16L / STD_DURATION_FACTOR_INV));
-            helper.assertTrue(chargeUsed == chargeNeeded,
-                    "Recipe didn't consume right amount, instead of " + chargeNeeded + " it used " + chargeUsed);
+        int runs = 7;
+        itemIn.setStackInSlot(0, new ItemStack(Items.BRICK_SLAB, runs));
+        // 1t to turn on, 12t to run the recipe 4 times
+        helper.succeedOnTickWhen(runs * 2 + 1, () -> {
+            ItemStack results = itemOut.getStackInSlot(0);
+            helper.assertTrue(TestUtils.isWithinRange(results, 1, 57),
+                    "Singleblock CR didn't produce correct number of items, produced " +
+                            results.getCount() + "items");
+            helper.assertFalse((results.getCount() == runs * 9),
+                    "Singleblock CR rolled max value on every roll");
+            helper.assertFalse((results.getCount() == runs),
+                    "Singleblock CR rolled min value on every roll");
+
         });
     }
 }
--- src/test/java/com/gregtechceu/gtceu/gametest/util/TestUtils.java
+++ src/test/java/com/gregtechceu/gtceu/gametest/util/TestUtils.java
@@ -7,7 +7,6 @@ import com.gregtechceu.gtceu.api.machine.multiblock.MultiblockControllerMachine;
 import com.gregtechceu.gtceu.api.recipe.GTRecipeType;
 import com.gregtechceu.gtceu.api.registry.GTRegistries;
 
-import net.minecraft.util.valueproviders.IntProvider;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.RecipeType;
@@ -24,42 +23,44 @@ public class TestUtils {
         return ItemStack.isSameItem(stack1, stack2) && stack1.getCount() == stack2.getCount();
     }
 
-    // Compares two itemstacks and an IntProvider.
-    // Checks if items are equal, and if stack2's amount is within the IntProvider's range.
-    public static boolean isItemStackWithinRange(ItemStack stack1, ItemStack stack2, IntProvider provider) {
-        return ItemStack.isSameItem(stack1, stack2) && stack2.getCount() < provider.getMaxValue() &&
-                stack2.getCount() > provider.getMinValue();
+    // Compares two itemstacks and a range.
+    // Checks if items are equal, and if stack2's amount is within range.
+    public static boolean isItemStackWithinRange(ItemStack stack1, ItemStack stack2, int min, int max) {
+        return ItemStack.isSameItem(stack1, stack2) && isWithinRange(stack2, min, max);
     }
 
-    // Compares an itemstack with a number of batched parallels run
-    // Returns true if the itemstack is NOT an exact multiple of the batch/parallel count
-    // Intended to test if an IntProvider is being rolled correctly for a batch, or if it is returning a single value for all rolls
-    // This test can cause false failures from bad luck and should be run twice to reduce the odds of bad luck, and only fail if both fail.
-    public static boolean isItemStackExactlyEvenMultiple(ItemStack stack, int batches, int parallels) {
-        return  stack.getCount() % (batches * parallels)!= 0;
+    // Compares an int representing an itemstack's size with a number of batches, parallels, and runs
+    // Returns true if the size is NOT an exact multiple of the total run count
+    // Intended to test if an IntProvider is being rolled correctly for a batch, or if it is returning a single value
+    // multiplied
+    // This test can cause false failures from bad luck and should be run more than once to reduce the odds of bad luck
+    public static boolean isStackSizeExactlyEvenMultiple(int size, int batches, int parallels, int runs) {
+        return size % (batches * parallels * runs) != 0;
     }
 
-    // Compares two fluidstacks' items and amounts
+    // Compares two fluidstacks' fluids and amounts
     // DOES NOT CHECK TAGS OR NBT ETC!
     public static boolean isFluidStackEqual(FluidStack stack1, FluidStack stack2) {
         return stack1.isFluidEqual(stack2) && stack1.getAmount() == stack2.getAmount();
     }
 
-    // Compares two fluidstacks and an IntProvider.
-    // Checks if items are equal, and if stack2's amount is within the IntProvider's range.
-    public static boolean isFluidStackWithinRange(FluidStack stack1, FluidStack stack2, IntProvider provider) {
-        return stack1.isFluidEqual(stack2) && stack2.getAmount() < provider.getMaxValue() &&
-                stack2.getAmount() > provider.getMinValue();
+    // Compares two fluidstacks and a range.
+    // Checks if items are equal, and if stack2's amount is within range.
+    public static boolean isFluidStackWithinRange(FluidStack stack1, FluidStack stack2, int min, int max) {
+        return stack1.isFluidEqual(stack2) && isWithinRange(stack2, min, max);
     }
 
-    // Compares an fluidstacks with a number of batched parallels run
-    // Returns true if the fluidstack is NOT an exact multiple of the batch/parallel count
-    // Intended to test if an IntProvider is being rolled correctly for a batch, or if it is returning a single value for all rolls
-    // This test can cause false failures from bad luck and should be run twice to reduce the odds of bad luck, and only fail if both fail.
-    public static boolean isFluidStackExactlyEvenMultiple(FluidStack stack, int batches, int parallels) {
-        return  stack.getAmount() % (batches * parallels)!= 0;
+    // compares an ItemStack with a range
+    // returns true if the ItemStack's count is within range
+    public static boolean isWithinRange(ItemStack stack, int min, int max) {
+        return stack.getCount() <= max && stack.getCount() >= min;
     }
 
+    // compares a FluidStack with a range
+    // returns true if the FluidStack's amount is within range
+    public static boolean isWithinRange(FluidStack stack, int min, int max) {
+        return stack.getAmount() <= max && stack.getAmount() >= min;
+    }
 
     // Forces a structure check on multiblocks after being placed, to avoid having to wait ticks.
     // Ideally this doesn't need to happen, but it seems not doing this makes the multiblock tests flakey
