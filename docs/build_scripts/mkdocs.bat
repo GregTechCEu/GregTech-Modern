cd41adb2c6534c54ad3eeaeb99bfeda073319d64
jurrejelle <jurre@jilles.com>
2025-09-01 09:25:14 +0200

    Add kill script, build script, exec scripts

---
+++ docs/build_scripts/mkdocs.bat
@@ -0,0 +1,4 @@
+cd docs
+python -m venv .venv
+.\.venv\Scripts\pip.exe install -r requirements.txt
+.\.venv\Scripts\mkdocs.exe %1
---
+++ docs/build_scripts/mkdocs.sh
@@ -0,0 +1,4 @@
+cd docs
+python3 -m venv .venv
+./.venv/bin/pip3 install -r requirements.txt
+./.venv/bin/mkdocs $1
