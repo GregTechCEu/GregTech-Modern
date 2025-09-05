5c06f874cbc913a7545784bd75d32019b28a477d
TarLaboratories <levtaranov3@gmail.com>
2025-09-05 09:24:41 +0300

    shutter cover test

--- src/test/java/com/gregtechceu/gtceu/common/cover/ItemFilterCoverTest.java
+++ src/test/java/com/gregtechceu/gtceu/common/cover/ItemFilterCoverTest.java
@@ -116,7 +116,7 @@ public class ItemFilterCoverTest {
                 Direction.UP);
         filterCover.setFilterMode(FilterMode.FILTER_INSERT); // filter is for insert only, so should block transfer
 
-        helper.succeedWhen(() -> {
+        helper.succeedOnTickWhen(40, () -> {
             TestUtils.assertEqual(helper, crate2.getInventory().getStackInSlot(0), ItemStack.EMPTY);
             TestUtils.assertEqual(helper, crate2.getInventory().getStackInSlot(1), ItemStack.EMPTY);
             helper.succeed();
