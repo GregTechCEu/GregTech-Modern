b673bffe0259020c17b22720751a01c911424b6a
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2025-09-07 14:55:49 -0600

    screen overlays

---
+++ src/main/java/com/gregtechceu/gtceu/api/mui/overlay/OverlayManager.java
@@ -0,0 +1,47 @@
+package com.gregtechceu.gtceu.api.mui.overlay;
+
+import com.gregtechceu.gtceu.GTCEu;
+import com.gregtechceu.gtceu.client.mui.screen.ModularScreen;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.jetbrains.annotations.ApiStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@ApiStatus.Experimental
+@Mod.EventBusSubscriber(modid = GTCEu.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class OverlayManager {
+
+    public static final List<OverlayHandler> overlays = new ArrayList<>();
+
+    public static void register(OverlayHandler handler) {
+        if (!overlays.contains(handler)) {
+            overlays.add(handler);
+            overlays.sort(OverlayHandler::compareTo);
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void onOpenScreen(ScreenEvent.Opening event) {
+        if (event.getNewScreen() == event.getCurrentScreen()) return;
+        OverlayStack.closeAll();
+        for (OverlayHandler handler : overlays) {
+            if (handler.isValidFor(event.getNewScreen())) {
+                ModularScreen overlay = Objects.requireNonNull(handler.createOverlay(event.getNewScreen()),
+                        "Overlays must not be null!");
+                overlay.constructOverlay(event.getNewScreen());
+                OverlayStack.open(overlay);
+            }
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void onCloseScreen(ScreenEvent.Closing event) {
+        OverlayStack.closeAll();
+    }
+}
---
+++ src/main/java/com/gregtechceu/gtceu/api/mui/overlay/OverlayStack.java
@@ -0,0 +1,112 @@
+package com.gregtechceu.gtceu.api.mui.overlay;
+
+import com.gregtechceu.gtceu.api.mui.base.widget.IGuiElement;
+import com.gregtechceu.gtceu.client.mui.screen.ClientScreenHandler;
+import com.gregtechceu.gtceu.client.mui.screen.ModularScreen;
+import com.mojang.blaze3d.platform.Lighting;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.gui.GuiGraphics;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+@ApiStatus.Experimental
+public class OverlayStack {
+
+    private static final List<ModularScreen> overlay = new ArrayList<>();
+
+    public static void foreach(Consumer<ModularScreen> function, boolean topToBottom) {
+        if (topToBottom) {
+            for (int i = overlay.size() - 1; i >= 0; i--) {
+                function.accept(overlay.get(i));
+            }
+        } else {
+            for (ModularScreen screen : overlay) {
+                function.accept(screen);
+            }
+        }
+    }
+
+    public static boolean interact(Predicate<ModularScreen> function, boolean topToBottom) {
+        if (topToBottom) {
+            for (int i = overlay.size() - 1; i >= 0; i--) {
+                // overlay.get(i).getContext().updateEventState();
+                if (function.test(overlay.get(i))) {
+                    return true;
+                }
+            }
+        } else {
+            for (ModularScreen screen : overlay) {
+                // screen.getContext().updateEventState();
+                if (function.test(screen)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void draw(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
+        ModularScreen hovered = null;
+        ModularScreen fallback = null;
+        for (ModularScreen screen : overlay) {
+            screen.getContext().setGraphics(graphics);
+            screen.getContext().updateState(mouseX, mouseY, partialTicks);
+            RenderSystem.enableBlend();
+            RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
+            screen.render(graphics, mouseX, mouseY, partialTicks);
+            RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
+            screen.drawForeground(graphics, partialTicks);
+            if (screen.getContext().getHovered() != null) hovered = screen;
+            fallback = screen;
+        }
+        ClientScreenHandler.drawDebugScreen(graphics, hovered, fallback);
+        RenderSystem.enableDepthTest();
+        Lighting.setupFor3DItems();
+    }
+
+    public static void open(ModularScreen screen) {
+        int i = overlay.indexOf(screen);
+        if (i >= 0 && i < overlay.size() - 1) {
+            overlay.remove(i);
+        }
+        overlay.add(screen);
+        screen.onOpen();
+    }
+
+    public static void close(ModularScreen screen) {
+        if (overlay.remove(screen)) {
+            screen.onCloseParent();
+        }
+    }
+
+    static void closeAll() {
+        for (int i = overlay.size() - 1; i >= 0; i--) {
+            ModularScreen screen = overlay.remove(i);
+            screen.onCloseParent();
+        }
+    }
+
+    public static void onTick() {
+        foreach(ModularScreen::onUpdate, true);
+    }
+
+    @Nullable
+    public static IGuiElement getHoveredElement() {
+        for (int i = overlay.size() - 1; i >= 0; i--) {
+            ModularScreen screen = overlay.get(i);
+            IGuiElement hovered = screen.getContext().getHovered();
+            if (hovered == null) continue;
+            return hovered;
+        }
+        return null;
+    }
+
+    public static boolean isHoveringOverlay() {
+        return getHoveredElement() != null;
+    }
+}
