730c7afb1042c1c3342e9c0405aebdbcbc64aa08
jurrejelle <jurre@jilles.com>
2025-09-08 12:55:59 +0200

    spotless

--- src/main/java/com/gregtechceu/gtceu/common/block/BatteryBlock.java
+++ src/main/java/com/gregtechceu/gtceu/common/block/BatteryBlock.java
@@ -10,9 +10,9 @@ import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
 
 import lombok.Getter;
-import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
