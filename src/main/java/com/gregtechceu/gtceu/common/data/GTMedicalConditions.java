cdc4a6f9b3bf1893006b21a073da51a7e431099a
screret <68943070+screret@users.noreply.github.com>
2025-10-03 21:47:39 +0300

    add descriptions of the various "generic" conditions to GTMedicalConditions

--- src/main/java/com/gregtechceu/gtceu/common/data/GTMedicalConditions.java
+++ src/main/java/com/gregtechceu/gtceu/common/data/GTMedicalConditions.java
@@ -21,35 +21,69 @@ public class GTMedicalConditions {
     // General Conditions
     public static final MedicalCondition NONE = register("none", 0xffffff, 0,
             MedicalCondition.IdleProgressionType.NONE, 0, false);
+    // takes 5 minutes of having burn-causing items in the player's inventory for them to get the weakness effect
+    // heals 2 seconds' worth of progression every second when not holding those items
     public static final MedicalCondition CHEMICAL_BURNS = register("chemical_burns", 0xbc305a, 300,
             MedicalCondition.IdleProgressionType.HEAL, 2, false,
             new Symptom.ConfiguredSymptom(Symptom.WEAKNESS))
             .setRecipeModifier(builder -> builder
                     .outputFluids(DilutedHydrochloricAcid.getFluid(500))
                     .outputFluids(DilutedSulfuricAcid.getFluid(750)));
+    // takes 5 minutes of having poisonous items in the player's inventory for them to get the weakness effect
+    // at 10 minutes, they get a weaker version of the poison effect
+    // heals 2 seconds' worth of progression every second when not holding those items
     public static final MedicalCondition POISON = register("poison", 0xA36300, 600,
             MedicalCondition.IdleProgressionType.HEAL, 2, true,
             new Symptom.ConfiguredSymptom(Symptom.WEAK_POISONING),
             new Symptom.ConfiguredSymptom(Symptom.WEAKNESS, 300, 600))
             .setRecipeModifier(builder -> builder
                     .outputFluids(SulfurTrioxide.getFluid(1000)));
+    // having weakly poisonous items in the player's inventory gives them the weak poison effect
+    // the effect ramps up in 6 "stages", getting progressively worse every stage. It caps out at
+    // does NOT heal automatically
     public static final MedicalCondition WEAK_POISON = register("weak_poison", 0x6D7917, 1800,
             MedicalCondition.IdleProgressionType.NONE, 0, false,
             new Symptom.ConfiguredSymptom(Symptom.WEAK_POISONING, 6, 1800))
             .setRecipeModifier(builder -> builder
                     .outputFluids(NitricOxide.getFluid(1000)));
+    // takes 2.5 minutes of having irritating items in the player's inventory for them to get the weakness effect
+    // at 5 minutes, they begin getting random damage every so often
+    // heals 5 seconds' worth of progression every second when not holding those items
     public static final MedicalCondition IRRITANT = register("irritant", 0x02512f, 600,
             MedicalCondition.IdleProgressionType.HEAL, 5, false,
             new Symptom.ConfiguredSymptom(Symptom.RANDOM_DAMAGE),
             new Symptom.ConfiguredSymptom(Symptom.WEAKNESS, 300, 600))
             .setRecipeModifier(builder -> builder
                     .outputItems(dust, DarkAsh, 4));
+    // takes 5 minutes of having nauseating items in the player's inventory for them to get the nausea effect
+    // heals 5 seconds' worth of progression every second when not holding those items
     public static final MedicalCondition NAUSEA = register("nausea", 0x1D4A00, 600,
             MedicalCondition.IdleProgressionType.HEAL, 5, false,
             new Symptom.ConfiguredSymptom(Symptom.NAUSEA, 1, 420, 600))
             .setRecipeModifier(builder -> builder
                     .outputFluids(CarbonMonoxide.getFluid(50)));
-    public static final MedicalCondition CARCINOGEN = register("carcinogen", 0x0f570f, 20000,
+    /**
+     * takes (a total of) 10 hours of having radioactive items in the player's inventory for them to die.<br>
+     * <p>
+     * All effects:
+     * <ul>
+     * <li>After an hour, the player's attack damage and mining speed will start lowering.</li>
+     * <li>After 2 hours, the player's max air supply and movement speed will start lowering.</li>
+     * <li>After 3 hours, the player's max health will start lowering.</li>
+     * <li>After 4 hours, the player's attack damage and mining speed are at their lowest.</li>
+     * <li>After 5 hours, the player will have the lowest max air supply of 200/300 (so 6/10 bubbles) and their speed is
+     * at its lowest.</li>
+     * <li>After 6 hours, the player will have the lowest max HP of 10/20 (so 5/10 hearts).</li>
+     * <li>After 10 real-life hours (30 MC days) of having cancer, the player will die.</li>
+     * </ul>
+     * </p>
+     * Do note that while the effects do not get worse when the player isn't holding anything radioactive,
+     * the progression they've already gained will stay as is and reactivate if they pick up e.g. a uranium ingot.<br>
+     *
+     * This condition does <strong>NOT</strong> heal automatically. You can use {@linkplain GTItems#RAD_AWAY_PILL
+     * rad-away pills} to heal it.
+     */
+    public static final MedicalCondition CARCINOGEN = register("carcinogen", 0x0f570f, 36000,
             MedicalCondition.IdleProgressionType.NONE, 0, true,
             new Symptom.ConfiguredSymptom(Symptom.DEATH),
             new Symptom.ConfiguredSymptom(Symptom.HEALTH_DEBUFF, 10800, 21600),
@@ -74,7 +108,7 @@ public class GTMedicalConditions {
             new Symptom.ConfiguredSymptom(Symptom.NAUSEA),
             new Symptom.ConfiguredSymptom(Symptom.SLOWNESS, 2, 500, 1000),
             new Symptom.ConfiguredSymptom(Symptom.WEAKNESS, 2, 330, 1000))
-            //new Symptom.ConfiguredSymptom(Symptom.HUNGER, 2, .2f))
+            // new Symptom.ConfiguredSymptom(Symptom.HUNGER, 2, .2f))
             .setRecipeModifier(builder -> builder
                     .outputItems(dust, Arsenic, 4));
     public static final MedicalCondition METHANOL_POISONING = register("methanol_poisoning", 0xaa8800, 600,
