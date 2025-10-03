73959291b26f92ccb16592273699e8842622b4eb
screret <68943070+screret@users.noreply.github.com>
2025-10-03 22:12:05 +0300

    tweak the language of `/medical_condition query` to be easier to read, datagen

--- src/main/java/com/gregtechceu/gtceu/data/lang/LangHandler.java
+++ src/main/java/com/gregtechceu/gtceu/data/lang/LangHandler.java
@@ -125,9 +125,9 @@ public class LangHandler {
 
         provider.add("command.gtceu.medical_condition.get", "Player %s has");
         provider.add("command.gtceu.medical_condition.get.empty", "Player %s is perfectly healthy.");
-        provider.add("command.gtceu.medical_condition.get.element", "Condition %s§r: %s minutes %s seconds");
+        provider.add("command.gtceu.medical_condition.get.element", "- %s§r for %s minutes %s seconds");
         provider.add("command.gtceu.medical_condition.get.element.permanent",
-                "Condition %s§r: %s minutes %s seconds (permanent)");
+                "- %s§r for %s minutes %s seconds (permanent)");
         provider.add("command.gtceu.dump_data.success", "Dumped %s resources from registry %s to %s");
         provider.add("command.gtceu.place_vein.failure", "Failed to place vein %s at position %s");
         provider.add("command.gtceu.place_vein.success", "Placed vein %s at position %s");
