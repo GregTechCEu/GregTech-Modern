8586f3e54dcc48290f22b16f45e164be9e2bf259
TarLaboratories <levtaranov3@gmail.com>
2025-10-04 14:32:43 +0300

    change some formulas

--- src/main/java/com/gregtechceu/gtceu/common/data/materials/FirstDegreeMaterials.java
+++ src/main/java/com/gregtechceu/gtceu/common/data/materials/FirstDegreeMaterials.java
@@ -77,7 +77,7 @@ public class FirstDegreeMaterials {
                 .gem(3).ore(2, 1)
                 .color(0xdbfeff).secondaryColor(0xa0c4d7).iconSet(GEM_HORIZONTAL)
                 .appendFlags(EXT_METAL, NO_SMASHING, NO_SMELTING, HIGH_SIFTER_OUTPUT)
-                .components(Aluminium, 2, Silicon, 1, Fluorine, 2, Hydrogen, 2, Oxygen, 6)
+                .components(Aluminium, 2, Silicon, 1, Oxygen, 4, Fluorine, 2)
                 .buildAndRegister();
 
         Bone = new Material.Builder(GTCEu.id("bone"))
@@ -324,7 +324,7 @@ public class FirstDegreeMaterials {
                 .color(0x2836f1).secondaryColor(0x183ca3).iconSet(LAPIS)
                 .flags(GENERATE_PLATE, NO_SMASHING, NO_SMELTING, CRYSTALLIZABLE, GENERATE_ROD,
                         DECOMPOSITION_BY_ELECTROLYZING)
-                .components(Aluminium, 6, Silicon, 6, Calcium, 8, Sodium, 8)
+                .components(Sodium, 2, Calcium, 2, Aluminium, 3, Silicon, 3, Oxygen, 12)
                 .buildAndRegister();
 
         Magnalium = new Material.Builder(GTCEu.id("magnalium"))
@@ -565,7 +565,7 @@ public class FirstDegreeMaterials {
                 .color(0x3d54ff).secondaryColor(0x210d78).iconSet(LAPIS)
                 .flags(GENERATE_PLATE, GENERATE_ROD, NO_SMASHING, NO_SMELTING, CRYSTALLIZABLE,
                         DECOMPOSITION_BY_ELECTROLYZING)
-                .components(Aluminium, 3, Silicon, 3, Sodium, 4, Chlorine, 1)
+                .components(Sodium, 4, Aluminium, 3, Silicon, 3, Oxygen, 12, Chlorine, 1)
                 .buildAndRegister();
 
         AluminiumSulfite = new Material.Builder(GTCEu.id("aluminium_sulfite"))
@@ -652,7 +652,7 @@ public class FirstDegreeMaterials {
         Tetrahedrite = new Material.Builder(GTCEu.id("tetrahedrite"))
                 .dust().ore()
                 .color(0xa3a09b).secondaryColor(0x143313)
-                .components(Copper, 3, Antimony, 1, Sulfur, 3, Iron, 1)
+                .components(Copper, 2, Iron, 1, Antimony, 1, Sulfur, 3)
                 .buildAndRegister();
 
         TinAlloy = new Material.Builder(GTCEu.id("tin_alloy"))
@@ -668,7 +668,7 @@ public class FirstDegreeMaterials {
                 .gem(3).ore()
                 .color(0xe8d73a).secondaryColor(0xf4680f).iconSet(GEM_HORIZONTAL)
                 .appendFlags(EXT_METAL, NO_SMASHING, NO_SMELTING, HIGH_SIFTER_OUTPUT)
-                .components(Aluminium, 2, Silicon, 1, Fluorine, 1, Hydrogen, 2)
+                .components(Aluminium, 2, Silicon, 1, Oxygen, 5, Fluorine, 1, Hydrogen, 1)
                 .buildAndRegister();
 
         Tungstate = new Material.Builder(GTCEu.id("tungstate"))
@@ -1059,7 +1059,7 @@ public class FirstDegreeMaterials {
         GlauconiteSand = new Material.Builder(GTCEu.id("glauconite_sand"))
                 .dust().ore(3, 1)
                 .color(0x1da351).secondaryColor(0x1a6e8f).iconSet(SAND)
-                .components(Potassium, 1, Magnesium, 2, Aluminium, 4, Hydrogen, 2, Oxygen, 12)
+                .components(Potassium, 1, Magnesium, 2, Aluminium, 2, Silicon, 3, Oxygen, 12, Hydrogen, 2, Water)
                 .buildAndRegister();
 
         Malachite = new Material.Builder(GTCEu.id("malachite"))
@@ -1083,7 +1083,7 @@ public class FirstDegreeMaterials {
         Alunite = new Material.Builder(GTCEu.id("alunite"))
                 .dust().ore(3, 1)
                 .color(0xfbd677).secondaryColor(0xe11e0a).iconSet(METALLIC)
-                .components(Potassium, 1, Aluminium, 3, Silicon, 2, Hydrogen, 6, Oxygen, 14)
+                .components(Potassium, 1, Aluminium, 2, Silicon, 2, Hydrogen, 6, Oxygen, 14)
                 .buildAndRegister();
 
         Talc = new Material.Builder(GTCEu.id("talc"))
@@ -1551,7 +1551,7 @@ public class FirstDegreeMaterials {
         Pyrochlore = new Material.Builder(GTCEu.id("pyrochlore"))
                 .dust().ore()
                 .color(0x5b4838).secondaryColor(0x331400).iconSet(METALLIC)
-                .components(Calcium, 2, Niobium, 2, Oxygen, 7)
+                .components(Calcium, 2, Niobium, 2, Oxygen, 6, Fluorine, 1)
                 .buildAndRegister();
 
         PotassiumHydroxide = new Material.Builder(GTCEu.id("potassium_hydroxide"))
