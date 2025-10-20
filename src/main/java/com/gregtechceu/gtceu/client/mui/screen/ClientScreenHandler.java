bb0f75d1b10c3150c8aaf397bf19201ba2cd2b7f
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2025-10-19 23:58:56 -0600

    no more prio

--- src/main/java/com/gregtechceu/gtceu/client/mui/screen/ClientScreenHandler.java
+++ src/main/java/com/gregtechceu/gtceu/client/mui/screen/ClientScreenHandler.java
@@ -82,7 +82,7 @@ public class ClientScreenHandler {
         onGuiChanged(event.getCurrentScreen(), event.getNewScreen());
     }
 
-    @SubscribeEvent(priority = EventPriority.LOWEST)
+    @SubscribeEvent
     public static void onCloseScreen(ScreenEvent.Closing event) {
         onGuiChanged(event.getScreen(), null);
     }
