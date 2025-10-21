7e79aa842553e318a9ecbae67a8ecac13d97a32f
jurrejelle <jurre@jilles.com>
2025-10-21 12:20:14 +0200

    Add default machine UI

--- src/main/java/com/gregtechceu/gtceu/common/data/machines/GTMachineUtils.java
+++ src/main/java/com/gregtechceu/gtceu/common/data/machines/GTMachineUtils.java
@@ -25,6 +25,7 @@ import com.gregtechceu.gtceu.api.machine.multiblock.PartAbility;
 import com.gregtechceu.gtceu.api.machine.property.GTMachineModelProperties;
 import com.gregtechceu.gtceu.api.machine.steam.SimpleSteamMachine;
 import com.gregtechceu.gtceu.api.machine.trait.RecipeLogic;
+import com.gregtechceu.gtceu.api.mui.factory.PanelEditor;
 import com.gregtechceu.gtceu.api.mui.factory.PanelFactory;
 import com.gregtechceu.gtceu.api.pattern.FactoryBlockPattern;
 import com.gregtechceu.gtceu.api.pattern.Predicates;
@@ -39,7 +40,9 @@ import com.gregtechceu.gtceu.client.renderer.machine.DynamicRenderHelper;
 import com.gregtechceu.gtceu.common.block.BoilerFireboxType;
 import com.gregtechceu.gtceu.common.data.*;
 import com.gregtechceu.gtceu.common.data.models.GTMachineModels;
+import com.gregtechceu.gtceu.common.data.mui.GTMuiEditors;
 import com.gregtechceu.gtceu.common.data.mui.GTMuiPanels;
+import com.gregtechceu.gtceu.common.data.mui.GTMuiRecipeTypePanel;
 import com.gregtechceu.gtceu.common.machine.electric.BatteryBufferMachine;
 import com.gregtechceu.gtceu.common.machine.electric.ChargerMachine;
 import com.gregtechceu.gtceu.common.machine.electric.ConverterMachine;
@@ -979,7 +982,8 @@ public class GTMachineUtils {
         public MachineDefinition[] register() {
             if (panelFactory == null) {
                 // TODO: Set default panel factory
-                panelFactory = GTMuiPanels.TEST_PANEL;
+                // TODO 2: Find a way to change it in the other builders in GTMachines? e.g. GTMachines.MACERATOR
+                panelFactory = GTMuiRecipeTypePanel.RECIPE_TYPE.andThen(GTMuiEditors.CHARGE_SLOT);
             }
             return registerTieredMachines(registrate, name,
                     (holder, tier) -> new SimpleTieredMachine(holder, tier, tankScalingFunction), (tier, builder) -> {
