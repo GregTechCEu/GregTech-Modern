0cfa63d1bc879336ae2b6f2382ee98ab84e18a9f
DilithiumThoride <lilithiumathena@gmail.com>
2025-10-26 23:18:52 -0500

    Fix jade provider displaying no energy for addon steam machines consuming 1mB/t

--- src/main/java/com/gregtechceu/gtceu/integration/jade/provider/RecipeLogicProvider.java
+++ src/main/java/com/gregtechceu/gtceu/integration/jade/provider/RecipeLogicProvider.java
@@ -88,18 +88,19 @@ public class RecipeLogicProvider extends CapabilityBlockProvider<RecipeLogic> {
                 var isInput = recipeInfo.getBoolean("isInput");
                 boolean isSteam = false;
 
-                if (blockEntity instanceof MetaMachineBlockEntity mbe) {
-                    var machine = mbe.getMetaMachine();
-                    if (machine instanceof SimpleSteamMachine ssm) {
-                        EUt = (long) (EUt * ssm.getConversionRate());
-                        isSteam = true;
-                    } else if (machine instanceof SteamParallelMultiblockMachine smb) {
-                        EUt = (long) (EUt * smb.getConversionRate());
-                        isSteam = true;
-                    }
-                }
-
                 if (EUt > 0) {
+                    if (blockEntity instanceof MetaMachineBlockEntity mbe) {
+                        var machine = mbe.getMetaMachine();
+                        if (machine instanceof SimpleSteamMachine ssm) {
+                            EUt = (long) (EUt * ssm.getConversionRate());
+                            isSteam = true;
+                        } else if (machine instanceof SteamParallelMultiblockMachine smb) {
+                            EUt = (long) (EUt * smb.getConversionRate());
+                            isSteam = true;
+                        }
+                        if (EUt == 0) EUt = 1; //
+                    }
+
                     MutableComponent text;
 
                     if (isSteam) {
