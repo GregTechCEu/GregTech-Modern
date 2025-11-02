317d665a418f65c4d1e6ce5e48b79af931ecdf84
jurrejelle <jurre@jilles.com>
2025-11-02 09:16:01 +0100

    Add plate check before adding plate pipe crafting recipes

--- src/main/java/com/gregtechceu/gtceu/data/recipe/generated/PipeRecipeHandler.java
+++ src/main/java/com/gregtechceu/gtceu/data/recipe/generated/PipeRecipeHandler.java
@@ -106,7 +106,7 @@ public final class PipeRecipeHandler {
                     .duration((int) (material.getMass()))
                     .EUt(6L * getVoltageMultiplier(material))
                     .save(provider);
-        } else {
+        } else if (plate.doGenerateItem(material)) {
             VanillaRecipeHelper.addShapedRecipe(provider, String.format("tiny_%s_pipe", material.getName()),
                     pipeStack.copyWithCount(2), " s ", "hXw",
                     'X', new MaterialEntry(plate, material));
@@ -147,7 +147,7 @@ public final class PipeRecipeHandler {
                     .duration((int) (material.getMass()))
                     .EUt(6L * getVoltageMultiplier(material))
                     .save(provider);
-        } else {
+        } else if (plate.doGenerateItem(material)) {
             VanillaRecipeHelper.addShapedRecipe(provider, String.format("small_%s_pipe", material.getName()),
                     pipeStack, "wXh",
                     'X', new MaterialEntry(plate, material));
@@ -188,7 +188,7 @@ public final class PipeRecipeHandler {
                     .duration((int) material.getMass() * 3)
                     .EUt(6L * getVoltageMultiplier(material))
                     .save(provider);
-        } else {
+        } else if (plate.doGenerateItem(material)) {
             VanillaRecipeHelper.addShapedRecipe(provider, String.format("medium_%s_pipe", material.getName()),
                     pipeStack, "XXX", "w h",
                     'X', new MaterialEntry(plate, material));
@@ -229,7 +229,7 @@ public final class PipeRecipeHandler {
                     .duration((int) material.getMass() * 6)
                     .EUt(6L * getVoltageMultiplier(material))
                     .save(provider);
-        } else {
+        } else if (plate.doGenerateItem(material)) {
             VanillaRecipeHelper.addShapedRecipe(provider, String.format("large_%s_pipe", material.getName()),
                     pipeStack, "XXX", "w h", "XXX",
                     'X', new MaterialEntry(plate, material));
@@ -323,18 +323,22 @@ public final class PipeRecipeHandler {
     }
 
     private static void addDuctRecipes(Consumer<FinishedRecipe> provider, Material material, int outputAmount) {
-        VanillaRecipeHelper.addShapedRecipe(provider, "small_duct_%s".formatted(material.getName()),
-                GTBlocks.DUCT_PIPES[DuctPipeType.SMALL.ordinal()].asStack(outputAmount * 2), "w", "X", "h",
-                'X', new MaterialEntry(plate, material));
-        VanillaRecipeHelper.addShapedRecipe(provider, "medium_duct_%s".formatted(material.getName()),
-                GTBlocks.DUCT_PIPES[DuctPipeType.NORMAL.ordinal()].asStack(outputAmount), " X ", "wXh", " X ",
-                'X', new MaterialEntry(plate, material));
-        VanillaRecipeHelper.addShapedRecipe(provider, "large_duct_%s".formatted(material.getName()),
-                GTBlocks.DUCT_PIPES[DuctPipeType.LARGE.ordinal()].asStack(outputAmount), "XwX", "X X", "XhX",
-                'X', new MaterialEntry(plate, material));
-        VanillaRecipeHelper.addShapedRecipe(provider, "huge_duct_%s".formatted(material.getName()),
-                GTBlocks.DUCT_PIPES[DuctPipeType.HUGE.ordinal()].asStack(outputAmount), "XwX", "X X", "XhX",
-                'X', new MaterialEntry(plateDouble, material));
+        if (plate.doGenerateItem(material)) {
+            VanillaRecipeHelper.addShapedRecipe(provider, "small_duct_%s".formatted(material.getName()),
+                    GTBlocks.DUCT_PIPES[DuctPipeType.SMALL.ordinal()].asStack(outputAmount * 2), "w", "X", "h",
+                    'X', new MaterialEntry(plate, material));
+            VanillaRecipeHelper.addShapedRecipe(provider, "medium_duct_%s".formatted(material.getName()),
+                    GTBlocks.DUCT_PIPES[DuctPipeType.NORMAL.ordinal()].asStack(outputAmount), " X ", "wXh", " X ",
+                    'X', new MaterialEntry(plate, material));
+            VanillaRecipeHelper.addShapedRecipe(provider, "large_duct_%s".formatted(material.getName()),
+                    GTBlocks.DUCT_PIPES[DuctPipeType.LARGE.ordinal()].asStack(outputAmount), "XwX", "X X", "XhX",
+                    'X', new MaterialEntry(plate, material));
+        }
+        if (plateDouble.doGenerateItem(material)) {
+            VanillaRecipeHelper.addShapedRecipe(provider, "huge_duct_%s".formatted(material.getName()),
+                    GTBlocks.DUCT_PIPES[DuctPipeType.HUGE.ordinal()].asStack(outputAmount), "XwX", "X X", "XhX",
+                    'X', new MaterialEntry(plateDouble, material));
+        }
     }
 
     private static int getVoltageMultiplier(Material material) {
