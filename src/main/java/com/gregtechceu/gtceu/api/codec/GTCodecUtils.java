1088ae386723ac911a75cb94220635a74ec9095c
screret <68943070+screret@users.noreply.github.com>
2025-11-14 14:18:12 +0200

    undo AdjacentBlockCondition and AdjacentFluidCondition being serialized as a string blob "reverts" #3833 and #3830

--- src/main/java/com/gregtechceu/gtceu/api/codec/GTCodecUtils.java
+++ src/main/java/com/gregtechceu/gtceu/api/codec/GTCodecUtils.java
@@ -1,5 +1,8 @@
 package com.gregtechceu.gtceu.api.codec;
 
+import com.gregtechceu.gtceu.utils.memoization.GTMemoizer;
+import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.DynamicOps;
 import net.minecraft.util.ExtraCodecs;
 
 import com.mojang.datafixers.util.Either;
@@ -7,8 +10,11 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
-public class GTCodecUtils {
+public final class GTCodecUtils {
+
+    private GTCodecUtils() {}
 
     public static final Codec<Long> NON_NEGATIVE_LONG = longRangeWithMessage(0, Long.MAX_VALUE,
             (val) -> "Value must be non-negative: " + val);
@@ -32,4 +38,29 @@ public class GTCodecUtils {
     public static <T> T unboxEither(Either<T, T> either) {
         return either.map(Function.identity(), Function.identity());
     }
+
+    public static <T> Codec<Supplier<T>> lazyParsingCodec(Codec<T> delegate) {
+        return new LazyParsingCodec<>(delegate);
+    }
+
+    private record LazyParsingCodec<A>(Codec<A> codec) implements Codec<Supplier<A>> {
+
+        @Override
+        public <T> DataResult<Pair<Supplier<A>, T>> decode(DynamicOps<T> ops, T input) {
+            return DataResult.success(Pair.of(GTMemoizer.memoize(() -> deferredDecode(ops, input)), input));
+        }
+
+        @Override
+        public <T> DataResult<T> encode(Supplier<A> input, DynamicOps<T> ops, T prefix) {
+            return input.get() == null ? DataResult.success(prefix) : this.codec.encode(input.get(), ops, prefix);
+        }
+
+        private <T> A deferredDecode(DynamicOps<T> ops, T input) {
+            return this.codec.decode(ops, input).get()
+                    .map(Pair::getFirst, partial -> {
+                        throw new IllegalStateException("Unable to parse deferred value: " + partial.message());
+                    });
+        }
+    }
+
 }
--- src/main/java/com/gregtechceu/gtceu/data/recipe/builder/GTRecipeBuilder.java
+++ src/main/java/com/gregtechceu/gtceu/data/recipe/builder/GTRecipeBuilder.java
@@ -47,6 +47,7 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -1117,10 +1118,18 @@ public class GTRecipeBuilder {
     }
 
     public GTRecipeBuilder dimension(ResourceLocation dimension, boolean reverse) {
-        return addCondition(new DimensionCondition(dimension).setReverse(reverse));
+        return dimension(ResourceKey.create(Registries.DIMENSION, dimension), reverse);
     }
 
     public GTRecipeBuilder dimension(ResourceLocation dimension) {
+        return dimension(dimension, false);
+    }
+
+    public GTRecipeBuilder dimension(ResourceKey<Level> dimension, boolean reverse) {
+        return addCondition(new DimensionCondition(dimension).setReverse(reverse));
+    }
+
+    public GTRecipeBuilder dimension(ResourceKey<Level> dimension) {
         return dimension(dimension, false);
     }
 
