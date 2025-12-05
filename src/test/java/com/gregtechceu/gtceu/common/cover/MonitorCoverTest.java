d03596f3905187ad4fe6fec04a3aedad5b2afece
TarLaboratories <levtaranov3@gmail.com>
2025-12-05 11:30:12 +0300

    unused import

--- src/test/java/com/gregtechceu/gtceu/common/cover/MonitorCoverTest.java
+++ src/test/java/com/gregtechceu/gtceu/common/cover/MonitorCoverTest.java
@@ -14,8 +14,6 @@ import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraftforge.gametest.GameTestHolder;
 import net.minecraftforge.gametest.PrefixGameTestTemplate;
 
-import java.util.List;
-
 @PrefixGameTestTemplate(false)
 @GameTestHolder(GTCEu.MOD_ID)
 public class MonitorCoverTest {
