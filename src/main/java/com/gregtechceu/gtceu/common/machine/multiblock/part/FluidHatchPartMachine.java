83aca8c8c2623560a21fa74dab7dc187e12f1e19
TarLaboratories <levtaranov3@gmail.com>
2025-12-19 11:33:47 +0300

    remove commented out code

--- src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/part/FluidHatchPartMachine.java
+++ src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/part/FluidHatchPartMachine.java
@@ -281,71 +281,6 @@ public class FluidHatchPartMachine extends TieredIOPartMachine implements IMachi
         return true;
     }
 
-    //////////////////////////////////////
-    // ********** GUI ***********//
-    //////////////////////////////////////
-
-    /*
-     * @Override
-     * public void attachConfigurators(ConfiguratorPanel configuratorPanel) {
-     * super.attachConfigurators(configuratorPanel);
-     * if (isCircuitSlotEnabled() && this.io == IO.IN) {
-     * configuratorPanel.attachConfigurators(new CircuitFancyConfigurator(circuitInventory.storage));
-     * }
-     * }
-     * 
-     * @Override
-     * public Widget createUIWidget() {
-     * if (slots == 1) {
-     * return createSingleSlotGUI();
-     * } else {
-     * return createMultiSlotGUI();
-     * }
-     * }
-     * 
-     * protected Widget createSingleSlotGUI() {
-     * var group = new WidgetGroup(0, 0, 89, 63);
-     * group.addWidget(new ImageWidget(4, 4, 81, 55, GuiTextures.DISPLAY));
-     * TankWidget tankWidget;
-     * 
-     * // Add input/output-specific widgets
-     * if (this.io == IO.OUT) {
-     * // if this is an output hatch, assign tankWidget to the phantom widget displaying the locked fluid...
-     * group.addWidget(tankWidget = new PhantomFluidWidget(this.tank.getLockedFluid(), 0, 67, 40, 18, 18,
-     * () -> this.tank.getLockedFluid().getFluid(), f -> {
-     * if (!this.tank.getFluidInTank(0).isEmpty()) {
-     * return;
-     * }
-     * if (f == null || f.isEmpty()) {
-     * this.tank.setLocked(false);
-     * } else {
-     * FluidStack newFluid = f.copy();
-     * newFluid.setAmount(1);
-     * this.tank.setLocked(true, newFluid);
-     * }
-     * }).setShowAmount(false).setDrawHoverTips(true).setBackground(GuiTextures.FLUID_SLOT));
-     * 
-     * group.addWidget(new ToggleButtonWidget(7, 40, 18, 18,
-     * GuiTextures.BUTTON_LOCK, this.tank::isLocked, this.tank::setLocked)
-     * .setTooltipText("gtceu.gui.fluid_lock.tooltip")
-     * .setShouldUseBaseBackground())
-     * // ...and add the actual tank widget separately.
-     * .addWidget(new TankWidget(tank.getStorages()[0], 67, 22, 18, 18, true, io.support(IO.IN))
-     * .setShowAmount(true).setDrawHoverTips(true).setBackground(GuiTextures.FLUID_SLOT));
-     * } else {
-     * group.addWidget(tankWidget = new TankWidget(tank.getStorages()[0], 67, 22, 18, 18, true, io.support(IO.IN))
-     * .setShowAmount(true).setDrawHoverTips(true).setBackground(GuiTextures.FLUID_SLOT));
-     * }
-     * 
-     * group.addWidget(new LabelWidget(8, 8, "gtceu.gui.fluid_amount"))
-     * .addWidget(new LabelWidget(8, 18, () -> getFluidAmountText(tankWidget)))
-     * .addWidget(new LabelWidget(8, 28, () -> getFluidNameText(tankWidget).getString()));
-     * 
-     * group.setBackground(GuiTextures.BACKGROUND_INVERSE);
-     * return group;
-     * }
-     */
-
     private Component getFluidNameText() {
         Component translation;
         if (!this.tank.getFluidInTank(0).isEmpty()) {
@@ -376,34 +311,6 @@ public class FluidHatchPartMachine extends TieredIOPartMachine implements IMachi
     public String getFormattedFluidAmount(FluidStack fluidStack) {
         return String.format("%,d", fluidStack.isEmpty() ? 0 : fluidStack.getAmount());
     }
-
-    /*
-     * protected Widget createMultiSlotGUI() {
-     * int rowSize = (int) Math.sqrt(slots);
-     * int colSize = rowSize;
-     * if (slots == 8) {
-     * rowSize = 4;
-     * colSize = 2;
-     * }
-     * 
-     * var group = new WidgetGroup(0, 0, 18 * rowSize + 16, 18 * colSize + 16);
-     * var container = new WidgetGroup(4, 4, 18 * rowSize + 8, 18 * colSize + 8);
-     * 
-     * int index = 0;
-     * for (int y = 0; y < colSize; y++) {
-     * for (int x = 0; x < rowSize; x++) {
-     * container.addWidget(
-     * new TankWidget(tank.getStorages()[index++], 4 + x * 18, 4 + y * 18, true, io.support(IO.IN))
-     * .setBackground(GuiTextures.FLUID_SLOT));
-     * }
-     * }
-     * 
-     * container.setBackground(GuiTextures.BACKGROUND_INVERSE);
-     * group.addWidget(container);
-     * 
-     * return group;
-     * }
-     */
 
     @Override
     public ModularPanel buildUI(PosGuiData data, PanelSyncManager syncManager, UISettings settings) {
