651caae444989e719e2a6495b03a21106141eda6
screret <68943070+screret@users.noreply.github.com>
2026-01-19 12:01:36 +0200

    implement the relative scaling logic shimmer had

--- src/main/java/com/gregtechceu/gtceu/core/mixins/client/PostChainMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/client/PostChainMixin.java
@@ -4,6 +4,8 @@ import net.minecraft.client.renderer.PostChain;
 import net.minecraft.util.GsonHelper;
 
 import com.google.gson.JsonObject;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.pipeline.RenderTarget;
 import org.lwjgl.opengl.GL11;
@@ -23,12 +25,36 @@ public class PostChainMixin {
     @Final
     private Map<String, RenderTarget> customRenderTargets;
 
+    @WrapOperation(method = "parseTargetNode",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/util/GsonHelper;getAsInt(Lcom/google/gson/JsonObject;Ljava/lang/String;I)I",
+                    ordinal = 0))
+    private int gtceu$scaleTargetWidth(JsonObject json, String name, int screenWidth, Operation<Integer> original) {
+        if (GsonHelper.isNumberValue(json, "scale_width")) {
+            return (int) (GsonHelper.getAsFloat(json, "scale_width", 1.0f) * screenWidth);
+        }
+        return original.call(json, name, screenWidth);
+    }
+
+    @WrapOperation(method = "parseTargetNode",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/util/GsonHelper;getAsInt(Lcom/google/gson/JsonObject;Ljava/lang/String;I)I",
+                    ordinal = 1))
+    private int gtceu$scaleTargetHeight(JsonObject json, String name, int screenHeight, Operation<Integer> original) {
+        if (GsonHelper.isNumberValue(json, "scale_height")) {
+            return (int) (GsonHelper.getAsFloat(json, "scale_height", 1.0f) * screenHeight);
+        }
+        return original.call(json, name, screenHeight);
+    }
+
     @Inject(method = "parseTargetNode",
             at = @At(value = "INVOKE",
                      target = "Lnet/minecraft/client/renderer/PostChain;addTempTarget(Ljava/lang/String;II)V",
                      ordinal = 1,
                      shift = At.Shift.AFTER))
-    private void gtceu$scaleTargetSize(CallbackInfo ci, @Local JsonObject json, @Local String name) {
+    private void gtceu$makeTargetLinear(CallbackInfo ci, @Local JsonObject json, @Local String name) {
         if (GsonHelper.getAsBoolean(json, "bilinear", false)) {
             this.customRenderTargets.get(name).setFilterMode(GL11.GL_LINEAR);
         }
