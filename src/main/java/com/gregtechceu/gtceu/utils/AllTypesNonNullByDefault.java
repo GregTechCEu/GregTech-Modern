a799f81d9b56bd3f744907e24bfe39dfcda2989b
Gustavo <ofoxsmith@outlook.com>
2026-01-26 10:32:30 +1100

    documented

--- src/main/java/com/gregtechceu/gtceu/utils/AllTypesNonNullByDefault.java
+++ src/main/java/com/gregtechceu/gtceu/utils/AllTypesNonNullByDefault.java
@@ -1,13 +1,13 @@
 package com.gregtechceu.gtceu.utils;
 
 
-import javax.annotation.Nonnull;
 import javax.annotation.meta.TypeQualifierDefault;
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-@Nonnull
+@Documented
 @TypeQualifierDefault({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
 @Retention(RetentionPolicy.SOURCE)
 public @interface AllTypesNonNullByDefault {}
