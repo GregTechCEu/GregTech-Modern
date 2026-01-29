79566d4c5765c4bc464984b409c414fd59371c31
Gustavo <ofoxsmith@outlook.com>
2026-01-30 08:50:11 +1100

    switch to using normal forge update packet

--- src/main/java/com/gregtechceu/gtceu/syncsystem/ManagedSyncBlockEntity.java
+++ src/main/java/com/gregtechceu/gtceu/syncsystem/ManagedSyncBlockEntity.java
@@ -2,11 +2,13 @@ package com.gregtechceu.gtceu.syncsystem;
 
 import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.blockentity.BlockEntityCreationInfo;
-import com.gregtechceu.gtceu.common.network.GTNetwork;
-import com.gregtechceu.gtceu.syncsystem.network.SPacketUpdateBESyncValue;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
@@ -14,6 +16,9 @@ import net.minecraft.world.level.block.state.BlockState;
 import lombok.Getter;
 import lombok.Setter;
 import org.jetbrains.annotations.MustBeInvokedByOverriders;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
 
 /**
  * A BlockEntity that manages sync and save data via the {@code ISyncManaged} syncdata system.
@@ -36,14 +41,20 @@ public abstract class ManagedSyncBlockEntity extends BlockEntity implements ISyn
         super(type, pos, blockState);
     }
 
-    // Called when this BlockEntity is saved or loaded
-
+    /**
+     * Saves BE data to world save.
+     */
     @Override
     protected final void saveAdditional(CompoundTag tag) {
         super.saveAdditional(tag);
         tag.merge(getSyncDataHolder().serializeNBT(false));
     }
 
+    /**
+     * Loads BE data from world save or from a client update packet.
+     * Override this to add logic for modifying saved data before it is loaded (e.g. for cross-version compatibility).
+     * When overriding, {@code super.load(tag)} must be called AFTER any custom logic.
+     */
     @Override
     @MustBeInvokedByOverriders
     public void load(CompoundTag tag) {
@@ -52,12 +63,23 @@ public abstract class ManagedSyncBlockEntity extends BlockEntity implements ISyn
                 (getLevel() == null ? GTCEu.isClientThread() : getLevel().isClientSide));
     }
 
+    /**
+     * Called to gather BE data to be sent when a client loads this BE.
+     */
     @Override
     public CompoundTag getUpdateTag() {
         CompoundTag tag = new CompoundTag();
         getSyncDataHolder().resyncAllFields();
         tag.merge(getSyncDataHolder().serializeNBT(true, true));
         return tag;
+    }
+
+    /**
+     * Called to get an update packet which is sent to clients to notify them when a loaded BE's data changes.
+     */
+    @Override
+    public @Nullable Packet<ClientGamePacketListener> getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this, b -> getSyncDataHolder().serializeNBT(true));
     }
 
     @Override
@@ -68,10 +90,7 @@ public abstract class ManagedSyncBlockEntity extends BlockEntity implements ISyn
     public final void updateTick() {
         setChanged();
         if (isDirty) {
-            var level = getLevel();
-            if (level == null) return;
-            GTNetwork.sendToAllPlayersTrackingChunk(level.getChunkAt(getBlockPos()),
-                    new SPacketUpdateBESyncValue(this));
+            Objects.requireNonNull(getLevel()).sendBlockUpdated(getBlockPos(), getBlockState(), getBlockState(), Block.UPDATE_CLIENTS);
             isDirty = false;
         }
     }
