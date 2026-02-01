f4ff940dcc39fce8e0090a3b42ccae4231b38bf7
screret <68943070+screret@users.noreply.github.com>
2026-02-01 15:47:03 +0200

    add a way to enable checks in RuntimeExistingFileHelper, making it no-op won't work as `#exists` is actually used in a couple things

--- src/main/java/com/gregtechceu/gtceu/utils/data/RuntimeExistingFileHelper.java
+++ src/main/java/com/gregtechceu/gtceu/utils/data/RuntimeExistingFileHelper.java
@@ -10,6 +10,9 @@ import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
 import java.io.FileNotFoundException;
 import java.util.Collections;
 import java.util.List;
@@ -19,17 +22,31 @@ import javax.annotation.ParametersAreNonnullByDefault;
 /**
  * Existing file helper that wraps the client/server resource manager instead of creating its own.<br>
  * Useful for using data generators outside datagen.
+ * <p>
+ * By default, this class assumes all resources exist and does not check any references' validity.
+ * To enable actual checking, you may use a try-with-resources statement like this:
+ * <pre>{@code
+ * try (var helper = RuntimeExistingFileHelper.INSTANCE.enableChecks()) {
+ *     // If you don't use a try-with-resources or try-finally block to
+ *     // enable checking, calling `exists` will always return true.
+ *     if (helper.exists(texture, GTBlockstateProvider.TEXTURE)) {
+ *         // do stuff
+ *     }
+ * }
+ * }</pre>
+ *
  */
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class RuntimeExistingFileHelper extends ExistingFileHelper {
 
-    public static final RuntimeExistingFileHelper INSTANCE = new RuntimeExistingFileHelper();
+    public static final RuntimeExistingFileHelper INSTANCE = new RuntimeExistingFileHelper(HashMultimap.create());
 
-    // protected final Multimap<PackType, ResourceLocation> generated = HashMultimap.create();
+    protected final Multimap<PackType, ResourceLocation> generated;
 
-    protected RuntimeExistingFileHelper() {
+    protected RuntimeExistingFileHelper(Multimap<PackType, ResourceLocation> generated) {
         super(Collections.emptySet(), Collections.emptySet(), false, null, null);
+        this.generated = generated;
     }
 
     public static ResourceManager getManager(PackType packType) {
@@ -49,10 +66,22 @@ public class RuntimeExistingFileHelper extends ExistingFileHelper {
         return base.withPath(path -> prefix + "/" + path + suffix);
     }
 
+    public RuntimeExistingFileHelper.Active enableChecks() {
+        // pass the same generated resources map into the subclass
+        // so any resources added/checked by it are automatically updated here
+        return new Active(this.generated);
+    }
+
+    /**
+     * Bypass the normal {@code exists} function so missing/invalid references etc. don't cause runtime errors.<br>
+     * A toggle for enabling proper functionality is implemented in the form of {@link #enableChecks()}-
+     */
     @Override
     public boolean exists(ResourceLocation loc, PackType packType) {
-        return true; // generated.get(packType).contains(loc) || getManager(packType).getResource(loc).isPresent();
+        return true;
     }
+
+    /// Implement a copy of the normal {@code exists} function that we can use for checking
 
     @Override
     public void trackGenerated(ResourceLocation loc, IResourceType type) {
@@ -61,7 +90,7 @@ public class RuntimeExistingFileHelper extends ExistingFileHelper {
 
     @Override
     public void trackGenerated(ResourceLocation loc, PackType packType, String suffix, String prefix) {
-        // this.generated.put(packType, getLocation(loc, prefix, suffix));
+        this.generated.put(packType, getLocation(loc, prefix, suffix));
     }
 
     @Override
@@ -78,5 +107,32 @@ public class RuntimeExistingFileHelper extends ExistingFileHelper {
     @Override
     public List<Resource> getResourceStack(ResourceLocation loc, PackType packType) {
         return getManager(packType).getResourceStack(loc);
+    }
+
+    /**
+     * This class implements {@link AutoCloseable} for ease of enabling actual checking when it is required.
+     * <p>
+     * Note that it's safe to ignore "unclosed AutoCloseable"/{@code resource} warnings on this class, as is done in
+     * {@linkplain com.gregtechceu.gtceu.client.model.machine.overlays.WorkableOverlays#get(ResourceLocation, ExistingFileHelper) WorkableOverlays#get}.
+     * </p>
+     */
+    public static class Active extends RuntimeExistingFileHelper implements AutoCloseable {
+
+        protected Active(Multimap<PackType, ResourceLocation> parentGenerated) {
+            super(parentGenerated);
+        }
+
+        @Override
+        public Active enableChecks() {
+            return this;
+        }
+
+        @Override
+        public boolean exists(ResourceLocation loc, PackType packType) {
+            return this.generated.get(packType).contains(loc) || getManager(packType).getResource(loc).isPresent();
+        }
+
+        @Override
+        public void close() {}
     }
 }
