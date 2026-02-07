6dd1918879b51e170697f4c59d342cc43746ffc7
Ghostipedia <Ghostipedia@gmail.com>
2026-02-06 22:40:59 -0500

    GO GO GADGET ENUMS

---
+++ src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/fission/ReactorComponentType.java
@@ -0,0 +1,10 @@
+package com.gregtechceu.gtceu.common.machine.multiblock.fission;
+
+public enum ReactorComponentType {
+    FUEL_ROD,
+    COOLANT_CHANNEL,
+    HEAT_EXCHANGER,
+    NEUTRON_REFLECTOR,
+    MODERATOR,
+    CONTROL_ROD
+}
