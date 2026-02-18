0d5c6915eff8d1f4284d76a3004ab18a3670ab17
Gustavo <ofoxsmith@outlook.com>
2026-02-18 16:55:14 +1100

    fix sync objects not being fully loaded on client

--- src/main/java/com/gregtechceu/gtceu/api/sync_system/data_transformers/ValueTransformer.java
+++ src/main/java/com/gregtechceu/gtceu/api/sync_system/data_transformers/ValueTransformer.java
@@ -5,7 +5,6 @@ import com.gregtechceu.gtceu.api.sync_system.TypeDeclaration;
 
 import net.minecraft.nbt.Tag;
 
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -19,17 +18,21 @@ public interface ValueTransformer<T> {
     /**
      * A record holding information about the context from which this value transformer is currently being invoked.
      * 
-     * @param holder       The sync object which holds the specific field being serialized by this transformer.
-     * @param type         An object describing the type of the field currently being serialized/deserialized.
-     * @param currentValue The current value (if any) of the field currently being serialized/deserialized.
-     * @param fieldName    The name of the field being serialized, or a string denoting the current sync context if not
-     *                     being invoked directly on a field.
-     * @param isClientSync Whether NBT is currently being generated as part of a sync update to the client, not as NBT
-     *                     being
-     *                     written to the server save.
+     * @param holder         The sync object which holds the specific field being serialized by this transformer.
+     * @param type           An object describing the type of the field currently being serialized/deserialized.
+     * @param currentValue   The current value (if any) of the field currently being serialized/deserialized.
+     * @param fieldName      The name of the field being serialized, or a string denoting the current sync context if
+     *                       not
+     *                       being invoked directly on a field.
+     * @param isClientSync   Whether NBT is currently being generated as part of a sync update to the client, not as NBT
+     *                       being
+     *                       written to the server save.
+     * @param fullClientSync Whether NBT is currently being generated to send to the client as part of a clientside
+     *                       chunk load
      */
-    record TransformerContext<U>(@NotNull ISyncManaged holder, @NotNull TypeDeclaration type,
-                                 @Nullable U currentValue, @Nullable String fieldName, boolean isClientSync) {}
+    record TransformerContext<U>(ISyncManaged holder, TypeDeclaration type,
+                                 @Nullable U currentValue, @Nullable String fieldName, boolean isClientSync,
+                                 boolean fullClientSync) {}
 
     /**
      * Casts a given NBT tag to a specific tag type, throwing an error if the tag cannot be casted.
