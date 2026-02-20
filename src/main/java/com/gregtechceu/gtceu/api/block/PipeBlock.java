0e9eec1b87217182b7548d5e05a54b42a99f3e56
Bernd <133775547+bernd-sbr@users.noreply.github.com>
2026-02-20 20:12:05 +0100

    PipeBlock neighbor change logic fix (#4664)

--- src/main/java/com/gregtechceu/gtceu/api/block/PipeBlock.java
+++ src/main/java/com/gregtechceu/gtceu/api/block/PipeBlock.java
@@ -270,6 +270,7 @@ public abstract class PipeBlock<PipeType extends Enum<PipeType> & IPipeType<Node
                     pipeTile.setConnection(facing, false, false);
                 updateActiveNodeStatus(level, pos, pipeTile);
             }
+            pipeTile.getCoverContainer().onNeighborChanged(block, fromPos, isMoving);
         }
     }
 
