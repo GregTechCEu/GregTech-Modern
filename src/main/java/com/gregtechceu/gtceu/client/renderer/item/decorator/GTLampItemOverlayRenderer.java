5e913ddd8ac2df4a8053e3802cb00cbd922510b3
screret <68943070+screret@users.noreply.github.com>
2026-02-21 20:03:33 +0200

    make item decorators less convoluted

--- src/main/java/com/gregtechceu/gtceu/client/renderer/item/decorator/GTLampItemOverlayRenderer.java
+++ src/main/java/com/gregtechceu/gtceu/client/renderer/item/decorator/GTLampItemOverlayRenderer.java
@@ -1,6 +1,7 @@
 package com.gregtechceu.gtceu.client.renderer.item.decorator;
 
 import com.gregtechceu.gtceu.api.gui.GuiTextures;
+import com.gregtechceu.gtceu.api.item.LampBlockItem;
 import com.gregtechceu.gtceu.data.item.GTDataComponents;
 
 import net.minecraft.client.gui.Font;
@@ -20,51 +21,24 @@ public class GTLampItemOverlayRenderer implements IItemDecorator {
 
     private GTLampItemOverlayRenderer() {}
 
-    public static OverlayType getOverlayType(boolean light, boolean bloom) {
-        if (light) {
-            return bloom ? OverlayType.NONE : OverlayType.NO_BLOOM;
-        } else {
-            return bloom ? OverlayType.NO_LIGHT : OverlayType.NO_BLOOM_NO_LIGHT;
-        }
-    }
-
     @Override
     public boolean render(@NotNull GuiGraphics graphics, @NotNull Font font,
                           ItemStack stack, int xPosition, int yPosition) {
-        var lampData = stack.get(GTDataComponents.LAMP_DATA);
-        if (lampData != null) {
-            var overlayType = getOverlayType(lampData.lit(), lampData.bloom());
-            if (overlayType == OverlayType.NONE) {
-                return true;
-            }
-
-            RenderSystem.disableDepthTest();
-            if (overlayType.noBloom()) {
-                GuiTextures.LAMP_NO_BLOOM.draw(graphics, 0, 0, xPosition, yPosition, 16, 16);
-            }
-
-            if (overlayType.noLight()) {
-                GuiTextures.LAMP_NO_LIGHT.draw(graphics, 0, 0, xPosition, yPosition, 16, 16);
-            }
-            RenderSystem.enableDepthTest();
-            return true;
+        LampBlockItem.LampData lampData = stack.get(GTDataComponents.LAMP_DATA);
+        if (lampData == null) {
+            return false;
         }
-        return false;
-    }
-
-    public enum OverlayType {
-
-        NONE,
-        NO_BLOOM,
-        NO_LIGHT,
-        NO_BLOOM_NO_LIGHT;
-
-        public boolean noLight() {
-            return this == NO_LIGHT || this == NO_BLOOM_NO_LIGHT;
+        if (lampData.lit() && lampData.bloom()) {
+            return false;
         }
 
-        public boolean noBloom() {
-            return this == NO_BLOOM || this == NO_BLOOM_NO_LIGHT;
+        RenderSystem.disableDepthTest();
+        if (!lampData.bloom()) {
+            GuiTextures.LAMP_NO_BLOOM.draw(graphics, 0, 0, xPosition, yPosition, 16, 16);
         }
+        if (!lampData.lit()) {
+            GuiTextures.LAMP_NO_LIGHT.draw(graphics, 0, 0, xPosition, yPosition, 16, 16);
+        }
+        return true;
     }
 }
