884df657fe5146f4bc58492e2270f02e0b3cffeb
screret <68943070+screret@users.noreply.github.com>
2026-02-21 20:07:48 +0200

    javac didn't like this cast

--- src/main/java/com/gregtechceu/gtceu/integration/kjs/builders/block/ActiveBlockBuilder.java
+++ src/main/java/com/gregtechceu/gtceu/integration/kjs/builders/block/ActiveBlockBuilder.java
@@ -23,7 +23,7 @@ public class ActiveBlockBuilder extends BlockBuilder {
     // spotless:off
     public static final UnaryOperator<String> ACTIVE = s -> s + "_active";
     public static final UnaryOperator<String> BLOOM = s -> s + "_bloom";
-    public static final UnaryOperator<String> ACTIVE_BLOCK = (UnaryOperator<String>) ID.BLOCK.andThen(ACTIVE);
+    public static final UnaryOperator<String> ACTIVE_BLOCK = ID.BLOCK.andThen(ACTIVE)::apply;
     public static final ResourceLocation CUBE_2_LAYER_ALL_MODEL = GTCEu.id("block/cube_2_layer/all");
     public static final ResourceLocation CUBE_BOTTOM_TOP_MODEL = ResourceLocation.withDefaultNamespace("block/cube_bottom_top");
     public static final ResourceLocation FIRE_BOX_ACTIVE_MODEL = GTCEu.id("block/fire_box_active");
