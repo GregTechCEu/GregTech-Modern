838584cc07b53e03dab94ea331239cf267879fdf
Mqrius <Nuntius.Marii@Gmail.com>
2026-02-23 08:41:31 +0100

    Clear categoryMap on reload to reload EMI correctly (#4683)

--- src/main/java/com/gregtechceu/gtceu/api/recipe/GTRecipeType.java
+++ src/main/java/com/gregtechceu/gtceu/api/recipe/GTRecipeType.java
@@ -332,6 +332,12 @@ public class GTRecipeType implements RecipeType<GTRecipe> {
         return db;
     }
 
+    @ApiStatus.Internal
+    public void beginStagingRecipes() {
+        categoryMap.clear();
+        additionHandler.beginStaging();
+    }
+
     public interface ICustomRecipeLogic {
 
         /**
