716727f626694561ffab1d11010c32458d9f2c8d
Gustavo <ofoxsmith@outlook.com>
2026-02-23 18:20:43 +1100

    spotless

--- src/main/java/com/gregtechceu/gtceu/common/item/GTBoatItem.java
+++ src/main/java/com/gregtechceu/gtceu/common/item/GTBoatItem.java
@@ -2,8 +2,8 @@ package com.gregtechceu.gtceu.common.item;
 
 import com.gregtechceu.gtceu.common.entity.GTBoat;
 import com.gregtechceu.gtceu.common.entity.GTChestBoat;
-
 import com.gregtechceu.gtceu.common.item.behavior.GTBoatItemDispenseBehaviour;
+
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
