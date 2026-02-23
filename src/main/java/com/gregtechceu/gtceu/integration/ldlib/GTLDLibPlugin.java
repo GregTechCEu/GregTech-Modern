35d81c1a5d2aa3094a46605069d9ec1c1db4935f
screret <68943070+screret@users.noreply.github.com>
2026-02-23 20:32:46 +0200

    remove debug logging in GTLDLibPlugin

--- src/main/java/com/gregtechceu/gtceu/integration/ldlib/GTLDLibPlugin.java
+++ src/main/java/com/gregtechceu/gtceu/integration/ldlib/GTLDLibPlugin.java
@@ -5,15 +5,12 @@ import com.gregtechceu.gtceu.data.inject.GTSyncedFieldAccessors;
 import com.lowdragmc.lowdraglib.plugin.ILDLibPlugin;
 import com.lowdragmc.lowdraglib.plugin.LDLibPlugin;
 
-import org.apache.logging.log4j.LogManager;
-
 @SuppressWarnings("unused")
 @LDLibPlugin
 public class GTLDLibPlugin implements ILDLibPlugin {
 
     @Override
     public void onLoad() {
-        LogManager.getLogger().warn("LDLib plugin is loading!");
         GTSyncedFieldAccessors.init();
     }
 }
