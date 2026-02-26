0318697a4c1846d026b9f29a471740fdb52484bd
Gustavo <ofoxsmith@outlook.com>
2026-02-27 10:20:03 +1100

    fix recipe builder issue

--- src/test/java/com/gregtechceu/gtceu/api/recipe/GTRecipeSerializerTest.java
+++ src/test/java/com/gregtechceu/gtceu/api/recipe/GTRecipeSerializerTest.java
@@ -2,10 +2,10 @@ package com.gregtechceu.gtceu.api.recipe;
 
 import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.registry.GTRegistries;
-import com.gregtechceu.gtceu.common.recipe.builder.GTRecipeBuilder;
 import com.gregtechceu.gtceu.common.recipe.condition.AdjacentBlockCondition;
 import com.gregtechceu.gtceu.common.recipe.condition.AdjacentFluidCondition;
 
+import com.gregtechceu.gtceu.data.recipe.builder.GTRecipeBuilder;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
