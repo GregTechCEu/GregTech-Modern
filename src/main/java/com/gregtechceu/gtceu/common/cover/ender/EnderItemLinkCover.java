e99d50a1b3fed2a86d9bf28f64b35ac043b755e9
Lapis <45414333+Lapis256@users.noreply.github.com>
2026-03-04 18:25:28 +0900

    Fix Ender Link Cover memory card copy/paste and required-item handling (#4691)

--- src/main/java/com/gregtechceu/gtceu/common/cover/ender/EnderItemLinkCover.java
+++ src/main/java/com/gregtechceu/gtceu/common/cover/ender/EnderItemLinkCover.java
@@ -18,11 +18,16 @@ import com.lowdragmc.lowdraglib.syncdata.annotation.DescSynced;
 import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
 
 import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.items.IItemHandler;
 
 import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class EnderItemLinkCover extends AbstractEnderLinkCover<VirtualItemStorage> {
 
@@ -93,6 +98,27 @@ public class EnderItemLinkCover extends AbstractEnderLinkCover<VirtualItemStorag
 
     public @Nullable IItemHandler getOwnItemHandler() {
         return coverHolder.getItemHandlerCap(attachedSide, false);
+    }
+
+    @Override
+    public CompoundTag copyConfig(CompoundTag tag) {
+        tag.put("filter", filterHandler.getFilterItem().serializeNBT());
+        return super.copyConfig(tag);
+    }
+
+    @Override
+    public void pasteConfig(ServerPlayer player, CompoundTag tag) {
+        filterHandler.setFilterItem(ItemStack.of(tag.getCompound("filter")));
+        super.pasteConfig(player, tag);
+    }
+
+    @Override
+    public @NotNull List<ItemStack> getAdditionalDrops() {
+        var list = super.getAdditionalDrops();
+        if (!filterHandler.getFilterItem().isEmpty()) {
+            list.add(filterHandler.getFilterItem());
+        }
+        return list;
     }
 
     @Override
