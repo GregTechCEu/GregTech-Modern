4a4b975c8bfdb2ce34f1cfba6b370952b2141a1a
screret <68943070+screret@users.noreply.github.com>
2026-04-20 16:14:09 +0300

    Add a CTM UV test block

---
+++ src/main/java/com/gregtechceu/gtceu/common/data/blocks/GTDevBlocks.java
@@ -0,0 +1,17 @@
+package com.gregtechceu.gtceu.common.data.blocks;
+
+import com.tterrag.registrate.util.entry.BlockEntry;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+
+import static com.gregtechceu.gtceu.common.registry.GTRegistration.REGISTRATE;
+
+public class GTDevBlocks {
+
+    public static final BlockEntry<Block> CTM_TEST = REGISTRATE.block("ctm_test", Block::new)
+            .properties(p -> p.noLootTable().sound(SoundType.SCULK))
+            .simpleItem()
+            .register();
+
+    public static void init() {}
+}
