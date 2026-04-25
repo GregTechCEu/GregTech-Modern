8008eaef7d2f671719dc30667a5cc61b2f564dc1
screret <68943070+screret@users.noreply.github.com>
2026-04-25 20:54:23 +0300

    Use MixinExtras expression instead of janky slices to target tripwire layer draws

--- src/main/java/com/gregtechceu/gtceu/core/mixins/client/bloom/LevelRendererMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/client/bloom/LevelRendererMixin.java
@@ -10,13 +10,14 @@ import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.core.SectionPos;
 
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.vertex.PoseStack;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = LevelRenderer.class)
@@ -39,31 +40,10 @@ public class LevelRendererMixin {
         }
     }
 
-    // spotless:off
-    // Capture both calls to #renderChunkLayer(RenderType.tripwire(), ...) separately. Capturing them in one @At target
-    // means the translucent layer draw also gets caught in the targets, and we don't want that.
-    @Inject(method = "renderLevel",
-            at = {
-                    @At(value = "INVOKE",
-                            target = "Lnet/minecraft/client/renderer/LevelRenderer;renderChunkLayer(Lnet/minecraft/client/renderer/RenderType;Lcom/mojang/blaze3d/vertex/PoseStack;DDDLorg/joml/Matrix4f;)V",
-                            shift = At.Shift.AFTER,
-                            slice = "slice1"),
-                    @At(value = "INVOKE",
-                            target = "Lnet/minecraft/client/renderer/LevelRenderer;renderChunkLayer(Lnet/minecraft/client/renderer/RenderType;Lcom/mojang/blaze3d/vertex/PoseStack;DDDLorg/joml/Matrix4f;)V",
-                            shift = At.Shift.AFTER,
-                            slice = "slice2"),
-            },
-            slice = {
-                    @Slice(id = "slice1",
-                            from = @At(value = "INVOKE:FIRST", target = "Lnet/minecraft/client/renderer/RenderType;tripwire()Lnet/minecraft/client/renderer/RenderType;"),
-                            to = @At(value = "CONSTANT:FIRST", args = "stringValue=particles")),
-                    @Slice(id = "slice2",
-                            from = @At(value = "INVOKE:LAST", target = "Lnet/minecraft/client/renderer/RenderType;tripwire()Lnet/minecraft/client/renderer/RenderType;"),
-                            to = @At(value = "CONSTANT:LAST", args = "stringValue=particles")),
-            },
-            expect = 2
-    )
-    // spotless:on
+    @Expression("this.renderChunkLayer(tripwire(), ?, ?, ?, ?, ?)")
+    @Definition(id = "renderChunkLayer", method = "Lnet/minecraft/client/renderer/LevelRenderer;renderChunkLayer(Lnet/minecraft/client/renderer/RenderType;Lcom/mojang/blaze3d/vertex/PoseStack;DDDLorg/joml/Matrix4f;)V")
+    @Definition(id = "tripwire", method = "Lnet/minecraft/client/renderer/RenderType;tripwire()Lnet/minecraft/client/renderer/RenderType;")
+    @Inject(method = "renderLevel", at = @At("MIXINEXTRAS:EXPRESSION"))
     private void gtceu$renderBloom(PoseStack poseStack, float partialTick, long finishNanoTime,
                                    boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer,
                                    LightTexture lightTexture, Matrix4f projectionMatrix,
