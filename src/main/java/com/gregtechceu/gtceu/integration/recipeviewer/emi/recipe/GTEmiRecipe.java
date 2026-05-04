c01b6b2d83cf099de2781753a66c5d48ed66a849
Gustavo <ofoxsmith@outlook.com>
2026-05-04 20:38:34 +1000

    fix emi recipe inputs/outputs not being registered

--- src/main/java/com/gregtechceu/gtceu/integration/recipeviewer/emi/recipe/GTEmiRecipe.java
+++ src/main/java/com/gregtechceu/gtceu/integration/recipeviewer/emi/recipe/GTEmiRecipe.java
@@ -1,13 +1,21 @@
 package com.gregtechceu.gtceu.integration.recipeviewer.emi.recipe;
 
+import brachy.modularui.api.widget.IWidget;
+import brachy.modularui.integration.emi.EmiRecipeViewerSlot;
 import brachy.modularui.integration.emi.recipe.ModularUIEmiRecipe;
+import brachy.modularui.widget.WidgetTree;
+import com.gregtechceu.gtceu.api.capability.recipe.FluidRecipeCapability;
+import com.gregtechceu.gtceu.api.capability.recipe.IO;
+import com.gregtechceu.gtceu.api.capability.recipe.ItemRecipeCapability;
 import com.gregtechceu.gtceu.api.recipe.GTRecipe;
+import com.gregtechceu.gtceu.api.recipe.RecipeHelper;
 import com.gregtechceu.gtceu.api.recipe.gui.GTRecipeViewerWidget;
 
 import dev.emi.emi.api.stack.EmiStack;
 
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
 import dev.emi.emi.api.stack.EmiIngredient;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 
 import java.util.List;
 
@@ -30,71 +38,58 @@ public class GTEmiRecipe extends ModularUIEmiRecipe {
 
     @Override
     public List<EmiIngredient> getInputs() {
-        return List.of();
+
+        List<EmiIngredient> ingredients = new ObjectArrayList<>();
+
+        var items = recipe.getInputContents(ItemRecipeCapability.CAP);
+        var fluids = recipe.getInputContents(FluidRecipeCapability.CAP);
+
+        for (var itemContent: items) {
+            float chance = (float) recipe.recipeType.getChanceFunction()
+                    .getBoostedChance(itemContent, RecipeHelper.getRecipeEUtTier(recipe), RecipeHelper.getRecipeEUtTier(recipe)) / itemContent.maxChance();
+
+            var mapped = ItemRecipeCapability.mapIngredientToEntryList(ItemRecipeCapability.CAP.of(itemContent.content()));
+
+            ingredients.add(EmiRecipeViewerSlot.EmiIngredientHandler.toEmiIngredient(mapped, chance, $ -> $));
+        }
+
+        for (var fluidContent: fluids) {
+            float chance = (float) recipe.recipeType.getChanceFunction()
+                    .getBoostedChance(fluidContent, RecipeHelper.getRecipeEUtTier(recipe), RecipeHelper.getRecipeEUtTier(recipe)) / fluidContent.maxChance();
+
+            var mapped = FluidRecipeCapability.mapIngredientToEntryList(FluidRecipeCapability.CAP.of(fluidContent.content()));
+
+            ingredients.add(EmiRecipeViewerSlot.EmiIngredientHandler.toEmiIngredient(mapped, chance));
+        }
+
+        return ingredients;
     }
 
     @Override
     public List<EmiStack> getOutputs() {
-        return List.of();
-    }
+        List<EmiStack> outputs = new ObjectArrayList<>();
 
-    //    @Override
-//    public void addWidgets(WidgetHolder widgets) {
-//        var widget = this.widget.get();
-//        var modular = new ModularWrapper<>(widget);
-//        modular.setRecipeWidget(0, 0);
-//
-//        synchronized (CACHE_OPENED) {
-//            CACHE_OPENED.add(modular);
-//        }
-//        List<Widget> slots = new ArrayList<>();
-//        for (com.lowdragmc.lowdraglib.gui.widget.Widget w : getFlatWidgetCollection(widget)) {
-//            if (w instanceof IRecipeIngredientSlot slot) {
-//                if (w.getParent() instanceof DraggableScrollableWidgetGroup draggable && draggable.isUseScissor()) {
-//                    // don't add the EMI widget at all if we have a draggable group, let the draggable widget handle it
-//                    // instead.
-//                    continue;
-//                }
-//                var io = slot.getIngredientIO();
-//                if (io != null && io != IngredientIO.RENDER_ONLY) {
-//                    // noinspection unchecked
-//                    var ingredients = EmiIngredient
-//                            .of((List<? extends EmiIngredient>) (List<?>) slot.getXEIIngredients());
-//
-//                    SlotWidget slotWidget = null;
-//                    // Clear the LDLib slots & add EMI slots based on them.
-//                    if (slot instanceof com.gregtechceu.gtceu.api.gui.widget.SlotWidget slotW) {
-//                        slotW.setHandlerSlot((IItemHandlerModifiable) EmptyHandler.INSTANCE, 0);
-//                        slotW.setDrawHoverOverlay(false).setDrawHoverTips(false);
-//                    } else if (slot instanceof com.gregtechceu.gtceu.api.gui.widget.TankWidget tankW) {
-//                        tankW.setFluidTank(EmptyFluidHandler.INSTANCE);
-//                        tankW.setDrawHoverOverlay(false).setDrawHoverTips(false);
-//                        long capacity = Math.max(1, ingredients.getAmount());
-//                        slotWidget = new TankWidget(ingredients, w.getPosition().x, w.getPosition().y,
-//                                w.getSize().width, w.getSize().height, capacity);
-//                    }
-//                    if (slotWidget == null) {
-//                        slotWidget = new SlotWidget(ingredients, w.getPosition().x, w.getPosition().y);
-//                    }
-//
-//                    slotWidget
-//                            .customBackground(null, w.getPosition().x, w.getPosition().y, w.getSize().width,
-//                                    w.getSize().height)
-//                            .drawBack(false);
-//                    if (io == IngredientIO.CATALYST) {
-//                        slotWidget.catalyst(true);
-//                    } else if (io == IngredientIO.OUTPUT) {
-//                        slotWidget.recipeContext(this);
-//                    }
-//                    for (Component component : w.getTooltipTexts()) {
-//                        slotWidget.appendTooltip(component);
-//                    }
-//                    slots.add(slotWidget);
-//                }
-//            }
-//        }
-//        widgets.add(new ModularWrapperWidget(modular, slots));
-//        slots.forEach(widgets::add);
-//        widgets.add(new ModularForegroundRenderWidget(modular));
-//    }
+        var items = recipe.getOutputContents(ItemRecipeCapability.CAP);
+        var fluids = recipe.getOutputContents(FluidRecipeCapability.CAP);
+
+        for (var itemContent: items) {
+            float chance = (float) recipe.recipeType.getChanceFunction()
+                    .getBoostedChance(itemContent, RecipeHelper.getRecipeEUtTier(recipe), RecipeHelper.getRecipeEUtTier(recipe)) / itemContent.maxChance();
+
+            var mapped = ItemRecipeCapability.mapIngredientToEntryList(ItemRecipeCapability.CAP.of(itemContent.content()));
+
+            outputs.add(EmiStack.of(mapped.getStacks().get(0)).setChance(chance));
+        }
+
+        for (var fluidContent: fluids) {
+            float chance = (float) recipe.recipeType.getChanceFunction()
+                    .getBoostedChance(fluidContent, RecipeHelper.getRecipeEUtTier(recipe), RecipeHelper.getRecipeEUtTier(recipe)) / fluidContent.maxChance();
+
+            var mapped = FluidRecipeCapability.mapIngredientToEntryList(FluidRecipeCapability.CAP.of(fluidContent.content()));
+            var fluid = mapped.getStacks().get(0);
+            outputs.add(EmiStack.of(fluid.getFluid(), fluid.getAmount()).setChance(chance));
+        }
+
+        return outputs;
+    }
 }
