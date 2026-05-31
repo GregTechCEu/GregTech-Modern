3b0a64b0f54ad9db741fb877a0cd03008810c586
screret <68943070+screret@users.noreply.github.com>
2026-05-31 18:35:10 +0300

    spot dn

--- src/main/java/com/gregtechceu/gtceu/client/util/quad/transformers/GTQuadTransformers.java
+++ src/main/java/com/gregtechceu/gtceu/client/util/quad/transformers/GTQuadTransformers.java
@@ -5,7 +5,6 @@ import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.Direction;
 import net.minecraft.util.Mth;
-import net.minecraft.world.phys.AABB;
 import net.minecraftforge.client.model.IQuadTransformer;
 import net.minecraftforge.client.model.QuadTransformers;
 
