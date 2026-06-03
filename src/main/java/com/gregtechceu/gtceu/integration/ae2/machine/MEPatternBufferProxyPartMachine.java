e4b68876378e3813f502bdd5df97d9c8d5697248
Tetiana Kobzar <2207252@gmail.com>
2026-06-03 19:56:07 +0200

    fix: defer setBuffer to next tick via TickTask to prevent premature load

--- src/main/java/com/gregtechceu/gtceu/integration/ae2/machine/MEPatternBufferProxyPartMachine.java
+++ src/main/java/com/gregtechceu/gtceu/integration/ae2/machine/MEPatternBufferProxyPartMachine.java
@@ -1,21 +1,25 @@
 package com.gregtechceu.gtceu.integration.ae2.machine;
 
 import com.gregtechceu.gtceu.api.GTValues;
-import com.gregtechceu.gtceu.api.blockentity.BlockEntityCreationInfo;
 import com.gregtechceu.gtceu.api.capability.recipe.IO;
+import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 import com.gregtechceu.gtceu.api.machine.MetaMachine;
 import com.gregtechceu.gtceu.api.machine.feature.IDataStickInteractable;
+import com.gregtechceu.gtceu.api.machine.feature.IMachineLife;
 import com.gregtechceu.gtceu.api.machine.multiblock.part.TieredIOPartMachine;
 import com.gregtechceu.gtceu.api.machine.trait.RecipeHandlerList;
-import com.gregtechceu.gtceu.api.sync_system.annotations.SaveField;
-import com.gregtechceu.gtceu.api.sync_system.annotations.SyncToClient;
 import com.gregtechceu.gtceu.integration.ae2.machine.trait.ProxySlotRecipeHandler;
 
 import com.lowdragmc.lowdraglib.gui.modular.ModularUI;
+import com.lowdragmc.lowdraglib.syncdata.annotation.DescSynced;
+import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
+import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.Tag;
+import net.minecraft.server.TickTask;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -31,28 +35,34 @@ import javax.annotation.ParametersAreNonnullByDefault;
 
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
-public class MEPatternBufferProxyPartMachine extends TieredIOPartMachine implements IDataStickInteractable {
+public class MEPatternBufferProxyPartMachine extends TieredIOPartMachine
+                                             implements IMachineLife, IDataStickInteractable {
+
+    protected static final ManagedFieldHolder MANAGED_FIELD_HOLDER = new ManagedFieldHolder(
+            MEPatternBufferProxyPartMachine.class, TieredIOPartMachine.MANAGED_FIELD_HOLDER);
 
     @Getter
     private final ProxySlotRecipeHandler proxySlotRecipeHandler;
 
-    @SaveField
+    @Persisted
     @Getter
-    @SyncToClient
+    @DescSynced
     private @Nullable BlockPos bufferPos;
 
     private @Nullable MEPatternBufferPartMachine buffer = null;
     private boolean bufferResolved = false;
 
-    public MEPatternBufferProxyPartMachine(BlockEntityCreationInfo info) {
-        super(info, GTValues.LuV, IO.IN);
+    public MEPatternBufferProxyPartMachine(IMachineBlockEntity holder) {
+        super(holder, GTValues.LuV, IO.IN);
         proxySlotRecipeHandler = new ProxySlotRecipeHandler(this, MEPatternBufferPartMachine.MAX_PATTERN_COUNT);
     }
 
     @Override
     public void onLoad() {
         super.onLoad();
-        if (!isRemote()) this.setBuffer(bufferPos);
+        if (getLevel() instanceof ServerLevel level) {
+            level.getServer().tell(new TickTask(0, () -> this.setBuffer(bufferPos)));
+        }
     }
 
     @Override
@@ -73,7 +83,6 @@ public class MEPatternBufferProxyPartMachine extends TieredIOPartMachine impleme
         } else {
             buffer = null;
         }
-        syncDataHolder.markClientSyncFieldDirty("bufferPos");
     }
 
     @Nullable
@@ -94,8 +103,12 @@ public class MEPatternBufferProxyPartMachine extends TieredIOPartMachine impleme
     }
 
     @Override
-    public void onMachineDestroyed() {
-        super.onMachineDestroyed();
+    public ManagedFieldHolder getFieldHolder() {
+        return MANAGED_FIELD_HOLDER;
+    }
+
+    @Override
+    public void onMachineRemoved() {
         var buf = getBuffer();
         if (buf != null) {
             buf.removeProxy(this);
