a196a7f451ccfb4e4bd6a6e190f61d81a51e4c3f
screret <68943070+screret@users.noreply.github.com>
2026-06-08 13:40:20 +0300

    Fix GTDyeableArmorItem adding the item model generator a second time

--- src/main/java/com/gregtechceu/gtceu/common/item/armor/GTDyeableArmorItem.java
+++ src/main/java/com/gregtechceu/gtceu/common/item/armor/GTDyeableArmorItem.java
@@ -1,9 +1,7 @@
 package com.gregtechceu.gtceu.common.item.armor;
 
-import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.data.chemical.material.Material;
 import com.gregtechceu.gtceu.api.data.chemical.material.properties.ArmorProperty;
-import com.gregtechceu.gtceu.client.model.runtimegen.ArmorItemModelGenerator;
 
 import net.minecraft.world.item.*;
 
@@ -12,8 +10,5 @@ public class GTDyeableArmorItem extends GTArmorItem implements DyeableLeatherIte
     public GTDyeableArmorItem(ArmorMaterial armorMaterial, ArmorItem.Type type, Item.Properties properties,
                               Material material, ArmorProperty armorProperty) {
         super(armorMaterial, type, properties, material, armorProperty);
-        if (GTCEu.isClientSide()) {
-            ArmorItemModelGenerator.add(this, type);
-        }
     }
 }
