3d69121e2a9af5dd7832d6851441990085db9f81
Gustavo <ofoxsmith@outlook.com>
2026-06-19 13:11:09 +1000

    fix cover panels not sizing properly

--- src/main/java/com/gregtechceu/gtceu/api/cover/filter/SmartItemFilter.java
+++ src/main/java/com/gregtechceu/gtceu/api/cover/filter/SmartItemFilter.java
@@ -97,7 +97,7 @@ public class SmartItemFilter implements ItemFilter {
 
         syncManager.syncValue("mode", mode);
 
-        return Flow.row().coverChildrenHeight().width(166)
+        return Flow.row().coverChildrenHeight().width(162)
                 .child(new ContextMenuButton<>("smart_filter")
                         .size(18)
                         .requiresClick()
