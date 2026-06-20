5d89218c41922834f9e57fb8ab61040facfb725c
Gustavo <ofoxsmith@outlook.com>
2026-06-20 19:35:18 +1000

    add pattern error register event

--- src/main/java/com/gregtechceu/gtceu/api/multiblock/error/GTPatternErrors.java
+++ src/main/java/com/gregtechceu/gtceu/api/multiblock/error/GTPatternErrors.java
@@ -1,9 +1,15 @@
 package com.gregtechceu.gtceu.api.multiblock.error;
 
+import com.gregtechceu.gtceu.api.GTCEuAPI;
+import com.gregtechceu.gtceu.api.registry.GTRegistries;
+import com.mojang.serialization.Codec;
+import net.minecraftforge.fml.ModLoader;
+
 import static com.gregtechceu.gtceu.api.registry.GTRegistries.PATTERN_ERRORS;
 
 public class GTPatternErrors {
 
+    @SuppressWarnings("unchecked")
     public static void init() {
         PATTERN_ERRORS.unfreeze();
         PATTERN_ERRORS.register(PlaceholderError.ID, PlaceholderError.CODEC);
@@ -14,5 +20,9 @@ public class GTPatternErrors {
         PATTERN_ERRORS.register(PatternStringError.ID, PatternStringError.CODEC);
         PATTERN_ERRORS.register(SinglePredicateError.ID, SinglePredicateError.CODEC);
         PATTERN_ERRORS.register(SimplePatternError.ID, SimplePatternError.CODEC);
+
+        ModLoader.get().postEvent(new GTCEuAPI.RegisterEvent<>(PATTERN_ERRORS, (Class<Codec<? extends PatternError>>)(Class<?>)Codec.class));
+
+        GTRegistries.COVERS.freeze();
     }
 }
