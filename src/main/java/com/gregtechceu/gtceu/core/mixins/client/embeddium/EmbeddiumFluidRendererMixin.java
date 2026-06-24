b2b0d59a57cef255307c1a7f2b42834ab6d60efb
screret <68943070+screret@users.noreply.github.com>
2026-06-25 01:51:21 +0300

    Fix upside down fluids' vertices being fucky wucky

--- src/main/java/com/gregtechceu/gtceu/core/mixins/client/embeddium/EmbeddiumFluidRendererMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/client/embeddium/EmbeddiumFluidRendererMixin.java
@@ -10,14 +10,12 @@ import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import org.embeddedt.embeddium.impl.model.quad.properties.ModelQuadFacing;
 import org.embeddedt.embeddium.impl.render.chunk.compile.pipeline.FluidRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = FluidRenderer.class, remap = false)
@@ -38,22 +36,12 @@ public class EmbeddiumFluidRendererMixin {
 
     @ModifyArg(method = "render", at = @At(value = "INVOKE",
             target = "Lorg/embeddedt/embeddium/impl/render/chunk/compile/pipeline/FluidRenderer;setVertex(Lorg/embeddedt/embeddium/impl/model/quad/ModelQuadViewMutable;IFFFFF)V"),
-            index = 4)
+            index = 3)
     private float gtceu$invertFluidFlowDirection(float originalY) {
         if (gtceu$drawingUpsideDownFluid) {
             return 1.0f - originalY;
         } else {
             return originalY;
-        }
-    }
-
-    @ModifyVariable(method = "writeQuad", at = @At(value = "HEAD"), index = 6, argsOnly = true)
-    private boolean gtceu$invertFluidVertexOrder(boolean flip) {
-        // reverse vertex order if we're drawing an upside-down fluid
-        if (gtceu$drawingUpsideDownFluid) {
-            return !flip;
-        } else {
-            return flip;
         }
     }
 
@@ -66,24 +54,7 @@ public class EmbeddiumFluidRendererMixin {
             @At(value = "MIXINEXTRAS:EXPRESSION", id = "down"),
     }, expect = 10)
     private Direction gtceu$invertFluidCulling(Direction original) {
-        if (original.getAxis() == Direction.Axis.Y && gtceu$drawingUpsideDownFluid) {
-            return original.getOpposite();
-        } else {
-            return original;
-        }
-    }
-
-    @Definition(id = "POS_Y", field = "Lorg/embeddedt/embeddium/impl/model/quad/properties/ModelQuadFacing;POS_Y")
-    @Definition(id = "NEG_Y", field = "Lorg/embeddedt/embeddium/impl/model/quad/properties/ModelQuadFacing;NEG_Y")
-    @Expression(value = "POS_Y", id = "pos_y")
-    @Expression(value = "NEG_Y", id = "neg_y")
-    @ModifyExpressionValue(method = "*", at = {
-            @At(value = "MIXINEXTRAS:EXPRESSION", id = "pos_y"),
-            @At(value = "MIXINEXTRAS:EXPRESSION", id = "neg_y")
-    }, expect = 3)
-    private ModelQuadFacing gtceu$invertFluidQuadFacing(ModelQuadFacing original) {
-        if ((original == ModelQuadFacing.POS_Y || original == ModelQuadFacing.NEG_Y) &&
-                gtceu$drawingUpsideDownFluid) {
+        if (gtceu$drawingUpsideDownFluid) {
             return original.getOpposite();
         } else {
             return original;
--- src/main/java/com/gregtechceu/gtceu/core/mixins/client/sodium/SodiumDefaultFluidRendererMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/client/sodium/SodiumDefaultFluidRendererMixin.java
@@ -2,7 +2,6 @@ package com.gregtechceu.gtceu.core.mixins.client.sodium;
 
 import com.gregtechceu.gtceu.client.renderer.fluid.InvertedFluidRenderer;
 
-import net.caffeinemc.mods.sodium.client.model.quad.properties.ModelQuadFacing;
 import net.caffeinemc.mods.sodium.client.render.chunk.compile.pipeline.DefaultFluidRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -18,34 +17,11 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Debug(export = true)
 @Mixin(value = DefaultFluidRenderer.class, remap = false)
 public class SodiumDefaultFluidRendererMixin {
-
-// this probably isn't required; I don't see anything actually use the light face.
-//    @Inject(method = "updateQuad", at = @At(value = "HEAD"))
-//    private static void gtceu$invertQuadLightFace(ModelQuadViewMutable quad, LevelSlice level, BlockPos pos,
-//                                                       LightPipeline lighter, Direction dir, ModelQuadFacing facing,
-//                                                       float brightness, ColorProvider<FluidState> colorProvider,
-//                                                       FluidState fluidState,
-//                                                       CallbackInfo ci) {
-//        if (gtceu$drawingUpsideDownFluid) {
-//            // flip the light face (default is UP)
-//            quad.setLightFace(Direction.DOWN);
-//        }
-//    }
-//
-//    @Inject(method = "writeQuad", at = @At(value = "RETURN"))
-//    private static void gtceu$resetQuadLightFace(ChunkModelBuilder builder, TranslucentGeometryCollector collector,
-//                                                 Material material, BlockPos offset, ModelQuadView quad,
-//                                                 ModelQuadFacing facing, boolean flip,
-//                                                 CallbackInfo ci) {
-//        // reset the light face after every write
-//        quad.setLightFace(Direction.UP);
-//    }
 
     @Unique
     private boolean gtceu$drawingUpsideDownFluid = false;
@@ -62,22 +38,12 @@ public class SodiumDefaultFluidRendererMixin {
 
     @ModifyArg(method = "render", at = @At(value = "INVOKE",
             target = "Lnet/caffeinemc/mods/sodium/client/render/chunk/compile/pipeline/DefaultFluidRenderer;setVertex(Lnet/caffeinemc/mods/sodium/client/model/quad/ModelQuadViewMutable;IFFFFF)V"),
-            index = 4)
+            index = 3)
     private float gtceu$invertFluidFlowDirection(float originalY) {
         if (gtceu$drawingUpsideDownFluid) {
             return 1.0f - originalY;
         } else {
             return originalY;
-        }
-    }
-
-    @ModifyVariable(method = "writeQuad", at = @At(value = "HEAD"), index = 7, argsOnly = true)
-    private boolean gtceu$invertFluidVertexOrder(boolean flip) {
-        // reverse vertex order if we're drawing an upside-down fluid
-        if (gtceu$drawingUpsideDownFluid) {
-            return !flip;
-        } else {
-            return flip;
         }
     }
 
@@ -91,22 +57,6 @@ public class SodiumDefaultFluidRendererMixin {
     }, expect = 9)
     private Direction gtceu$invertFluidCulling(Direction original) {
         if (original.getAxis() == Direction.Axis.Y && gtceu$drawingUpsideDownFluid) {
-            return original.getOpposite();
-        } else {
-            return original;
-        }
-    }
-
-    @Definition(id = "POS_Y", field = "Lnet/caffeinemc/mods/sodium/client/model/quad/properties/ModelQuadFacing;POS_Y")
-    @Definition(id = "NEG_Y", field = "Lnet/caffeinemc/mods/sodium/client/model/quad/properties/ModelQuadFacing;NEG_Y")
-    @Expression(value = "POS_Y", id = "pos_y")
-    @Expression(value = "NEG_Y", id = "neg_y")
-    @ModifyExpressionValue(method = "*", at = {
-            @At(value = "MIXINEXTRAS:EXPRESSION", id = "pos_y"),
-            @At(value = "MIXINEXTRAS:EXPRESSION", id = "neg_y")
-    }, expect = 5)
-    private ModelQuadFacing gtceu$invertFluidQuadFacing(ModelQuadFacing original) {
-        if (original.getAxis() == 1 && gtceu$drawingUpsideDownFluid) {
             return original.getOpposite();
         } else {
             return original;
