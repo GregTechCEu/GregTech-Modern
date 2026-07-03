6e1b46da08fea460723f21507d6aaa1d89021384
Gustavo <ofoxsmith@outlook.com>
2026-07-03 12:39:55 +1000

    recipes

--- src/main/java/com/gregtechceu/gtceu/data/recipe/misc/CraftingRecipeLoader.java
+++ src/main/java/com/gregtechceu/gtceu/data/recipe/misc/CraftingRecipeLoader.java
@@ -76,6 +76,17 @@ public class CraftingRecipeLoader {
                 "XCX", "XEX", 'X', new MaterialEntry(foil, Zinc), 'C', CustomTags.LV_CIRCUITS, 'E',
                 new MaterialEntry(plate, Ruby));
 
+        VanillaRecipeHelper.addShapedRecipe(provider, "item_composite_filter", COMPOSITE_ITEM_FILTER.asStack(), "XFX",
+                "SCS", "XFX", 'X', new MaterialEntry(foil, Zinc), 'F', ITEM_FILTER.asStack(), 'C', CustomTags.LV_CIRCUITS, 'S', new MaterialEntry(plate, Steel));
+
+        VanillaRecipeHelper.addShapedRecipe(provider, "fluid_composite_filter_lapis", COMPOSITE_FLUID_FILTER.asStack(), "XFX",
+                "SCS", "XFX", 'X', new MaterialEntry(foil, Zinc), 'F', FLUID_FILTER.asStack(), 'C', CustomTags.LV_CIRCUITS, 'S', new MaterialEntry(plate, Lapis));
+        VanillaRecipeHelper.addShapedRecipe(provider, "fluid_composite_filter_lazurite", COMPOSITE_FLUID_FILTER.asStack(), "XFX",
+                "SCS", "XFX", 'X', new MaterialEntry(foil, Zinc), 'F', FLUID_FILTER.asStack(), 'C', CustomTags.LV_CIRCUITS, 'S', new MaterialEntry(plate, Lazurite));
+        VanillaRecipeHelper.addShapedRecipe(provider, "fluid_composite_filter_sodalite", COMPOSITE_FLUID_FILTER.asStack(), "XFX",
+                "SCS", "XFX", 'X', new MaterialEntry(foil, Zinc), 'F', FLUID_FILTER.asStack(), 'C', CustomTags.LV_CIRCUITS, 'S', new MaterialEntry(plate, Sodalite));
+
+
         VanillaRecipeHelper.addShapedRecipe(provider, "plank_to_wooden_shape", WOODEN_FORM_EMPTY.asStack(), "   ",
                 " X ", "s  ", 'X', ItemTags.PLANKS);
         VanillaRecipeHelper.addShapedRecipe(provider, "wooden_shape_brick", WOODEN_FORM_BRICK.asStack(), "k ", " X",
