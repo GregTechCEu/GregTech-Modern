GregTechCEu/GregTech-Modern

commit 80ffd2cab67c0ae4d08704cff0119b58b045c620

Turn IMappedRegistryAccess into an accessor mixin (#5037)

src/main/java/com/gregtechceu/gtceu/api/data/chemical/material/IMaterialRegistry.java
@@ -1,7 +1,7 @@
 package com.gregtechceu.gtceu.api.data.chemical.material;
 
 import com.gregtechceu.gtceu.GTCEu;
-import com.gregtechceu.gtceu.core.IMappedRegistryAccess;
+import com.gregtechceu.gtceu.core.mixins.MappedRegistryAccessor;
 
 import net.minecraft.resources.ResourceLocation;
 
@@ -83,7 +83,7 @@ public interface IMaterialRegistry extends Iterable<Material> {
     /**
      *
      * @return {@code true} if this registry is frozen, {@code false} otherwise
-     * @see IMappedRegistryAccess#gtceu$isFrozen()
+     * @see MappedRegistryAccessor#isFrozen()
      */
     boolean isFrozen();
 }

src/main/java/com/gregtechceu/gtceu/api/data/chemical/material/registry/MaterialRegistry.java
@@ -4,6 +4,7 @@ import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.data.chemical.material.IMaterialRegistry;
 import com.gregtechceu.gtceu.api.data.chemical.material.Material;
 import com.gregtechceu.gtceu.common.data.GTMaterials;
+import com.gregtechceu.gtceu.core.mixins.MappedRegistryAccessor;
 
 import net.minecraft.core.Holder;
 import net.minecraft.core.MappedRegistry;
@@ -110,7 +111,7 @@ public final class MaterialRegistry extends MappedRegistry<Material> implements 
 
     @Override
     public boolean isFrozen() {
-        return this.gtceu$isFrozen();
+        return ((MappedRegistryAccessor) (Object) this).isFrozen();
     }
 
     public void close() {

src/main/java/com/gregtechceu/gtceu/core/mixins/MappedRegistryAccessor.java
@@ -1,0 +1,13 @@
+package com.gregtechceu.gtceu.core.mixins;
+
+import net.minecraft.core.MappedRegistry;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(MappedRegistry.class)
+public interface MappedRegistryAccessor {
+
+    @Accessor
+    boolean isFrozen();
+}

src/main/java/com/gregtechceu/gtceu/core/IMappedRegistryAccess.java
@@ -1,72 +1,0 @@
-package com.gregtechceu.gtceu.core;
-
-import net.minecraft.core.Holder;
-import net.minecraft.core.RegistrationInfo;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
-
-import it.unimi.dsi.fastutil.objects.ObjectList;
-import it.unimi.dsi.fastutil.objects.Reference2IntMap;
-import org.jetbrains.annotations.TestOnly;
-import org.jetbrains.annotations.VisibleForTesting;
-
-import java.util.Map;
-
-public interface IMappedRegistryAccess<T> {
-
-    default boolean gtceu$isFrozen() {
-        throw new AssertionError();
-    }
-
-    default ObjectList<Holder.Reference<T>> gtceu$getById() {
-        throw new AssertionError();
-    }
-
-    default Reference2IntMap<T> gtceu$getToId() {
-        throw new AssertionError();
-    }
-
-    default Map<ResourceLocation, Holder.Reference<T>> gtceu$getByLocation() {
-        throw new AssertionError();
-    }
-
-    default Map<ResourceKey<T>, Holder.Reference<T>> gtceu$getByKey() {
-        throw new AssertionError();
-    }
-
-    default Map<T, Holder.Reference<T>> gtceu$getByValue() {
-        throw new AssertionError();
-    }
-
-    default Map<ResourceKey<T>, RegistrationInfo> gtceu$getRegistrationInfos() {
-        throw new AssertionError();
-    }
-
-    /// FOR TESTING ONLY; THIS WILL FUCK UP THINGS IF THINGS ARE IN USE!
-    @TestOnly
-    @VisibleForTesting
-    default void gtceu$remove(ResourceKey<T> key) {
-        if (this.gtceu$isFrozen()) {
-            throw new IllegalStateException("Cannot remove entry from a frozen registry: " + key);
-        }
-
-        Holder.Reference<T> ref = this.gtceu$getByKey().remove(key);
-        if (ref == null) {
-            return; // not present, nothing to remove
-        }
-
-        this.gtceu$getByLocation().remove(key.location());
-
-        T value = ref.value();
-        this.gtceu$getByValue().remove(value);
-
-        int id = this.gtceu$getToId().removeInt(value);
-        ObjectList<Holder.Reference<T>> byId = this.gtceu$getById();
-
-        if (id >= 0 && id < byId.size()) {
-            byId.set(id, null);
-        }
-
-        this.gtceu$getRegistrationInfos().remove(key);
-    }
-}

src/main/java/com/gregtechceu/gtceu/core/mixins/MappedRegistryMixin.java
@@ -1,81 +1,0 @@
-package com.gregtechceu.gtceu.core.mixins;
-
-import com.gregtechceu.gtceu.core.IMappedRegistryAccess;
-
-import net.minecraft.core.Holder;
-import net.minecraft.core.MappedRegistry;
-import net.minecraft.core.RegistrationInfo;
-import net.minecraft.core.WritableRegistry;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
-import net.neoforged.neoforge.registries.BaseMappedRegistry;
-
-import it.unimi.dsi.fastutil.objects.ObjectList;
-import it.unimi.dsi.fastutil.objects.Reference2IntMap;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-
-import java.util.Map;
-
-@SuppressWarnings("UnstableApiUsage")
-@Mixin(MappedRegistry.class)
-public abstract class MappedRegistryMixin<T> extends BaseMappedRegistry<T>
-                                         implements WritableRegistry<T>, IMappedRegistryAccess<T> {
-
-    @Shadow
-    private boolean frozen;
-    @Shadow
-    @Final
-    private ObjectList<Holder.Reference<T>> byId;
-    @Shadow
-    @Final
-    private Reference2IntMap<T> toId;
-    @Shadow
-    @Final
-    private Map<ResourceLocation, Holder.Reference<T>> byLocation;
-    @Shadow
-    @Final
-    private Map<ResourceKey<T>, Holder.Reference<T>> byKey;
-    @Shadow
-    @Final
-    private Map<T, Holder.Reference<T>> byValue;
-    @Shadow
-    @Final
-    private Map<ResourceKey<T>, RegistrationInfo> registrationInfos;
-
-    @Override
-    public boolean gtceu$isFrozen() {
-        return this.frozen;
-    }
-
-    @Override
-    public ObjectList<Holder.Reference<T>> gtceu$getById() {
-        return this.byId;
-    }
-
-    @Override
-    public Reference2IntMap<T> gtceu$getToId() {
-        return this.toId;
-    }
-
-    @Override
-    public Map<ResourceLocation, Holder.Reference<T>> gtceu$getByLocation() {
-        return this.byLocation;
-    }
-
-    @Override
-    public Map<ResourceKey<T>, Holder.Reference<T>> gtceu$getByKey() {
-        return this.byKey;
-    }
-
-    @Override
-    public Map<T, Holder.Reference<T>> gtceu$getByValue() {
-        return this.byValue;
-    }
-
-    @Override
-    public Map<ResourceKey<T>, RegistrationInfo> gtceu$getRegistrationInfos() {
-        return this.registrationInfos;
-    }
-}

commit e7e208c0036dba022ce9c590b08d4bbc531368cb

Amend Crash caused by #5037

src/main/java/com/gregtechceu/gtceu/api/data/chemical/material/IMaterialRegistry.java
@@ -83,7 +83,7 @@ public interface IMaterialRegistry extends Iterable<Material> {
     /**
      *
      * @return {@code true} if this registry is frozen, {@code false} otherwise
-     * @see MappedRegistryAccessor#isFrozen()
+     * @see MappedRegistryAccessor#gtceu$isFrozen()
      */
     boolean isFrozen();
 }

src/main/java/com/gregtechceu/gtceu/api/data/chemical/material/registry/MaterialRegistry.java
@@ -111,7 +111,7 @@ public final class MaterialRegistry extends MappedRegistry<Material> implements 
 
     @Override
     public boolean isFrozen() {
-        return ((MappedRegistryAccessor) (Object) this).isFrozen();
+        return ((MappedRegistryAccessor) (Object) this).gtceu$isFrozen();
     }
 
     public void close() {

src/main/java/com/gregtechceu/gtceu/core/mixins/MappedRegistryAccessor.java
@@ -8,6 +8,6 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 @Mixin(MappedRegistry.class)
 public interface MappedRegistryAccessor {
 
-    @Accessor
-    boolean isFrozen();
+    @Accessor("frozen")
+    boolean gtceu$isFrozen();
 }