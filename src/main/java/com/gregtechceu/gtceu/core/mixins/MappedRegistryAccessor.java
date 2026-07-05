7cadd9eeac1a33ae5b620234c81412e1ef57c5f8
Ghostipedia / Caitlynn <46772882+Ghostipedia@users.noreply.github.com>
2026-07-05 00:26:39 -0400

    Fixed a Accessor missing a prefix. (#5059)

--- src/main/java/com/gregtechceu/gtceu/core/mixins/MappedRegistryAccessor.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/MappedRegistryAccessor.java
@@ -8,6 +8,6 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 @Mixin(MappedRegistry.class)
 public interface MappedRegistryAccessor {
 
-    @Accessor
-    boolean isFrozen();
+    @Accessor("frozen")
+    boolean gtceu$isFrozen();
 }
