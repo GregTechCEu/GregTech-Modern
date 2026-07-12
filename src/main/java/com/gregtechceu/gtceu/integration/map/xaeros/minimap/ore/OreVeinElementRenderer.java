7082be2719132f4bb41c5ef46eafdae2db4136df
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2026-07-12 03:44:08 -0600

    fix xaero ore veins using the wrong icons and lang key

--- src/main/java/com/gregtechceu/gtceu/integration/map/xaeros/minimap/ore/OreVeinElementRenderer.java
+++ src/main/java/com/gregtechceu/gtceu/integration/map/xaeros/minimap/ore/OreVeinElementRenderer.java
@@ -6,6 +6,7 @@ import com.gregtechceu.gtceu.api.data.worldgen.ores.GeneratedVeinMetadata;
 import com.gregtechceu.gtceu.client.util.RenderUtil;
 import com.gregtechceu.gtceu.config.ConfigHolder;
 import com.gregtechceu.gtceu.integration.map.GroupingMapRenderer;
+import com.gregtechceu.gtceu.integration.map.layer.builtin.OreRenderLayer;
 import com.gregtechceu.gtceu.integration.map.xaeros.common.ore.OreVeinElement;
 
 import net.minecraft.client.Minecraft;
@@ -42,13 +43,13 @@ public class OreVeinElementRenderer extends MinimapElementRenderer<OreVeinElemen
         GeneratedVeinMetadata vein = element.getVein();
         int iconSize = ConfigHolder.INSTANCE.compat.minimap.oreIconSize;
 
-        Material firstMaterial = vein.definition().veinGenerator().getAllMaterials().get(0);
-        int materialARGB = firstMaterial.getMaterialARGB();
+        Material material = OreRenderLayer.getMaterial(vein);
+        int materialARGB = material.getMaterialARGB();
         float[] colors = RenderUtil.floats(materialARGB);
         RenderSystem.setShaderColor(1, 1, 1, 1);
 
         ResourceLocation oreTexture = MaterialIconType.rawOre
-                .getItemTexturePath(firstMaterial.getMaterialIconSet(), true);
+                .getItemTexturePath(material.getMaterialIconSet(), true);
         if (oreTexture != null) {
             var oreSprite = Minecraft.getInstance()
                     .getTextureAtlas(InventoryMenu.BLOCK_ATLAS)
