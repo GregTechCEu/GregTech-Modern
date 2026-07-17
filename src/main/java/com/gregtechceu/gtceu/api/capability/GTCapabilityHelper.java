e93d421ca18ddcc74cca911f572aaeb2e1cbad2f
screret <68943070+screret@users.noreply.github.com>
2026-07-17 19:34:57 +0300

    Add additional safety to GTCapabilityHelper methods

--- src/main/java/com/gregtechceu/gtceu/api/capability/GTCapabilityHelper.java
+++ src/main/java/com/gregtechceu/gtceu/api/capability/GTCapabilityHelper.java
@@ -16,16 +16,17 @@ import net.minecraftforge.items.IItemHandler;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+@SuppressWarnings("DataFlowIssue")
 public class GTCapabilityHelper {
 
     @Nullable
     public static IElectricItem getElectricItem(ItemStack itemStack) {
-        return itemStack.getCapability(GTCapability.CAPABILITY_ELECTRIC_ITEM).resolve().orElse(null);
+        return itemStack.getCapability(GTCapability.CAPABILITY_ELECTRIC_ITEM).orElse(null);
     }
 
     @Nullable
     public static IEnergyStorage getForgeEnergyItem(ItemStack itemStack) {
-        return itemStack.getCapability(ForgeCapabilities.ENERGY).resolve().orElse(null);
+        return itemStack.getCapability(ForgeCapabilities.ENERGY).orElse(null);
     }
 
     @Nullable
@@ -65,13 +66,7 @@ public class GTCapabilityHelper {
 
     @Nullable
     public static IEnergyStorage getForgeEnergy(Level level, BlockPos pos, @Nullable Direction side) {
-        if (level.getBlockState(pos).hasBlockEntity()) {
-            var blockEntity = level.getBlockEntity(pos);
-            if (blockEntity != null) {
-                return blockEntity.getCapability(ForgeCapabilities.ENERGY, side).resolve().orElse(null);
-            }
-        }
-        return null;
+        return getBlockEntityCapability(ForgeCapabilities.ENERGY, level, pos, side);
     }
 
     @Nullable
@@ -106,7 +101,7 @@ public class GTCapabilityHelper {
         if (level.getBlockState(pos).hasBlockEntity()) {
             var blockEntity = level.getBlockEntity(pos);
             if (blockEntity != null) {
-                return blockEntity.getCapability(capability, side).resolve().orElse(null);
+                return blockEntity.getCapability(capability, side).orElse(null);
             }
         }
         return null;
@@ -114,6 +109,6 @@ public class GTCapabilityHelper {
 
     @Nullable
     public static MedicalConditionTracker getMedicalConditionTracker(@NotNull Entity entity) {
-        return entity.getCapability(GTCapability.CAPABILITY_MEDICAL_CONDITION_TRACKER, null).resolve().orElse(null);
+        return entity.getCapability(GTCapability.CAPABILITY_MEDICAL_CONDITION_TRACKER, null).orElse(null);
     }
 }
