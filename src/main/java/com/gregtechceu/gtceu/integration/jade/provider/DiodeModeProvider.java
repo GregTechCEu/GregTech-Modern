8300238469e917cdeab1ee8d6c25f664a43980bc
Gustavo <ofoxsmith@outlook.com>
2026-07-24 23:37:30 +1000

    spotless

--- src/main/java/com/gregtechceu/gtceu/integration/jade/provider/DiodeModeProvider.java
+++ src/main/java/com/gregtechceu/gtceu/integration/jade/provider/DiodeModeProvider.java
@@ -3,10 +3,12 @@ package com.gregtechceu.gtceu.integration.jade.provider;
 import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.common.machine.multiblock.part.DiodePartMachine;
+
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.entity.BlockEntity;
+
 import snownee.jade.api.BlockAccessor;
 import snownee.jade.api.ITooltip;
 import snownee.jade.api.config.IPluginConfig;
@@ -26,7 +28,8 @@ public class DiodeModeProvider extends MachineInfoProvider<DiodePartMachine, Com
     }
 
     @Override
-    protected void addTooltip(CompoundTag data, ITooltip tooltip, Player player, BlockAccessor block, BlockEntity blockEntity, IPluginConfig config) {
+    protected void addTooltip(CompoundTag data, ITooltip tooltip, Player player, BlockAccessor block,
+                              BlockEntity blockEntity, IPluginConfig config) {
         tooltip.add(Component.translatable(
                 "gtceu.top.transform_output",
                 (GTValues.VNF[data.getInt("voltage")] + " §r(" + data.getInt("amps") + "A)")));
