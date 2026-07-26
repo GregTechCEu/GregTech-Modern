3f76d3440ea2d6d5f0c2310db02594344d2af661
Jurre Groenendijk <jurre@jilles.com>
2026-07-26 12:46:19 -0500

    Add getComponentPos for monitor (#5157)

--- src/main/java/com/gregtechceu/gtceu/api/capability/IMonitorComponent.java
+++ src/main/java/com/gregtechceu/gtceu/api/capability/IMonitorComponent.java
@@ -2,6 +2,7 @@ package com.gregtechceu.gtceu.api.capability;
 
 import net.minecraft.core.BlockPos;
 import net.neoforged.neoforge.items.IItemHandler;
+import net.minecraft.world.level.block.entity.BlockEntity;
 
 import brachy.modularui.api.drawable.IDrawable;
 import org.jetbrains.annotations.Nullable;
@@ -14,7 +15,13 @@ public interface IMonitorComponent {
 
     IDrawable getIcon();
 
-    BlockPos getBlockPos();
+    default BlockPos getComponentPos() {
+        if (this instanceof BlockEntity be) {
+            return be.getBlockPos();
+        }
+        throw new UnsupportedOperationException(
+                "IMonitorComponent implementations that are not BlockEntities must override getComponentPos()");
+    }
 
     default @Nullable IItemHandler getDataItems() {
         return null;
