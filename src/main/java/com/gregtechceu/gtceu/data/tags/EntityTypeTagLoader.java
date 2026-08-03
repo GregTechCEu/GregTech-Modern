0cf97eb1d76068756065c52bfe56202beee2a5cf
screret <68943070+screret@users.noreply.github.com>
2026-08-03 20:09:05 +0300

    Add minecarts and a bunch of alex's caves mobs to `#c:constructs` (as a start)

--- src/main/java/com/gregtechceu/gtceu/data/tags/EntityTypeTagLoader.java
+++ src/main/java/com/gregtechceu/gtceu/data/tags/EntityTypeTagLoader.java
@@ -2,7 +2,10 @@ package com.gregtechceu.gtceu.data.tags;
 
 import com.gregtechceu.gtceu.data.recipe.CustomTags;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.world.entity.EntityType;
+import net.neoforged.neoforge.common.Tags;
 
 import com.tterrag.registrate.providers.RegistrateTagsProvider;
 
@@ -16,6 +19,13 @@ public class EntityTypeTagLoader {
         provider.addTag(CustomTags.CHEMICAL_IMMUNE)
                 .addTag(EntityTypeTags.SKELETONS);
 
-        provider.addTag(CustomTags.CONSTRUCTS).add(EntityType.IRON_GOLEM);
+        provider.addTag(CustomTags.CONSTRUCTS).add(EntityType.IRON_GOLEM)
+                .addTag(Tags.EntityTypes.MINECARTS)
+                .addOptional(ResourceLocation.fromNamespaceAndPath("alexscaves", "teletor"))
+                .addOptional(ResourceLocation.fromNamespaceAndPath("alexscaves", "notor"))
+                .addOptional(ResourceLocation.fromNamespaceAndPath("alexscaves", "magnetron"))
+                .addOptional(ResourceLocation.fromNamespaceAndPath("alexscaves", "boundroid"))
+                .addOptional(ResourceLocation.fromNamespaceAndPath("alexscaves", "boundroid_winch"))
+                .addOptional(ResourceLocation.fromNamespaceAndPath("alexscaves", "nucleeper"));
     }
 }
