329165f8170c94d91c174bc46e8a12fde1006f04
Gustavo <ofoxsmith@outlook.com>
2026-08-05 18:28:07 +1000

    fix load order

--- src/main/java/com/gregtechceu/gtceu/core/mixins/forge/GameDataMixin.java
+++ src/main/java/com/gregtechceu/gtceu/core/mixins/forge/GameDataMixin.java
@@ -1,15 +1,17 @@
 package com.gregtechceu.gtceu.core.mixins.forge;
 
 import com.gregtechceu.gtceu.api.registry.GTRegistries;
-
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.registries.GameData;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static java.util.Arrays.compare;
 
@@ -17,18 +19,23 @@ import static java.util.Arrays.compare;
 public class GameDataMixin {
 
     // Make GT register events fire first, even before minecraft registries.
-    @ModifyArg(method = "postRegisterEvents",
-               at = @At(value = "INVOKE",
-                        target = "Ljava/util/stream/Stream;sorted(Ljava/util/Comparator;)Ljava/util/stream/Stream;",
-                        ordinal = 0))
-    private static Comparator<ResourceLocation> gtceu$forceGTRegistriesFirst(Comparator<ResourceLocation> original) {
-        return ((Comparator<ResourceLocation>) (a, b) -> {
+    @ModifyVariable(
+            method = "postRegisterEvents",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/util/Set;addAll(Ljava/util/Collection;)Z",
+                    ordinal = 1,
+                    shift = At.Shift.AFTER),
+            name = "ordered")
+    private static Set<ResourceLocation> gtceu$forceGTRegistriesFirst(Set<ResourceLocation> ordered) {
+        return ordered.stream().sorted(((Comparator<ResourceLocation>) (a, b) -> {
             boolean aIsGT = GTRegistries.getRegistryOrder().contains(a);
             boolean bIsGT = GTRegistries.getRegistryOrder().contains(b);
             if (aIsGT && !bIsGT) return -1;
             if (!aIsGT && bIsGT) return 1;
             return Integer.compare(GTRegistries.getRegistryOrder().indexOf(a),
                     GTRegistries.getRegistryOrder().indexOf(b));
-        }).thenComparing(original);
+        }).thenComparing(Comparator.naturalOrder()))
+                .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 }
