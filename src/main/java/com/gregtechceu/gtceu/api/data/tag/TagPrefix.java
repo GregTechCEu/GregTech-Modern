fb2c28dd0f5c993ff2c51709b17b99e3b8f40902
Jurre Groenendijk <jurre@jilles.com>
2026-08-06 08:35:15 +0000

    Add nullability checks with proper errors in TagPrefix BlockProperties (#5019)

--- src/main/java/com/gregtechceu/gtceu/api/data/tag/TagPrefix.java
+++ src/main/java/com/gregtechceu/gtceu/api/data/tag/TagPrefix.java
@@ -966,8 +966,8 @@ public class TagPrefix {
                           Supplier<BlockBehaviour.Properties> template, ResourceLocation baseModelLocation,
                           boolean isDoubleDrops, boolean isSand, boolean shouldDropAsItem) {}
 
-    public record BlockProperties(Supplier<Supplier<RenderType>> renderType,
-                                  UnaryOperator<BlockBehaviour.Properties> properties) {}
+    public record BlockProperties(@NotNull Supplier<Supplier<RenderType>> renderType,
+                                  @NotNull UnaryOperator<BlockBehaviour.Properties> properties) {}
 
     @Getter
     public final ResourceLocation id;
@@ -1009,7 +1009,6 @@ public class TagPrefix {
     @Setter
     private BlockItemConstructor blockItemConstructor = MaterialBlockItem::new;
     @Getter
-    @Setter
     private BlockProperties blockProperties = new BlockProperties(() -> RenderType::translucent,
             UnaryOperator.identity());
 
@@ -1162,6 +1161,19 @@ public class TagPrefix {
 
     public TagPrefix miningToolTag(TagKey<Block> tag) {
         this.miningToolTag.add(tag);
+        return this;
+    }
+
+    public TagPrefix blockProperties(BlockProperties blockProperties) {
+        if (blockProperties.renderType() == null) {
+            throw new IllegalArgumentException(
+                    "Could not set blockProperties with null renderType in TagPrefix " + this.id());
+        }
+        if (blockProperties.properties() == null) {
+            throw new IllegalArgumentException(
+                    "Could not set blockProperties with null properties in TagPrefix " + this.id());
+        }
+        this.blockProperties = blockProperties;
         return this;
     }
 
