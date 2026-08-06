702e5f83f0b5b8ac495a62b41392cc63ae92dd97
screret <68943070+screret@users.noreply.github.com>
2026-08-06 11:34:46 +0300

    Move runtime model generators to their own package and rename the classes (#4923)

--- src/main/java/com/gregtechceu/gtceu/api/data/tag/TagPrefix.java
+++ src/main/java/com/gregtechceu/gtceu/api/data/tag/TagPrefix.java
@@ -1019,7 +1019,7 @@ public class TagPrefix {
     private boolean generateBlock;
     @Getter
     @Setter
-    private BlockConstructor blockConstructor = MaterialBlock::new;
+    private BlockConstructor blockConstructor = MaterialBlock::createAndAddModel;
     @Getter
     @Setter
     private BlockItemConstructor blockItemConstructor = MaterialBlockItem::new;
