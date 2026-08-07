194b1aca3cdec995c28d6039fff4deaf98135c48
Oktawia Hańderek <oktawia.handerek@gmail.com>
2026-08-07 20:15:14 +0200

    fix ender covers title being 'air' by making sure, covers can do a full client sync

--- src/main/java/com/gregtechceu/gtceu/api/sync_system/SyncDataHolder.java
+++ src/main/java/com/gregtechceu/gtceu/api/sync_system/SyncDataHolder.java
@@ -57,13 +57,15 @@ public class SyncDataHolder {
 
         CompoundTag tag = new CompoundTag();
         for (var field : fieldsToSerialize) {
-            if (shouldSerializeField(field, writeClientFields, fullSync)) {
-                Tag nbtValue = serializeField(holder, field, writeClientFields, fullSync);
+            if (shouldSerializeField(field, writeClientFields, fullSync || resyncAll)) {
+                Tag nbtValue = serializeField(holder, field, writeClientFields, fullSync || resyncAll);
                 tag.put(field.nbtSaveKey, nbtValue);
             }
         }
-        resyncAll = false;
-        dirtySyncFields.clear();
+        if (writeClientFields) {
+            resyncAll = false;
+            dirtySyncFields.clear();
+        }
         return tag;
     }
 
