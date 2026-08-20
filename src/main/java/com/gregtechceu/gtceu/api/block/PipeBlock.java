206b0c12c8e9443f1d530bf1e6008ea0e9cdbf78
Gustavo <ofoxsmith@outlook.com>
2026-08-20 23:24:07 +1000

    isAir

--- src/main/java/com/gregtechceu/gtceu/api/block/PipeBlock.java
+++ src/main/java/com/gregtechceu/gtceu/api/block/PipeBlock.java
@@ -209,7 +209,7 @@ public abstract class PipeBlock<PipeType extends Enum<PipeType> & IPipeType<Node
         BlockPos pos = selfTile.self().getBlockPos();
         if (level == null) return false;
 
-        if (level.getBlockState(pos.relative(facing)).getBlock() == Blocks.AIR)
+        if (level.getBlockState(pos.relative(facing)).getBlock().defaultBlockState().isAir())
             return false;
         CoverBehavior cover = selfTile.getCoverContainer().getCoverAtSide(facing);
         if (cover != null && !cover.canPipePassThrough()) {
