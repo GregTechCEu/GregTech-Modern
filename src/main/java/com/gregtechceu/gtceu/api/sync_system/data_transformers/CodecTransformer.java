43751466215b3612ea0c8a39dce121f2c0247ae7
Gustavo <ofoxsmith@outlook.com>
2026-08-22 18:38:09 +1000

    use registry ops

--- src/main/java/com/gregtechceu/gtceu/api/sync_system/data_transformers/CodecTransformer.java
+++ src/main/java/com/gregtechceu/gtceu/api/sync_system/data_transformers/CodecTransformer.java
@@ -4,10 +4,13 @@ import net.minecraft.nbt.NbtOps;
 import net.minecraft.nbt.Tag;
 
 import com.mojang.serialization.Codec;
+import net.minecraft.resources.RegistryOps;
+import org.jetbrains.annotations.Nullable;
 
 public class CodecTransformer<T> implements ValueTransformer<T> {
 
     private final Codec<T> codec;
+    private @Nullable RegistryOps<Tag> ops;
 
     public CodecTransformer(Codec<T> codec) {
         this.codec = codec;
@@ -15,11 +18,13 @@ public class CodecTransformer<T> implements ValueTransformer<T> {
 
     @Override
     public Tag serializeNBT(T value, ValueTransformer.TransformerContext<T> context) {
-        return codec.encodeStart(NbtOps.INSTANCE, value).getOrThrow();
+        if (ops == null) ops = context.lookup().createSerializationContext(NbtOps.INSTANCE);
+        return codec.encodeStart(ops, value).getOrThrow();
     }
 
     @Override
     public T deserializeNBT(Tag tag, ValueTransformer.TransformerContext<T> context) {
-        return codec.parse(NbtOps.INSTANCE, tag).getOrThrow();
+        if (ops == null) ops = context.lookup().createSerializationContext(NbtOps.INSTANCE);
+        return codec.parse(ops, tag).getOrThrow();
     }
 }
