027ff7ed9982e26fe1b0766aa08c18fd73bfc81c
Jurre Groenendijk <jurre@jilles.com>
2026-08-26 10:50:46 +0000

    Fix output hatch (#5351)

--- src/main/java/com/gregtechceu/gtceu/integration/ae2/machine/MEOutputHatchPartMachine.java
+++ src/main/java/com/gregtechceu/gtceu/integration/ae2/machine/MEOutputHatchPartMachine.java
@@ -16,6 +16,7 @@ import com.gregtechceu.gtceu.utils.FormattingUtil;
 import com.gregtechceu.gtceu.utils.GTMath;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.fluids.FluidStack;
 
@@ -30,7 +31,6 @@ import brachy.modularui.value.sync.PanelSyncManager;
 import brachy.modularui.widget.ParentWidget;
 import brachy.modularui.widget.scroll.VerticalScrollData;
 import brachy.modularui.widgets.DynamicSyncedWidget;
-import brachy.modularui.widgets.TextWidget;
 import brachy.modularui.widgets.layout.Flow;
 
 import java.util.Collections;
@@ -117,21 +117,26 @@ public class MEOutputHatchPartMachine extends MEHatchPartMachine {
                 .widgetProvider((sm, value) -> {
                     var col = Flow.col().leftRel(0.5f).coverChildrenHeight();
                     var list = value.getValue();
-                    if (list.isEmpty()) return col.child(new TextWidget<>(Text.lang("gtceu.gui.waiting_list_empty")));
-                    col.child(new TextWidget<>(Text.lang("gtceu.gui.waiting_list")).margin(0, 2));
+                    if (list.isEmpty()) return col.child(Text.lang("gtceu.gui.waiting_list_empty").asWidget());
+                    col.child(Text.lang("gtceu.gui.waiting_list").asWidget().margin(0, 2));
                     col.child(new ScrollPreservingGrid(savedScroll)
-                            .size(167, 70)
+                            .size(167, 67)
                             .scrollable(new VerticalScrollData())
-                            .gridOfSizeWidth(9, 1, (x, y, index) -> {
+                            .gridOfSizeWidth(list.size(), 1, (x, y, index) -> {
                                 var widget = new AEStackDisplayWidget(list, index);
                                 var row = Flow.row()
                                         .coverChildrenHeight()
                                         .child(widget);
-                                if (index >= list.size()) return row;
                                 var entry = list.get(index);
                                 return row
-                                        .child(Text.str("%sB %s", FormattingUtil.formatNumbers(entry.amount()),
-                                                entry.what().getDisplayName()).asWidget());
+                                        .child(Text.comp(Component
+                                                .translatable("gtceu.universal.liters",
+                                                        FormattingUtil.formatNumbers(entry.amount()))
+                                                .append(CommonComponents.SPACE)
+                                                .append(entry.what().getDisplayName()))
+                                                .asWidget()
+                                                .width(140)
+                                                .marginLeft(3));
                             }));
                     return col;
                 });
