d43b1c235fb6150fac8fb6ecd7b62851f4238659
Gustavo <ofoxsmith@outlook.com>
2026-08-28 17:27:24 +1000

    remove unnecessary kjs event

--- src/main/java/com/gregtechceu/gtceu/integration/kjs/GregTechKubeJSPlugin.java
+++ src/main/java/com/gregtechceu/gtceu/integration/kjs/GregTechKubeJSPlugin.java
@@ -86,10 +86,7 @@ import com.gregtechceu.gtceu.integration.kjs.builders.material.OreTagPrefixBuild
 import com.gregtechceu.gtceu.integration.kjs.builders.material.TagPrefixBuilder;
 import com.gregtechceu.gtceu.integration.kjs.builders.recipe.GTRecipeCategoryBuilder;
 import com.gregtechceu.gtceu.integration.kjs.builders.recipe.GTRecipeTypeBuilder;
-import com.gregtechceu.gtceu.integration.kjs.builders.worldgen.BedrockFluidDefinitionBuilderJS;
-import com.gregtechceu.gtceu.integration.kjs.builders.worldgen.BedrockOreDefinitionBuilderJS;
-import com.gregtechceu.gtceu.integration.kjs.builders.worldgen.DimensionMarkerBuilder;
-import com.gregtechceu.gtceu.integration.kjs.builders.worldgen.OreVeinDefinitionBuilderJS;
+import com.gregtechceu.gtceu.integration.kjs.builders.worldgen.*;
 import com.gregtechceu.gtceu.integration.kjs.helpers.GTResourceLocation;
 import com.gregtechceu.gtceu.integration.kjs.helpers.MachineConstructors;
 import com.gregtechceu.gtceu.integration.kjs.helpers.MachineModifiers;
@@ -163,6 +160,8 @@ public class GregTechKubeJSPlugin implements KubeJSPlugin {
             reg.add(GTCEu.id("active"), ActiveBlockBuilder.class, ActiveBlockBuilder::new);
             reg.add(GTCEu.id("coil"), CoilBlockBuilder.class, CoilBlockBuilder::new);
         });
+
+        registry.addDefault(GTRegistries.Keys.WORLD_GEN_LAYER, WorldGenLayerBuilderJS.class, WorldGenLayerBuilderJS::new);
 
         registry.addDefault(GTRegistries.Keys.ORE_VEIN, OreVeinDefinitionBuilderJS.class,
                 OreVeinDefinitionBuilderJS::new);
--- src/main/java/com/gregtechceu/gtceu/integration/kjs/builders/worldgen/WorldGenLayerBuilderJS.java
+++ src/main/java/com/gregtechceu/gtceu/integration/kjs/builders/worldgen/WorldGenLayerBuilderJS.java
@@ -3,6 +3,7 @@ package com.gregtechceu.gtceu.integration.kjs.builders.worldgen;
 import com.gregtechceu.gtceu.api.data.worldgen.IWorldGenLayer;
 import com.gregtechceu.gtceu.api.data.worldgen.SimpleWorldGenLayer;
 
+import dev.latvian.mods.kubejs.registry.BuilderBase;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
@@ -16,30 +17,30 @@ import java.util.List;
 import java.util.Set;
 
 @Accessors(fluent = true, chain = true)
-public class WorldGenLayerBuilder {
+public class WorldGenLayerBuilderJS extends BuilderBase<IWorldGenLayer> {
 
-    public transient ResourceLocation id;
     public transient List<IWorldGenLayer.RuleTestSupplier> targets = new ObjectArrayList<>();
     public transient List<ResourceKey<Level>> dimensions = new ObjectArrayList<>();
 
-    public WorldGenLayerBuilder(ResourceLocation id) {
-        this.id = id;
+    public WorldGenLayerBuilderJS(ResourceLocation id) {
+        super(id);
     }
 
-    public SimpleWorldGenLayer build() {
-        return new SimpleWorldGenLayer(
-                this.id,
-                () -> new AnyMatchRuleTest(targets.stream().map(IWorldGenLayer.RuleTestSupplier::get).toList()),
-                Set.copyOf(dimensions));
-    }
-
-    public WorldGenLayerBuilder targets(IWorldGenLayer.RuleTestSupplier... targets) {
+    public WorldGenLayerBuilderJS targets(IWorldGenLayer.RuleTestSupplier... targets) {
         Collections.addAll(this.targets, targets);
         return this;
     }
 
-    public WorldGenLayerBuilder dimensions(List<ResourceKey<Level>> dimensions) {
+    public WorldGenLayerBuilderJS dimensions(List<ResourceKey<Level>> dimensions) {
         this.dimensions.addAll(dimensions);
         return this;
+    }
+
+    @Override
+    public IWorldGenLayer createObject() {
+        return new SimpleWorldGenLayer(
+                this.id,
+                () -> new AnyMatchRuleTest(targets.stream().map(IWorldGenLayer.RuleTestSupplier::get).toList()),
+                Set.copyOf(dimensions));
     }
 }
