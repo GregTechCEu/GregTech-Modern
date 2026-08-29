db1415fd303a680eec6c7f557067d58d1b2abf81
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2026-08-28 17:26:10 -0700

    make settings a constructor param improve nonevalid in xor

--- src/main/java/com/gregtechceu/gtceu/api/multiblock/XorPredicate.java
+++ src/main/java/com/gregtechceu/gtceu/api/multiblock/XorPredicate.java
@@ -11,6 +11,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.function.UnaryOperator;
 
 public class XorPredicate extends MultiPredicate {
 
@@ -20,31 +21,16 @@ public class XorPredicate extends MultiPredicate {
     /// meaning that it is possible that no predicates may be present in the multi.
     protected boolean noneValid;
 
-    public XorPredicate(List<MultiPredicate> children, List<BasePredicate> predicates, boolean hasAir) {
-        super(Logic.XOR, children, predicates, hasAir);
-    }
-
-    @Override
-    public void setSettings(@Nullable PredicateSettings settings) {
-        super.setSettings(settings);
+    public XorPredicate(List<MultiPredicate> children, List<BasePredicate> predicates, boolean hasAir,
+                        @Nullable PredicateSettings settings) {
+        super(Logic.XOR, children, predicates, hasAir, settings);
         this.noneValid = isNoneValid(this);
     }
 
-    private static boolean isNoneValid(MultiPredicate multiPredicate) {
-        PredicateSettings settings = multiPredicate.getSettings();
-        if (settings != null) return settings.isNoneValid();
-
-        for (BasePredicate predicate : multiPredicate.predicates()) {
-            if (predicate.getSettings().isNoneValid()) {
-                return true;
-            }
-        }
-        for (MultiPredicate child : multiPredicate.children()) {
-            if (isNoneValid(child)) {
-                return true;
-            }
-        }
-        return false;
+    @Override
+    public void updateSettings(UnaryOperator<PredicateSettings> configurator, boolean shouldCreate) {
+        super.updateSettings(configurator, shouldCreate);
+        this.noneValid = isNoneValid(this);
     }
 
     @Override
@@ -109,6 +95,23 @@ public class XorPredicate extends MultiPredicate {
         context.appendError(
                 PatternStringError.literal("XOR error\n" + found.getString() + "\n" + expected.getString()));
         context.skipFlipCheck();
+    }
+
+    private static boolean isNoneValid(MultiPredicate multiPredicate) {
+        PredicateSettings settings = multiPredicate.getSettings();
+        if (settings != null) return settings.isNoneValid();
+
+        for (BasePredicate predicate : multiPredicate.predicates()) {
+            if (predicate.getSettings().isNoneValid()) {
+                return true;
+            }
+        }
+        for (MultiPredicate child : multiPredicate.children()) {
+            if (isNoneValid(child)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static PassedPredicate ofPredicate(BasePredicate predicate) {
