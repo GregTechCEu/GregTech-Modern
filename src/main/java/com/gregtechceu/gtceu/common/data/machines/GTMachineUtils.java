439f3e1d8784ec9b4571926c0df214ad7b596537
Gustavo <ofoxsmith@outlook.com>
2026-08-30 12:15:59 +1000

    start working on deferred machine registration

--- src/main/java/com/gregtechceu/gtceu/common/data/machines/GTMachineUtils.java
+++ src/main/java/com/gregtechceu/gtceu/common/data/machines/GTMachineUtils.java
@@ -1,5 +1,6 @@
 package com.gregtechceu.gtceu.common.data.machines;
 
+import brachy.modularui.api.drawable.Text;
 import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.GTCEuAPI;
 import com.gregtechceu.gtceu.api.GTValues;
