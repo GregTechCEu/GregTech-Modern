cde2ac57cea8705e0e7e74e890ed3f89e926b443
screret <68943070+screret@users.noreply.github.com>
2026-09-03 17:21:20 +0300

    Fix den lille toolentippen

--- src/main/java/com/gregtechceu/gtceu/common/item/datacomponents/ResolvableItemEnchantments.java
+++ src/main/java/com/gregtechceu/gtceu/common/item/datacomponents/ResolvableItemEnchantments.java
@@ -2,8 +2,10 @@ package com.gregtechceu.gtceu.common.item.datacomponents;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import it.unimi.dsi.fastutil.Hash;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntMaps;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenCustomHashMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
@@ -31,20 +33,20 @@ import java.util.function.Predicate;
 public class ResolvableItemEnchantments implements TooltipProvider {
 
     // spotless:off
-    public static final ResolvableItemEnchantments EMPTY = new ResolvableItemEnchantments(new Object2IntOpenHashMap<>(), true);
+    public static final ResolvableItemEnchantments EMPTY = new ResolvableItemEnchantments(createLevelMap(), true);
 
     private static final Codec<Integer> LEVEL_CODEC = Codec.intRange(0, 255);
     private static final Codec<EitherHolder<Enchantment>> ENCHANTMENT_CODEC = EitherHolder.codec(Registries.ENCHANTMENT, Enchantment.CODEC);
-    private static final Codec<Object2IntOpenHashMap<EitherHolder<Enchantment>>> LEVELS_CODEC = Codec.unboundedMap(ENCHANTMENT_CODEC, LEVEL_CODEC)
-            .xmap(Object2IntOpenHashMap::new, Function.identity());
+    private static final Codec<Object2IntOpenCustomHashMap<EitherHolder<Enchantment>>> LEVELS_CODEC = Codec.unboundedMap(ENCHANTMENT_CODEC, LEVEL_CODEC)
+            .xmap(ResolvableItemEnchantments::createLevelMap, Function.identity());
     private static final Codec<ResolvableItemEnchantments> FULL_CODEC = RecordCodecBuilder.create(instance -> instance.group(
             LEVELS_CODEC.fieldOf("levels").forGetter(enchantments -> enchantments.enchantments),
-            Codec.BOOL.optionalFieldOf("show_in_tooltip", Boolean.TRUE).forGetter(enchantments -> enchantments.showInTooltip)
+            Codec.BOOL.optionalFieldOf("show_in_tooltip", true).forGetter(enchantments -> enchantments.showInTooltip)
     ).apply(instance, ResolvableItemEnchantments::new));
     public static final Codec<ResolvableItemEnchantments> CODEC = Codec.withAlternative(FULL_CODEC, LEVELS_CODEC, enchants -> new ResolvableItemEnchantments(enchants, true));
 
-    private static final StreamCodec<RegistryFriendlyByteBuf, Object2IntOpenHashMap<EitherHolder<Enchantment>>> LEVELS_STREAM_CODEC = ByteBufCodecs.map(
-            Object2IntOpenHashMap::new,
+    private static final StreamCodec<RegistryFriendlyByteBuf, Object2IntOpenCustomHashMap<EitherHolder<Enchantment>>> LEVELS_STREAM_CODEC = ByteBufCodecs.map(
+            ResolvableItemEnchantments::createLevelMap,
             EitherHolder.streamCodec(Registries.ENCHANTMENT, Enchantment.STREAM_CODEC),
             ByteBufCodecs.VAR_INT
     );
@@ -55,10 +57,10 @@ public class ResolvableItemEnchantments implements TooltipProvider {
     );
     // spotless:on
 
-    private final Object2IntOpenHashMap<EitherHolder<Enchantment>> enchantments;
+    private final Object2IntOpenCustomHashMap<EitherHolder<Enchantment>> enchantments;
     private final boolean showInTooltip;
 
-    ResolvableItemEnchantments(Object2IntOpenHashMap<EitherHolder<Enchantment>> enchantments, boolean showInTooltip) {
+    ResolvableItemEnchantments(Object2IntOpenCustomHashMap<EitherHolder<Enchantment>> enchantments, boolean showInTooltip) {
         this.enchantments = enchantments;
         this.showInTooltip = showInTooltip;
 
@@ -80,28 +82,29 @@ public class ResolvableItemEnchantments implements TooltipProvider {
 
     @Override
     public void addToTooltip(Item.TooltipContext context, Consumer<Component> tooltipAdder, TooltipFlag tooltipFlag) {
-        if (this.showInTooltip) {
-            HolderLookup.Provider registries = context.registries();
-            HolderSet<Enchantment> tooltipOrder = getTooltipOrderOrEmpty(registries);
+        if (!this.showInTooltip) {
+            return;
+        }
+        HolderLookup.Provider registries = context.registries();
+        HolderSet<Enchantment> tooltipOrder = getTooltipOrderOrEmpty(registries);
 
-            for (Holder<Enchantment> enchantment : tooltipOrder) {
-                int i = this.enchantments.getInt(enchantment);
-                if (i > 0) {
-                    tooltipAdder.accept(Enchantment.getFullname(enchantment, i));
-                }
+        for (Holder<Enchantment> enchantment : tooltipOrder) {
+            int i = this.enchantments.getInt(new EitherHolder<>(enchantment));
+            if (i > 0) {
+                tooltipAdder.accept(Enchantment.getFullname(enchantment, i));
             }
+        }
 
-            for (Object2IntMap.Entry<EitherHolder<Enchantment>> entry : this.enchantments.object2IntEntrySet()) {
-                EitherHolder<Enchantment> eitherHolder = entry.getKey();
-                var optional = registries != null ? eitherHolder.unwrap(registries) : eitherHolder.holder();
-                if (optional.isEmpty()) {
-                    continue;
-                }
-                Holder<Enchantment> enchantment = optional.get();
+        for (Object2IntMap.Entry<EitherHolder<Enchantment>> entry : this.enchantments.object2IntEntrySet()) {
+            EitherHolder<Enchantment> eitherHolder = entry.getKey();
+            var optional = registries != null ? eitherHolder.unwrap(registries) : eitherHolder.holder();
+            if (optional.isEmpty()) {
+                continue;
+            }
+            Holder<Enchantment> enchantment = optional.get();
 
-                if (!tooltipOrder.contains(enchantment)) {
-                    tooltipAdder.accept(Enchantment.getFullname(enchantment, entry.getIntValue()));
-                }
+            if (!tooltipOrder.contains(enchantment)) {
+                tooltipAdder.accept(Enchantment.getFullname(enchantment, entry.getIntValue()));
             }
         }
     }
@@ -144,16 +147,28 @@ public class ResolvableItemEnchantments implements TooltipProvider {
         return resolvedEnchantments.toImmutable();
     }
 
+    private Object2IntOpenHashMap<Holder<Enchantment>> resolveToMap(@Nullable HolderLookup.Provider registries) {
+        Object2IntOpenHashMap<Holder<Enchantment>> resolvedEnchantments = new Object2IntOpenHashMap<>();
+        Object2IntMaps.fastForEach(this.enchantments, entry -> {
+            var enchantmentOptional = entry.getKey().holder()
+                    .or(() -> Optional.ofNullable(registries).flatMap(reg -> reg.holder(entry.getKey().key())));
+            if (enchantmentOptional.isEmpty()) return;
+
+            Holder<Enchantment> enchantment = enchantmentOptional.get();
+            int level = entry.getIntValue();
+            resolvedEnchantments.put(enchantment, level);
+        });
+
+        return resolvedEnchantments;
+    }
 
     @Override
     public boolean equals(Object other) {
-        if (this == other) {
-            return true;
-        } else {
-            return other instanceof ResolvableItemEnchantments resolvableEnchantments &&
-                    this.showInTooltip == resolvableEnchantments.showInTooltip &&
-                    this.enchantments.equals(resolvableEnchantments.enchantments);
-        }
+        if (this == other) return true;
+
+        return other instanceof ResolvableItemEnchantments resolvableEnchantments &&
+                this.showInTooltip == resolvableEnchantments.showInTooltip &&
+                this.enchantments.equals(resolvableEnchantments.enchantments);
     }
 
     @Override
@@ -168,7 +183,7 @@ public class ResolvableItemEnchantments implements TooltipProvider {
     }
 
     public static class Mutable {
-        private final Object2IntOpenHashMap<EitherHolder<Enchantment>> enchantments = new Object2IntOpenHashMap<>();
+        private final Object2IntOpenCustomHashMap<EitherHolder<Enchantment>> enchantments = createLevelMap();
         private final boolean showInTooltip;
 
         public Mutable(ResolvableItemEnchantments enchantments) {
@@ -232,6 +247,37 @@ public class ResolvableItemEnchantments implements TooltipProvider {
 
         public ResolvableItemEnchantments toImmutable() {
             return new ResolvableItemEnchantments(this.enchantments, this.showInTooltip);
+        }
+    }
+
+    private static Object2IntOpenCustomHashMap<EitherHolder<Enchantment>> createLevelMap() {
+        return new Object2IntOpenCustomHashMap<>(HashStrategy.INSTANCE);
+    }
+
+    private static Object2IntOpenCustomHashMap<EitherHolder<Enchantment>> createLevelMap(int size) {
+        return new Object2IntOpenCustomHashMap<>(size, HashStrategy.INSTANCE);
+    }
+
+    private static Object2IntOpenCustomHashMap<EitherHolder<Enchantment>> createLevelMap(Map<EitherHolder<Enchantment>, Integer> map) {
+        return new Object2IntOpenCustomHashMap<>(map, HashStrategy.INSTANCE);
+    }
+
+    private enum HashStrategy implements Hash.Strategy<EitherHolder<Enchantment>> {
+        INSTANCE;
+
+        @Override
+        public int hashCode(@Nullable EitherHolder<Enchantment> o) {
+            if (o == null) return 0;
+            return o.key().hashCode();
+        }
+
+        @Override
+        public boolean equals(@Nullable EitherHolder<Enchantment> a, @Nullable EitherHolder<Enchantment> b) {
+            if (a == b) return true;
+            if (a == null) return false;
+            if (b == null) return false;
+
+            return a.key().equals(b.key());
         }
     }
 }
