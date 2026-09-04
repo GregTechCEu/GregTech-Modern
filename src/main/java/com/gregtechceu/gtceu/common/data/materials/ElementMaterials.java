68a9d38c3b3c085344d04a5656561708ba5cb294
Gustavo <ofoxsmith@outlook.com>
2026-09-04 20:23:21 +1000

    pointer schmointer

--- src/main/java/com/gregtechceu/gtceu/common/data/materials/ElementMaterials.java
+++ src/main/java/com/gregtechceu/gtceu/common/data/materials/ElementMaterials.java
@@ -10,7 +10,6 @@ import com.gregtechceu.gtceu.api.fluids.attribute.FluidAttributes;
 import com.gregtechceu.gtceu.api.fluids.store.FluidStorageKeys;
 import com.gregtechceu.gtceu.api.item.tool.GTToolType;
 import com.gregtechceu.gtceu.common.data.GTElements;
-import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.gregtechceu.gtceu.common.data.GTMedicalConditions;
 
 import static com.gregtechceu.gtceu.api.GTValues.*;
