8dc4947e2c3fcae5464aa1b5cd6b88fea8fedbcd
Gustavo <ofoxsmith@outlook.com>
2026-09-05 18:04:09 +1000

    spotless

--- src/main/java/com/gregtechceu/gtceu/data/recipe/builder/GTRecipeBuilder.java
+++ src/main/java/com/gregtechceu/gtceu/data/recipe/builder/GTRecipeBuilder.java
@@ -1632,7 +1632,8 @@ public class GTRecipeBuilder {
         return new GTRecipe(recipeType, id.withPrefix(recipeType.registryName.getPath() + "/"),
                 input, output, tickInput, tickOutput,
                 inputChanceLogic, outputChanceLogic, tickInputChanceLogic, tickOutputChanceLogic,
-                conditions, List.of(), data, duration, recipeCategory == null ? recipeType.getCategory() : recipeCategory, -1,
+                conditions, List.of(), data, duration,
+                recipeCategory == null ? recipeType.getCategory() : recipeCategory, -1,
                 keepSpoilingProgress);
     }
 
